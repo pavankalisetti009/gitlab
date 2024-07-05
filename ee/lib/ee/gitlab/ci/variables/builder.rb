46f285ca729eff92636179dd44277e31fc8c68a5
Furkan Ayhan <furkanayhn@gmail.com>
2024-07-05 09:17:55 +0200

    Optimize variables on pipeline creation for yaml and node
    On pipeline creation, a stub build is created for every job candidate
    to run rules against it. This is an expensive operation for many
    reasons. More details are at
    https://gitlab.com/gitlab-org/gitlab/-/issues/350057.
    
    One of the downsides of using a stub build is that variable calculation
    becomes also expensive because it tries to reach sub-relations,
    for example "metadata" in the scope of this change.
    
    Eventually, our goal is to not use a stub build at all, but I'd like to
    gradually lose the connection between the AR object and variables. And,
    this is the first step; we are generating yaml_variables and
    node_variables from the hash instead of the AR object.
    
    More information about the performance optimization:
    https://gitlab.com/groups/gitlab-org/-/epics/14348#note_1982576223
    
    This change is behind the feature flag
    `ci_variables_optimization_for_yaml_and_node`.

--- ee/lib/ee/gitlab/ci/variables/builder.rb
+++ ee/lib/ee/gitlab/ci/variables/builder.rb
@@ -16,11 +16,11 @@ module EE
           end
 
           override :scoped_variables
-          def scoped_variables(job, environment:, dependencies:)
+          def scoped_variables(job, environment:, dependencies:, job_attributes: {})
             super.tap do |variables|
               variables.concat(scan_execution_policies_variables_builder.variables(job))
               # Reapply the PEP job YAML variables at the end to enforce the highest precedence
-              variables.concat(pipeline_execution_policy_variables(job))
+              variables.concat(pipeline_execution_policy_variables(job, job_attributes))
             end
           end
 
@@ -28,11 +28,11 @@ module EE
 
           attr_reader :scan_execution_policies_variables_builder
 
-          def pipeline_execution_policy_variables(job)
+          def pipeline_execution_policy_variables(job, job_attributes)
             return [] if ::Feature.disabled?(:pipeline_execution_policy_type, project.group)
             return [] unless job.execution_policy_job?
 
-            job.yaml_variables
+            job_attributes[:yaml_variables] || job.yaml_variables
           end
         end
       end
