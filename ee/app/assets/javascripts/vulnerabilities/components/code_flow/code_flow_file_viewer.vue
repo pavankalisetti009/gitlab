b5d31c71f698b8edd9fa7505be330f1bba351d52
Chen Charnolevsky <ccharnolevsky@gitlab.com>
2024-07-06 11:35:09 +0300

    Add the left side of 'code flow' that including description and steps
    Disable feature flag
    
    Remove comments
    
    Unit test changes
    
    colors to var
    
    Tailwind CSS changes
    
    CR changes
    
    CR changes
    
    translate pot
    
    CR changes
    
    CR changes

---
+++ ee/app/assets/javascripts/vulnerabilities/components/code_flow/code_flow_file_viewer.vue
@@ -0,0 +1,14 @@
+<script>
+import { s__ } from '~/locale';
+
+export default {
+  name: 'CodeFlowFileViewer',
+  i18n: {
+    tempText: s__('Vulnerability|code area'),
+  },
+};
+</script>
+
+<template>
+  <div class="file-holder">{{ $options.i18n.tempText }}</div>
+</template>
