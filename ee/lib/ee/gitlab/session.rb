f01f8ff48f4c51cc359a69a8a8b020f9125f6096
Mark Chao <mchao@gitlab.com>
2024-07-09 11:04:30 +0800

    Extract session_id_for_worker to EE

---
+++ ee/lib/ee/gitlab/session.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Session
+      extend ActiveSupport::Concern
+
+      class_methods do
+        def session_id_for_worker
+          session = current
+
+          return unless session
+
+          if session.is_a?(::ActionDispatch::Request::Session)
+            session.id.private_id
+          elsif session.respond_to?(:[]) # Hash-like
+            session[::Gitlab::SidekiqMiddleware::SetSession::Server::SESSION_ID_HASH_KEY]
+          else
+            raise("Unsupported session class: #{session.class}")
+          end
+        rescue StandardError => e
+          ::Gitlab::ErrorTracking.track_and_raise_for_dev_exception(e)
+          nil
+        end
+      end
+    end
+  end
+end
