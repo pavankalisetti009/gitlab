2540a50b105bf4971a1501e16d594548ea3b9491
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2024-07-10 18:53:08 +0000

    Authenticate the GitLab Duo usage controller
    Adds the necessary safeguards to the GitLab Duo usage controller.
    
    Changelog: fixed
    EE: true

--- ee/app/controllers/groups/settings/gitlab_duo_usage_controller.rb
+++ ee/app/controllers/groups/settings/gitlab_duo_usage_controller.rb
@@ -3,12 +3,21 @@
 module Groups
   module Settings
     class GitlabDuoUsageController < Groups::ApplicationController
+      before_action :authorize_read_usage_quotas!
+      before_action :verify_usage_quotas_enabled!
+
       feature_category :duo_chat
 
       include ::Nav::GitlabDuoUsageSettingsPage
 
       def index
         render_404 unless show_gitlab_duo_usage_menu_item?(group)
+      end
+
+      private
+
+      def verify_usage_quotas_enabled!
+        render_404 unless group.usage_quotas_enabled?
       end
     end
   end
--- ee/spec/requests/groups/settings/gitlab_duo_usage_controller_spec.rb
+++ ee/spec/requests/groups/settings/gitlab_duo_usage_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe Groups::Settings::GitlabDuoUsageController, type: :request, feature_category: :duo_chat do
-  let(:admin) { create(:admin) }
+  let_it_be(:user) { create(:user) }
   let_it_be(:group) { create(:group) }
 
   subject(:get_index) { get group_settings_gitlab_duo_usage_index_path(group) }
@@ -12,31 +12,65 @@ RSpec.describe Groups::Settings::GitlabDuoUsageController, type: :request, featu
     stub_licensed_features(code_suggestions: true)
     add_on = create(:gitlab_subscription_add_on)
     create(:gitlab_subscription_add_on_purchase, quantity: 50, namespace: group, add_on: add_on)
-    sign_in(admin)
+    sign_in(user)
   end
 
-  context "when show_gitlab_duo_usage_app? returns false" do
+  context 'when user has read_usage_quotas permission' do
+    before_all do
+      group.add_owner(user)
+    end
+
+    context "when show_gitlab_duo_usage_app? returns false" do
+      before do
+        stub_saas_features(gitlab_com_subscriptions: false)
+      end
+
+      it "renders 404" do
+        get_index
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
+
+    context "when show_gitlab_duo_usage_app? returns true" do
+      before do
+        stub_saas_features(gitlab_com_subscriptions: true)
+      end
+
+      it "renders index with 200 status code" do
+        get_index
+
+        expect(response).to have_gitlab_http_status(:ok)
+        expect(response).to render_template(:index)
+      end
+    end
+
+    context 'when in a subgroup' do
+      let(:subgroup) { create(:group, :private, parent: group) }
+
+      before do
+        subgroup.add_owner(user)
+        stub_saas_features(gitlab_com_subscriptions: true)
+      end
+
+      it "renders 404" do
+        get group_settings_gitlab_duo_usage_index_path(subgroup)
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
+  end
+
+  context 'when user does not have read_usage_quotas permission' do
     before do
-      stub_saas_features(gitlab_com_subscriptions: false)
+      group.add_maintainer(user) # rubocop:disable RSpec/BeforeAllRoleAssignment -- cannot use stub_saas_features in before_all
+      stub_saas_features(gitlab_com_subscriptions: true)
     end
 
     it "renders 404" do
       get_index
 
       expect(response).to have_gitlab_http_status(:not_found)
-    end
-  end
-
-  context "when show_gitlab_duo_usage_app? returns true" do
-    before do
-      stub_saas_features(gitlab_com_subscriptions: true)
-    end
-
-    it "renders index with 200 status code" do
-      get_index
-
-      expect(response).to have_gitlab_http_status(:ok)
-      expect(response).to render_template(:index)
     end
   end
 end
