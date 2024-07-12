ea1dfe80e4a38c8aefbb373d7f1edcf293c04640
imand3r <ianderson@gitlab.com>
2024-07-12 23:31:27 +0000

    Override execute! method when auto-banning in EE

--- ee/spec/services/ee/users/auto_ban_service_spec.rb
+++ ee/spec/services/ee/users/auto_ban_service_spec.rb
@@ -3,20 +3,18 @@
 require 'spec_helper'
 
 RSpec.describe Users::AutoBanService, feature_category: :instance_resiliency do
-  let_it_be(:user) { create(:user) }
+  let_it_be_with_reload(:user) { create(:user) }
   let(:reason) { 'reason' }
   let(:service) { described_class.new(user: user, reason: reason) }
 
-  subject(:execute) { service.execute }
-
-  describe '#execute' do
+  shared_examples 'executing the service' do
     context 'when the feature is enabled' do
       it 'executes the Arkose truth data service' do
         expect_next_instance_of(Arkose::TruthDataService, user: user, is_legit: false) do |instance|
           expect(instance).to receive(:execute)
         end
 
-        execute
+        subject
       end
     end
 
@@ -28,8 +26,20 @@ RSpec.describe Users::AutoBanService, feature_category: :instance_resiliency do
       it 'does not execute the arkose truth data service' do
         expect(Arkose::TruthDataService).not_to receive(:new)
 
-        execute
+        subject
       end
     end
+  end
+
+  describe '#execute' do
+    subject { service.execute }
+
+    it_behaves_like 'executing the service'
+  end
+
+  describe '#execute!' do
+    subject { service.execute! }
+
+    it_behaves_like 'executing the service'
   end
 end
