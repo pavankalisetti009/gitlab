70de62c11595c69060be1e7803d87e56c4ee6f3c
Jarka Košanová <jarka@gitlab.com>
2024-07-12 20:37:16 +0200

    Add worker to export detailed group memberships information
    - create a base worker for exporting group members
    - add a new worker

---
+++ ee/app/services/namespaces/export/detailed_data_service.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# TODO: this service is being implemented in https://gitlab.com/gitlab-org/gitlab/-/merge_requests/157296
+module Namespaces
+  module Export
+    class DetailedDataService < BaseService
+    end
+  end
+end
