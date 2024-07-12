c939401186fff153f36d69a40a1c07bcfcacce57
Kassio Borges <kborges@gitlab.com>
2024-07-12 19:54:52 +0000

    WorkItems: Add feature specs for rolled up dates
    Related to: https://gitlab.com/gitlab-org/gitlab/-/issues/451904

--- ee/spec/features/groups/work_items/work_item_spec.rb
+++ ee/spec/features/groups/work_items/work_item_spec.rb
@@ -7,13 +7,12 @@ RSpec.describe 'Work item', :js, feature_category: :team_planning do
 
   let_it_be_with_reload(:user) { create(:user) }
 
-  let_it_be(:group) { create(:group, :nested) }
+  let_it_be(:group) { create(:group, :nested, developers: user) }
   let_it_be(:work_item) { create(:work_item, :epic, :group_level, namespace: group) }
   let(:work_items_path) { group_work_item_path(group, work_item.iid) }
 
   context 'for signed in user' do
     before do
-      group.add_developer(user) # rubocop:disable RSpec/BeforeAllRoleAssignment -- we can remove this when we have more ee features specs
       sign_in(user)
       visit work_items_path
     end
