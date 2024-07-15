7a3905e2e4504bd70467b90d51c020f4917b18d0
Shreyas Agarwal <sagarwal@gitlab.com>
2024-07-15 10:39:04 +0000

    Create internal namespace api

--- ee/lib/gitlab_subscriptions/api/internal/api.rb
+++ ee/lib/gitlab_subscriptions/api/internal/api.rb
@@ -8,10 +8,11 @@ module GitlabSubscriptions
           authenticated_as_admin!
         end
 
-        mount ::GitlabSubscriptions::API::Internal::Subscriptions
-        mount ::GitlabSubscriptions::API::Internal::Users
         mount ::GitlabSubscriptions::API::Internal::Members
+        mount ::GitlabSubscriptions::API::Internal::Namespaces
+        mount ::GitlabSubscriptions::API::Internal::Subscriptions
         mount ::GitlabSubscriptions::API::Internal::UpcomingReconciliations
+        mount ::GitlabSubscriptions::API::Internal::Users
       end
     end
   end
