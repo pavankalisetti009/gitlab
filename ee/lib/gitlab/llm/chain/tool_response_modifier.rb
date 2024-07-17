e2cb4955f5b769b3477d5918adec9fde4a467a08
Tetiana Chupryna <tchupryna@gitlab.com>
2024-07-17 14:20:11 +0200

    Remove picked tool method
    This method isn't used as we don't show picked tool in UI
    
    EE:true

--- ee/lib/gitlab/llm/chain/tool_response_modifier.rb
+++ ee/lib/gitlab/llm/chain/tool_response_modifier.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Deprecation: SingleActionExecutor doesn't use this modifier
+# as picked_tool_action method isn't used anymore.
+# This class will be removed alongside ZeroShot::Executor
+# see https://gitlab.com/gitlab-org/gitlab/-/issues/469087
+
 module Gitlab
   module Llm
     module Chain
