0e64f7de8b2c41cbcb4c28b57338661c8d833dea
Lee Tickett <ltickett@gitlab.com>
2024-07-17 13:18:51 +0000

    Dynamically map work item widgets to types

--- ee/app/graphql/ee/types/work_items/widget_interface.rb
+++ ee/app/graphql/ee/types/work_items/widget_interface.rb
@@ -9,47 +9,26 @@ module EE
         class_methods do
           extend ::Gitlab::Utils::Override
 
-          override :resolve_type
-          def resolve_type(object, context)
-            case object
-            when ::WorkItems::Widgets::Weight
-              ::Types::WorkItems::Widgets::WeightType
-            when ::WorkItems::Widgets::Status
-              ::Types::WorkItems::Widgets::StatusType
-            when ::WorkItems::Widgets::Iteration
-              ::Types::WorkItems::Widgets::IterationType
-            when ::WorkItems::Widgets::HealthStatus
-              ::Types::WorkItems::Widgets::HealthStatusType
-            when ::WorkItems::Widgets::Progress
-              ::Types::WorkItems::Widgets::ProgressType
-            when ::WorkItems::Widgets::RequirementLegacy
-              ::Types::WorkItems::Widgets::RequirementLegacyType
-            when ::WorkItems::Widgets::TestReports
-              ::Types::WorkItems::Widgets::TestReportsType
-            when ::WorkItems::Widgets::Color
-              ::Types::WorkItems::Widgets::ColorType
-            when ::WorkItems::Widgets::RolledupDates
-              ::Types::WorkItems::Widgets::RolledupDatesType
-            else
-              super
-            end
+          override :type_mappings
+          def type_mappings
+            super.merge(EE_TYPE_MAPPINGS)
           end
         end
 
         prepended do
-          EE_ORPHAN_TYPES = [
-            ::Types::WorkItems::Widgets::WeightType,
-            ::Types::WorkItems::Widgets::StatusType,
-            ::Types::WorkItems::Widgets::IterationType,
-            ::Types::WorkItems::Widgets::HealthStatusType,
-            ::Types::WorkItems::Widgets::ProgressType,
-            ::Types::WorkItems::Widgets::RequirementLegacyType,
-            ::Types::WorkItems::Widgets::TestReportsType,
-            ::Types::WorkItems::Widgets::ColorType,
-            ::Types::WorkItems::Widgets::RolledupDatesType
-          ].freeze
+          EE_TYPE_MAPPINGS = {
+            ::WorkItems::Widgets::Weight => ::Types::WorkItems::Widgets::WeightType,
+            ::WorkItems::Widgets::Status => ::Types::WorkItems::Widgets::StatusType,
+            ::WorkItems::Widgets::Iteration => ::Types::WorkItems::Widgets::IterationType,
+            ::WorkItems::Widgets::HealthStatus => ::Types::WorkItems::Widgets::HealthStatusType,
+            ::WorkItems::Widgets::Progress => ::Types::WorkItems::Widgets::ProgressType,
+            ::WorkItems::Widgets::RequirementLegacy => ::Types::WorkItems::Widgets::RequirementLegacyType,
+            ::WorkItems::Widgets::TestReports => ::Types::WorkItems::Widgets::TestReportsType,
+            ::WorkItems::Widgets::Color => ::Types::WorkItems::Widgets::ColorType,
+            ::WorkItems::Widgets::RolledupDates => ::Types::WorkItems::Widgets::RolledupDatesType
+          }.freeze
 
-          orphan_types(*ce_orphan_types, *EE_ORPHAN_TYPES)
+          orphan_types(*type_mappings.values)
         end
       end
     end
