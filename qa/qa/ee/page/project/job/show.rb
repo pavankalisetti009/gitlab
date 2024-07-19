7cf0d1455e854ff3d0306675493c2da44dc83589
Tiffany Rea <trea@gitlab.com>
2024-07-19 00:21:00 +0000

    E2E - Root cause analysis with Duo Chat

--- qa/qa/ee/page/project/job/show.rb
+++ qa/qa/ee/page/project/job/show.rb
@@ -8,6 +8,16 @@ module QA
           module Show
             extend QA::Page::PageConcern
 
+            def self.prepended(base)
+              super
+
+              base.class_eval do
+                view 'ee/app/assets/javascripts/ci/job_details/components/job_log_top_bar.vue' do
+                  element 'rca-duo-button'
+                end
+              end
+            end
+
             # These wait times cover both the time for pipeline job to complete and time for log and artifact to Geo replicate, so the max duration has been doubled
             def wait_for_job_log_replication
               QA::Runtime::Logger.debug(%(#{self.class.name} - wait_for_job_log_replication))
@@ -21,6 +31,10 @@ module QA
               wait_until(max_duration: 2 * Runtime::Geo.max_file_replication_time) do
                 has_browse_button?
               end
+            end
+
+            def click_duo_troubleshoot_button
+              click_element('rca-duo-button')
             end
           end
         end
