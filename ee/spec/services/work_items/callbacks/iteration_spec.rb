3802cdb4b4cffefa4096d1bedc9b6f89e80aa624
Heinrich Lee Yu <heinrich@gitlab.com>
2024-07-19 17:08:17 +0800

    Refactor iteration service to use callback classes
    Move the work item iteration widget services to the new callback classes
    architecture.

--- ee/spec/services/work_items/callbacks/iteration_spec.rb
+++ ee/spec/services/work_items/callbacks/iteration_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe WorkItems::Widgets::IterationService::UpdateService, feature_category: :team_planning do
+RSpec.describe WorkItems::Callbacks::Iteration, feature_category: :team_planning do
   let_it_be(:user) { create(:user) }
   let_it_be(:group) { create(:group) }
   let_it_be(:project) { create(:project, group: group) }
@@ -10,12 +10,16 @@ RSpec.describe WorkItems::Widgets::IterationService::UpdateService, feature_cate
   let_it_be(:iteration) { create(:iteration, iterations_cadence: cadence) }
   let_it_be_with_reload(:work_item) { create(:work_item, project: project, author: user) }
 
-  let(:widget) { work_item.widgets.find { |widget| widget.is_a?(WorkItems::Widgets::Iteration) } }
+  let(:callback) { described_class.new(issuable: work_item, current_user: user, params: params) }
 
-  describe '#before_update_callback' do
-    let(:service) { described_class.new(widget: widget, current_user: user) }
+  describe '#before_create' do
+    subject { callback.before_create }
 
-    subject { service.before_update_callback(params: params) }
+    it_behaves_like 'iteration change is handled'
+  end
+
+  describe '#before_update' do
+    subject(:before_update_callback) { callback.before_update }
 
     before do
       stub_licensed_features(iterations: true)
@@ -25,9 +29,12 @@ RSpec.describe WorkItems::Widgets::IterationService::UpdateService, feature_cate
       context 'when user can admin the work item' do
         let_it_be(:other_iteration) { create(:iteration, iterations_cadence: cadence) }
 
+        before_all do
+          project.add_reporter(user)
+        end
+
         before do
           work_item.update!(iteration: iteration)
-          project.add_reporter(user)
         end
 
         where(:new_iteration) do
@@ -38,8 +45,7 @@ RSpec.describe WorkItems::Widgets::IterationService::UpdateService, feature_cate
           let(:params) { { iteration: new_iteration } }
 
           it 'sets a new iteration value for the work item' do
-            expect { subject }
-              .to change(work_item, :iteration).to(new_iteration).from(iteration)
+            expect { before_update_callback }.to change { work_item.iteration }.to(new_iteration).from(iteration)
           end
         end
 
@@ -47,12 +53,12 @@ RSpec.describe WorkItems::Widgets::IterationService::UpdateService, feature_cate
           let(:params) { {} }
 
           before do
-            allow(service).to receive(:new_type_excludes_widget?).and_return(true)
+            allow(callback).to receive(:excluded_in_new_type?).and_return(true)
             work_item.iteration = iteration
           end
 
           it "resets the work item's iteration" do
-            expect { subject }.to change(work_item, :iteration).from(iteration).to(nil)
+            expect { before_update_callback }.to change { work_item.iteration }.from(iteration).to(nil)
           end
         end
       end
