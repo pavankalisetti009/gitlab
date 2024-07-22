2e79bcccb8c2be27688ad1baa6b96971e248581e
Zamir Martins <zfilho@gitlab.com>
2024-07-22 14:10:57 +0000

    Add for_occurrences and with_advisory scopes
    Changelog: added
    EE: true

--- ee/app/models/package_metadata/affected_package.rb
+++ ee/app/models/package_metadata/affected_package.rb
@@ -4,6 +4,7 @@ module PackageMetadata
   class AffectedPackage < ApplicationRecord
     include Gitlab::Utils::StrongMemoize
     include BulkInsertSafe
+    include EachBatch
 
     belongs_to :advisory, class_name: 'PackageMetadata::Advisory', optional: false, foreign_key: :pm_advisory_id,
       inverse_of: :affected_packages
@@ -18,5 +19,26 @@ module PackageMetadata
     validates :overridden_advisory_fields, json_schema: { filename: 'pm_affected_package_overridden_advisory_fields' }
     validates :fixed_versions, length: { maximum: 10 }
     validates :versions, json_schema: { filename: 'pm_affected_package_versions' }
+
+    scope :for_occurrences, ->(occurrences) { with(occurrence_cte(occurrences)).joins(occurrence_cte_join) }
+
+    scope :with_advisory, -> { includes(:advisory).distinct }
+
+    def self.occurrence_cte(occurrences)
+      occurrence_data = occurrences.map do |occ|
+        [::Enums::Sbom.purl_types[occ.purl_type], occ.component_name]
+      end
+
+      Arel::Nodes::As.new(
+        Hashie::Mash.new(
+          name: Arel.sql('occurrences_cte(purl_type, name)')
+        ), Arel::Nodes::Grouping.new(Arel::Nodes::ValuesList.new(occurrence_data))
+      )
+    end
+
+    def self.occurrence_cte_join
+      'INNER JOIN occurrences_cte ON occurrences_cte.purl_type = pm_affected_packages.purl_type ' \
+        'AND occurrences_cte.name = pm_affected_packages.package_name'
+    end
   end
 end
