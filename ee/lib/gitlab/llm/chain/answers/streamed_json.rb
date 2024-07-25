0a5b000c410ebfdd338841a426336fb084160670
Tetiana Chupryna <tchupryna@gitlab.com>
2024-07-25 16:11:14 +0200

    Update parser for streaming
    Fix issue when multiple chunks are queued to be parsed

--- ee/lib/gitlab/llm/chain/answers/streamed_json.rb
+++ ee/lib/gitlab/llm/chain/answers/streamed_json.rb
@@ -5,40 +5,15 @@ module Gitlab
     module Chain
       module Answers
         class StreamedJson < StreamedAnswer
-          def initialize
-            @final_answer_started = false
-            @full_message = ''
-
-            super
-          end
-
           def next_chunk(content)
             return if content.empty?
 
-            content = ::Gitlab::Json.parse(content)
-            answer_chunk = final_answer_chunk(content)
+            parser = Parsers::SingleActionParser.new(output: content)
+            parser.parse
 
-            return unless answer_chunk
-            return payload(answer_chunk) if final_answer_started
+            return unless parser.final_answer
 
-            @full_message += answer_chunk
-
-            return unless final_answer_start(content)
-
-            @final_answer_started = true
-            payload(answer_chunk)
-          end
-
-          private
-
-          attr_accessor :full_message, :final_answer_started
-
-          def final_answer_start(content)
-            'final_answer_delta' == content['type']
-          end
-
-          def final_answer_chunk(content)
-            content.dig('data', 'text')
+            payload(parser.final_answer)
           end
         end
       end
