15a29ea22413302713a40007851ed63c9482a81f
Joe Woodward <j@joewoodward.me>
2024-07-25 11:52:55 +0100

    Track last time project.path_locks changed
    This change keeps track of the last time a Path Lock changed. In a
    following MR we will be using this information to cache a new MR
    mergeability check. The check will ensure that we are not merging locked
    files.
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/428247

--- ee/app/services/path_locks/unlock_service.rb
+++ ee/app/services/path_locks/unlock_service.rb
@@ -9,7 +9,9 @@ module PathLocks
     def execute(path_lock)
       raise AccessDenied, _('You have no permissions') unless can_unlock?(path_lock)
 
-      path_lock.destroy
+      path_lock.destroy.tap do |record|
+        project.refresh_path_locks_changed_epoch if record.destroyed?
+      end
     end
   end
 end
--- ee/spec/services/path_locks/unlock_service_spec.rb
+++ ee/spec/services/path_locks/unlock_service_spec.rb
@@ -8,20 +8,40 @@ RSpec.describe PathLocks::UnlockService, feature_category: :source_code_manageme
   let(:project)      { path_lock.project }
   let(:path)         { path_lock.path }
 
-  it 'unlocks path' do
-    allow_next_instance_of(described_class) do |instance|
-      allow(instance).to receive(:can?).and_return(true)
+  describe '#execute(path_lock)' do
+    subject(:execute) { described_class.new(project, current_user).execute(path_lock) }
+
+    context 'when the user can unlock the path lock' do
+      before do
+        allow_next_instance_of(described_class) do |instance|
+          allow(instance).to receive(:can?).with(current_user, :admin_path_locks, path_lock).and_return(true)
+        end
+      end
+
+      it 'unlocks path' do
+        expect { execute }.to change { project.path_locks.for_paths(path).count }.from(1).to(0)
+      end
+
+      it_behaves_like 'refreshes project.path_locks_changed_epoch value'
     end
-    described_class.new(project, current_user).execute(path_lock)
 
-    expect(project.path_locks.find_by(path: path)).to be_falsey
-  end
+    context 'when the user cannot unlock the path lock' do
+      let(:current_user) { build(:user) }
 
-  it 'raises exception if user has no permissions' do
-    user = create :user
+      let(:exception) { PathLocks::UnlockService::AccessDenied }
 
-    expect do
-      described_class.new(project, user).execute(path_lock)
-    end.to raise_exception(PathLocks::UnlockService::AccessDenied)
+      it 'raises exception if user has no permissions' do
+        expect { execute }.to raise_exception(exception)
+      end
+
+      context 'when the exception has been handled' do
+        subject do
+          execute
+        rescue exception
+        end
+
+        it_behaves_like 'does not refresh project.path_locks_changed_epoch'
+      end
+    end
   end
 end
