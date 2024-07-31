dfaa02597d4dc6025838e7d2ce093e62967b4e32
Heinrich Lee Yu <heinrich@gitlab.com>
2024-07-31 10:43:35 +0800

    Refactor status service to use callback classes
    Move the work item status widget service to the new callback classes
    architecture.

--- ee/spec/services/work_items/callbacks/status_spec.rb
+++ ee/spec/services/work_items/callbacks/status_spec.rb
@@ -2,21 +2,20 @@
 
 require 'spec_helper'
 
-RSpec.describe WorkItems::Widgets::StatusService::UpdateService, feature_category: :team_planning do
+RSpec.describe WorkItems::Callbacks::Status, feature_category: :team_planning do
   let_it_be(:user) { create(:user) }
   let_it_be(:project) { create(:project) }
   let_it_be_with_reload(:work_item) { create(:work_item, :requirement, project: project, author: user) }
 
-  let(:service) { described_class.new(widget: widget, current_user: user) }
-  let(:widget) { work_item.widgets.find { |widget| widget.is_a?(WorkItems::Widgets::Status) } }
+  let(:callback) { described_class.new(issuable: work_item, current_user: user, params: params) }
 
   def work_item_status
     state = work_item.reload.requirement&.last_test_report_state
     ::WorkItems::Widgets::Status::STATUS_MAP[state]
   end
 
-  describe '#update' do
-    subject { service.before_update_in_transaction(params: params) }
+  describe '#before_update' do
+    subject(:before_update_callback) { callback.before_update }
 
     shared_examples 'work item and status is unchanged' do
       it 'does not change work item status value' do
@@ -41,7 +40,7 @@ RSpec.describe WorkItems::Widgets::StatusService::UpdateService, feature_categor
       context 'when user cannot update work item' do
         let(:params) { { status: "failed" } }
 
-        before do
+        before_all do
           project.add_guest(user)
         end
 
@@ -49,7 +48,7 @@ RSpec.describe WorkItems::Widgets::StatusService::UpdateService, feature_categor
       end
 
       context 'when user can update work item' do
-        before do
+        before_all do
           project.add_reporter(user)
         end
 
@@ -75,7 +74,7 @@ RSpec.describe WorkItems::Widgets::StatusService::UpdateService, feature_categor
               let(:params) { { status: new_status } }
 
               it 'errors' do
-                expect { subject }.to raise_error(ArgumentError, /is not a valid state/)
+                expect { before_update_callback }.to raise_error(ArgumentError, /is not a valid state/)
               end
             end
           end
@@ -91,13 +90,13 @@ RSpec.describe WorkItems::Widgets::StatusService::UpdateService, feature_categor
           end
 
           before do
-            allow(service).to receive(:new_type_excludes_widget?).and_return(true)
+            allow(callback).to receive(:excluded_in_new_type?).and_return(true)
           end
 
           it "deletes the associated test report and requirement" do
             requirement = work_item.requirement
 
-            expect { subject }.to change { work_item.test_reports.count }.from(2).to(0)
+            expect { before_update_callback }.to change { work_item.test_reports.count }.from(2).to(0)
 
             expect(RequirementsManagement::TestReport.exists?(test_report1.id)).to eq(false)
             expect(RequirementsManagement::TestReport.exists?(test_report2.id)).to eq(false)
