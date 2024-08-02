e4933958b7d7115774f032f11beb9e7b2af49bc1
Artur Fedorov <afedorov@gitlab.com>
2024-08-02 11:41:54 +0000

    This MR updated dispatch service
    If shared secret is provided HMAC
    header needs to be added to header
    
    Changelog: changed
    EE: true

--- ee/spec/services/external_status_checks/dispatch_service_spec.rb
+++ ee/spec/services/external_status_checks/dispatch_service_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe ExternalStatusChecks::DispatchService, feature_category: :source_code_management do
+  include StubRequests
+
   let_it_be(:rule) { build_stubbed(:external_status_check, external_url: 'https://test.example.com/callback') }
 
   subject { described_class.new(rule, {}).execute }
@@ -10,7 +12,7 @@ RSpec.describe ExternalStatusChecks::DispatchService, feature_category: :source_
   describe '#execute' do
     context 'service responds with success' do
       before do
-        stub_success
+        stub_full_request(rule.external_url, method: :post)
       end
 
       it 'is successful' do
@@ -19,6 +21,29 @@ RSpec.describe ExternalStatusChecks::DispatchService, feature_category: :source_
 
       it 'passes back the http status code' do
         expect(subject.http_status).to eq(200)
+      end
+
+      context 'without shared secret' do
+        it 'sets correct headers when there is no HMAC secret' do
+          expect(subject).to have_requested(:post, stubbed_hostname(rule.external_url)).with(
+            headers: { 'Content-Type' => 'application/json' }
+          ).once
+        end
+      end
+
+      context 'with shared secret' do
+        let_it_be(:shared_secret) { 'shared_secret' }
+        let_it_be(:request_body_size_limit) { 25.megabytes }
+        let_it_be(:rule) { build_stubbed(:external_status_check, external_url: 'https://test.example.com/callback', shared_secret: shared_secret) }
+
+        subject { described_class.new(rule, {}).execute }
+
+        it 'sets correct headers when there is no HMAC secret' do
+          expect(subject).to have_requested(:post, stubbed_hostname(rule.external_url)).with(
+            headers: { 'Content-Type' => 'application/json',
+                       'X-GitLab-Signature': '6c52a647de5847fc387ead12919dc6a5b9ed08e9538583c83da266d958e33aec' }
+          ).once
+        end
       end
     end
 
@@ -38,12 +63,6 @@ RSpec.describe ExternalStatusChecks::DispatchService, feature_category: :source_
   end
 
   private
-
-  def stub_success
-    stub_request(:post, 'https://test.example.com/callback')
-      .with(headers: { 'Content-Type' => 'application/json' })
-      .to_return(status: 200, body: "", headers: {})
-  end
 
   def stub_failure
     stub_request(:post, 'https://test.example.com/callback')
