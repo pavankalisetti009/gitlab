ba3360000e58eb8a0633cfddf94c0743b009b948
Patrick Bajao <ebajao@gitlab.com>
2024-08-05 20:16:34 +0000

    Show alert about not rendering files due to path encoding
    Merge branch 'security-encoded-file-paths' into 'master'
    
    See merge request gitlab-org/security/gitlab!4267
    
    Changelog: security

--- db/structure.sql
+++ db/structure.sql
@@ -12515,7 +12515,8 @@ CREATE TABLE merge_request_context_commit_diff_files (
     diff text,
     "binary" boolean,
     merge_request_context_commit_id bigint NOT NULL,
-    generated boolean
+    generated boolean,
+    encoded_file_path boolean DEFAULT false NOT NULL
 );
 
 CREATE TABLE merge_request_context_commits (
@@ -12610,7 +12611,8 @@ CREATE TABLE merge_request_diff_files (
     "binary" boolean,
     external_diff_offset integer,
     external_diff_size integer,
-    generated boolean
+    generated boolean,
+    encoded_file_path boolean DEFAULT false NOT NULL
 );
 
 CREATE TABLE merge_request_diffs (
