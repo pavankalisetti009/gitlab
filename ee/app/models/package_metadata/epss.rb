1e1d3d03038ea74054cf9d6e662727ff6e8395d6
Yasha Rise <yrise@gitlab.com>
2024-08-08 15:10:59 +0000

    Add EPSS to GitLab database
    Changelog: added
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/158908
    EE: true

---
+++ ee/app/models/package_metadata/epss.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module PackageMetadata
+  class Epss < ApplicationRecord
+    include BulkInsertSafe
+
+    self.table_name = 'pm_epss'
+
+    # The 15 (total 24) character limit is abitrary. CVE IDs are not limited
+    # but we do not expect them to exceed this limit.
+    # See https://cve.mitre.org/cve/identifiers/syntaxchange.html
+    CVE_REGEX = /\ACVE-\d{4}-\d{4,15}\z/
+
+    validates :cve, presence: true, format: { with: CVE_REGEX }
+    validates :score, presence: true
+  end
+end
