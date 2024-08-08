9e978daaca1d36d8dc97289d11e1ed08c5833b4f
Max Fan <mfan@gitlab.com>
2024-08-08 14:51:41 -0700

    Not using instance variables when pushing FE FF
    Instead of relying on a variable that might be nil
    We get it directly from the source

--- ee/app/controllers/ee/projects/jobs_controller.rb
+++ ee/app/controllers/ee/projects/jobs_controller.rb
@@ -7,13 +7,17 @@ module EE
 
       prepended do
         before_action only: [:show] do
-          push_frontend_ability(ability: :troubleshoot_job_with_ai, resource: @build, user: @current_user)
+          push_frontend_ability(
+            ability: :troubleshoot_job_with_ai,
+            resource: find_job_as_processable,
+            user: current_user
+          )
           set_application_context!
         end
       end
 
       def set_application_context!
-        ::Gitlab::ApplicationContext.push(ai_resource: @build.try(:to_global_id)) # rubocop:disable Gitlab/ModuleWithInstanceVariables -- build comes from the main jobs controller
+        ::Gitlab::ApplicationContext.push(ai_resource: find_job_as_processable.try(:to_global_id))
       end
     end
   end
