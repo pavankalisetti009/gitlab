b2a57db58eb817eac96235724ce99cd54dcbda1b
Huzaifa Iftikhar <hiftikhar@gitlab.com>
2024-08-08 07:33:00 +0000

    Add compliance_checks and compliance_requirements tables
    Changelog: added

---
+++ ee/app/models/compliance_management/compliance_framework/compliance_requirement.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module ComplianceManagement
+  module ComplianceFramework
+    class ComplianceRequirement < ApplicationRecord
+      self.table_name = 'compliance_requirements'
+
+      belongs_to :framework, class_name: 'ComplianceManagement::Framework', optional: false
+
+      validates_presence_of :framework, :namespace_id, :name, :description
+      validates :name, uniqueness: { scope: :framework_id }
+
+      has_many :compliance_checks, class_name: 'ComplianceManagement::ComplianceFramework::ComplianceCheck',
+        foreign_key: :requirement_id, inverse_of: :compliance_requirement
+    end
+  end
+end
---
+++ ee/spec/models/compliance_management/compliance_framework/compliance_requirement_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ComplianceManagement::ComplianceFramework::ComplianceRequirement,
+  type: :model, feature_category: :compliance_management do
+  describe 'validations' do
+    let_it_be(:group) { create(:group) }
+    let_it_be(:compliance_framework) { create(:compliance_framework, namespace: group) }
+    let_it_be(:requirement) { create(:compliance_requirement, framework: compliance_framework) }
+
+    it { is_expected.to validate_uniqueness_of(:name).scoped_to(:framework_id) }
+    it { is_expected.to validate_presence_of(:namespace_id) }
+    it { is_expected.to validate_presence_of(:framework) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:framework).optional(false) }
+
+    it do
+      is_expected.to have_many(:compliance_checks)
+        .class_name('ComplianceManagement::ComplianceFramework::ComplianceCheck')
+    end
+  end
+end
