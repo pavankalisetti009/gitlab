5785b4e21bf90f56107a6282280790f84ffec9a8
Dan LaManna <me@danlamanna.com>
2024-08-08 07:02:13 +0000

    Add setting for max_artifact_archive_size_bytes
    Add a new application setting called max_artifact_archive_size_bytes to
    control the maximum size of the archive of CI artifacts that can be
    stored.
    
    Fixes https://gitlab.com/gitlab-org/gitlab/-/issues/443575
    
    Changelog: added

--- db/structure.sql
+++ db/structure.sql
@@ -5883,6 +5883,7 @@ CREATE TABLE application_settings (
     ai_action_api_rate_limit integer DEFAULT 160 NOT NULL,
     require_personal_access_token_expiry boolean DEFAULT true NOT NULL,
     duo_workflow jsonb DEFAULT '{}'::jsonb,
+    max_artifacts_content_include_size integer DEFAULT 5242880 NOT NULL,
     CONSTRAINT app_settings_container_reg_cleanup_tags_max_list_size_positive CHECK ((container_registry_cleanup_tags_service_max_list_size >= 0)),
     CONSTRAINT app_settings_dep_proxy_ttl_policies_worker_capacity_positive CHECK ((dependency_proxy_ttl_group_policy_worker_capacity >= 0)),
     CONSTRAINT app_settings_ext_pipeline_validation_service_url_text_limit CHECK ((char_length(external_pipeline_validation_service_url) <= 255)),
