654e36964ba018db182a82cc135ce541ab460e04
Chad Woolley <cwoolley@gitlab.com>
2024-08-09 13:19:50 +0000

    Move Remote Dev settings back to EE

--- ee/spec/lib/remote_development/rd_fast_spec_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Require non-EE helper logic
-require "fast_spec_helper"
-
-# EE-specific helper logic
-require_relative '../../support/shared_contexts/remote_development/agent_info_status_fixture_not_implemented_error'
-require_relative '../../support/shared_contexts/remote_development/remote_development_shared_contexts'
-require_relative '../../../app/models/remote_development/enums/workspace'
