da4e7f1b5b422aa3e49b875fea7de58422bc3f28
Scott de Jonge <sdejonge@gitlab.com>
2024-08-12 01:34:46 +0000

    Remove gl-text-gray-900! overrides on buttons
    Update icon buttons to use `icon` prop in `GlButton`

--- ee/app/assets/javascripts/sidebar/components/incidents/escalation_policies_empty_state.vue
+++ ee/app/assets/javascripts/sidebar/components/incidents/escalation_policies_empty_state.vue
@@ -1,5 +1,5 @@
 <script>
-import { GlIcon, GlButton } from '@gitlab/ui';
+import { GlButton } from '@gitlab/ui';
 import { i18nPolicyText } from '../../constants';
 import EscalationPolicyHelpState from './escalation_policy_help_state.vue';
 import EscalationPolicyCollapsedState from './escalation_policy_collapsed_state.vue';
@@ -7,7 +7,6 @@ import EscalationPolicyCollapsedState from './escalation_policy_collapsed_state.
 export default {
   i18n: i18nPolicyText,
   components: {
-    GlIcon,
     GlButton,
     EscalationPolicyCollapsedState,
     EscalationPolicyHelpState,
@@ -35,13 +34,12 @@ export default {
         <gl-button
           :data-testid="showHelp ? 'close-help-button' : 'help-button'"
           category="tertiary"
+          :icon="showHelp ? 'close' : 'question-o'"
           size="small"
           variant="link"
           class="gl-ml-auto"
           @click="toggleHelpState"
-        >
-          <gl-icon :name="showHelp ? 'close' : 'question-o'" class="gl-text-gray-900!" />
-        </gl-button>
+        />
       </div>
 
       <div data-testid="select-escalation-policy" class="hide-collapsed gl-line-height-1">
