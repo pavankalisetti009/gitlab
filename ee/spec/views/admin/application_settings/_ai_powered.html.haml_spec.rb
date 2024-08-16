03e4c0c98d4b5ce44f5fb057e23f4b35a3bdeada
Lindsey Shelton <lshelton@gitlab.com>
2024-08-16 13:40:20 -0500

    Ban AI remove Duo Admin Settings FF
    Remove ai_settings_vue_admin feature flag
    Remove and decommission old haml AI-Powered settings
    
    Changelog: removed

--- ee/spec/views/admin/application_settings/_ai_powered.html.haml_spec.rb
+++ ee/spec/views/admin/application_settings/_ai_powered.html.haml_spec.rb
@@ -9,62 +9,15 @@ RSpec.describe 'admin/application_settings/_ai_powered', :with_cloud_connector, 
     assign(:application_setting, application_setting)
   end
 
-  context 'with `ai_settings_vue_admin` enabled' do
+  context 'when duo chat is available' do
     before do
-      stub_feature_flags(ai_settings_vue_admin: true)
+      stub_licensed_features(ai_chat: true)
     end
 
-    context 'when duo chat is available' do
-      before do
-        stub_licensed_features(ai_chat: true)
-      end
+    it 'renders the settings app root' do
+      render
 
-      it 'renders the settings app root' do
-        render
-
-        expect(rendered).to have_selector('#js-ai-settings')
-        expect(rendered).not_to have_selector('#application_setting_disabled_direct_code_suggestions')
-      end
-
-      it 'does not render the haml anchor' do
-        expect(rendered).not_to have_selector('#js-ai-powered-settings')
-      end
-    end
-  end
-
-  context 'with `ai_settings_vue_admin` disabled' do
-    before do
-      stub_feature_flags(ai_settings_vue_admin: false)
-    end
-
-    context 'when duo chat is available' do
-      before do
-        stub_licensed_features(ai_chat: true)
-      end
-
-      it 'renders the settings app root' do
-        render
-
-        expect(rendered).to have_selector('#js-ai-powered-settings')
-        expect(rendered).not_to have_selector('#application_setting_disabled_direct_code_suggestions')
-      end
-    end
-
-    context 'when duo pro is available' do
-      before do
-        allow(CloudConnector::AvailableServices)
-          .to receive_message_chain(:find_by_name, :purchased?).and_return(true)
-      end
-
-      it 'does not render the haml anchor' do
-        expect(rendered).not_to have_selector('#js-ai-settings')
-      end
-
-      it 'renders the settings app root' do
-        render
-
-        expect(rendered).to have_selector('#application_setting_disabled_direct_code_suggestions')
-      end
+      expect(rendered).to have_selector('#js-ai-settings')
     end
   end
 end
