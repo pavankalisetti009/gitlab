36b335c8bb1ec0d14042b9bb6feec355f796a77b
Dmitry Gruzd <dgruzd@gitlab.com>
2024-08-16 11:54:34 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: DANGER_GITLAB_API_TOKEN <project_278964_bot_445da1b60fc7336b6b6776383134d10f@noreply.gitlab.com>

--- ee/app/services/search/zoekt/task_presenter_service.rb
+++ ee/app/services/search/zoekt/task_presenter_service.rb
@@ -4,13 +4,21 @@ module Search
   module Zoekt
     class TaskPresenterService
       DEFAULT_LIMIT = 20
-      MAX_LIMIT = 100
+      MAX_LIMIT = 200
 
       attr_reader :node, :concurrency_limit
 
+      def self.execute(...)
+        new(...).execute
+      end
+
       def initialize(node)
         @node = node
-        @concurrency_limit = get_concurrency_limit(node: node)
+        @concurrency_limit = if Feature.enabled?(:zoekt_new_concurrency_limit_logic, Feature.current_request)
+                               get_concurrency_limit(node: node)
+                             else
+                               get_legacy_concurrency_limit(node: node)
+                             end
       end
 
       def execute
@@ -23,13 +31,18 @@ module Search
         end
       end
 
-      def self.execute(...)
-        new(...).execute
-      end
-
       private
 
       def get_concurrency_limit(node:)
+        # concurrency override is going to become a setting https://gitlab.com/gitlab-org/gitlab/-/issues/478595
+        concurrency = node.metadata['concurrency_override'] || node.metadata['concurrency']
+
+        return DEFAULT_LIMIT if concurrency.to_i == 0
+
+        concurrency.clamp(0, MAX_LIMIT)
+      end
+
+      def get_legacy_concurrency_limit(node:)
         task_count = node.metadata['task_count']
         # concurrency override is going to become a setting https://gitlab.com/gitlab-org/gitlab/-/issues/478595
         concurrency = node.metadata['concurrency_override'] || node.metadata['concurrency']
