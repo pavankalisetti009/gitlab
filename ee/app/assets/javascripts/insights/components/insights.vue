c7748d47760864b9489442a695e325b187145fba
Lorena Ciutacu <lciutacu@gitlab.com>
2024-08-16 21:04:36 +0000

    Update links from UI to docs

--- ee/app/assets/javascripts/insights/components/insights.vue
+++ ee/app/assets/javascripts/insights/components/insights.vue
@@ -133,10 +133,7 @@ export default {
     insightsConfigurationText: INSIGHTS_CONFIGURATION_TEXT,
     insightsPageFilteredOut: INSIGHTS_PAGE_FILTERED_OUT,
   },
-  // eslint-disable-next-line local-rules/require-valid-help-page-path
-  insightsDocumentationLink: helpPagePath('user/group/insights/index.md', {
-    anchor: 'configure-your-insights',
-  }),
+  insightsDocumentationLink: helpPagePath('user/project/insights/index'),
 };
 </script>
 <template>
