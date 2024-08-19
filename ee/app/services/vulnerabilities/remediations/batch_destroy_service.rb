11bf423c158d83beb3a210bf21968f797a26e8fa
Michael Becker <11881043-wandering_person@users.noreply.gitlab.com>
2024-08-19 13:19:50 +0000

    Deleted `vulnerability_remediations` leave behind `uploads` records
    During [issue 378269][0], we added a cron worker that deletes orphaned
    `remediations` from the database. An "orphaned remediation" is a
    remediation that is no longer associated with a
    `vulnerability_occurrence` (aka `finding`).
    
    Because of the use of `delete_all` rather than `destroy_all`, we ended
    up with orphaned `Uploads` instead of orphaned remediations.
    
    This change moves the deletion logic to a new service. This service:
    
    1. iterates over orphaned remediations in batches
    2. for each batch, finds all `Uploads` associated with it and calls
       [`fast_destroy_all`][1] on those
    3. then calls `delete_all` on the batch of remediations
    
    The specs for this new service have been expanded to also assert the
    `Uploads` are being deleted
    
    [0]:https://gitlab.com/gitlab-org/gitlab/-/issues/378269
    [1]:https://gitlab.com/gitlab-org/gitlab/-/blob/f1ebbe522423b514610449d0f6dc7a262f855314/app/models/concerns/fast_destroy_all.rb
    
    ---
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/462724
    MR:https://gitlab.com/gitlab-org/gitlab/-/merge_requests/160579
    
    Changelog: fixed
    EE: true

---
+++ ee/app/services/vulnerabilities/remediations/batch_destroy_service.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Vulnerabilities
+  module Remediations
+    class BatchDestroyService
+      include BaseServiceUtility
+
+      def initialize(remediations:)
+        @remediations = remediations
+      end
+
+      def execute
+        return success_response if remediations.blank?
+        raise argument_error unless remediations.is_a?(ActiveRecord::Relation)
+
+        remediations
+          .tap { |remediations| destroy_uploads!(remediations) }
+          .then(&:delete_all)
+          .then { |deleted_count| success_response(deleted_count) }
+      end
+
+      private
+
+      attr_reader :remediations
+
+      def argument_error
+        ArgumentError.new('remediations must be of type ActiveRecord::Relation')
+      end
+
+      def destroy_uploads!(remediations)
+        # rubocop: disable CodeReuse/ActiveRecord -- couldn't find a Finder to use
+        Upload.where(
+          model_type: Vulnerabilities::Remediation,
+          model_id: remediations,
+          uploader: AttachmentUploader
+        ).then { |uploads| [uploads, uploads.begin_fast_destroy] }
+          .tap { |uploads, _| uploads.delete_all }
+          .tap { |_, files| Upload.finalize_fast_destroy(files) }
+        # rubocop: enable CodeReuse/ActiveRecord
+      end
+
+      def success_response(deleted_count = 0)
+        ServiceResponse.success(payload: { rows_deleted: deleted_count })
+      end
+    end
+  end
+end
