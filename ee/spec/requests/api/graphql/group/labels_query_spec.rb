fd348c33d4fec50e252b115007c3bdc52ca78ca2
charlieablett <cablett@gitlab.com>
2024-08-20 21:21:04 +1200

    Allow labels search by exact title
    Changelog: added

--- ee/spec/requests/api/graphql/group/labels_query_spec.rb
+++ ee/spec/requests/api/graphql/group/labels_query_spec.rb
@@ -5,7 +5,8 @@ require 'spec_helper'
 RSpec.describe 'getting group label information', feature_category: :team_planning do
   include GraphqlHelpers
 
-  let_it_be(:group) { create(:group, :public) }
+  let_it_be(:parent_group) { create(:group) }
+  let_it_be(:group) { create(:group, :public, parent: parent_group) }
   let_it_be(:label_factory) { :group_label }
   let_it_be(:label_attrs) { { group: group } }
 
@@ -14,6 +15,32 @@ RSpec.describe 'getting group label information', feature_category: :team_planni
 
     def make_query(fields)
       graphql_query_for('group', { full_path: group.full_path }, fields)
+    end
+  end
+
+  context 'when searching title within a hierarchy' do
+    let_it_be(:current_user) { create(:user) }
+    let_it_be(:labels_params) { { title: 'priority::1', includeAncestorGroups: true, includeDescendantGroups: true } }
+    let_it_be(:project) { create(:project, :public, namespace: group) }
+    let_it_be(:subgroup) { create(:group, :public, parent: group) }
+    let_it_be(:project_label) { create(:label, project: project, title: 'priority::1', color: '#FF0000') }
+    let_it_be(:parent_group_label) { create(:group_label, group: parent_group, title: 'priority::1', color: '#FF00FF') }
+    let_it_be(:subgroup_label) { create(:group_label, group: subgroup, title: 'priority::1', color: '#000000') }
+
+    let(:labels_response) { graphql_data.dig('group', 'labels', 'nodes') }
+    let(:query) do
+      graphql_query_for('group', { full_path: group.full_path }, [
+        query_graphql_field(:labels, labels_params, [query_graphql_field(:nodes, nil, %w[id title])])
+      ])
+    end
+
+    it 'finds the labels with exact title matching' do
+      post_graphql(query, current_user: current_user)
+      expect(graphql_errors).not_to be_present
+
+      expect(labels_response.pluck('id')).to contain_exactly(
+        *[project_label, parent_group_label, subgroup_label].map { |l| l.to_global_id.to_s }
+      )
     end
   end
 
