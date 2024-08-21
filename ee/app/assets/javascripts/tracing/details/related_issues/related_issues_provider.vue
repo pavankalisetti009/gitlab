502214294dec5bbddc73ec7f58f06a5bc22b3568
Daniele Rossetti <drossetti@gitlab.com>
2024-08-21 14:08:51 +0000

    Fetch trace related issues

---
+++ ee/app/assets/javascripts/tracing/details/related_issues/related_issues_provider.vue
@@ -0,0 +1,49 @@
+<script>
+import getTraceRelatedIssues from './graphql/get_trace_related_issues.query.graphql';
+
+export default {
+  props: {
+    projectFullPath: {
+      type: String,
+      required: true,
+    },
+    traceId: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      relatedIssues: [],
+      error: null,
+      isLoading: false,
+    };
+  },
+  apollo: {
+    relatedIssues: {
+      query: getTraceRelatedIssues,
+      variables() {
+        return {
+          projectFullPath: this.projectFullPath,
+          traceId: this.traceId,
+        };
+      },
+      update(data) {
+        return data.project?.issues?.nodes || [];
+      },
+      error(error) {
+        this.error = error;
+      },
+    },
+  },
+  render() {
+    if (!this.$scopedSlots.default) return null;
+
+    return this.$scopedSlots.default({
+      issues: this.relatedIssues,
+      loading: this.$apollo.loading,
+      error: this.error,
+    });
+  },
+};
+</script>
