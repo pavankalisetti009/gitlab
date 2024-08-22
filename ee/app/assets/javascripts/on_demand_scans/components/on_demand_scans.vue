b1080089aa55d6bba17790a1c1fe379d7d259afd
Artur Fedorov <afedorov@gitlab.com>
2024-08-23 01:00:48 +0200

    Fix specs for Vue 3 migration
    Vue 3 specs with router without
    defined components and default route

--- ee/app/assets/javascripts/on_demand_scans/components/on_demand_scans.vue
+++ ee/app/assets/javascripts/on_demand_scans/components/on_demand_scans.vue
@@ -72,6 +72,7 @@ export default {
   data() {
     return {
       activeTabIndex: 0,
+      liveOnDemandScanCounts: undefined,
       showAuditorMessageAlert: !this.canEditOnDemandScans,
     };
   },
