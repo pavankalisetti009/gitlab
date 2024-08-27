14a6b20c34e723545d4d8d093795385ec83289d2
Zachary Cuddy <zcuddy@gitlab.com>
2024-08-27 09:31:22 -0500

    Duo Health Check - Child components
    This change refactors the GitLab
    Duo Health List into having
    childing components.  This helps
    isolate the tests and make the code
    generally more readable

---
+++ ee/app/assets/javascripts/usage_quotas/code_suggestions/components/health_check_list_probe.vue
@@ -0,0 +1,39 @@
+<script>
+import { GlIcon } from '@gitlab/ui';
+
+export default {
+  name: 'HealthCheckListProbe',
+  components: {
+    GlIcon,
+  },
+  props: {
+    probe: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    probeCSS() {
+      return this.probe.success
+        ? 'gl-text-green-900 gl-bg-green-50'
+        : 'gl-text-red-900 gl-bg-red-50';
+    },
+    probeIcon() {
+      return this.probe.success
+        ? { name: 'check-circle', variant: 'success' }
+        : { name: 'error', variant: 'danger' };
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    class="gl-my-3 gl-rounded-small gl-px-3 gl-py-2"
+    :class="probeCSS"
+    data-testid="health-check-probe"
+  >
+    <gl-icon v-bind="probeIcon" />
+    {{ probe.message }}
+  </div>
+</template>
