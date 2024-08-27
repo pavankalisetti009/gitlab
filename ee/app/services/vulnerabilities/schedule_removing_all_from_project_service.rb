cac3389a774500932dae6e8a83c037b03940b83b
Mehmet Emin INAC <minac@gitlab.com>
2024-08-27 14:42:53 +0200

    Clarify the name of the service class scheduling vulnerability removal

--- ee/app/services/vulnerabilities/schedule_removing_all_from_project_service.rb
+++ ee/app/services/vulnerabilities/schedule_removing_all_from_project_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Vulnerabilities
-  class RemoveAllFromProjectService
+  class ScheduleRemovingAllFromProjectService
     def initialize(projects)
       @projects = projects
     end
