366b21a9145d61862129e1fa1cbdce2bbf3a6319
Erick Bajao <fbajao@gitlab.com>
2024-08-27 15:26:41 +0000

    Add graphql query for project secrets manager

--- ee/spec/graphql/types/query_type_spec.rb
+++ ee/spec/graphql/types/query_type_spec.rb
@@ -47,7 +47,8 @@ RSpec.describe GitlabSchema.types['Query'], feature_category: :shared do
       :audit_events_instance_streaming_destinations,
       :self_managed_users_queued_for_role_promotion,
       :ai_self_hosted_models,
-      :cloud_connector_status
+      :cloud_connector_status,
+      :project_secrets_manager
     ]
 
     all_expected_fields = expected_foss_fields + expected_ee_fields
