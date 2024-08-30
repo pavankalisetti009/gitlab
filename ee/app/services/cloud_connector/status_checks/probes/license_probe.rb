a4d63a3d837e256cb4941244895eccb7d6fdfe61
Nikola Milojevic <nmilojevic@gitlab.com>
2024-08-30 18:28:55 +0200

    Refactor license probe
    - Use ActiveModel validators
    - Add license details
    - Improve specs

--- ee/app/services/cloud_connector/status_checks/probes/license_probe.rb
+++ ee/app/services/cloud_connector/status_checks/probes/license_probe.rb
@@ -4,15 +4,44 @@ module CloudConnector
   module StatusChecks
     module Probes
       class LicenseProbe < BaseProbe
-        def execute(*)
-          license = License.current
-          return failure(missing_license_text) unless license
-          return failure(wrong_license_text) unless license.online_cloud_license?
+        extend ::Gitlab::Utils::Override
 
-          success('Subscription can be synchronized.')
-        end
+        validate :check_license_exists
+        validate :check_license_valid
+
+        after_validation :collect_license_details
 
         private
+
+        def license
+          @license ||= License.current
+        end
+
+        override :success_message
+        def success_message
+          _('Subscription can be synchronized.')
+        end
+
+        def check_license_exists
+          errors.add(:base, missing_license_text) unless license
+        end
+
+        def check_license_valid
+          return unless license
+          return if license.online_cloud_license?
+
+          errors.add(:base, wrong_license_text)
+        end
+
+        def collect_license_details
+          return unless license
+
+          details.add(:plan, license.plan)
+          details.add(:trial, license.trial?)
+          details.add(:expires_at, license.expires_at)
+          details.add(:grace_period_expired, license.grace_period_expired?)
+          details.add(:online_cloud_license, license.online_cloud_license?)
+        end
 
         def missing_license_text
           _("Subscription for this instance cannot be synchronized. " \
