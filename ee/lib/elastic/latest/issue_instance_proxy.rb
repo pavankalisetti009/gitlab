c0034b01a6612c4cc6f630671c108551b2073bc9
maddievn <mvanniekerk@gitlab.com>
2024-09-02 11:41:42 +0200

    Handle issue references when project is nil
    Changelog: changed
    EE: true

--- ee/lib/elastic/latest/issue_instance_proxy.rb
+++ ee/lib/elastic/latest/issue_instance_proxy.rb
@@ -9,6 +9,8 @@ module Elastic
       def as_indexed_json(options = {})
         data = {}
 
+        return data unless target.project_id
+
         # We don't use as_json(only: ...) because it calls all virtual and serialized attributes
         # https://gitlab.com/gitlab-org/gitlab/issues/349
         [:id, :iid, :title, :description, :created_at, :updated_at, :state, :project_id, :author_id, :confidential].each do |attr|
