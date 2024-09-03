c387eef6cc488e8960a94aea52291bb69ddf65b7
Janis Altherr <jaltherr@gitlab.com>
2024-09-03 15:51:59 +0000

    Add expire_in property to pages CI definition
    Signed-off-by: janis <jaltherr@gitlab.com>

--- ee/app/services/ee/projects/update_pages_service.rb
+++ ee/app/services/ee/projects/update_pages_service.rb
@@ -15,11 +15,30 @@ module EE
 
       private
 
-      def expires_at
-        return unless ::Gitlab::CurrentSettings.pages_extra_deployments_default_expiry_seconds&.nonzero?
-        return unless extra_deployment?
+      def fallback_expiry_date
+        value = ::Gitlab::CurrentSettings.pages_extra_deployments_default_expiry_seconds
 
-        ::Gitlab::CurrentSettings.pages_extra_deployments_default_expiry_seconds.seconds.from_now
+        # A value of 0 means deployments should not expire, in this case return nil
+        value.seconds.from_now if value.nonzero?
+      end
+
+      def custom_expire_in
+        build.pages&.fetch(:expire_in, nil)
+      end
+
+      def custom_expiry_date
+        ::Gitlab::Ci::Build::DurationParser.new(custom_expire_in).seconds_from_now
+      end
+
+      def expiry_customised?
+        custom_expire_in.present?
+      end
+
+      # returns a datetime for the expiry or may return nil if expire_in='never'
+      def expires_at
+        return custom_expiry_date if expiry_customised?
+
+        fallback_expiry_date if extra_deployment?
       end
     end
   end
