315bea33c8ecb89148d39e581c901353f720ddff
Gabriel Mazetto <gabriel@gitlab.com>
2024-09-05 18:29:17 +0200

    Add error handling to GitlabConfig

--- gems/gitlab-backup-cli/spec/gitlab/backup/cli/gitlab_config_spec.rb
+++ gems/gitlab-backup-cli/spec/gitlab/backup/cli/gitlab_config_spec.rb
@@ -11,5 +11,34 @@ RSpec.describe Gitlab::Backup::Cli::GitlabConfig do
         expect(gitlab_config.keys).to include('test')
       end
     end
+
+    context 'when provided with a filepath that doesnt exist' do
+      let(:config_fixture) { fixtures_path.join('unknown-gitlab.yml') }
+
+      it 'does not raise an exception', :silence_output do
+        expect { gitlab_config }.not_to raise_error
+        expect(gitlab_config).not_to be_loaded
+      end
+
+      it 'displays an error message' do
+        expect { gitlab_config }.to output(/GitLab configuration file: .+ does not exist/).to_stderr
+      end
+    end
+
+    context 'when process lack enough permission to read provided config file' do
+      before do
+        allow(ActiveSupport::ConfigurationFile).to receive(:parse).and_raise(Errno::EACCES)
+      end
+
+      it 'does not raise an exception', :silence_output do
+        expect { gitlab_config }.not_to raise_error
+        expect(gitlab_config).not_to be_loaded
+      end
+
+      it 'displays an error message' do
+        error_message_pattern = /GitLab configuration file: .+ can't be read \(permission denied\)/
+        expect { gitlab_config }.to output(error_message_pattern).to_stderr
+      end
+    end
   end
 end
