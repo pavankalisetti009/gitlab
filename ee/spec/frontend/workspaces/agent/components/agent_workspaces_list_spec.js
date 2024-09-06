d35097c0a49f6c41e415c6221173a076f52dbac9
Marina Mosti <mmosti@gitlab.com>
2024-09-06 13:04:02 +0000

    fix agent_workspaces_list_spec.js Vue 3 specs

--- ee/spec/frontend/workspaces/agent/components/agent_workspaces_list_spec.js
+++ ee/spec/frontend/workspaces/agent/components/agent_workspaces_list_spec.js
@@ -1,19 +1,16 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import VueApollo from 'vue-apollo';
 import Vue, { nextTick } from 'vue';
-import { GlAlert, GlButton, GlLink, GlSkeletonLoader } from '@gitlab/ui';
 import { logError } from '~/lib/logger';
 import createMockApollo from 'helpers/mock_apollo_helper';
 import waitForPromises from 'helpers/wait_for_promises';
-import WorkspaceEmptyState from 'ee/workspaces/common/components/workspaces_list/empty_state.vue';
-import WorkspacesTable from 'ee/workspaces/common/components/workspaces_list/workspaces_table.vue';
-import WorkspacesListPagination from 'ee/workspaces/common/components/workspaces_list/workspaces_list_pagination.vue';
+
 import getProjectsDetailsQuery from 'ee/workspaces/common/graphql/queries/get_projects_details.query.graphql';
 import agentWorkspacesListQuery from 'ee/workspaces/agent/graphql/queries/agent_workspaces_list.query.graphql';
 import AgentWorkspacesList from 'ee/workspaces/agent/components/agent_workspaces_list.vue';
 import { populateWorkspacesWithProjectDetails } from 'ee/workspaces/common/services/utils';
+import WorkspacesList from 'ee/workspaces/common/components/workspaces_list/workspaces_list.vue';
 import {
-  AGENT_WORKSPACES_LIST_QUERY_EMPTY_RESULT,
   AGENT_WORKSPACES_LIST_QUERY_RESULT,
   GET_PROJECTS_DETAILS_QUERY_RESULT,
 } from '../../mock_data';
@@ -47,7 +44,7 @@ describe('workspaces/agent/components/agent_workspaces_list.vue', () => {
   };
   const createWrapper = () => {
     // noinspection JSCheckFunctionSignatures
-    wrapper = mount(AgentWorkspacesList, {
+    wrapper = shallowMount(AgentWorkspacesList, {
       apolloProvider: mockApollo,
       provide: {
         emptyStateSvgPath: SVG_PATH,
@@ -58,48 +55,11 @@ describe('workspaces/agent/components/agent_workspaces_list.vue', () => {
       },
     });
   };
-  const findAlert = () => wrapper.findComponent(GlAlert);
-  const findHelpLink = () => wrapper.findComponent(GlLink);
-  const findTable = () => wrapper.findComponent(WorkspacesTable);
-  const findPagination = () => wrapper.findComponent(WorkspacesListPagination);
-  const findAllConfirmButtons = () =>
-    wrapper.findAllComponents(GlButton).filter((button) => button.props().variant === 'confirm');
+
+  const findWorkspacesList = () => wrapper.findComponent(WorkspacesList);
 
   beforeEach(() => {
     buildMockApollo();
-  });
-
-  describe('when no workspaces are available', () => {
-    beforeEach(async () => {
-      agentWorkspacesListQueryHandler.mockReset();
-      agentWorkspacesListQueryHandler.mockResolvedValueOnce(
-        AGENT_WORKSPACES_LIST_QUERY_EMPTY_RESULT,
-      );
-
-      createWrapper();
-      await waitForPromises();
-    });
-
-    it('renders empty state when no workspaces are available', () => {
-      expect(wrapper.findComponent(WorkspaceEmptyState).exists()).toBe(true);
-    });
-
-    it('does not render a confirm button when empty state is present', () => {
-      expect(findAllConfirmButtons().length).toBe(0);
-    });
-
-    it('does not render the workspaces table', () => {
-      expect(findTable().exists()).toBe(false);
-    });
-
-    it('does not render the workspaces pagination', () => {
-      expect(findPagination().exists()).toBe(false);
-    });
-  });
-
-  it('shows loading state when workspaces are being fetched', () => {
-    createWrapper();
-    expect(wrapper.findComponent(GlSkeletonLoader).exists()).toBe(true);
   });
 
   describe('default (with nodes)', () => {
@@ -108,16 +68,8 @@ describe('workspaces/agent/components/agent_workspaces_list.vue', () => {
       await waitForPromises();
     });
 
-    it('renders table', () => {
-      expect(findTable().exists()).toBe(true);
-    });
-
-    it('renders pagination', () => {
-      expect(findPagination().exists()).toBe(true);
-    });
-
-    it('provides workspaces data to the workspaces table', () => {
-      expect(findTable(wrapper).props('workspaces')).toEqual(
+    it('sets the workspaces prop of the workspaces list', () => {
+      expect(findWorkspacesList().props('workspaces')).toEqual(
         populateWorkspacesWithProjectDetails(
           AGENT_WORKSPACES_LIST_QUERY_RESULT.data.project.clusterAgent.workspaces.nodes,
           GET_PROJECTS_DETAILS_QUERY_RESULT.data.projects.nodes,
@@ -129,11 +81,7 @@ describe('workspaces/agent/components/agent_workspaces_list.vue', () => {
       expect(logError).not.toHaveBeenCalled();
     });
 
-    it('does not show alert', () => {
-      expect(findAlert(wrapper).exists()).toBe(false);
-    });
-
-    describe('when pagination component emits input event', () => {
+    describe('when workspaces list emits page', () => {
       it('refetches workspaces starting at the specified cursor', async () => {
         const pageVariables = {
           after: 'end',
@@ -148,7 +96,7 @@ describe('workspaces/agent/components/agent_workspaces_list.vue', () => {
 
         expect(agentWorkspacesListQueryHandler).toHaveBeenCalledTimes(1);
 
-        findPagination().vm.$emit('input', pageVariables);
+        findWorkspacesList().vm.$emit('page', pageVariables);
 
         await waitForPromises();
 
@@ -158,31 +106,31 @@ describe('workspaces/agent/components/agent_workspaces_list.vue', () => {
     });
   });
 
-  describe('when workspace table emits updateFailed event', () => {
+  describe('when workspaces list emits error event', () => {
     const error = 'Failed to stop workspace';
 
     beforeEach(async () => {
       createWrapper();
       await waitForPromises();
 
-      findTable().vm.$emit('updateFailed', { error });
+      findWorkspacesList().vm.$emit('error', error);
     });
 
-    it('displays the error attached to the event', async () => {
+    it('sets the error for the workspaces list', async () => {
       await nextTick();
 
-      expect(findAlert().text()).toBe(error);
+      expect(findWorkspacesList().props('error')).toBe(error);
     });
 
-    describe('when workspace table emits updateSucceed event', () => {
+    describe('when workspaces list emits an empty error event', () => {
       it('dismisses the previous update error', async () => {
-        expect(findAlert().text()).toBe(error);
+        expect(findWorkspacesList().props('error')).toBe(error);
 
-        findTable().vm.$emit('updateSucceed');
+        findWorkspacesList().vm.$emit('error', '');
 
         await nextTick();
 
-        expect(findAlert().exists()).toBe(false);
+        expect(findWorkspacesList().props('error')).toBe('');
       });
     });
   });
@@ -204,38 +152,14 @@ describe('workspaces/agent/components/agent_workspaces_list.vue', () => {
       await waitForPromises();
     });
 
-    it('does not render table', () => {
-      expect(findTable().exists()).toBe(false);
-    });
-
-    it('logs error', () => {
-      expect(logError).toHaveBeenCalledWith(ERROR);
-    });
-
-    it('shows alert', () => {
-      expect(findAlert().text()).toBe(
+    it('sets the error prop of the workspaces list', () => {
+      expect(findWorkspacesList().props('error')).toBe(
         'Unable to load current workspaces. Please try again or contact an administrator.',
       );
     });
 
-    it('hides error when alert is dismissed', async () => {
-      findAlert().vm.$emit('dismiss');
-
-      await nextTick();
-
-      expect(findAlert().exists()).toBe(false);
-    });
-  });
-
-  describe('fixed elements', () => {
-    beforeEach(async () => {
-      createWrapper();
-
-      await waitForPromises();
-    });
-
-    it('displays a link that navigates to the workspaces help page', () => {
-      expect(findHelpLink().attributes().href).toContain('user/workspace/index.md');
+    it('logs error', () => {
+      expect(logError).toHaveBeenCalledWith(ERROR);
     });
   });
 });
