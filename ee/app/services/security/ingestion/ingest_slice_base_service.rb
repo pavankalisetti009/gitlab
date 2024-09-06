a042655f25f54f2dc22e8f235582945a0fbc225a
Gregory Havenga <11164960-ghavenga@users.noreply.gitlab.com>
2024-09-06 14:13:23 +0000

    Resolve "Set `vulnerability_findings_remediations` schema to`gitlab_sec`"

--- ee/app/services/security/ingestion/ingest_slice_base_service.rb
+++ ee/app/services/security/ingestion/ingest_slice_base_service.rb
@@ -16,16 +16,20 @@ module Security
         Gitlab::Database::QueryAnalyzers::PreventCrossDatabaseModification.temporary_ignore_tables_in_transaction(
           %w[
             vulnerabilities
+            vulnerability_reads
+            vulnerability_statistics
             vulnerability_flags
             vulnerability_finding_evidences
             vulnerability_finding_links
             vulnerability_finding_signatures
+            vulnerability_findings_remediations
             vulnerability_identifiers
             vulnerability_occurrences
             vulnerability_occurrence_identifiers
             vulnerability_occurrence_pipelines
             vulnerability_scanners
             security_findings
+            project_statistics
           ], url: 'https://gitlab.com/gitlab-org/gitlab/-/issues/474635'
         ) do
           ApplicationRecord.transaction do
