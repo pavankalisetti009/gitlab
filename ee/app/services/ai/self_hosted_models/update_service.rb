b0d37115e4e3db1d8aa6b5bc0e47fc9cab53c4b5
Eduardo Bonet <ebonet@gitlab.com>
2024-09-10 20:36:50 +0000

    Adds audit events for updating self-hosted models
    Refactors update of SelfHostedModels to a service
    Audits update to self-hosted model configurations
    
    Changelog: added

---
+++ ee/app/services/ai/self_hosted_models/update_service.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Ai
+  module SelfHostedModels
+    class UpdateService
+      def initialize(self_hosted_model, user, update_params)
+        @self_hosted_model = self_hosted_model
+        @user = user
+        @params = update_params
+      end
+
+      def execute
+        if self_hosted_model.update(params)
+          record_audit_event
+
+          ServiceResponse.success(payload: self_hosted_model)
+        else
+          ServiceResponse.error(message: self_hosted_model.errors.full_messages.join(", "))
+        end
+      end
+
+      private
+
+      attr_accessor :self_hosted_model, :user, :params
+
+      def record_audit_event
+        model = self_hosted_model
+        audit_context = {
+          name: 'self_hosted_model_updated',
+          author: user,
+          scope: user,
+          target: model,
+          message: "Self-hosted model #{model.name}/#{model.model}/#{model.endpoint} updated"
+        }
+
+        ::Gitlab::Audit::Auditor.audit(audit_context)
+      end
+    end
+  end
+end
