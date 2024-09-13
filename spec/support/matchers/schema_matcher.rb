5de05d977caab522d5f2d5eae60e1bacd141c192
Ash McKenzie <amckenzie@gitlab.com>
2024-09-13 11:35:22 +1000

    Assign schema where assigned as options
    In https://github.com/rspec/rspec-support/pull/591
    changes were made to the kwargs parsing which
    introduced a regression which in this case results
    in `schema` being `nil`.
    
    https://github.com/rspec/rspec-expectations/issues/1451
    raised this regression.

--- spec/support/matchers/schema_matcher.rb
+++ spec/support/matchers/schema_matcher.rb
@@ -81,6 +81,9 @@ end
 
 RSpec::Matchers.define :match_schema do |schema, dir: nil, **options|
   match do |data|
+    # NOTE: https://github.com/rspec/rspec-support/pull/591 broke kwarg parsing
+    schema ||= options
+
     schema = SchemaPath.expand(schema, dir)
     schema = Pathname.new(schema) if schema.is_a?(String)
     validator = SchemaPath.validator(schema)
