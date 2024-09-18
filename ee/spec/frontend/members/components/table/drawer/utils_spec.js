23930e78d4076a0972cd5e4777cb5d7793010844
Hinam Mehra <hmehra@gitlab.com>
2024-09-18 17:05:30 +1000

    Send null memberRoleId instead of undefined
    - When a user or group's role is being updated
    to a default role, send memberRoleId as null
    instead of not sending it in the request
    
    Changelog: fixed

--- ee/spec/frontend/members/components/table/drawer/utils_spec.js
+++ ee/spec/frontend/members/components/table/drawer/utils_spec.js
@@ -1,9 +1,13 @@
+import MockAdapter from 'axios-mock-adapter';
 import { cloneDeep } from 'lodash';
 import {
   getMemberRole,
   getRoleDropdownItems,
   ldapRole,
 } from 'ee/members/components/table/drawer/utils';
+import { callRoleUpdateApi } from '~/members/components/table/drawer/utils';
+import axios from '~/lib/utils/axios_utils';
+import { HTTP_STATUS_OK } from '~/lib/utils/http_status';
 import { roleDropdownItems } from 'ee/members/utils';
 import { upgradedMember, ldapMember } from '../../../mock_data';
 
@@ -48,5 +52,30 @@ describe('Role details drawer utils', () => {
 
       expect(role.text).toBe('custom role 1');
     });
+  });
+
+  describe('callRoleUpdateApi', () => {
+    it.each`
+      namespace  | propertyName
+      ${'user'}  | ${'access_level'}
+      ${'group'} | ${'group_access'}
+    `(
+      'calls update API with expected data for $namespace namespace',
+      async ({ namespace, propertyName }) => {
+        const memberPath = 'member/path/123';
+        const member = { ...upgradedMember, memberPath, namespace };
+
+        const mockAxios = new MockAdapter(axios);
+        mockAxios.onPut(memberPath).replyOnce(HTTP_STATUS_OK);
+
+        const customRole = roleDropdownItems(upgradedMember).flatten[6];
+        await callRoleUpdateApi(member, customRole);
+
+        expect(mockAxios.history.put).toHaveLength(1);
+        expect(mockAxios.history.put[0].data).toBe(
+          JSON.stringify({ [propertyName]: 10, member_role_id: customRole.memberRoleId }),
+        );
+      },
+    );
   });
 });
