7b12f82bc8c204a8ff9330f07326c3415301d9e1
nrosandich <nrosandich@gitlab.com>
2024-09-19 10:59:23 +1200

    Update pipeline migration text and link
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/compliance_dashboard/components/frameworks_report/edit_framework/constants.js
+++ ee/app/assets/javascripts/compliance_dashboard/components/frameworks_report/edit_framework/constants.js
@@ -86,10 +86,10 @@ export const i18n = {
   deprecationWarning: {
     title: s__('ComplianceReport|Compliance pipelines are deprecated'),
     message: s__(
-      'ComplianceReport|Avoid creating new compliance pipelines and use pipeline execution policy actions instead. %{linkStart}Pipeline execution policy%{linkEnd} actions provide the ability to enforce CI/CD jobs, execute security scans, and better manage compliance.',
+      'ComplianceReport|Avoid creating new compliance pipelines and use pipeline execution policies instead. %{linkStart}Pipeline execution policies%{linkEnd} provide the ability to enforce CI/CD jobs, execute security scans, and better manage compliance enforcement in pipelines.',
     ),
     details: s__(
-      'ComplianceReport|For more information, see %{linkStart}how to migrate from compliance pipelines to pipeline execution policy actions%{linkEnd}.',
+      'ComplianceReport|For more information, see %{linkStart}how to migrate from compliance pipelines to pipeline execution policies%{linkEnd}.',
     ),
     dismiss: s__('ComplianceReport|Dismiss'),
     migratePipelineToPolicy: s__('ComplianceReport|Migrate pipeline to a policy'),
