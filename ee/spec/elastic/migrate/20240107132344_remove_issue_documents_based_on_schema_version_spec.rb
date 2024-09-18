32177974b4be1d4a0f50b5272fa01f75d0fca92a
Terri Chu <tchu@gitlab.com>
2024-09-18 14:20:30 +0000

    Mark 20240107132344 as obsolete
    This migration marks the 20240107132344 RemoveIssueDocumentsBasedOnSchemaVersion Advanced search migration as obsolete.
    
    [Search for references to `remove_issue_documents_based_on_schema_version` in code](https://gitlab.com/search?project_id=278964&scope=blobs&search=remove_issue_documents_based_on_schema_version&regex=false))
    
    At the moment, the `gitlab-housekeeper` is not always capable of removing all references so
    you must check the diff and pipeline failures to confirm if there are any issues.
    It is the responsibility of the assignee (picked from ~"group::global search") to push those changes to this branch.
    
    [Read more](https://docs.gitlab.com/ee/development/search/advanced_search_migration_styleguide.html#cleaning-up-advanced-search-migrations)
    about the process for marking Advanced search migrations as obsolete.
    
    All Advanced search migrations must have had at least one
    [required stop](https : // docs.gitlab.com / ee / development / database / required_stops.html)
    to process the migration. Therefore we mark any Advanced search migrations added before the
    last required stop as obsolete.
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    using the Keeps::MarkOldAdvancedSearchMigrationsAsObsolete keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please create an issue with the
    label ~"GitLab Housekeeper" and consider pinging the author of this keep.
    
    Changelog: other
    EE: true

--- ee/spec/elastic/migrate/20240107132344_remove_issue_documents_based_on_schema_version_spec.rb
+++ ee/spec/elastic/migrate/20240107132344_remove_issue_documents_based_on_schema_version_spec.rb
@@ -3,12 +3,6 @@
 require 'spec_helper'
 require File.expand_path('ee/elastic/migrate/20240107132344_remove_issue_documents_based_on_schema_version.rb')
 
-RSpec.describe RemoveIssueDocumentsBasedOnSchemaVersion, :elastic, :sidekiq_inline, feature_category: :global_search,
-  quarantine: 'https://gitlab.com/gitlab-org/gitlab/-/issues/446204' do
-  include_examples 'migration deletes documents based on schema version' do
-    let(:version) { 20240107132344 }
-    let(:objects) { create_list(:issue, 3) }
-    let(:expected_throttle_delay) { 1.minute }
-    let(:expected_batch_size) { 10000 }
-  end
+RSpec.describe RemoveIssueDocumentsBasedOnSchemaVersion, feature_category: :global_search do
+  it_behaves_like 'a deprecated Advanced Search migration', 20240107132344
 end
