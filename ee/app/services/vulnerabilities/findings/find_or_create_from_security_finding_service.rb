9b782ac7b949aeedb73f2e32830de4681536f547
Gregory <11164960-ghavenga@users.noreply.gitlab.com>
2024-09-18 19:21:30 +0200

    Remove unneeded cross join allowance for sec decomposition
    Changelog: removed
    EE: true

--- ee/app/services/vulnerabilities/findings/find_or_create_from_security_finding_service.rb
+++ ee/app/services/vulnerabilities/findings/find_or_create_from_security_finding_service.rb
@@ -30,25 +30,10 @@ module Vulnerabilities
 
         vulnerability_finding = build_vulnerability_finding(security_finding)
 
-        ::Gitlab::Database::QueryAnalyzers::PreventCrossDatabaseModification.temporary_ignore_tables_in_transaction(
-          %w[
-            vulnerabilities
-            vulnerability_finding_evidences
-            vulnerability_finding_links
-            vulnerability_finding_signatures
-            vulnerability_flags
-            vulnerability_identifiers
-            vulnerability_occurrences
-            vulnerability_occurrence_identifiers
-            vulnerability_occurrence_pipelines
-          ],
-          url: 'https://gitlab.com/gitlab-org/gitlab/-/issues/474632'
-        ) do
-          Vulnerabilities::Finding.transaction do
-            save_identifiers(vulnerability_finding.identifiers)
+        Vulnerabilities::Finding.transaction do
+          save_identifiers(vulnerability_finding.identifiers)
 
-            raise ActiveRecord::Rollback unless vulnerability_finding.save
-          end
+          raise ActiveRecord::Rollback unless vulnerability_finding.save
         end
 
         vulnerability_finding
