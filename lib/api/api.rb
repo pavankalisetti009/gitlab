9e2df64ca337ac3883e038a4ffabb54aba06c422
fdegier <fdegier@gitlab.com>
2024-09-19 16:01:39 +0200

    Model Registry ability to load model
    Changelog: added

--- lib/api/api.rb
+++ lib/api/api.rb
@@ -398,6 +398,7 @@ module API
       mount ::API::UsageDataNonSqlMetrics
       mount ::API::VsCode::Settings::VsCodeSettingsSync
       mount ::API::Ml::Mlflow::Entrypoint
+      mount ::API::Ml::MlflowArtifacts::Entrypoint
     end
 
     mount ::API::Internal::AutoFlow
