5c8428ccc1875e452b101cefd651c34465c3d1db
Jay McCure <jmccure@gitlab.com>
2024-09-20 00:13:26 +0000

    Removes Airborne from E2E tests

--- qa/qa/specs/features/ee/api/12_systems/geo/geo_nodes_spec.rb
+++ qa/qa/specs/features/ee/api/12_systems/geo/geo_nodes_spec.rb
@@ -1,44 +1,60 @@
 # frozen_string_literal: true
 
-require 'airborne'
-
 module QA
   RSpec.describe 'Systems', :orchestrated, :geo, product_group: :geo do
     describe 'Geo Nodes API' do
-      before(:all) do
-        get_personal_access_token
+      include Support::API
+
+      # go to the primary and create a personal_access_token, which will be used
+      # for accessing both the primary and secondary
+      let(:personal_access_token) do
+        Runtime::API::Client.as_admin.personal_access_token
       end
+
+      let(:api_client) { Runtime::API::Client.new(node_type, personal_access_token: personal_access_token) }
+
+      let(:nodes) do
+        response = Support::API.get(api_endpoint('/geo_nodes'))
+        parse_body(response)
+      end
+
+      let(:primary_node) { nodes.detect { |node| node[:primary] == true } }
+      let(:secondary_node) { nodes.detect { |node| node[:primary] == false } }
 
       shared_examples 'retrieving configuration about Geo nodes' do |testcase|
         it 'GET /geo_nodes', testcase: testcase do
-          get api_endpoint('/geo_nodes')
+          response = Support::API.get(api_endpoint('/geo_nodes'))
+          response_body = parse_body(response)
 
-          expect_status(200)
-          expect(json_body.size).to be >= 2
-          expect_json('?', primary: true)
-          expect_json_types(
-            '*', primary: :boolean, current: :boolean,
-            files_max_capacity: :integer, repos_max_capacity: :integer,
-            clone_protocol: :string, _links: :object
-          )
+          expect(response.code).to eq(QA::Support::API::HTTP_STATUS_OK)
+          expect(response_body.size).to be >= 2
+
+          expect(response_body).to include(include(primary: true))
+
+          response_body.each do |node|
+            expect(!!node[:primary]).to eq node[:primary]
+            expect(!!node[:current]).to eq node[:primary]
+            expect(node[:files_max_capacity]).to be_a(Integer)
+            expect(node[:repos_max_capacity]).to be_a(Integer)
+            expect(node[:clone_protocol]).to be_a(String)
+            expect(node[:_links]).to be_a(Object)
+          end
         end
       end
 
       shared_examples 'retrieving configuration about Geo nodes/:id' do |testcase|
         it 'GET /geo_nodes/:id', testcase: testcase do
-          get api_endpoint("/geo_nodes/#{geo_node[:id]}")
+          response = Support::API.get(api_endpoint("/geo_nodes/#{geo_node[:id]}"))
+          response_body = parse_body(response)
 
-          expect_status(200)
-          expect(json_body).to eq geo_node
+          expect(response.code).to eq(QA::Support::API::HTTP_STATUS_OK)
+          expect(response_body).to eq geo_node
         end
       end
 
       describe 'on primary node', :geo do
-        before(:context) do
-          fetch_nodes(:geo_primary)
-        end
-
-        let(:geo_node) { @primary_node }
+        let(:node_type) { :geo_primary }
+        let(:geo_node) { primary_node }
 
         include_examples 'retrieving configuration about Geo nodes', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348042'
         include_examples 'retrieving configuration about Geo nodes/:id', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348041'
@@ -46,54 +62,38 @@ module QA
         describe 'editing a Geo node' do
           it 'PUT /geo_nodes/:id for secondary node',
             testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348043' do
-            endpoint = api_endpoint("/geo_nodes/#{@secondary_node[:id]}")
+            endpoint = api_endpoint("/geo_nodes/#{secondary_node[:id]}")
             new_attributes = { enabled: false, files_max_capacity: 1000, repos_max_capacity: 2000 }
 
-            put endpoint, new_attributes
+            response = Support::API.put(endpoint, new_attributes)
+            response_body = parse_body(response)
 
-            expect_status(200)
-            expect_json(new_attributes)
+            expect(response.code).to eq(QA::Support::API::HTTP_STATUS_OK)
+
+            expect(response_body[:enabled]).to eq(false)
+            expect(response_body[:files_max_capacity]).to eq(1000)
+            expect(response_body[:repos_max_capacity]).to eq(2000)
 
             # restore the original values
-            put endpoint, { enabled: @secondary_node[:enabled],
-                            files_max_capacity: @secondary_node[:files_max_capacity],
-                            repos_max_capacity: @secondary_node[:repos_max_capacity] }
+            Support::API.put(endpoint, { enabled: secondary_node[:enabled],
+                                         files_max_capacity: secondary_node[:files_max_capacity],
+                                         repos_max_capacity: secondary_node[:repos_max_capacity] })
 
-            expect_status(200)
+            expect(response.code).to eq(QA::Support::API::HTTP_STATUS_OK)
           end
         end
       end
 
       describe 'on secondary node', :geo do
-        before(:context) do
-          fetch_nodes(:geo_secondary)
-        end
-
-        let(:geo_node) { @nodes.first }
+        let(:node_type) { :geo_secondary }
+        let(:geo_node) { nodes.first }
 
         include_examples 'retrieving configuration about Geo nodes', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348044'
         include_examples 'retrieving configuration about Geo nodes/:id', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348045'
       end
 
       def api_endpoint(endpoint)
-        QA::Runtime::API::Request.new(@api_client, endpoint).url
-      end
-
-      def fetch_nodes(node_type)
-        @api_client = Runtime::API::Client.new(node_type, personal_access_token: @personal_access_token)
-
-        get api_endpoint('/geo_nodes')
-
-        @nodes          = json_body
-        @primary_node   = @nodes.detect { |node| node[:primary] == true }
-        @secondary_node = @nodes.detect { |node| node[:primary] == false }
-      end
-
-      # go to the primary and create a personal_access_token, which will be used
-      # for accessing both the primary and secondary
-      def get_personal_access_token
-        api_client = Runtime::API::Client.as_admin
-        @personal_access_token = api_client.personal_access_token
+        QA::Runtime::API::Request.new(api_client, endpoint).url
       end
     end
   end
