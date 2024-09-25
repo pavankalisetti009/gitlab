fb1ae36c0f4fb5d29a9d6e5e4cf5be84e108139d
Marc Shaw <mshaw@gitlab.com>
2024-09-25 14:35:23 +0000

    Introduce a delete MR dependency api endpoint
    MR: gitlab.com/gitlab-org/gitlab/-/merge_requests/166339
    
    Changelog: added
    EE: true

--- ee/spec/requests/api/merge_request_dependencies_spec.rb
+++ ee/spec/requests/api/merge_request_dependencies_spec.rb
@@ -3,23 +3,32 @@
 require "spec_helper"
 
 RSpec.describe API::MergeRequestDependencies, 'MergeRequestDependencies', feature_category: :code_review_workflow do
-  let(:user) { create(:user) }
-  let!(:project) { create(:project, :repository, group: group) }
-  let_it_be(:group) { create :group }
-  let(:merge_request) { create(:merge_request, :unique_branches, source_project: project, author: user) }
-  let(:other_merge_request) { create(:merge_request, :unique_branches, source_project: project, author: user) }
-  let(:another_merge_request) { create(:merge_request, :unique_branches, source_project: project, author: user) }
+  let_it_be(:maintainer) { create(:user) }
+  let_it_be(:guest) { create(:user) }
+  let_it_be(:project) { create(:project, :repository) }
+  let_it_be(:other_project) { create(:project, :repository) }
 
-  before do
-    merge_request.blocks_as_blockee.create!(blocking_merge_request: other_merge_request)
-    merge_request.blocks_as_blockee.create!(blocking_merge_request: another_merge_request)
-    project.add_maintainer(user)
+  let_it_be(:merge_request) { create(:merge_request, :unique_branches, source_project: project, author: maintainer) }
+  let_it_be(:other_merge_request) do
+    create(:merge_request, :unique_branches, source_project: project, author: maintainer)
+  end
+
+  let_it_be(:another_merge_request) do
+    create(:merge_request, :unique_branches, source_project: project, author: maintainer)
+  end
+
+  let_it_be(:block_1) { merge_request.blocks_as_blockee.create!(blocking_merge_request: other_merge_request) }
+  let_it_be(:block_2) { merge_request.blocks_as_blockee.create!(blocking_merge_request: another_merge_request) }
+
+  before_all do
+    project.add_maintainer(maintainer)
+    project.add_guest(guest)
+    other_project.add_guest(guest)
   end
 
   describe 'GET /projects/:id/merge_requests/:merge_request_iid/blocks' do
     it 'returns 200 for a valid merge request' do
-      get api("/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks", user)
-      merge_request_dependency = merge_request.blocks_as_blockee.first
+      get api("/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks", maintainer)
 
       aggregate_failures('response') do
         expect(response).to have_gitlab_http_status(:ok)
@@ -29,66 +38,122 @@ RSpec.describe API::MergeRequestDependencies, 'MergeRequestDependencies', featur
       aggregate_failures('json_response') do
         expect(json_response).to be_an Array
         expect(json_response.size).to eq(merge_request.blocks_as_blockee.size)
-        expect(json_response.first['id']).to eq(merge_request_dependency.id)
+        expect(json_response.first['id']).to eq(block_1.id)
         expect(json_response.first.dig('blocking_merge_request', 'id'))
-          .to eq(merge_request_dependency.blocking_merge_request.id)
+          .to eq(block_1.blocking_merge_request.id)
         expect(json_response.first.dig('blocked_merge_request', 'id'))
-          .to eq(merge_request_dependency.blocked_merge_request.id)
+          .to eq(block_1.blocked_merge_request.id)
       end
     end
 
-    it 'returns a 404 when merge_request id is used instead of the iid' do
-      get api("/projects/#{project.id}/merge_requests/#{merge_request.id}/blocks", user)
-      expect(response).to have_gitlab_http_status(:not_found)
-    end
-
     it 'returns a 404 when merge_request_iid not found' do
-      get api("/projects/#{project.id}/merge_requests/0/blocks", user)
+      get api("/projects/#{project.id}/merge_requests/0/blocks", maintainer)
       expect(response).to have_gitlab_http_status(:not_found)
     end
 
     context 'when merge request author has only guest access' do
       it_behaves_like 'rejects user from accessing merge request info' do
+        let(:user) { guest }
         let(:url) { "/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks" }
       end
     end
   end
 
   describe 'GET /projects/:id/merge_requests/:merge_request_iid/blocks/:block_id' do
-    let(:blocked_mr) { merge_request.blocks_as_blockee.first }
-
     it 'returns a 200 for a valid merge request' do
-      get api("/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks/#{blocked_mr.id}", user)
+      get api("/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks/#{block_1.id}", maintainer)
 
       expect(response).to have_gitlab_http_status(:ok)
 
       aggregate_failures('json_response') do
-        expect(json_response['id']).to eq(blocked_mr.id)
+        expect(json_response['id']).to eq(block_1.id)
         expect(json_response.dig('blocking_merge_request', 'id'))
-          .to eq(blocked_mr.blocking_merge_request.id)
+          .to eq(block_1.blocking_merge_request.id)
         expect(json_response.dig('blocked_merge_request', 'id'))
-          .to eq(blocked_mr.blocked_merge_request.id)
+          .to eq(block_1.blocked_merge_request.id)
       end
     end
 
-    it 'returns a 404 when merge_request id is used instead of the iid' do
-      get api("/projects/#{project.id}/merge_requests/#{merge_request.id}/blocks/#{blocked_mr.id}", user)
-      expect(response).to have_gitlab_http_status(:not_found)
-    end
-
     it 'returns a 404 when merge_request block id is not found' do
-      get api("/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks/0", user)
+      get api("/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks/0", maintainer)
       expect(response).to have_gitlab_http_status(:not_found)
     end
 
     it 'returns a 404 when merge_request_iid is not found' do
-      get api("/projects/#{project.id}/merge_requests/#{non_existing_record_iid}/blocks/#{blocked_mr.id}", user)
+      get api("/projects/#{project.id}/merge_requests/#{non_existing_record_iid}/blocks/#{block_1.id}", maintainer)
       expect(response).to have_gitlab_http_status(:not_found)
     end
 
     context 'when merge request author has only guest access' do
       it_behaves_like 'rejects user from accessing merge request info' do
-        let(:url) { "/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks/#{blocked_mr.id}" }
+        let(:user) { guest }
+        let(:url) { "/projects/#{project.id}/merge_requests/#{merge_request.iid}/blocks/#{block_1.id}" }
+      end
+    end
+  end
+
+  describe 'DELETE /projects/:id/merge_requests/:merge_request_iid/blocks/:block_id' do
+    let(:merge_request_iid) { merge_request.iid }
+    let(:block_id) { block_1.id }
+    let(:user) { maintainer }
+
+    let(:request) do
+      delete api("/projects/#{project.id}/merge_requests/#{merge_request_iid}/blocks/#{block_id}", user)
+    end
+
+    it 'returns 204 for a valid merge request' do
+      request
+
+      aggregate_failures('response') do
+        expect(response).to have_gitlab_http_status(:no_content)
+      end
+    end
+
+    context 'when the block id is invalid' do
+      let(:block_id) { -1 }
+
+      it 'returns a 404 when block is not found' do
+        request
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
+
+    context 'when the merge request iid is invalid' do
+      let(:merge_request_iid) { -1 }
+
+      it 'returns a 404 when merge_request is not found' do
+        request
+
+        expect(response).to have_gitlab_http_status(:not_found)
+      end
+    end
+
+    context 'when user can not read the blocking merge request' do
+      let(:other_project_merge_request) do
+        create(:merge_request, :unique_branches, source_project: other_project, author: maintainer)
+      end
+
+      let(:block_3) { merge_request.blocks_as_blockee.create!(blocking_merge_request: other_project_merge_request) }
+      let(:block_id) { block_3.id }
+      let(:user) { guest }
+
+      it 'returns a 403 error' do
+        request
+
+        expect(response).to have_gitlab_http_status(:forbidden)
+        expect(json_response['message']).to eq('403 Forbidden')
+      end
+    end
+
+    context 'when user can not update the merge request' do
+      let(:user) { guest }
+
+      it 'returns a 403 error' do
+        request
+
+        expect(response).to have_gitlab_http_status(:forbidden)
+        expect(json_response['message']).to eq('403 Forbidden')
       end
     end
   end
