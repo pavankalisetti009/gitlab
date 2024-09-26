ca06f505d35a539de02763610764269bd122de8f
Gregory <11164960-ghavenga@users.noreply.gitlab.com>
2024-09-26 20:04:04 +0200

    Remove transitional cross DB warning suppresion
    Changelog: removed
    EE: true

--- ee/spec/models/vulnerabilities/finding_spec.rb
+++ ee/spec/models/vulnerabilities/finding_spec.rb
@@ -871,13 +871,9 @@ RSpec.describe Vulnerabilities::Finding, feature_category: :vulnerability_manage
       subject { finding.identifier_names }
 
       before do
-        Gitlab::Database::QueryAnalyzers::PreventCrossDatabaseModification.temporary_ignore_tables_in_transaction(
-          %w[vulnerability_occurrence_identifiers], url: 'https://gitlab.com/gitlab-org/gitlab/-/issues/480534'
-        ) do
-          finding.identifiers << create(:vulnerabilities_identifier, external_type: 'cwe', name: cwe_1)
-          finding.identifiers << create(:vulnerabilities_identifier, external_type: 'cwe', name: cwe_2)
-          finding.identifiers << create(:vulnerabilities_identifier, external_type: 'cwe', name: cwe_3)
-        end
+        finding.identifiers << create(:vulnerabilities_identifier, external_type: 'cwe', name: cwe_1)
+        finding.identifiers << create(:vulnerabilities_identifier, external_type: 'cwe', name: cwe_2)
+        finding.identifiers << create(:vulnerabilities_identifier, external_type: 'cwe', name: cwe_3)
       end
 
       it { is_expected.to eql(finding.identifiers.pluck(:name)) }
