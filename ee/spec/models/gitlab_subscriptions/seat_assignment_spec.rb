4dff8c5eea8efdcc227b85e7a4e3af8ffd92d855
Bishwa Hang Rai <bhrai@gitlab.com>
2024-09-27 16:44:11 +0000

    Add UserActivity class
    This is a new ee only service class to track
    members activity using new model SeatAssignment
    
    EE: true
    Changelog: added

--- ee/spec/models/gitlab_subscriptions/seat_assignment_spec.rb
+++ ee/spec/models/gitlab_subscriptions/seat_assignment_spec.rb
@@ -3,6 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe GitlabSubscriptions::SeatAssignment, feature_category: :seat_cost_management do
+  let_it_be(:namespace) { create(:group) }
+  let_it_be(:user) { create(:user) }
+  let_it_be(:extra_dummy_record) { create(:gitlab_subscription_seat_assignment) }
+
   subject { build(:gitlab_subscription_seat_assignment) }
 
   describe 'associations' do
@@ -12,5 +16,31 @@ RSpec.describe GitlabSubscriptions::SeatAssignment, feature_category: :seat_cost
 
   describe 'validations' do
     it { is_expected.to validate_uniqueness_of(:namespace_id).scoped_to(:user_id) }
+  end
+
+  describe 'scopes' do
+    describe '.by_namespace' do
+      it 'returns records filtered by namespace' do
+        result = create(:gitlab_subscription_seat_assignment, namespace: namespace)
+
+        expect(described_class.by_namespace(namespace)).to match_array(result)
+      end
+    end
+
+    describe '.by_user' do
+      it 'returns records filtered by namespace' do
+        result = create(:gitlab_subscription_seat_assignment, user: user)
+
+        expect(described_class.by_user(user)).to match_array(result)
+      end
+    end
+  end
+
+  describe '.find_by_namespace_and_user' do
+    it 'returns single record by namespace and user' do
+      result = create(:gitlab_subscription_seat_assignment, user: user, namespace: namespace)
+
+      expect(described_class.find_by_namespace_and_user(namespace, user)).to eq(result)
+    end
   end
 end
