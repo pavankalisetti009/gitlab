324fad1992e76a97b711a588f8f8b5f2c0470774
Subashis  Chakraborty <schakraborty@gitlab.com>
2024-10-01 21:31:30 +0000

    Revert "Merge branch '496332-add-group-instance-permission-ability' into 'master'"
    This reverts merge request !167583

--- ee/spec/controllers/groups/security/vulnerabilities_controller_spec.rb
+++ ee/spec/controllers/groups/security/vulnerabilities_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Groups::Security::VulnerabilitiesController, feature_category: :v
   end
 
   describe 'GET index' do
-    subject(:show_security_dashboard) { get :index, params: { group_id: group.to_param } }
+    subject { get :index, params: { group_id: group.to_param } }
 
     context 'when security dashboard feature is enabled' do
       before do
@@ -26,7 +26,7 @@ RSpec.describe Groups::Security::VulnerabilitiesController, feature_category: :v
         it { is_expected.to have_gitlab_http_status(:ok) }
 
         it_behaves_like 'tracks govern usage event', 'users_visiting_security_vulnerabilities' do
-          let(:request) { show_security_dashboard }
+          let(:request) { subject }
         end
       end
 
@@ -35,7 +35,7 @@ RSpec.describe Groups::Security::VulnerabilitiesController, feature_category: :v
         it { is_expected.to render_template(:unavailable) }
 
         it_behaves_like "doesn't track govern usage event", 'users_visiting_security_vulnerabilities' do
-          let(:request) { show_security_dashboard }
+          let(:request) { subject }
         end
       end
     end
@@ -45,37 +45,7 @@ RSpec.describe Groups::Security::VulnerabilitiesController, feature_category: :v
       it { is_expected.to render_template(:unavailable) }
 
       it_behaves_like "doesn't track govern usage event", 'users_visiting_security_vulnerabilities' do
-        let(:request) { show_security_dashboard }
-      end
-    end
-
-    context "when resolveVulnerabilityWithAi ability is allowed" do
-      before do
-        allow(Ability).to receive(:allowed?).and_call_original
-        allow(Ability).to receive(:allowed?).with(user, :resolve_vulnerability_with_ai, group).and_return(true)
-
-        show_security_dashboard
-      end
-
-      render_views
-
-      it 'sets the frontend ability to true when allowed' do
-        expect(response.body).to have_pushed_frontend_ability(resolveVulnerabilityWithAi: true)
-      end
-    end
-
-    context "when resolveVulnerabilityWithAi ability is not allowed" do
-      before do
-        allow(Ability).to receive(:allowed?).and_call_original
-        allow(Ability).to receive(:allowed?).with(user, :resolve_vulnerability_with_ai, group).and_return(false)
-
-        show_security_dashboard
-      end
-
-      render_views
-
-      it 'sets the frontend ability to false not allowed' do
-        expect(response.body).to have_pushed_frontend_ability(resolveVulnerabilityWithAi: false)
+        let(:request) { subject }
       end
     end
   end
