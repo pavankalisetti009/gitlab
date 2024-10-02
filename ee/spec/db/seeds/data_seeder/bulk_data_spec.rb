734ca0bf797251374a3d923de78e6b915a005208
Mario Celi <mcelicalderon@gitlab.com>
2024-10-02 13:03:37 -0500

    Data seeder excludes factories
    When generating test data with
    ee/db/seeds/data_seeder/bulk_data.rb
    there are some factories we should avoid
    as by running creating data with them will produce
    data that was meant only for testing purposes
    and will not be similar to production data
    as expected.

---
+++ ee/spec/db/seeds/data_seeder/bulk_data_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../../../../db/seeds/data_seeder/bulk_data'
+
+RSpec.describe DataSeeder, feature_category: :scalability do # rubocop:disable RSpec/FilePath -- This is the current structure for this file
+  it 'does not create records from the excluded factories', :aggregate_failures do
+    # Creating them all takes time and this spec only cares about excluding some factories from the process,
+    # so not calling original here
+    allow(FactoryBot).to receive(:create)
+
+    expect(TestEnv).to receive(:seed_db)
+    expect(TestEnv).to receive(:setup_factory_repo)
+    expect(TestEnv).to receive(:setup_forked_repo)
+
+    described_class::EXCLUDED_FACTORIES.map(&:to_sym).each do |factory_name|
+      expect(FactoryBot).not_to receive(:create).with(factory_name)
+    end
+
+    described_class.new(true).seed
+  end
+end
