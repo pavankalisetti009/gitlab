9806a670ec6dc56b2f9e3136e38cea060c4ce519
Radu Birsan <rbirsan@gitlab.com>
2024-10-08 19:36:31 +0000

    Added pre_receive_secret_detection_enabled to App Settings API
    This allows get and put requests to Application Settings API,
    added tests and doc changes too

--- ee/lib/ee/api/entities/application_setting.rb
+++ ee/lib/ee/api/entities/application_setting.rb
@@ -24,6 +24,7 @@ module EE
           expose :updating_name_disabled_for_users, if: ->(_instance, _opts) { ::License.feature_available?(:disable_name_update_for_users) }
           expose :maven_package_requests_forwarding, if: ->(_instance, _opts) { ::License.feature_available?(:package_forwarding) }
           expose :npm_package_requests_forwarding, if: ->(_instance, _opts) { ::License.feature_available?(:package_forwarding) }
+          expose :pre_receive_secret_detection_enabled, if: ->(_instance, _opts) { ::License.feature_available?(:pre_receive_secret_detection) }
           expose :pypi_package_requests_forwarding, if: ->(_instance, _opts) { ::License.feature_available?(:package_forwarding) }
           expose :group_owners_can_manage_default_branch_protection, if: ->(_instance, _opts) { ::License.feature_available?(:default_branch_protection_restriction_in_groups) }
           expose :maintenance_mode, if: ->(_instance, _opts) { RegistrationFeatures::MaintenanceMode.feature_available? }
