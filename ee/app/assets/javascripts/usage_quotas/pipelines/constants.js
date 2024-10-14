74373e356869c28a7c8bee557ddc6b46d2f0a55e
Marcel Amirault <mamirault@gitlab.com>
2024-10-14 15:46:10 +0000

    Split up compute minutes page
    Admin content should be in /administration and purchasing details
    purchasing content should be in /subscription. Finally, this copy edits
    the user docs for clarity and simplicity.

--- ee/app/assets/javascripts/usage_quotas/pipelines/constants.js
+++ ee/app/assets/javascripts/usage_quotas/pipelines/constants.js
@@ -64,9 +64,9 @@ export const LABEL_BUY_ADDITIONAL_MINUTES = s__('UsageQuota|Buy additional compu
 export const LABEL_CI_MINUTES_DISABLED = s__(
   'UsageQuota|No compute usage data because %{linkStart}Instance runners%{linkEnd} are disabled, or there are no projects in this group.',
 );
-export const ADDITIONAL_MINUTES_HELP_LINK = helpPagePath('ci/pipelines/compute_minutes', {
-  anchor: 'purchase-additional-compute-minutes',
-});
+export const ADDITIONAL_MINUTES_HELP_LINK = helpPagePath(
+  'subscriptions/gitlab_com/compute_minutes.md',
+);
 export const SHARED_RUNNERS_DOC_LINK = helpPagePath('ci/runners/index.md');
 export const CI_MINUTES_HELP_LINK = helpPagePath('ci/pipelines/compute_minutes');
 export const CI_MINUTES_HELP_LINK_LABEL = __('Instance runners help link');
