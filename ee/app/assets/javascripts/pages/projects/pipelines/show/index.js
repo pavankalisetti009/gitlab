d0603e31169340ec0e1d0d74ea23780074cff28e
Coung Ngo <cngo@gitlab.com>
2024-10-15 16:45:48 +0100

    Delete unused codequality report code
    Code is no longer mounted anywhere in the DOM so can be deleted

--- ee/app/assets/javascripts/pages/projects/pipelines/show/index.js
+++ ee/app/assets/javascripts/pages/projects/pipelines/show/index.js
@@ -1,7 +1,5 @@
 import initCCValidationRequiredAlert from 'ee/credit_card_validation_required_alert';
 import initPipelineDetails from '~/ci/pipeline_details/pipeline_details_bundle';
-import initCodequalityReport from './codequality_report';
 
 initPipelineDetails();
-initCodequalityReport();
 initCCValidationRequiredAlert();
