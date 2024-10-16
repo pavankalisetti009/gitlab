19205b66d636ac7073f74e21e538b9f854e875e7
Abdul Wadood <awadood@gitlab.com>
2024-10-16 06:29:59 +0000

    Fix admin_mode enforcement in AdjournedProjectDeletionWorker
    This change addresses an issue where project deletion fails when
    scheduled by an admin with admin mode enabled. The problem occurs
    because the `admin_mode` session value is not persisted to Sidekiq.
    
    To resolve this, we introduce an `admin_mode` flag for
    `ProjectDestroyWorker`, creating a temporary admin session for
    project deletion. This approach ensures that other policy checks,
    such as blocking or user permissions, remain enforced while
    bypassing the admin_mode check. The admin_mode check should only be
    performed when the deletion is scheduled. We have previously
    implemented a similar solution for groups in a separate merge
    request https://gitlab.com/gitlab-org/gitlab/-/merge_requests/163071.
    
    Changelog: fixed
    EE: true

--- ee/spec/workers/group_destroy_worker_spec.rb
+++ ee/spec/workers/group_destroy_worker_spec.rb
@@ -13,15 +13,11 @@ RSpec.describe GroupDestroyWorker, feature_category: :groups_and_projects do
   subject(:worker) { described_class.new }
 
   context 'with protective settings', :request_store do
-    where(:admin_mode_enabled, :user_is_admin, :worker_run_in_admin_mode, :should_delete) do
-      true  | true   | true   | true
-      true  | true   | false  | false
-      true  | false  | true   | false
-      true  | false  | false  | false
-      false | true   | true   | true
-      false | true   | false  | true
-      false | false  | true   | false
-      false | false  | false  | false
+    where(:admin_mode_enabled, :user_is_admin, :should_delete) do
+      true  | true   | true
+      true  | false  | false
+      false | true   | true
+      false | false  | false
     end
 
     with_them do
@@ -33,13 +29,12 @@ RSpec.describe GroupDestroyWorker, feature_category: :groups_and_projects do
         worker_user = user_is_admin ? admin : user
 
         if should_delete
-          worker.perform(group.id, worker_user.id, admin_mode: worker_run_in_admin_mode)
+          worker.perform(group.id, worker_user.id)
           expect(Group.all).not_to include(group)
           expect(Project.all).not_to include(project)
         else
           expect do
-            worker.perform(group.id, worker_user.id,
-              admin_mode: worker_run_in_admin_mode)
+            worker.perform(group.id, worker_user.id)
           end.to raise_error(Groups::DestroyService::DestroyError)
         end
       end
