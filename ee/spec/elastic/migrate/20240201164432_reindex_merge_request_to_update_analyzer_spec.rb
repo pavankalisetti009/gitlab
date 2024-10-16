1bde603cf2077e1446240b79063595c83dd6b488
Terri Chu <tchu@gitlab.com>
2024-10-16 14:13:43 +0000

    Mark 20240201164432 as obsolete
    This migration marks the 20240201164432 ReindexMergeRequestToUpdateAnalyzer Advanced search migration as obsolete.
    
    [Search for references to `reindex_merge_request_to_update_analyzer` in code](https://gitlab.com/search?project_id=278964&scope=blobs&search=reindex_merge_request_to_update_analyzer&regex=false))
    
    At the moment, the `gitlab-housekeeper` is not always capable of removing all references so
    you must check the diff and pipeline failures to confirm if there are any issues.
    It is the responsibility of the assignee (picked from ~"group::global search") to push those changes to this branch.
    
    [Read more](https://docs.gitlab.com/ee/development/search/advanced_search_migration_styleguide.html#cleaning-up-advanced-search-migrations)
    about the process for marking Advanced search migrations as obsolete.
    
    All Advanced search migrations must have had at least one
    [required stop](https : // docs.gitlab.com / ee / development / database / required_stops.html)
    to process the migration. Therefore we mark any Advanced search migrations added before the
    last required stop as obsolete.
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    using the Keeps::MarkOldAdvancedSearchMigrationsAsObsolete keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please create an issue with the
    label ~"GitLab Housekeeper" and consider pinging the author of this keep.
    
    Changelog: other
    EE: true

--- ee/spec/elastic/migrate/20240201164432_reindex_merge_request_to_update_analyzer_spec.rb
+++ ee/spec/elastic/migrate/20240201164432_reindex_merge_request_to_update_analyzer_spec.rb
@@ -4,26 +4,5 @@ require 'spec_helper'
 require File.expand_path('ee/elastic/migrate/20240201164432_reindex_merge_request_to_update_analyzer.rb')
 
 RSpec.describe ReindexMergeRequestToUpdateAnalyzer, feature_category: :global_search do
-  let(:version) { 20240201164432 }
-  let(:migration) { described_class.new(version) }
-
-  it 'does not have migration options set', :aggregate_failures do
-    expect(migration).not_to be_batched
-    expect(migration).not_to be_retry_on_failure
-  end
-
-  describe '#migrate' do
-    it 'creates reindexing task with correct target and options' do
-      expect { migration.migrate }.to change { Elastic::ReindexingTask.count }.by(1)
-      task = Elastic::ReindexingTask.last
-      expect(task.targets).to eq(%w[MergeRequest])
-      expect(task.options).to eq({ 'skip_pending_migrations_check' => true })
-    end
-  end
-
-  describe '#completed?' do
-    it 'always returns true' do
-      expect(migration.completed?).to eq(true)
-    end
-  end
+  it_behaves_like 'a deprecated Advanced Search migration', 20240201164432
 end
