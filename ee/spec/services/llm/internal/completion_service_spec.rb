16e9ee8ff849166f61ce866a7f2e3cefea6feb18
Shinya Maeda <shinya@gitlab.com>
2024-10-17 11:47:58 +0900

    Fix broken duo chat spec after free access cutoff

--- ee/spec/services/llm/internal/completion_service_spec.rb
+++ ee/spec/services/llm/internal/completion_service_spec.rb
@@ -9,6 +9,18 @@ RSpec.describe Llm::Internal::CompletionService, :saas, feature_category: :ai_ab
   let_it_be(:group) { create(:group_with_plan, plan: :ultimate_plan) }
   let_it_be(:project) { create(:project, group: group) }
   let_it_be(:resource) { create(:issue, project: project) }
+  let_it_be(:add_on_purchase) do
+    create(:gitlab_subscription_add_on_purchase, :duo_enterprise, namespace: group)
+  end
+
+  let_it_be(:seat_assignment) do
+    create(
+      :gitlab_subscription_user_add_on_assignment,
+      user: user,
+      add_on_purchase: add_on_purchase
+    )
+  end
+
   let(:options) { { 'key' => 'value' } }
   let(:ai_action_name) { :summarize_comments }
   let(:referer_url) { nil }
