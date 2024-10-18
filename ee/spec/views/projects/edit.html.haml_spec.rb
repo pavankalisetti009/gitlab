754f60ae614e060f34c70d90361e88995ed4ccc7
Zack Cuddy <zcuddy@gitlab.com>
2024-10-18 17:03:37 +0000

    Groups Projects - Unify Restore UX
    This change aligns the experience of restoring
    a Group or Project to be the same for both contexts.
    
    This includes using a updated card view with similar text
    and predictable location in the settings.
    
    Changelog: changed
    EE: true

--- ee/spec/views/projects/edit.html.haml_spec.rb
+++ ee/spec/views/projects/edit.html.haml_spec.rb
@@ -68,4 +68,56 @@ RSpec.describe 'projects/edit' do
       it { is_expected.to have_text(_('Delete this project')) }
     end
   end
+
+  context 'when restoring a project', feature_category: :groups_and_projects do
+    shared_examples_for 'renders restore project settings' do
+      it 'renders restore project card and action' do
+        render
+
+        expect(rendered).to render_template('shared/groups_projects/settings/_restore')
+        expect(rendered).to have_link('Restore project')
+      end
+    end
+
+    shared_examples_for 'does not render restore project settings' do
+      it 'does not render restore project card and action' do
+        render
+
+        expect(rendered).to render_template('shared/groups_projects/settings/_restore')
+        expect(rendered).not_to have_link('Restore project')
+      end
+    end
+
+    context 'when adjourned_deletion_for_projects_and_groups is enabled' do
+      before do
+        stub_licensed_features(adjourned_deletion_for_projects_and_groups: true)
+      end
+
+      context 'when project is pending deletion' do
+        let_it_be(:project) { build_stubbed(:project, marked_for_deletion_at: Date.current) }
+
+        it_behaves_like 'renders restore project settings'
+      end
+
+      context 'when project is not pending deletion' do
+        it_behaves_like 'does not render restore project settings'
+      end
+    end
+
+    context 'when adjourned_deletion_for_projects_and_groups is disabled' do
+      before do
+        stub_licensed_features(adjourned_deletion_for_projects_and_groups: false)
+      end
+
+      context 'when project is pending deletion' do
+        let_it_be(:project) { build_stubbed(:project, marked_for_deletion_at: Date.current) }
+
+        it_behaves_like 'does not render restore project settings'
+      end
+
+      context 'when project is not pending deletion' do
+        it_behaves_like 'does not render restore project settings'
+      end
+    end
+  end
 end
