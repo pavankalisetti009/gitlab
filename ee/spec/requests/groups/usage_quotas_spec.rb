81756db747e5bf4ca489e6229ef5c4d6cf4f36ca
Divya Mahadevan <dmahadevan@gitlab.com>
2024-10-23 07:05:16 +0000

    Create FF enable_add_on_users_pagesize_selection

--- ee/spec/requests/groups/usage_quotas_spec.rb
+++ ee/spec/requests/groups/usage_quotas_spec.rb
@@ -52,6 +52,26 @@ RSpec.describe 'view usage quotas', feature_category: :consumables_cost_manageme
         expect(response.body).not_to have_pushed_frontend_feature_flags(enableAddOnUsersFiltering: true)
       end
     end
+
+    context 'with enable_add_on_users_pagesize_selection enabled' do
+      it 'exposes the feature flag' do
+        request
+
+        expect(response.body).to have_pushed_frontend_feature_flags(enableAddOnUsersPagesizeSelection: true)
+      end
+    end
+
+    context 'with enable_add_on_users_pagesize_selection disabled' do
+      before do
+        stub_feature_flags(enable_add_on_users_pagesize_selection: false)
+      end
+
+      it 'does not expose feature flag' do
+        request
+
+        expect(response.body).not_to have_pushed_frontend_feature_flags(enableAddOnUsersPagesizeSelection: true)
+      end
+    end
   end
 
   describe 'GET /groups/:group/-/usage_quotas/subscription_history' do
