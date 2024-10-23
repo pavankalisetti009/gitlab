4a41a527e03baaaf7d3d3f7e3d2a615d92ac75c7
Eugie Limpin <elimpin@gitlab.com>
2024-10-23 08:30:58 +0000

    Remove frontend code used in ci_require_credit_card_on_(trial|free)_plan

--- ee/app/assets/javascripts/pages/projects/pipelines/show/index.js
+++ ee/app/assets/javascripts/pages/projects/pipelines/show/index.js
@@ -1,5 +1,3 @@
-import initCCValidationRequiredAlert from 'ee/credit_card_validation_required_alert';
 import initPipelineDetails from '~/ci/pipeline_details/pipeline_details_bundle';
 
 initPipelineDetails();
-initCCValidationRequiredAlert();
