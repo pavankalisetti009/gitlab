a0857e297eb2ca3dd1e989b75fe2f28d20208f3c
Chen Charnolevsky <ccharnolevsky@gitlab.com>
2024-10-23 13:03:16 +0000

    Add details to 'mr changes' graphql query
    Fix pipeline error
    
    Move graphQL query to another MR
    
    Add details to graphQL query
    
    Add details to graphQL query

--- ee/spec/graphql/types/security/finding_reports_comparer/finding_type_spec.rb
+++ ee/spec/graphql/types/security/finding_reports_comparer/finding_type_spec.rb
@@ -2,8 +2,14 @@
 
 require 'spec_helper'
 
-RSpec.describe GitlabSchema.types['ComparedSecurityReportFinding'], feature_category: :vulnerability_management do
-  let(:expected_fields) { %i[uuid title description state severity found_by_pipeline_iid location identifiers scanner] }
+RSpec.describe GitlabSchema.types['ComparedSecurityReportFinding'],
+  feature_category: :vulnerability_management do
+  let(:expected_fields) do
+    %i[
+      uuid title description state severity found_by_pipeline_iid
+      location identifiers scanner details
+    ]
+  end
 
   it { expect(described_class).to have_graphql_fields(expected_fields) }
 end
