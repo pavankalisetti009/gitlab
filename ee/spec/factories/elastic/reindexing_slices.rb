ffdbdc55d5dc462c3e2dae18d12177bf30e106d1
Terri Chu <tchu@gitlab.com>
2024-10-24 21:40:33 -0400

    Fix rubocop offenses for Elastic::ReindexingSlice model

--- ee/spec/factories/elastic/reindexing_slices.rb
+++ ee/spec/factories/elastic/reindexing_slices.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :elastic_reindexing_slice, class: 'Elastic::ReindexingSlice' do
+  factory :elastic_reindexing_slice, class: 'Search::Elastic::ReindexingSlice' do
     association :elastic_reindexing_subtask
     sequence(:elastic_slice) { |n| n - 1 }
     sequence(:elastic_max_slice) { 5 }
