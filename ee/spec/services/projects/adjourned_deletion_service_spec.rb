445606773414c4a51bf7056dcd24705852372006
Abdul Wadood <awadood@gitlab.com>
2024-10-25 12:12:50 +0530

    Fix silenced rubocop offenses
    By moving the expect call from the method to the `it` block.

--- ee/spec/services/projects/adjourned_deletion_service_spec.rb
+++ ee/spec/services/projects/adjourned_deletion_service_spec.rb
@@ -5,12 +5,11 @@ require 'spec_helper'
 RSpec.describe Projects::AdjournedDeletionService, feature_category: :groups_and_projects do
   let(:project) { create(:project, marked_for_deletion_at: 10.days.ago, marked_for_deletion_by_user_id: user&.id) }
   let(:resource) { project }
+  let(:destroy_worker) { ProjectDestroyWorker }
+  let(:destroy_worker_params) { [project.id, user.id, {}] }
+  let(:perform_method) { :perform_async }
 
   subject(:service) { described_class.new(project: project, current_user: user) }
-
-  def ensure_destroy_worker_scheduled
-    expect(ProjectDestroyWorker).to receive(:perform_async).with(project.id, user.id, {})
-  end
 
   include_examples 'adjourned deletion service'
 
