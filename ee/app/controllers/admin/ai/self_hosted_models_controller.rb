182b6dd64bc0fbb513b0695a3b4bdd5f1dd441c4
Julie Huang <julhuang@gitlab.com>
2024-10-30 08:18:13 +0000

    Remove custom_models_vue_app feature flag
    - Remove custom_models_vue_app feature flag and its references. This was a development flag used to turn self-hosted models HAML pages into Vue. Now that the Vue work is complete, this flag is no longer necessary.
    - Remove self-hosted models HAML code that is no longer needed
    - Remove obsolete endpoints from self-hosted models controller
    - Update translation file
    
    Changelog: other
    EE: true

--- ee/app/controllers/admin/ai/self_hosted_models_controller.rb
+++ ee/app/controllers/admin/ai/self_hosted_models_controller.rb
@@ -11,53 +11,8 @@ module Admin
       before_action :ensure_registration!
       before_action :ensure_feature_enabled!
 
-      def index
-        @self_hosted_models = ::Ai::SelfHostedModel.all
-      end
-
-      def new
-        @self_hosted_model = ::Ai::SelfHostedModel.new
-      end
-
-      def create
-        service_result = ::Ai::SelfHostedModels::CreateService.new(current_user, self_hosted_models_params).execute
-
-        if service_result.success?
-          @self_hosted_model = service_result.payload
-          redirect_to admin_ai_self_hosted_models_url, notice: _("Self-Hosted Model was created")
-        else
-          render :new
-        end
-      end
-
       def edit
         @self_hosted_model = ::Ai::SelfHostedModel.find(params[:id])
-      end
-
-      def update
-        @self_hosted_model = ::Ai::SelfHostedModel.find(params[:id])
-
-        result = ::Ai::SelfHostedModels::UpdateService.new(
-          @self_hosted_model, current_user, update_self_hosted_model_params
-        ).execute
-
-        if result.success?
-          redirect_to admin_ai_self_hosted_models_url, notice: _("Self-Hosted Model was updated")
-        else
-          render :edit
-        end
-      end
-
-      def destroy
-        @self_hosted_model = ::Ai::SelfHostedModel.find(params[:id])
-
-        result = ::Ai::SelfHostedModels::DestroyService.new(@self_hosted_model, current_user).execute
-
-        if result.success?
-          redirect_to admin_ai_self_hosted_models_url, notice: _("Self-Hosted Model was deleted")
-        else
-          render :index
-        end
       end
 
       private
@@ -66,19 +21,6 @@ module Admin
         return if ::Ai::TestingTermsAcceptance.has_accepted?
 
         redirect_to admin_ai_terms_and_conditions_url
-      end
-
-      def update_self_hosted_model_params
-        update_params = self_hosted_models_params
-        api_token = update_params[:api_token]
-
-        return update_params.except(:api_token) if api_token == ApplicationSetting::MASK_PASSWORD
-
-        update_params
-      end
-
-      def self_hosted_models_params
-        params.require(:self_hosted_model).permit(:name, :model, :endpoint, :api_token)
       end
 
       def ensure_feature_enabled!
--- ee/spec/requests/admin/ai/self_hosted_models_controller_spec.rb
+++ ee/spec/requests/admin/ai/self_hosted_models_controller_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Admin::Ai::SelfHostedModelsController, :enable_admin_mode, featur
   before do
     sign_in(admin)
     stub_ee_application_setting(duo_features_enabled: duo_features_enabled)
-    stub_feature_flags(custom_models_vue_app: false) # disables vue app while running test suite
 
     allow(::Ai::TestingTermsAcceptance).to receive(:has_accepted?).and_return(true)
   end
@@ -53,47 +52,6 @@ RSpec.describe Admin::Ai::SelfHostedModelsController, :enable_admin_mode, featur
     end
   end
 
-  describe 'GET #index' do
-    subject :perform_request do
-      get admin_ai_self_hosted_models_path
-    end
-
-    context 'when no self_hosted_model has an api_token set' do
-      before do
-        create(:ai_self_hosted_model, name: 'test1', api_token: nil)
-        create(:ai_self_hosted_model, name: 'test2', api_token: nil)
-      end
-
-      it 'returns a list of AI powered features' do
-        perform_request
-
-        expect(response).to have_gitlab_http_status(:ok)
-        expect(response.body).to include('test1')
-        expect(response.body).to include('test2')
-        expect(response.body).not_to include('api-key-icon')
-      end
-    end
-
-    context 'when a self_hosted_model has an api_token set' do
-      before do
-        create(:ai_self_hosted_model, name: 'test1', api_token: nil)
-        create(:ai_self_hosted_model, name: 'test2', api_token: 'this_is_a_test')
-      end
-
-      it 'returns a list of AI powered features' do
-        perform_request
-
-        expect(response).to have_gitlab_http_status(:ok)
-        expect(response.body).to include('test1')
-        expect(response.body).to include('test2')
-        expect(response.body).to include('api-key-icon').once
-      end
-    end
-
-    it_behaves_like 'returns 404'
-    it_behaves_like 'must accept terms and conditions'
-  end
-
   describe 'GET #edit' do
     let(:page) { Nokogiri::HTML(response.body) }
     let(:self_hosted_model) do
@@ -104,161 +62,12 @@ RSpec.describe Admin::Ai::SelfHostedModelsController, :enable_admin_mode, featur
       get edit_admin_ai_self_hosted_model_path(self_hosted_model)
     end
 
-    it 'returns a form for existing deployment info' do
+    it 'returns the self-hosted model for edit' do
       perform_request
 
       expect(response).to have_gitlab_http_status(:ok)
 
-      expect(page.at('#self_hosted_model_name')['value']).to eq(self_hosted_model.name)
-      expect(page.at('#self_hosted_model_model option[@selected="selected"]')['value']).to eq('mistral')
-      expect(page.at('#self_hosted_model_endpoint')['value']).to eq(self_hosted_model.endpoint)
-    end
-
-    context 'when attribute api_token is not set' do
-      it 'does not show the placeholder' do
-        perform_request
-
-        expect(response).to have_gitlab_http_status(:ok)
-        expect(page.at('#self_hosted_model_api_token')['value']).to be_empty
-      end
-    end
-
-    context 'when attribute api_token is set' do
-      let(:self_hosted_model) do
-        create(:ai_self_hosted_model, api_token: 'this_is_a_test')
-      end
-
-      it 'does not show the placeholder' do
-        perform_request
-
-        expect(response).to have_gitlab_http_status(:ok)
-        expect(page.at('#self_hosted_model_api_token')['value']).to eq(ApplicationSetting::MASK_PASSWORD)
-      end
-    end
-
-    it_behaves_like 'returns 404'
-    it_behaves_like 'must accept terms and conditions'
-  end
-
-  describe 'POST #create' do
-    let(:params) do
-      {
-        self_hosted_model: {
-          name: 'test',
-          model: :mistral,
-          endpoint: 'https://example.com'
-        }
-      }
-    end
-
-    subject :perform_request do
-      post admin_ai_self_hosted_models_path, params: params
-    end
-
-    it 'updates feature settings' do
-      expect { perform_request }.to change { ::Ai::SelfHostedModel.count }.by(1)
-
-      self_hosted_model = ::Ai::SelfHostedModel.last
-      expect(self_hosted_model.name).to eq 'test'
-      expect(self_hosted_model.model).to eq 'mistral'
-      expect(self_hosted_model.endpoint).to eq 'https://example.com'
-
-      expect(response).to redirect_to(admin_ai_self_hosted_models_url)
-    end
-
-    it_behaves_like 'returns 404'
-    it_behaves_like 'must accept terms and conditions'
-  end
-
-  describe 'PATCH #update' do
-    let(:self_hosted_model) do
-      create(:ai_self_hosted_model, name: 'test', model: :mistral, api_token: 'did_not_change')
-    end
-
-    subject :perform_request do
-      patch admin_ai_self_hosted_model_path(self_hosted_model), params: params
-    end
-
-    context 'when self-hosted models feature is not enabled' do
-      let(:params) do
-        {
-          self_hosted_model: {
-            name: 'test_edited',
-            model: :mistral,
-            endpoint: 'https://example.com'
-          }
-        }
-      end
-
-      it_behaves_like 'returns 404'
-      it_behaves_like 'must accept terms and conditions'
-    end
-
-    context 'when the api_token attribute did not changed' do
-      let(:params) do
-        {
-          self_hosted_model: {
-            name: 'test_edited',
-            model: :mistral,
-            endpoint: 'https://example.com',
-            api_token: ApplicationSetting::MASK_PASSWORD
-          }
-        }
-      end
-
-      it 'updates feature settings' do
-        perform_request
-
-        self_hosted_model.reload
-
-        expect(self_hosted_model.name).to eq 'test_edited'
-        expect(self_hosted_model.model).to eq 'mistral'
-        expect(self_hosted_model.endpoint).to eq 'https://example.com'
-        expect(self_hosted_model.api_token).to eq 'did_not_change'
-
-        expect(response).to redirect_to(admin_ai_self_hosted_models_url)
-      end
-    end
-
-    context 'when the api_token attribute did change' do
-      let(:params) do
-        {
-          self_hosted_model: {
-            name: 'test_edited',
-            model: :mistral,
-            endpoint: 'https://example.com',
-            api_token: 'api_token_changed'
-          }
-        }
-      end
-
-      it 'updates feature settings' do
-        perform_request
-
-        self_hosted_model.reload
-
-        expect(self_hosted_model.name).to eq 'test_edited'
-        expect(self_hosted_model.model).to eq 'mistral'
-        expect(self_hosted_model.endpoint).to eq 'https://example.com'
-        expect(self_hosted_model.api_token).to eq 'api_token_changed'
-
-        expect(response).to redirect_to(admin_ai_self_hosted_models_url)
-      end
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    before do
-      create(:ai_self_hosted_model)
-    end
-
-    subject :perform_request do
-      delete admin_ai_self_hosted_model_path(::Ai::SelfHostedModel.last)
-    end
-
-    it 'deletes the self_hosted_model' do
-      expect { perform_request }.to change { ::Ai::SelfHostedModel.count }.by(-1)
-      expect(response).to redirect_to(admin_ai_self_hosted_models_url)
+      expect(assigns(:self_hosted_model)).to eq(self_hosted_model)
     end
 
     it_behaves_like 'returns 404'
