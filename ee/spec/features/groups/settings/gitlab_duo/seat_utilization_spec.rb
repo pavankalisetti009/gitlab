f100a80382fe71105a54a90ed6f6044983e57f97
Lindsey Shelton <lshelton@gitlab.com>
2024-10-31 03:57:12 +0000

    Initial checkin for Groups Duo Home Page
    Start of migration of components from CS
    
    Changelog: changed
    EE: true

--- ee/spec/features/groups/settings/gitlab_duo/seat_utilization_spec.rb
+++ ee/spec/features/groups/settings/gitlab_duo/seat_utilization_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe 'Groups > Settings > GitLab Duo', :js, :saas, feature_category: :seat_cost_management do
+RSpec.describe 'Groups > Settings > GitLab Duo > Seat Utilization', :js, :saas, feature_category: :seat_cost_management do
   it_behaves_like 'Gitlab Duo administration' do
     let(:duo_page) { group_settings_gitlab_duo_seat_utilization_index_path(group) }
   end
