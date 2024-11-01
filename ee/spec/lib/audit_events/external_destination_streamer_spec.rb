52db19c6cde0333d29ecd969ca71d419e1f320db
Andrew Jung <ajung@gitlab.com>
2024-11-01 15:02:26 -0400

    Use feature flag to new streamers
    Creates a new feature flag: audit_events_external_destination_streamer_consolidation_refactor
    The flag is default to false
    Uses both original and new refactored  behaviors when turned on
    
    Changelog: added
    EE: true

--- ee/spec/lib/audit_events/external_destination_streamer_spec.rb
+++ ee/spec/lib/audit_events/external_destination_streamer_spec.rb
@@ -3,120 +3,171 @@
 require 'spec_helper'
 
 RSpec.describe AuditEvents::ExternalDestinationStreamer, feature_category: :audit_events do
+  let_it_be(:event) { create(:audit_event, :group_event) }
+  let(:group) { event.entity }
+  let(:streamer) { described_class.new('audit_operation', event) }
+  let(:request_body) { { id: event.id, event_type: 'audit_operation' }.to_json }
+
   before do
     stub_licensed_features(external_audit_events: true)
+    stub_feature_flags(audit_events_external_destination_streamer_consolidation_refactor: true)
+    freeze_time
 
     allow_next_instance_of(::AuditEvents::GoogleCloud::Authentication) do |instance|
       allow(instance).to receive(:generate_access_token).and_return("sample-token")
     end
+
+    stub_request(:post, /.*/).to_return(status: 200, body: "", headers: {})
+    stub_request(:put, /.*/).to_return(status: 200, body: "", headers: {})
+
+    allow_any_instance_of(Aws::S3::Client) do |instance|
+      allow(instance).to receive(:put_object).and_return(nil)
+    end
+
+    allow_next_instance_of(AuditEvents::GoogleCloud::LoggingService::Logger) do |instance|
+      allow(instance).to receive(:log).and_return(nil)
+    end
   end
 
   describe '#stream_to_destinations' do
-    let_it_be(:event) { create(:audit_event, :group_event) }
-    let(:group) { event.entity }
+    subject(:stream_to_destinations) { streamer.stream_to_destinations }
 
-    subject { described_class.new('audit_operation', event).stream_to_destinations }
+    context 'when no external streaming destinations are present' do
+      it_behaves_like 'external destination streamer'
 
-    context 'when no external audit event destinations are present' do
-      it 'does not make any HTTP call' do
-        expect(Gitlab::HTTP).not_to receive(:post)
+      context 'with feature flag disabled' do
+        before do
+          stub_feature_flags(audit_events_external_destination_streamer_consolidation_refactor: false)
+        end
 
-        subject
+        it_behaves_like 'external destination streamer'
       end
     end
 
-    context 'when external destinations are present' do
+    context 'when external streaming destinations are present' do
       before do
-        create(:external_audit_event_destination, group: group)
-        create_list(:instance_external_audit_event_destination, 2)
-        create(:google_cloud_logging_configuration, group: group)
-        create(:instance_google_cloud_logging_configuration)
-        create(:amazon_s3_configuration, group: group)
+        create(:audit_events_group_external_streaming_destination, :http,
+          group: group)
+        create(:audit_events_instance_external_streaming_destination, :gcp)
+        create(:audit_events_instance_external_streaming_destination, :aws)
+      end
+
+      it 'streams to all external streaming destinations' do
+        expect_next_instance_of(AuditEvents::Streaming::Group::Streamer) do |instance|
+          expect(instance).to receive(:execute)
+        end
+        expect_next_instance_of(AuditEvents::Streaming::Instance::Streamer) do |instance|
+          expect(instance).to receive(:execute)
+        end
+
+        stream_to_destinations
       end
 
       it 'makes correct number of external calls' do
-        expect(Gitlab::HTTP).to receive(:post).exactly(5).times
-
-        expect_next_instance_of(Aws::S3::Client) do |instance|
-          expected_body = event.to_json.merge({ event_type: "audit_operation" })
-          expect(instance).to receive(:put_object).with(hash_including(body: expected_body))
+        destination_classes = AuditEvents::Streaming::BaseStreamer::STREAMER_DESTINATIONS.values
+        destination_classes.each do |dest_class|
+          expect_next_instance_of(dest_class) do |instance|
+            expect(instance).to receive(:stream).once
+          end
         end
 
-        subject
-      end
-    end
-  end
+        expect_next_instance_of(AuditEvents::Streaming::Group::Streamer) do |streamer|
+          expect(streamer).to receive(:execute).once.and_call_original
+          expect(streamer).to receive(:streamable?).once.and_return(true)
+        end
 
-  describe '#streamable?' do
-    let_it_be(:event) { create(:audit_event, :group_event) }
-    let_it_be(:group) { event.entity }
+        expect_next_instance_of(AuditEvents::Streaming::Instance::Streamer) do |streamer|
+          expect(streamer).to receive(:execute).once.and_call_original
+          expect(streamer).to receive(:streamable?).once.and_return(true)
+        end
 
-    subject { described_class.new('audit_operation', event).streamable? }
-
-    context 'when none of them is streamable' do
-      it { is_expected.to be_falsey }
-    end
-
-    context 'when all of them are streamable' do
-      before do
-        create(:external_audit_event_destination, group: group)
-        create(:instance_external_audit_event_destination)
-        create(:google_cloud_logging_configuration, group: group)
-        create(:instance_google_cloud_logging_configuration)
-        create(:amazon_s3_configuration, group: group)
-        create(:instance_amazon_s3_configuration)
+        stream_to_destinations
       end
 
-      it { is_expected.to be_truthy }
-    end
-
-    context 'when at least one of them is streamable' do
-      context 'when only group external destination is streamable' do
+      context 'with feature flag disabled' do
         before do
+          stub_feature_flags(audit_events_external_destination_streamer_consolidation_refactor: false)
+
           create(:external_audit_event_destination, group: group)
-        end
-
-        it { is_expected.to be_truthy }
-      end
-
-      context 'when only instance destination is streamable' do
-        before do
-          create(:instance_external_audit_event_destination)
-        end
-
-        it { is_expected.to be_truthy }
-      end
-
-      context 'when only google cloud logging destination is streamable' do
-        before do
-          create(:google_cloud_logging_configuration, group: group)
-        end
-
-        it { is_expected.to be_truthy }
-      end
-
-      context 'when only instance google cloud logging destination is streamable' do
-        before do
+          create_list(:instance_external_audit_event_destination, 2)
           create(:instance_google_cloud_logging_configuration)
-        end
-
-        it { is_expected.to be_truthy }
-      end
-
-      context 'when only amazon s3 destination is streamable' do
-        before do
           create(:amazon_s3_configuration, group: group)
         end
 
-        it { is_expected.to be_truthy }
+        it 'makes correct number of external calls', :aggregate_failures do
+          expect(Gitlab::HTTP).to receive(:post).exactly(3).times
+          expect_next_instance_of(Aws::S3::Client) do |instance|
+            expected_body = event.to_json.merge({ event_type: "audit_operation" })
+            expect(instance).to receive(:put_object).with(hash_including(body: expected_body))
+          end
+
+          stream_to_destinations
+        end
+      end
+    end
+
+    describe '#streamable?' do
+      subject { streamer.streamable? }
+
+      context 'when none of the external streaming destinations are present' do
+        it { is_expected.to be_falsey }
+
+        context 'with feature flag disabled' do
+          before do
+            stub_feature_flags(audit_events_external_destination_streamer_consolidation_refactor: false)
+          end
+
+          it { is_expected.to be_falsey }
+        end
       end
 
-      context 'when only instance amazon s3 destination is streamable' do
+      context 'when external streaming destinations are present' do
         before do
-          create(:instance_amazon_s3_configuration)
+          create(:audit_events_group_external_streaming_destination, :http,
+            group: group)
+          create(:audit_events_instance_external_streaming_destination, :gcp)
         end
 
         it { is_expected.to be_truthy }
+
+        context 'with feature flag disabled' do
+          before do
+            stub_feature_flags(audit_events_external_destination_streamer_consolidation_refactor: false)
+            create(:external_audit_event_destination, group: group)
+            create(:instance_external_audit_event_destination)
+            create(:google_cloud_logging_configuration, group: group)
+            create(:instance_google_cloud_logging_configuration)
+            create(:amazon_s3_configuration, group: group)
+            create(:instance_amazon_s3_configuration)
+          end
+
+          it { is_expected.to be_truthy }
+        end
+      end
+
+      context 'when only one destination type is present' do
+        using RSpec::Parameterized::TableSyntax
+
+        where(:factory, :trait, :is_group_destination) do
+          :audit_events_group_external_streaming_destination | :aws | true
+          :audit_events_group_external_streaming_destination | :gcp | true
+          :audit_events_group_external_streaming_destination | :http | true
+          :audit_events_instance_external_streaming_destination | :aws | false
+          :audit_events_instance_external_streaming_destination | :gcp | false
+          :audit_events_instance_external_streaming_destination | :http | false
+        end
+
+        with_them do
+          before do
+            if is_group_destination
+              create(factory, trait, group: group)
+            else
+              create(factory, trait)
+            end
+          end
+
+          it { is_expected.to be_truthy }
+        end
       end
     end
   end
