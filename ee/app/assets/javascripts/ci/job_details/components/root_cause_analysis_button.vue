dc7812ce4ccdb83281818589163c98e76099f79b
Payton Burdette <pburdette@gitlab.com>
2024-11-01 22:00:50 +0000

    Add RCA feature to failed jobs widget

--- ee/app/assets/javascripts/ci/job_details/components/root_cause_analysis_button.vue
+++ ee/app/assets/javascripts/ci/job_details/components/root_cause_analysis_button.vue
@@ -1,27 +1,48 @@
 <script>
 import { GlButton } from '@gitlab/ui';
-import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
-import glAbilitiesMixin from '~/vue_shared/mixins/gl_abilities_mixin';
 import { sendDuoChatCommand } from 'ee/ai/utils';
+import { TYPENAME_CI_BUILD } from '~/graphql_shared/constants';
+import { convertToGraphQLId } from '~/graphql_shared/utils';
 
 export default {
   components: {
     GlButton,
   },
-  mixins: [glAbilitiesMixin(), glFeatureFlagMixin()],
-  inject: ['jobGid'],
   props: {
-    jobFailed: {
+    canTroubleshootJob: {
       type: Boolean,
+      required: true,
+    },
+    jobStatusGroup: {
+      type: String,
+      required: true,
+    },
+    jobId: {
+      type: Number,
       required: false,
-      default: false,
+      default: null,
+    },
+    jobGid: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  computed: {
+    resourceId() {
+      return this.jobGid || convertToGraphQLId(TYPENAME_CI_BUILD, this.jobId);
+    },
+    jobFailed() {
+      const failedGroups = ['failed', 'failed-with-warnings'];
+
+      return failedGroups.includes(this.jobStatusGroup);
     },
   },
   methods: {
     callDuo() {
       sendDuoChatCommand({
         question: '/troubleshoot',
-        resourceId: this.jobGid,
+        resourceId: this.resourceId,
       });
     },
   },
@@ -29,9 +50,8 @@ export default {
 </script>
 <template>
   <gl-button
-    v-if="glAbilities.troubleshootJobWithAi && jobFailed"
+    v-if="jobFailed && canTroubleshootJob"
     icon="duo-chat"
-    class="gl-mr-3"
     variant="confirm"
     data-testid="rca-duo-button"
     @click="callDuo"
