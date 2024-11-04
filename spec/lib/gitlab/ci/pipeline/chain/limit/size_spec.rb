05fb2beae141aa726ea49bca77bedce3f404f33b
Chou Yu Ta <lawrencechou1024@gmail.com>
2024-11-04 12:55:51 +0000

    Move 'Maximum number of jobs in a single pipeline' from EE to CE
    Fix https://gitlab.com/gitlab-org/gitlab/-/issues/287669
    
    > Originally these features were designed for GitLab EE but then we added PlanLimits class that also supports managing application limits for default plans (projects in Core and self-hosted).
    
    This commit move `Chain::Limit::Size` and `Quota::Size` from ee to core
    
    Changelog: added
    EE: true

--- spec/lib/gitlab/ci/pipeline/chain/limit/size_spec.rb
+++ spec/lib/gitlab/ci/pipeline/chain/limit/size_spec.rb
@@ -2,71 +2,70 @@
 
 require 'spec_helper'
 
-RSpec.describe ::Gitlab::Ci::Pipeline::Chain::Limit::Size, :saas do
+RSpec.describe ::Gitlab::Ci::Pipeline::Chain::Limit::Size, feature_category: :pipeline_composition do
   let_it_be(:namespace) { create(:namespace) }
   let_it_be(:project, reload: true) { create(:project, :repository, namespace: namespace) }
+  let_it_be(:default_plan) { create(:default_plan) }
   let_it_be(:user) { create(:user) }
 
   let(:pipeline) { build(:ci_pipeline, project: project) }
 
   let(:command) do
-    double(:command,
+    instance_double(::Gitlab::Ci::Pipeline::Chain::Command,
       project: project,
       current_user: user,
-      pipeline_seed: double(:seed, size: 1))
+      pipeline_seed: instance_double(::Gitlab::Ci::Pipeline::Seed::Pipeline, size: 1))
   end
 
   let(:step) { described_class.new(pipeline, command) }
 
-  subject { step.perform! }
+  subject(:perform_step) { step.perform! }
 
   context 'when pipeline size limit is exceeded' do
     before do
-      ultimate_plan = create(:ultimate_plan)
-      create(:plan_limits, plan: ultimate_plan, ci_pipeline_size: 1)
-      create(:gitlab_subscription, namespace: namespace, hosted_plan: ultimate_plan)
+      create(:plan_limits, plan: default_plan, ci_pipeline_size: 1)
     end
 
     context 'when saving incomplete pipelines' do
       let(:command) do
-        double(:command,
+        instance_double(::Gitlab::Ci::Pipeline::Chain::Command,
           project: project,
           current_user: user,
           save_incompleted: true,
-          pipeline_seed: double(:seed, size: 2))
+          pipeline_seed: instance_double(::Gitlab::Ci::Pipeline::Seed::Pipeline, size: 2))
       end
 
       it 'drops the pipeline' do
-        subject
+        perform_step
 
         expect(pipeline.reload).to be_failed
       end
 
       it 'persists the pipeline' do
-        subject
+        perform_step
 
         expect(pipeline).to be_persisted
       end
 
       it 'breaks the chain' do
-        subject
+        perform_step
 
         expect(step.break?).to be true
       end
 
       it 'sets a valid failure reason' do
-        subject
+        perform_step
 
         expect(pipeline.size_limit_exceeded?).to be true
       end
 
       it 'appends validation error' do
-        subject
+        perform_step
 
         expect(pipeline.errors.to_a)
-          .to include "The number of jobs has exceeded the limit of 1. "\
-          "Try splitting the configuration with parent-child-pipelines "\
-          "https://docs.gitlab.com/ee/ci/troubleshooting.html#pipeline-with-many-jobs-fails-to-start"
+          .to include "The number of jobs has exceeded the limit of 1. " \
+            "Try splitting the configuration with parent-child-pipelines " \
+            "http://localhost/help/ci/debugging.md#pipeline-with-many-jobs-fails-to-start"
       end
 
       it 'logs the error' do
@@ -79,22 +78,22 @@ RSpec.describe ::Gitlab::Ci::Pipeline::Chain::Limit::Size, :saas do
           }
         )
 
-        subject
+        perform_step
       end
     end
 
     context 'when not saving incomplete pipelines' do
       let(:command) do
-        double(:command,
+        instance_double(::Gitlab::Ci::Pipeline::Chain::Command,
           project: project,
           current_user: user,
           save_incompleted: false,
-          pipeline_seed: double(:seed, size: 2),
+          pipeline_seed: instance_double(::Gitlab::Ci::Pipeline::Seed::Pipeline, size: 2),
           increment_pipeline_failure_reason_counter: true)
       end
 
       it 'fails but does not persist the pipeline' do
-        subject
+        perform_step
 
         expect(pipeline).not_to be_persisted
         expect(pipeline).to be_size_limit_exceeded
@@ -102,7 +101,7 @@ RSpec.describe ::Gitlab::Ci::Pipeline::Chain::Limit::Size, :saas do
       end
 
       it 'breaks the chain' do
-        subject
+        perform_step
 
         expect(step.break?).to be true
       end
@@ -110,26 +109,24 @@ RSpec.describe ::Gitlab::Ci::Pipeline::Chain::Limit::Size, :saas do
       it 'increments the error metric' do
         expect(command).to receive(:increment_pipeline_failure_reason_counter).with(:size_limit_exceeded)
 
-        subject
+        perform_step
       end
     end
   end
 
   context 'when pipeline size limit is not exceeded' do
     before do
-      ultimate_plan = create(:ultimate_plan)
-      create(:plan_limits, plan: ultimate_plan, ci_pipeline_size: 100)
-      create(:gitlab_subscription, namespace: namespace, hosted_plan: ultimate_plan)
+      create(:plan_limits, plan: default_plan, ci_pipeline_size: 100)
     end
 
     it 'does not break the chain' do
-      subject
+      perform_step
 
       expect(step.break?).to be false
     end
 
     it 'does not persist the pipeline' do
-      subject
+      perform_step
 
       expect(pipeline).not_to be_persisted
     end
@@ -137,23 +134,21 @@ RSpec.describe ::Gitlab::Ci::Pipeline::Chain::Limit::Size, :saas do
     it 'does not log any error' do
       expect(Gitlab::ErrorTracking).not_to receive(:track_exception)
 
-      subject
+      perform_step
     end
   end
 
   context 'when pipeline size limit is disabled' do
     before do
-      ultimate_plan = create(:ultimate_plan)
-      create(:plan_limits, plan: ultimate_plan, ci_pipeline_size: 0)
-      create(:gitlab_subscription, namespace: namespace, hosted_plan: ultimate_plan)
+      create(:plan_limits, plan: default_plan, ci_pipeline_size: 0)
     end
 
     context 'when global pipeline size limit is exceeded' do
       let(:command) do
-        double(:command,
+        instance_double(::Gitlab::Ci::Pipeline::Chain::Command,
           project: project,
           current_user: user,
-          pipeline_seed: double(:seed, size: 2001))
+          pipeline_seed: instance_double(::Gitlab::Ci::Pipeline::Seed::Pipeline, size: 2001))
       end
 
       it 'logs the pipeline' do
@@ -166,7 +161,7 @@ RSpec.describe ::Gitlab::Ci::Pipeline::Chain::Limit::Size, :saas do
           }
         )
 
-        subject
+        perform_step
       end
     end
   end
--- spec/lib/gitlab/ci/pipeline/quota/size_spec.rb
+++ spec/lib/gitlab/ci/pipeline/quota/size_spec.rb
@@ -2,29 +2,30 @@
 
 require 'spec_helper'
 
-RSpec.describe EE::Gitlab::Ci::Pipeline::Quota::Size, :saas do
+RSpec.describe Gitlab::Ci::Pipeline::Quota::Size, feature_category: :pipeline_composition do
   let_it_be(:namespace) { create(:namespace) }
-  let_it_be(:ultimate_plan, reload: true) { create(:ultimate_plan) }
   let_it_be(:project, reload: true) { create(:project, :repository, namespace: namespace) }
-  let_it_be(:plan_limits) { create(:plan_limits, plan: ultimate_plan) }
-
-  let!(:subscription) { create(:gitlab_subscription, namespace: namespace, hosted_plan: ultimate_plan) }
+  let_it_be(:plan_limits, reload: true) { create(:plan_limits, :default_plan) }
 
   let(:pipeline) { build_stubbed(:ci_pipeline, project: project) }
 
   let(:command) do
-    double(:command, pipeline_seed: double(:pipeline_seed, size: 2))
+    instance_double(::Gitlab::Ci::Pipeline::Chain::Command, pipeline_seed: pipeline_seed_double)
+  end
+
+  let(:pipeline_seed_double) do
+    instance_double(::Gitlab::Ci::Pipeline::Seed::Pipeline, size: 2)
   end
 
   subject { described_class.new(namespace, pipeline, command) }
 
-  shared_context 'pipeline size limit exceeded' do
+  shared_context 'when pipeline size limit exceeded' do
     before do
       plan_limits.update!(ci_pipeline_size: 1)
     end
   end
 
-  shared_context 'pipeline size limit not exceeded' do
+  shared_context 'when pipeline size limit not exceeded' do
     before do
       plan_limits.update!(ci_pipeline_size: 2)
     end
@@ -37,7 +38,7 @@ RSpec.describe EE::Gitlab::Ci::Pipeline::Quota::Size, :saas do
       end
 
       it 'is enabled' do
-        expect(subject).to be_enabled
+        is_expected.to be_enabled
       end
     end
 
@@ -47,7 +48,7 @@ RSpec.describe EE::Gitlab::Ci::Pipeline::Quota::Size, :saas do
       end
 
       it 'is not enabled' do
-        expect(subject).not_to be_enabled
+        is_expected.not_to be_enabled
       end
     end
 
@@ -57,38 +58,37 @@ RSpec.describe EE::Gitlab::Ci::Pipeline::Quota::Size, :saas do
       end
 
       it 'is not enabled' do
-        expect(subject).not_to be_enabled
+        is_expected.not_to be_enabled
       end
     end
   end
 
   describe '#exceeded?' do
     context 'when limit is exceeded' do
-      include_context 'pipeline size limit exceeded'
+      include_context 'when pipeline size limit exceeded'
 
       it 'is exceeded' do
-        expect(subject).to be_exceeded
+        is_expected.to be_exceeded
       end
     end
 
     context 'when limit is not exceeded' do
-      include_context 'pipeline size limit not exceeded'
+      include_context 'when pipeline size limit not exceeded'
 
       it 'is not exceeded' do
-        expect(subject).not_to be_exceeded
+        is_expected.not_to be_exceeded
       end
     end
   end
 
   describe '#message' do
     context 'when limit is exceeded' do
-      include_context 'pipeline size limit exceeded'
+      include_context 'when pipeline size limit exceeded'
 
       it 'returns info about pipeline size limit exceeded' do
-        expect(subject.message)
-          .to eq "The number of jobs has exceeded the limit of 1. "\
-          "Try splitting the configuration with parent-child-pipelines "\
-          "https://docs.gitlab.com/ee/ci/troubleshooting.html#pipeline-with-many-jobs-fails-to-start"
+        is_expected.to have_attributes(message: "The number of jobs has exceeded the limit of 1. " \
+                                         "Try splitting the configuration with parent-child-pipelines " \
+                                         "http://localhost/help/ci/debugging.md#pipeline-with-many-jobs-fails-to-start")
       end
     end
   end
@@ -96,21 +96,29 @@ RSpec.describe EE::Gitlab::Ci::Pipeline::Quota::Size, :saas do
   describe '#log_exceeded_limit?' do
     context 'when there are more than 2000 jobs in the pipeline' do
       let(:command) do
-        double(:command, pipeline_seed: double(:pipeline_seed, size: 2001))
+        instance_double(::Gitlab::Ci::Pipeline::Chain::Command, pipeline_seed: pipeline_seed_double)
+      end
+
+      let(:pipeline_seed_double) do
+        instance_double(::Gitlab::Ci::Pipeline::Seed::Pipeline, size: 2001)
       end
 
       it 'returns true' do
-        expect(subject.log_exceeded_limit?).to be_truthy
+        is_expected.to be_log_exceeded_limit
       end
     end
 
     context 'when there are 2000 or less jobs in the pipeline' do
       let(:command) do
-        double(:command, pipeline_seed: double(:pipeline_seed, size: 2000))
+        instance_double(::Gitlab::Ci::Pipeline::Chain::Command, pipeline_seed: pipeline_seed_double)
+      end
+
+      let(:pipeline_seed_double) do
+        instance_double(::Gitlab::Ci::Pipeline::Seed::Pipeline, size: 2000)
       end
 
       it 'returns false' do
-        expect(subject.log_exceeded_limit?).to be_falsey
+        is_expected.not_to be_log_exceeded_limit
       end
     end
   end
