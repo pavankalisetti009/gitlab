67ba36706e03d725d04bb028769c2fa513a6dc00
Angelo Gulina <agulina@gitlab.com>
2024-11-04 09:51:27 +0000

    Update Admin notification when approaching its licensed user count

--- ee/app/helpers/license_monitoring_helper.rb
+++ ee/app/helpers/license_monitoring_helper.rb
@@ -5,8 +5,18 @@ module LicenseMonitoringHelper
 
   def show_active_user_count_threshold_banner?
     return if ::Gitlab.com?
+    return if Feature.enabled?(:sm_seat_control_block_overages, :instance, type: :wip)
     return unless admin_section?
     return if user_dismissed?(Users::CalloutsHelper::ACTIVE_USER_COUNT_THRESHOLD)
+    return if current_license.nil?
+
+    current_user&.can_admin_all_resources? && current_license.active_user_count_threshold_reached?
+  end
+
+  def show_block_seat_overages_user_count_banner?
+    return if ::Gitlab.com?
+    return if Feature.disabled?(:sm_seat_control_block_overages, :instance, type: :wip)
+    return unless admin_section?
     return if current_license.nil?
 
     current_user&.can_admin_all_resources? && current_license.active_user_count_threshold_reached?
