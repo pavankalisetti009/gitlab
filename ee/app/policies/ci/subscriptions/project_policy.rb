d11431241492bd79cbef6bd9aa03c424940af5d3
Hordur Freyr Yngvason <hfyngvason@gitlab.com>
2024-11-05 14:59:14 +0000

    Fix project subscriptions policy
    The policy mistakenly required developer access to the downstream
    project in order to read pipeline subscriptions, causing a problem in
    the GraphQL endpoint relying on the `:read_project_subscription`
    ability.
    
    See https://gitlab.com/gitlab-org/gitlab/-/issues/434831#note_1736956507
    
    Changelog: fixed
    EE: true

--- ee/app/policies/ci/subscriptions/project_policy.rb
+++ ee/app/policies/ci/subscriptions/project_policy.rb
@@ -3,23 +3,22 @@
 module Ci
   module Subscriptions
     class ProjectPolicy < BasePolicy
+      condition(:admin_upstream_project) do
+        can?(:admin_project, @subject.upstream_project)
+      end
+
       condition(:admin_downstream_project) do
         can?(:admin_project, @subject.downstream_project)
       end
 
-      condition(:admin_access_to_both_projects) do
-        can?(:admin_project, @subject.downstream_project)
-      end
-
-      condition(:developer_access_to_downstream_project) do
-        can?(:developer_access, @subject.upstream_project)
-      end
-
-      rule { admin_access_to_both_projects & developer_access_to_downstream_project }.policy do
+      rule { admin_upstream_project }.policy do
         enable :read_project_subscription
       end
 
-      rule { admin_downstream_project }.policy { enable :delete_project_subscription }
+      rule { admin_downstream_project }.policy do
+        enable :read_project_subscription
+        enable :delete_project_subscription
+      end
     end
   end
 end
