21701bc2e7c89cb21c2b937bc42091e1127db57b
Eva Kadlecová <ekadlecova@gitlab.com>
2024-11-05 13:40:23 +0000

    Track usage of /help command for Duo Chat

--- ee/app/services/llm/base_service.rb
+++ ee/app/services/llm/base_service.rb
@@ -3,6 +3,7 @@
 module Llm
   class BaseService
     include Gitlab::Llm::Concerns::Logger
+    include Gitlab::DuoChatResourceHelper
 
     INVALID_MESSAGE = 'AI features are not enabled or resource is not permitted to be sent.'
 
@@ -107,34 +108,6 @@ module Llm
 
     def error(message)
       ServiceResponse.error(message: message)
-    end
-
-    def namespace
-      case resource
-      when Group
-        resource
-      when Project
-        resource.group
-      when User
-        nil
-      else
-        case resource&.resource_parent
-        when Group
-          resource.resource_parent
-        when Project
-          resource.resource_parent.group
-        end
-      end
-    end
-
-    def project
-      if resource.is_a?(Project)
-        resource
-      elsif resource.is_a?(Group) || resource.is_a?(User)
-        nil
-      elsif resource&.resource_parent.is_a?(Project)
-        resource.resource_parent
-      end
     end
   end
 end
