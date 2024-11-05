f73e627d64cf91bba74e88898642344d1b514c33
Désirée Chevalier <dchevalier@gitlab.com>
2024-11-05 18:02:42 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Andrejs Cunskis <acunskis@gitlab.com>

--- qa/qa/specs/features/ee/api/2_plan/epics_milestone_dates_spec.rb
+++ qa/qa/specs/features/ee/api/2_plan/epics_milestone_dates_spec.rb
@@ -3,6 +3,7 @@
 # rubocop:disable Rails/Date -- e2e specs do not set the timezone
 module QA
   RSpec.describe 'Plan', product_group: :product_planning do
+    include_context 'work item epics migration'
     include Support::API
 
     describe 'Epics milestone dates API' do
@@ -26,6 +27,8 @@ module QA
         response = Support::API.put(request.url, start_date: new_milestone_start_date, due_date: new_milestone_due_date)
         expect(response.code).to eq(Support::API::HTTP_STATUS_OK)
 
+        epic.reload!
+
         expect_epic_to_have_updated_dates(epic, new_milestone_start_date, new_milestone_due_date)
       end
 
@@ -40,25 +43,40 @@ module QA
         second_issue = create_issue(second_milestone)
         add_issue_to_epic(epic, second_issue)
 
-        expect_epic_to_have_updated_dates(epic, new_milestone_start_date, new_milestone_due_date)
+        epic.reload!
+
+        aggregate_failures do
+          expect_epic_to_have_updated_dates(epic, new_milestone_start_date, new_milestone_due_date)
+
+          if epic.instance_of?(QA::EE::Resource::WorkItemEpic)
+            expect_epic_to_have_updated_milestone(epic, second_milestone)
+          end
+        end
       end
 
       it 'updates epic dates when removing issue', :smoke,
         testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347957' do
         epic = create_epic_issue_milestone[0]
 
-        # Get epic_issue_id
-        request = create_request("/groups/#{group.id}/epics/#{epic.iid}/issues")
-        response = Support::API.get(request.url)
-        expect(response.code).to eq(Support::API::HTTP_STATUS_OK)
-        response_body = parse_body(response)
+        if epic.instance_of?(QA::EE::Resource::WorkItemEpic)
+          issue_id = epic.child_items[0][:id]
+          epic.remove_child_items(issue_id)
+        else # legacy epics
+          # Get epic_issue_id
+          request = create_request("/groups/#{group.id}/epics/#{epic.iid}/issues")
+          response = Support::API.get(request.url)
+          expect(response.code).to eq(Support::API::HTTP_STATUS_OK)
+          response_body = parse_body(response)
 
-        epic_issue_id = response_body[0][:epic_issue_id]
+          epic_issue_id = response_body[0][:epic_issue_id]
 
-        # Remove Issue
-        request = create_request("/groups/#{group.id}/epics/#{epic.iid}/issues/#{epic_issue_id}")
-        response = Support::API.delete(request.url)
-        expect(response.code).to eq(Support::API::HTTP_STATUS_OK)
+          # Remove Issue
+          request = create_request("/groups/#{group.id}/epics/#{epic.iid}/issues/#{epic_issue_id}")
+          response = Support::API.delete(request.url)
+          expect(response.code).to eq(Support::API::HTTP_STATUS_OK)
+        end
+
+        epic.reload!
 
         expect_epic_to_have_updated_dates(epic, nil, nil)
       end
@@ -70,7 +88,11 @@ module QA
         milestone.remove_via_api!
         epic.reload!
 
-        expect_epic_to_have_updated_dates(epic, nil, nil)
+        aggregate_failures do
+          expect_epic_to_have_updated_dates(epic, nil, nil)
+          expect(epic.start_date_sourcing_milestone).to be_nil if epic.instance_of?(QA::EE::Resource::WorkItemEpic)
+          expect(epic.due_date_sourcing_milestone).to be_nil if epic.instance_of?(QA::EE::Resource::WorkItemEpic)
+        end
       end
 
       private
@@ -80,7 +102,7 @@ module QA
         milestone = create_milestone(milestone_start_date, milestone_due_date)
         issue = create_issue(milestone)
         add_issue_to_epic(epic, issue)
-        use_epics_milestone_dates(epic)
+        use_epics_milestone_dates(epic, milestone)
         [epic, milestone]
       end
 
@@ -97,51 +119,97 @@ module QA
       end
 
       def create_epic
-        create(:epic,
-          group: group,
-          title: 'My New Epic',
-          due_date_fixed: fixed_due_date,
-          start_date_fixed: fixed_start_date,
-          start_date_is_fixed: true,
-          due_date_is_fixed: true)
+        begin
+          epic = create(:work_item_epic,
+            group: group,
+            title: 'My New Epic',
+            is_fixed: true,
+            start_date: fixed_start_date,
+            due_date: fixed_due_date)
+        rescue ArgumentError, NotImplementedError
+          epic = create(:epic,
+            group: group,
+            title: 'My New Epic',
+            start_date_is_fixed: true,
+            start_date_fixed: fixed_start_date,
+            due_date_is_fixed: true,
+            due_date_fixed: fixed_due_date)
+        else
+          update_web_url(group, epic)
+        end
+        epic
       end
 
       def add_issue_to_epic(epic, issue)
         # Add Issue with milestone to an epic
-        request = create_request("/groups/#{group.id}/epics/#{epic.iid}/issues/#{issue.id}")
-        response = Support::API.post(request.url, {})
+        if epic.instance_of?(QA::EE::Resource::WorkItemEpic)
+          epic.add_child_item(issue.id)
+          epic.reload!
 
-        expect(response.code).to eq(Support::API::HTTP_STATUS_CREATED)
-        response_body = parse_body(response)
+          expect(epic.title).to eq('My New Epic')
+          expect(epic.child_items[0][:name]).to eq('My Test Issue')
+        else # legacy epics
+          request = create_request("/groups/#{group.id}/epics/#{epic.iid}/issues/#{issue.id}")
+          response = Support::API.post(request.url, {})
 
-        expect(response_body[:epic][:title]).to eq('My New Epic')
-        expect(response_body[:issue][:title]).to eq('My Test Issue')
+          expect(response.code).to eq(Support::API::HTTP_STATUS_CREATED)
+          response_body = parse_body(response)
+
+          expect(response_body[:epic][:title]).to eq('My New Epic')
+          expect(response_body[:issue][:title]).to eq('My Test Issue')
+        end
       end
 
-      def use_epics_milestone_dates(epic)
+      def use_epics_milestone_dates(epic, milestone)
         # Update Epic to use Milestone Dates
-        request = create_request("/groups/#{group.id}/epics/#{epic.iid}")
-        response = Support::API.put(request.url, start_date_is_fixed: false, due_date_is_fixed: false)
-        expect(response.code).to eq(Support::API::HTTP_STATUS_OK)
+        if epic.instance_of?(QA::EE::Resource::WorkItemEpic)
+          epic.set_is_fixed(fixed: false)
 
-        expect_epic_to_have_updated_dates(epic, milestone_start_date, milestone_due_date)
-        aggregate_failures do
-          expect { epic.reload!.start_date_fixed }.to eventually_eq(fixed_start_date).within(wait_args)
-          expect { epic.reload!.due_date_fixed }.to eventually_eq(fixed_due_date).within(wait_args)
+          epic.reload!
+
+          aggregate_failures do
+            expect_epic_to_have_updated_dates(epic, milestone_start_date, milestone_due_date)
+            expect_epic_to_have_updated_milestone(epic, milestone)
+            expect(epic.fixed?).to be(false)
+          end
+        else # legacy epics
+          request = create_request("/groups/#{group.id}/epics/#{epic.iid}")
+          response = Support::API.put(request.url, start_date_is_fixed: false, due_date_is_fixed: false)
+          expect(response.code).to eq(Support::API::HTTP_STATUS_OK)
+
+          expect_epic_to_have_updated_dates(epic, milestone_start_date, milestone_due_date)
+          aggregate_failures do
+            expect { epic.reload!.start_date_fixed }.to eventually_eq(fixed_start_date).within(wait_args)
+            expect { epic.reload!.due_date_fixed }.to eventually_eq(fixed_due_date).within(wait_args)
+          end
         end
       end
 
       def expect_epic_to_have_updated_dates(epic, new_milestone_start_date, new_milestone_due_date)
-        # reload! and eventually are used to wait for the epic dates to update since a sidekiq job needs to run
+        if epic.instance_of?(QA::EE::Resource::WorkItemEpic)
+          aggregate_failures do
+            expect(epic.start_date).to eq(new_milestone_start_date)
+            expect(epic.due_date).to eq(new_milestone_due_date)
+          end
+        else # legacy epics
+          # reload! and eventually are used to wait for the epic dates to update since a sidekiq job needs to run
+          aggregate_failures do
+            expect { epic.reload!.start_date_from_milestones }
+              .to eventually_eq(new_milestone_start_date).within(wait_args)
+            expect { epic.reload!.due_date_from_milestones }
+              .to eventually_eq(new_milestone_due_date).within(wait_args)
+            expect { epic.reload!.start_date }
+              .to eventually_eq(new_milestone_start_date).within(wait_args)
+            expect { epic.reload!.due_date }
+              .to eventually_eq(new_milestone_due_date).within(wait_args)
+          end
+        end
+      end
+
+      def expect_epic_to_have_updated_milestone(epic, milestone)
         aggregate_failures do
-          expect { epic.reload!.start_date_from_milestones }
-            .to eventually_eq(new_milestone_start_date).within(wait_args)
-          expect { epic.reload!.due_date_from_milestones }
-            .to eventually_eq(new_milestone_due_date).within(wait_args)
-          expect { epic.reload!.start_date }
-            .to eventually_eq(new_milestone_start_date).within(wait_args)
-          expect { epic.reload!.due_date }
-            .to eventually_eq(new_milestone_due_date).within(wait_args)
+          expect(epic.start_date_sourcing_milestone[:id]).to have_content(milestone.id)
+          expect(epic.due_date_sourcing_milestone[:id]).to have_content(milestone.id)
         end
       end
     end
