1ae6553e36e90e67ac27f12941366cf26d15ac84
Sheldon Led <sheldonled@gitlab.com>
2024-11-06 16:18:50 +0000

    Add feature test for role promotion management `rejection` flow

--- ee/spec/features/users/role_promotion_spec.rb
+++ ee/spec/features/users/role_promotion_spec.rb
@@ -25,12 +25,33 @@ RSpec.describe 'Member Role Promotion Management', :js, feature_category: :seat_
     visit current_path
   end
 
-  it 'asserts member role promotion flow' do
+  it 'asserts member role promotion `approval` flow' do
+    expect_pending_member
+
+    act_on_pending_member_as_admin('Approve')
+
+    visit group_group_members_path(group)
+    expect(find_member_row(developer)).to have_text('Developer')
+  end
+
+  it 'asserts member role promotion `rejection` flow' do
+    expect_pending_member
+
+    act_on_pending_member_as_admin('Reject')
+
+    visit group_group_members_path(group)
+    expect(page).not_to have_text(developer.username)
+    expect(has_testid?('admin-promotion-request-tab')).to be(false)
+  end
+
+  def expect_pending_member
     expect(page).not_to have_text(developer.username)
 
     find_by_testid('promotion-request-tab').click
     expect(page).to have_text(developer.username)
+  end
 
+  def act_on_pending_member_as_admin(approval_action)
     sign_out(owner)
     gitlab_sign_in(admin)
     enable_admin_mode!(admin)
@@ -38,11 +59,9 @@ RSpec.describe 'Member Role Promotion Management', :js, feature_category: :seat_
     visit admin_users_path
     find_by_testid('admin-promotion-request-tab').click
     expect(page).to have_text(developer.username)
-    click_button 'Approve'
+    click_button approval_action
 
     sign_out(admin)
     gitlab_sign_in(owner)
-    visit group_group_members_path(group)
-    expect(find_member_row(developer)).to have_text('Developer')
   end
 end
