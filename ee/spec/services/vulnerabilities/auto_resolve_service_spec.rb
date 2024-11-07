0f7bc5d5cdc6f231d94c96a8d902c14bd72daeb0
Brian Williams <bwilliams@gitlab.com>
2024-11-07 10:19:48 -0600

    Add AutoResolveService to automatically resolve vulnerabilities
    These vulnerabilities will be resolved when a vulnerability management
    policy is configured to do so. This class is only responsible for
    resolving a batch of vulnerabilities. The caller will check security
    policies and find vulnerabilities matching the policy rules. It will
    pass the IDs of those vulnerabilities to this service.

---
+++ ee/spec/services/vulnerabilities/auto_resolve_service_spec.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Vulnerabilities::AutoResolveService, feature_category: :vulnerability_management do
+  let_it_be(:user) { create(:user, :security_policy_bot) }
+  let_it_be(:namespace) { create(:namespace) }
+  let_it_be_with_reload(:project) { create(:project, namespace: namespace) }
+  let_it_be(:vulnerability) { create(:vulnerability, :with_findings, :detected, :high_severity, project: project) }
+  let(:vulnerability_ids) { [vulnerability.id] }
+  let(:comment) { _("Auto-resolved by vulnerability management policy") + " #{security_policy_name}" }
+  let(:security_policy_name) { 'resolve_low_severity_vulnerabilities' }
+
+  subject(:service) { described_class.new(project, vulnerability_ids, security_policy_name) }
+
+  before_all do
+    project.add_developer(user)
+  end
+
+  describe '#execute' do
+    it 'resolves each vulnerability', :freeze_time do
+      service.execute
+
+      vulnerability.reload
+      expect(vulnerability).to be_resolved
+      expect(vulnerability.resolved_by).to eq(project.security_policy_bot)
+      expect(vulnerability.resolved_at).to eq(Time.current)
+    end
+
+    it 'inserts a state transition for each vulnerability' do
+      service.execute
+
+      last_state = vulnerability.reload.state_transitions.last
+      expect(last_state.from_state).to eq('detected')
+      expect(last_state.to_state).to eq('resolved')
+      expect(last_state.comment).to eq(comment)
+      expect(last_state.author).to eq(project.security_policy_bot)
+    end
+
+    it 'inserts a system note for each vulnerability' do
+      service.execute
+
+      last_note = Note.last
+
+      expect(last_note.noteable).to eq(vulnerability)
+      expect(last_note.author).to eq(project.security_policy_bot)
+      expect(last_note.project).to eq(project)
+      expect(last_note.namespace_id).to eq(project.project_namespace_id)
+      expect(last_note.note).to eq(
+        "changed vulnerability status to Resolved and the following comment: \"#{comment}\""
+      )
+      expect(last_note).to be_system
+    end
+
+    it 'updates the statistics', :sidekiq_inline do
+      _active_vulnerability = create(:vulnerability, :with_finding, :high_severity, project: project)
+      Vulnerabilities::Read.update_all(traversal_ids: project.namespace.traversal_ids)
+
+      service.execute
+
+      expect(project.vulnerability_statistic).to be_present
+      expect(project.vulnerability_statistic.total).to eq(1)
+      expect(project.vulnerability_statistic.critical).to eq(0)
+      expect(project.vulnerability_statistic.high).to eq(1)
+      expect(project.vulnerability_statistic.medium).to eq(0)
+      expect(project.vulnerability_statistic.low).to eq(0)
+      expect(project.vulnerability_statistic.unknown).to eq(0)
+      expect(project.vulnerability_statistic.letter_grade).to eq('d')
+    end
+
+    context 'when project does not have a security_policy_bot' do
+      before_all do
+        project.security_policy_bots.delete_all
+      end
+
+      it 'returns an error response' do
+        result = service.execute
+
+        expect(result).to be_error
+        expect(result.errors).to eq(['Project does not have a security policy bot'])
+      end
+    end
+
+    context 'when an error occurs' do
+      before do
+        allow(Note).to receive(:insert_all!).and_raise(ActiveRecord::RecordNotUnique)
+      end
+
+      it 'does not bubble up the error' do
+        expect { service.execute }.not_to raise_error
+      end
+
+      it 'returns an appropriate service response' do
+        result = service.execute
+
+        expect(result).to be_error
+        expect(result.errors).to eq(['Could not resolve vulnerabilities'])
+      end
+    end
+
+    context 'when updating multiple vulnerabilities' do
+      let_it_be(:vulnerabilities) { create_list(:vulnerability, 2, :with_findings, project: project) }
+      let_it_be(:vulnerability_ids) { vulnerabilities.map(&:id) }
+
+      it 'does not introduce N+1 queries' do
+        control = ActiveRecord::QueryRecorder.new do
+          described_class.new(project, vulnerability_ids, security_policy_name).execute
+        end
+
+        new_vulnerability = create(:vulnerability, :with_findings, project: project)
+        vulnerability_ids << new_vulnerability.id
+
+        expect do
+          described_class.new(project, vulnerability_ids, security_policy_name).execute
+        end.not_to exceed_query_limit(control)
+      end
+    end
+  end
+end
