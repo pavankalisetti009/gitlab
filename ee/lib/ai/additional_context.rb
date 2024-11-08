a081b92f974d2f40216b97fd87c11b1fed1f6976
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-08 12:34:34 +0100

    Add explanation for additional context unit primitives

--- ee/lib/ai/additional_context.rb
+++ ee/lib/ai/additional_context.rb
@@ -3,6 +3,16 @@
 module Ai
   module AdditionalContext
     CODE_SUGGESTIONS_CONTEXT_TYPES = { file: 'file', snippet: 'snippet' }.freeze
+
+    # Introducing new types requires adding `include_*_context` unit primitives as well.
+    #
+    # First, decide whether a unit primitive is part of Duo Pro or Duo Enterprise.
+    # Then, follow the examples of `include_*_context` unit primitives:
+    # https://gitlab.com/gitlab-org/gitlab/-/blob/ddbd15c27268963f72e77beb4797d41e8e918f94/ee/config/cloud_connector/access_data.yml
+    # https://gitlab.com/gitlab-org/customers-gitlab-com/-/blob/3d1dbc421cca5ca86e8577213660b063f71bf27b/config/cloud_connector.yml
+    #
+    # The unit primitives need to be added to both the `gitlab-org/gitlab` and `gitlab-org/customers-gitlab-com`
+    # repositories.
     DUO_CHAT_CONTEXT_CATEGORIES = {
       file: 'file',
       snippet: 'snippet',
