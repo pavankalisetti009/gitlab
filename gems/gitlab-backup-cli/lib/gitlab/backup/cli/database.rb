9fb34e1f6246a9563a18821eac66ac3a0c2ec849
Gabriel Mazetto <gabriel@gitlab.com>
2024-11-14 18:04:38 +0100

    Removing unnecessary database related classes

--- gems/gitlab-backup-cli/lib/gitlab/backup/cli/database.rb
+++ gems/gitlab-backup-cli/lib/gitlab/backup/cli/database.rb
@@ -4,9 +4,6 @@ module Gitlab
   module Backup
     module Cli
       module Database
-        autoload :Configuration, 'gitlab/backup/cli/database/configuration'
-        autoload :Connection, 'gitlab/backup/cli/database/connection'
-        autoload :Postgres, 'gitlab/backup/cli/database/postgres'
       end
     end
   end
