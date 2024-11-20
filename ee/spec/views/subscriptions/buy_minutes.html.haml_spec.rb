f18fc3af8bf296f7eb0029bb83c7264e3f088708
Vamsi Vempati <vvempati@gitlab.com>
2024-11-20 10:06:19 +0000

    Clean up obsolete views as a result of purchase flow migration
    - Cleans up purchase flow HAML files that are no longer used

--- ee/spec/views/subscriptions/buy_minutes.html.haml_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe 'subscriptions/buy_minutes' do
-  it_behaves_like 'buy minutes addon form data', '#js-buy-minutes'
-end
--- ee/spec/views/subscriptions/buy_storage.html.haml_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe 'subscriptions/buy_storage' do
-  it_behaves_like 'buy storage addon form data', '#js-buy-storage'
-end
