92a7cf6c4fd5970dcdd5c4a3dd74f6a3570b6ccd
Paulo Barros <pbarros@gitlab.com>
2024-11-20 08:43:33 +0000

    Clean up sm_duo_pro_bulk_user_assignment
    Changelog: added
    EE: true

--- ee/app/helpers/gitlab_subscriptions/code_suggestions_helper.rb
+++ ee/app/helpers/gitlab_subscriptions/code_suggestions_helper.rb
@@ -4,12 +4,6 @@ module GitlabSubscriptions
   module CodeSuggestionsHelper
     include GitlabSubscriptions::SubscriptionHelper
 
-    def duo_pro_bulk_user_assignment_available?(_namespace = nil)
-      return true if gitlab_com_subscription?
-
-      Feature.enabled?(:sm_duo_pro_bulk_user_assignment)
-    end
-
     def add_duo_pro_seats_url(subscription_name)
       ::Gitlab::Routing.url_helpers.subscription_portal_add_sm_duo_pro_seats_url(subscription_name)
     end
--- ee/spec/helpers/gitlab_subscriptions/code_suggestions_helper_spec.rb
+++ ee/spec/helpers/gitlab_subscriptions/code_suggestions_helper_spec.rb
@@ -5,42 +5,6 @@ require 'spec_helper'
 RSpec.describe GitlabSubscriptions::CodeSuggestionsHelper, feature_category: :seat_cost_management do
   include SubscriptionPortalHelper
 
-  describe '#duo_pro_bulk_user_assignment_available?' do
-    context 'when GitLab is .com' do
-      let_it_be(:namespace) { build_stubbed(:group) }
-
-      before do
-        stub_saas_features(gitlab_com_subscriptions: true)
-      end
-
-      context 'when duo pro is available' do
-        it 'returns true' do
-          expect(helper.duo_pro_bulk_user_assignment_available?(namespace)).to be_truthy
-        end
-      end
-    end
-
-    context 'when GitLab is self managed' do
-      before do
-        stub_saas_features(gitlab_com_subscriptions: false)
-      end
-
-      it 'returns true' do
-        expect(helper.duo_pro_bulk_user_assignment_available?).to be_truthy
-      end
-
-      context 'when sm_duo_pro_bulk_user_assignment feature flag is disabled' do
-        before do
-          stub_feature_flags(sm_duo_pro_bulk_user_assignment: false)
-        end
-
-        it 'returns false' do
-          expect(helper.duo_pro_bulk_user_assignment_available?).to be_falsey
-        end
-      end
-    end
-  end
-
   describe '#add_duo_pro_seats_url' do
     let(:subscription_name) { 'A-S000XXX' }
     let(:env_value) { nil }
