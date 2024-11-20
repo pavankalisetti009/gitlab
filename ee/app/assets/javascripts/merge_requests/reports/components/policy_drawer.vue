43fdfc445f8d84828d2597112d6c87bae847d35a
Phil Hughes <me@iamphill.com>
2024-11-20 11:21:52 +0000

    Fetches merge request security policy comparison pipelines
    Updates the policy drawer component to send API requests to get the
    comparison pipelines data.

--- ee/app/assets/javascripts/merge_requests/reports/components/policy_drawer.vue
+++ ee/app/assets/javascripts/merge_requests/reports/components/policy_drawer.vue
@@ -5,6 +5,7 @@ import { getContentWrapperHeight } from '~/lib/utils/dom_utils';
 import TimeAgoTooltip from '~/vue_shared/components/time_ago_tooltip.vue';
 import ScanResultDetailsDrawer from 'ee/security_orchestration/components/policy_drawer/scan_result/details_drawer.vue';
 import InfoRow from 'ee/security_orchestration/components/policy_drawer/info_row.vue';
+import getPipelineQuery from '../queries/get_pipeline.query.graphql';
 
 export default {
   components: {
@@ -15,6 +16,7 @@ export default {
     InfoRow,
     ScanResultDetailsDrawer,
   },
+  inject: ['projectPath'],
   props: {
     open: {
       type: Boolean,
@@ -44,10 +46,68 @@ export default {
       default: null,
     },
   },
+  data() {
+    return {
+      pipelines: null,
+    };
+  },
   computed: {
     getDrawerHeaderHeight() {
       if (!this.open) return '0';
       return getContentWrapperHeight();
+    },
+  },
+  watch: {
+    comparisonPipelines: {
+      handler(newVal) {
+        if (newVal) {
+          this.fetchComparisonPipelines();
+        }
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    closeDrawer() {
+      this.pipelines = null;
+
+      this.$emit('close');
+    },
+    async fetchComparisonPipelines() {
+      this.pipelines = await Promise.all([
+        await Promise.all(
+          this.comparisonPipelines.source.map((id) =>
+            this.$apollo
+              .query({
+                query: getPipelineQuery,
+                variables: {
+                  projectPath: this.projectPath,
+                  id,
+                },
+                context: {
+                  batchKey: 'SecurityPolicyComparisonPipeline',
+                },
+              })
+              .then(({ data }) => data.project?.pipeline),
+          ),
+        ),
+        await Promise.all(
+          this.comparisonPipelines.target.map((id) =>
+            this.$apollo
+              .query({
+                query: getPipelineQuery,
+                variables: {
+                  projectPath: this.projectPath,
+                  id,
+                },
+                context: {
+                  batchKey: 'SecurityPolicyComparisonPipeline',
+                },
+              })
+              .then(({ data }) => data.project?.pipeline),
+          ),
+        ),
+      ]);
     },
   },
   DRAWER_Z_INDEX,
@@ -59,7 +119,7 @@ export default {
     :header-height="getDrawerHeaderHeight"
     :z-index="$options.DRAWER_Z_INDEX"
     :open="open"
-    @close="$emit('close')"
+    @close="closeDrawer"
   >
     <template #title>
       <h4 class="gl-my-0">{{ __('Security policy') }}</h4>
@@ -77,20 +137,38 @@ export default {
       </p>
       <h5 class="h4 gl-mb-0 gl-mt-0">{{ policy.name }}</h5>
       <scan-result-details-drawer :policy="policy" :show-policy-scope="false" :show-status="false">
-        <template v-if="comparisonPipelines" #additional-details>
+        <template v-if="comparisonPipelines && pipelines" #additional-details>
           <info-row :label="__('Comparison pipelines')">
             <ul class="gl-pl-6">
-              <li class="gl-mb-2">
-                <gl-sprintf :message="__('Target branch (%{branch})')">
+              <li v-if="pipelines[0]" class="gl-mb-2" data-testid="target-branch-pipeline">
+                <gl-sprintf :message="__('Target branch (%{branch}): %{pipelines}')">
                   <template #branch>
                     <code>{{ targetBranch }}</code>
                   </template>
+                  <template #pipelines>
+                    <gl-link
+                      v-for="targetBranchPipeline in pipelines[0]"
+                      :key="targetBranchPipeline.iid"
+                      :href="targetBranchPipeline.path"
+                    >
+                      #{{ targetBranchPipeline.iid }}
+                    </gl-link>
+                  </template>
                 </gl-sprintf>
               </li>
-              <li>
-                <gl-sprintf :message="__('Source branch (%{branch})')">
+              <li v-if="pipelines[1]" data-testid="source-branch-pipeline">
+                <gl-sprintf :message="__('Source branch (%{branch}): %{pipelines}')">
                   <template #branch>
                     <code>{{ sourceBranch }}</code>
+                  </template>
+                  <template #pipelines>
+                    <gl-link
+                      v-for="sourceBranchPipeline in pipelines[1]"
+                      :key="sourceBranchPipeline.iid"
+                      :href="sourceBranchPipeline.path"
+                    >
+                      #{{ sourceBranchPipeline.iid }}
+                    </gl-link>
                   </template>
                 </gl-sprintf>
               </li>
--- ee/spec/frontend/merge_requests/reports/components/policy_drawer_spec.js
+++ ee/spec/frontend/merge_requests/reports/components/policy_drawer_spec.js
@@ -1,5 +1,12 @@
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import createMockApollo from 'helpers/mock_apollo_helper';
+import waitForPromises from 'helpers/wait_for_promises';
 import { mountExtended } from 'helpers/vue_test_utils_helper';
+import getPipelineQuery from 'ee/merge_requests/reports/queries/get_pipeline.query.graphql';
 import PolicyDrawer from 'ee/merge_requests/reports/components/policy_drawer.vue';
+
+Vue.use(VueApollo);
 
 const createMockPolicy = (data = {}) => {
   return {
@@ -26,12 +33,27 @@ const createMockPolicy = (data = {}) => {
 
 describe('Merge request reports policy drawer component', () => {
   let wrapper;
+  let getPipelineQueryMock;
 
   const findSecurityPolicy = () => wrapper.findByTestId('security-policy');
   const findPipeline = () => wrapper.findByTestId('security-pipeline');
+  const findTargetPipeline = () => wrapper.findByTestId('target-branch-pipeline');
+  const findSourcePipeline = () => wrapper.findByTestId('source-branch-pipeline');
 
   function createComponent(propsData = {}) {
+    getPipelineQueryMock = jest.fn().mockResolvedValue({
+      data: { project: { id: 1, pipeline: { id: 1, iid: 1, path: '/' } } },
+    });
+
+    const apolloProvider = createMockApollo(
+      [[getPipelineQuery, getPipelineQueryMock]],
+      {},
+      { typePolicies: { Query: { fields: { project: { merge: false } } } } },
+    );
+
     wrapper = mountExtended(PolicyDrawer, {
+      apolloProvider,
+      provide: { projectPath: 'gitlab-org/gitlab' },
       propsData: {
         open: false,
         targetBranch: 'main',
@@ -67,5 +89,43 @@ describe('Merge request reports policy drawer component', () => {
     });
 
     expect(findPipeline().text()).toContain('in pipeline #1');
+  });
+
+  describe('with comparison pipelines', () => {
+    it('fetches source branch and target branch pipelines', async () => {
+      createComponent({
+        comparisonPipelines: {
+          source: ['1'],
+          target: ['2'],
+        },
+      });
+
+      await waitForPromises();
+
+      expect(getPipelineQueryMock).toHaveBeenCalledWith({
+        projectPath: 'gitlab-org/gitlab',
+        id: '1',
+      });
+      expect(getPipelineQueryMock).toHaveBeenCalledWith({
+        projectPath: 'gitlab-org/gitlab',
+        id: '2',
+      });
+    });
+
+    it('renders comparison pipelines', async () => {
+      createComponent({
+        open: true,
+        policy: createMockPolicy(),
+        comparisonPipelines: {
+          source: ['1'],
+          target: ['2'],
+        },
+      });
+
+      await waitForPromises();
+
+      expect(findTargetPipeline().text()).toContain('#1');
+      expect(findSourcePipeline().text()).toContain('#1');
+    });
   });
 });
