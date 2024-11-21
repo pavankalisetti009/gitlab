ae87dd91263f209578e09e4c8aac9ba1e3c9f745
Ayush Billore <abillore@gitlab.com>
2024-11-21 15:36:29 +0000

    Remove feature flag - require_identity_verification_for_old_users
    This feature flag was only added to test IV in staging before the the identity
    verification release date.
    
    
    Changelog: removed

--- ee/spec/features/users/identity_verification_spec.rb
+++ ee/spec/features/users/identity_verification_spec.rb
@@ -10,14 +10,11 @@ RSpec.describe 'Identity Verification', :js, feature_category: :instance_resilie
     create(:user, :identity_verification_eligible)
   end
 
-  let(:require_iv_for_old_users) { false }
-
   before do
     allow(Gitlab::ApplicationRateLimiter).to receive(:throttled?).and_call_original
     allow(Gitlab::ApplicationRateLimiter).to receive(:throttled?)
       .with(:phone_verification_send_code, scope: user).and_return(false)
 
-    stub_feature_flags(require_identity_verification_for_old_users: require_iv_for_old_users)
     stub_saas_features(identity_verification: true)
     stub_application_setting(
       arkose_labs_public_api_key: 'public_key',
@@ -31,40 +28,28 @@ RSpec.describe 'Identity Verification', :js, feature_category: :instance_resilie
     visit identity_verification_path
   end
 
-  shared_examples 'verifies the user' do
-    specify do
-      expect_to_see_identity_verification_page
+  it 'verifies the user' do
+    expect_to_see_identity_verification_page
 
-      verify_phone_number(solve_arkose_challenge: true)
+    verify_phone_number(solve_arkose_challenge: true)
 
-      expect(page).to have_content(_('Completed'))
-      expect(page).to have_content(_('Next'))
+    expect(page).to have_content(_('Completed'))
+    expect(page).to have_content(_('Next'))
 
-      click_link 'Next'
+    click_link 'Next'
 
-      wait_for_requests
+    wait_for_requests
 
-      expect_to_see_dashboard_page
-    end
+    expect_to_see_dashboard_page
   end
-
-  it_behaves_like 'verifies the user'
 
   context 'when the user was created before the feature relase date' do
     let_it_be(:user) do
       create(:user, created_at: IdentityVerifiable::IDENTITY_VERIFICATION_RELEASE_DATE - 1.day)
     end
 
-    context 'when identity verification is required for old users' do
-      let(:require_iv_for_old_users) { true }
-
-      it_behaves_like 'verifies the user'
-    end
-
-    context 'when identity verification is not required for old users' do
-      it 'does not verify the user' do
-        expect_to_see_dashboard_page
-      end
+    it 'does not verify the user' do
+      expect_to_see_dashboard_page
     end
   end
 
