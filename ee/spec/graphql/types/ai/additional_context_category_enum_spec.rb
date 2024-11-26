72edf08bbbe0e9e450a62f0cf50f1851fee120d4
Angelo Rivera <arivera@gitlab.com>
2024-11-26 15:33:56 +0000

    Add local_git context Additional Context category for Duo Chat

--- ee/spec/graphql/types/ai/additional_context_category_enum_spec.rb
+++ ee/spec/graphql/types/ai/additional_context_category_enum_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 RSpec.describe GitlabSchema.types['AiAdditionalContextCategory'], feature_category: :duo_chat do
   it 'exposes all additional context categories' do
-    expect(described_class.values.keys).to match_array(%w[FILE SNIPPET MERGE_REQUEST ISSUE DEPENDENCY])
+    expect(described_class.values.keys).to match_array(%w[FILE SNIPPET MERGE_REQUEST ISSUE DEPENDENCY LOCAL_GIT])
   end
 end
