a4b4e6d9fa0dc8b0bdec9dc45faffe2052f06a19
Julie Huang <julhuang@gitlab.com>
2024-12-02 11:20:03 +0000

    Remove redundant self-hosted models HAML views
    Since moving self-hosted models pages from a RESTful implementation to Vue routing
    in this MR https://gitlab.com/gitlab-org/gitlab/-/merge_requests/172223, these HAML
    views are now redundant. Refer to MR above for further details of the routing change.
    
    Changelog: changed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/174216
    EE: true

--- ee/app/assets/javascripts/pages/admin/ai/self_hosted_models/new/index.js
+++
@@ -1,4 +0,0 @@
-import { initSimpleApp } from '~/helpers/init_simple_app_helper';
-import NewSelfHostedModel from '../components/new_self_hosted_model.vue';
-
-initSimpleApp('#js-new-self-hosted-model', NewSelfHostedModel, { withApolloProvider: true });
