574ca006b08a3018f89e01d3b732810974858875
Roy Liu <rliu@gitlab.com>
2024-12-03 20:31:42 +0000

    Allows new role field in the onboarding_status jsonb column

--- ee/app/models/ee/user_detail.rb
+++ ee/app/models/ee/user_detail.rb
@@ -15,6 +15,20 @@ module EE
         :onboarding_status, :step_url, :email_opt_in, :initial_registration_type, :registration_type, :glm_content,
         :glm_source, prefix: true
       )
+
+      def self.onboarding_status_roles
+        {
+          software_developer: 0,
+          development_team_lead: 1,
+          devops_engineer: 2,
+          systems_administrator: 3,
+          security_analyst: 4,
+          data_analyst: 5,
+          product_manager: 6,
+          product_designer: 7,
+          other: 8
+        }
+      end
     end
   end
 end
