fb12090f1ad930ddb5942a1cddbf28a42bc5b4a8
Shubham Kumar <shukumar@gitlab.com>
2024-12-05 23:40:47 +0000

    Add and backfill namespace_id for status_page_published_incidents
    ## What does this MR do and why?
    
    Add and backfill namespace_id for status_page_published_incidents.
    
    This table has a
    [desired sharding key](https://docs.gitlab.com/ee/development/database/multiple_databases.html#define-a-desired_sharding_key-to-automatically-backfill-a-sharding_key)
    configured ([view configuration](https://gitlab.com/gitlab-org/gitlab/-/blob/master/db/docs/status_page_published_incidents.yml)).
    
    This merge request is the first step towards transforming the desired sharding key into a
    [sharding key](https://docs.gitlab.com/ee/development/database/multiple_databases.html#defining-a-sharding-key-for-all-cell-local-tables).
    
    This involves three changes:
    
    - Adding a new column that will serve as the sharding key (along with the relevant index and foreign key).
    - Populating the sharding key when new records are created by adding a database function and trigger.
    - Scheduling a [batched background migration](https://docs.gitlab.com/ee/development/database/batched_background_migrations.html)
      to set the sharding key for existing records.
    
    Once the background migration has completed, a second merge request will be created to finalize the background
    migration and validate the not null constraint.
    
    ## How to verify
    
    We have assigned a random backend engineer from ~"group::platform insights" to review these changes. Please review this merge
    request from a ~backend perspective. The main thing we are looking to verify is that the added column and association
    match the values specified by the [desired sharding key](https://gitlab.com/gitlab-org/gitlab/-/blob/master/db/docs/status_page_published_incidents.yml)
    configuration and that backfilling the column from this other table makes sense in the context of this feature.
    
    When you are finished, please:
    
    1. Trigger the [database testing pipeline](https://docs.gitlab.com/ee/development/database/database_migration_pipeline.html)
       as instructed by Danger.
    1. Request a review from the ~backend maintainer and ~database reviewer suggested by Danger.
    
    If you have any questions or concerns, reach out to `@tigerwnz` or @shubhamkrai.
    
    This merge request was generated by a once off keep implemented in
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/143774
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    using the Keeps::BackfillDesiredShardingKeySmallTable keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please create an issue with the
    label ~"GitLab Housekeeper" and consider pinging the author of this keep.
    
    Changelog: other

--- db/structure.sql
+++ db/structure.sql
@@ -2865,6 +2865,22 @@ RETURN NEW;
 END
 $$;
 
+CREATE FUNCTION trigger_dbe374a57cbb() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+IF NEW."namespace_id" IS NULL THEN
+  SELECT "namespace_id"
+  INTO NEW."namespace_id"
+  FROM "issues"
+  WHERE "issues"."id" = NEW."issue_id";
+END IF;
+
+RETURN NEW;
+
+END
+$$;
+
 CREATE FUNCTION trigger_dc13168b8025() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
@@ -20163,7 +20179,8 @@ CREATE TABLE status_page_published_incidents (
     id bigint NOT NULL,
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
-    issue_id bigint NOT NULL
+    issue_id bigint NOT NULL,
+    namespace_id bigint
 );
 
 CREATE SEQUENCE status_page_published_incidents_id_seq
@@ -32632,6 +32649,8 @@ CREATE INDEX index_status_check_responses_on_project_id ON status_check_response
 
 CREATE UNIQUE INDEX index_status_page_published_incidents_on_issue_id ON status_page_published_incidents USING btree (issue_id);
 
+CREATE INDEX index_status_page_published_incidents_on_namespace_id ON status_page_published_incidents USING btree (namespace_id);
+
 CREATE INDEX index_status_page_settings_on_project_id ON status_page_settings USING btree (project_id);
 
 CREATE INDEX index_subscription_add_on_purchases_on_namespace_id_add_on_id ON subscription_add_on_purchases USING btree (namespace_id, subscription_add_on_id);
@@ -35726,6 +35745,8 @@ CREATE TRIGGER trigger_dadd660afe2c BEFORE INSERT OR UPDATE ON packages_debian_g
 
 CREATE TRIGGER trigger_dbdd61a66a91 BEFORE INSERT OR UPDATE ON agent_activity_events FOR EACH ROW EXECUTE FUNCTION trigger_dbdd61a66a91();
 
+CREATE TRIGGER trigger_dbe374a57cbb BEFORE INSERT OR UPDATE ON status_page_published_incidents FOR EACH ROW EXECUTE FUNCTION trigger_dbe374a57cbb();
+
 CREATE TRIGGER trigger_dc13168b8025 BEFORE INSERT OR UPDATE ON vulnerability_flags FOR EACH ROW EXECUTE FUNCTION trigger_dc13168b8025();
 
 CREATE TRIGGER trigger_delete_project_namespace_on_project_delete AFTER DELETE ON projects FOR EACH ROW WHEN ((old.project_namespace_id IS NOT NULL)) EXECUTE FUNCTION delete_associated_project_namespace();
@@ -36897,6 +36918,9 @@ ALTER TABLE ONLY merge_requests_closing_issues
 
 ALTER TABLE ONLY issue_assignment_events
     ADD CONSTRAINT fk_a989e2acd0 FOREIGN KEY (namespace_id) REFERENCES namespaces(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY status_page_published_incidents
+    ADD CONSTRAINT fk_a9fb727793 FOREIGN KEY (namespace_id) REFERENCES namespaces(id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY ssh_signatures
     ADD CONSTRAINT fk_aa1efbe865 FOREIGN KEY (key_id) REFERENCES keys(id) ON DELETE SET NULL;
