63d95c3a4207a9839f44c2188d25f2e709ffd356
Jarka Košanová <jarka@gitlab.com>
2024-12-06 13:59:26 +0000

    Add endpoint to assign admin custom role to a user
    - add graphql mutation, type
    - add service
    - under a FF

--- ee/app/graphql/ee/types/mutation_type.rb
+++ ee/app/graphql/ee/types/mutation_type.rb
@@ -146,6 +146,7 @@ module EE
         mount_mutation ::Mutations::Security::ProjectSecurityExclusionUpdate
         mount_mutation ::Mutations::Security::ProjectSecurityExclusionDelete
         mount_mutation ::Mutations::Users::Abuse::NamespaceBans::Destroy
+        mount_mutation ::Mutations::Users::MemberRoles::Assign, experiment: { milestone: '17.7' }
         mount_mutation ::Mutations::AuditEvents::ExternalAuditEventDestinations::Create
         mount_mutation ::Mutations::AuditEvents::ExternalAuditEventDestinations::Destroy
         mount_mutation ::Mutations::AuditEvents::ExternalAuditEventDestinations::Update
