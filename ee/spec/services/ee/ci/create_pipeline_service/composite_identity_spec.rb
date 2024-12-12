d7eff11647c9bb4bd25549fd47a59680ce8e9cd9
Imre Farkas <ifarkas@gitlab.com>
2024-12-12 13:54:19 +0100

    Composite identity CI specs to use user setting

--- ee/spec/services/ee/ci/create_pipeline_service/composite_identity_spec.rb
+++ ee/spec/services/ee/ci/create_pipeline_service/composite_identity_spec.rb
@@ -4,16 +4,24 @@ require 'spec_helper'
 
 RSpec.describe Ci::CreatePipelineService, feature_category: :continuous_integration do # rubocop:disable RSpec/SpecFilePathFormat -- we breakdown Ci::CreatePipelineService E2E tests this way
   let_it_be(:project) { create(:project, :repository) }
-  let_it_be(:user) { project.first_owner }
-
   let(:ref) { 'refs/heads/master' }
   let(:service) { described_class.new(project, user, { ref: ref }) }
 
   subject(:pipeline) { service.execute(:push).payload }
 
   describe 'composite identity', :request_store do
+    let_it_be(:user) { create(:user, :service_account, composite_identity_enforced: true) }
+    let_it_be(:scoped_user) { create(:user) }
+
+    before_all do
+      project.add_owner(user)
+      project.add_maintainer(scoped_user)
+    end
+
     before do
       stub_ci_pipeline_yaml_file(config)
+
+      ::Gitlab::Auth::Identity.fabricate(user).link!(scoped_user)
     end
 
     context 'when job does not generate options' do
@@ -27,15 +35,15 @@ RSpec.describe Ci::CreatePipelineService, feature_category: :continuous_integrat
         YAML
       end
 
-      context 'when user does not support composite identity' do
-        it 'does not propagate the scoped user into each job' do
-          expect(pipeline).to be_created_successfully
-          expect(pipeline.builds).to be_present
+      it 'propagates the scoped user into each job without overriding `options`' do
+        expect(pipeline).to be_created_successfully
+        expect(pipeline.builds).to be_present
 
-          pipeline.builds.each do |job|
-            expect(job.options).not_to have_key(:scoped_user_id)
-          end
-        end
+        options = pipeline.statuses.map(&:options)
+        expect(options).to eq [
+          { script: ['echo'], job_timeout: 1.hour.to_i, scoped_user_id: scoped_user.id },
+          { trigger: { project: 'test-project' }, scoped_user_id: scoped_user.id }
+        ]
       end
     end
   end
