f383770061e92aaa3e3981129f89e2addc4d2657
Adrien Narinesingh <adinzey@gitlab.com>
2024-12-12 22:26:34 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Jonas Larsen <jlarsen@gitlab.com>

--- ee/app/controllers/security/dashboard_controller.rb
+++ ee/app/controllers/security/dashboard_controller.rb
@@ -2,7 +2,20 @@
 
 module Security
   class DashboardController < ::Security::ApplicationController
+    include GovernUsageGroupTracking
+
     layout 'instance_security'
+    track_internal_event :show, name: 'visit_security_dashboard', category: name
+
+    private
+
+    def tracking_namespace_source
+      nil
+    end
+
+    def tracking_project_source
+      nil
+    end
   end
 end
 
--- ee/spec/controllers/security/dashboard_controller_spec.rb
+++ ee/spec/controllers/security/dashboard_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Security::DashboardController, feature_category: :vulnerability_m
   let_it_be(:user) { create(:user) }
 
   describe 'GET #show' do
-    subject { get :show }
+    subject(:show_security_dashboard) { get :show }
 
     it_behaves_like Security::ApplicationController do
       let(:security_application_controller_child_action) do
@@ -25,11 +25,21 @@ RSpec.describe Security::DashboardController, feature_category: :vulnerability_m
         end
 
         it { is_expected.to render_template(:instance_security) }
+
+        it_behaves_like 'internal event tracking' do
+          let(:event) { 'visit_security_dashboard' }
+          let(:category) { described_class.name }
+          subject(:service_action) { show_security_dashboard }
+        end
       end
 
       context 'is disabled' do
         it { is_expected.to have_gitlab_http_status(:not_found) }
         it { is_expected.to render_template('errors/not_found') }
+
+        it 'does not record events or metrics' do
+          expect { show_security_dashboard }.not_to trigger_internal_events('visit_security_dashboard')
+        end
       end
     end
   end
