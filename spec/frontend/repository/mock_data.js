315450480e62676e213c3d395aa7e8070eb9b9e0
Chaoyue Zhao <czhao@gitlab.com>
2024-12-12 18:18:36 +0000

    Use commit changes modal for upload/replace blob
    - https://gitlab.com/gitlab-org/gitlab/-/merge_requests/174093
    
    Changelog: changed

--- spec/frontend/repository/mock_data.js
+++ spec/frontend/repository/mock_data.js
@@ -191,6 +191,7 @@ export const headerAppInjected = {
   canCollaborate: true,
   canEditTree: true,
   canPushCode: true,
+  canPushToBranch: true,
   originalBranch: 'main',
   selectedBranch: 'feature/new-ui',
   newBranchPath: '/project/new-branch',
