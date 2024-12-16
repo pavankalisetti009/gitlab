017a383e8b6c6a5f9bc473a60e2db875160fde71
Shakahwath Hussain <shakahwath.hussain@accenture.com>
2024-12-16 18:31:24 +0000

    fix rubocop offenses: import_service

--- ee/app/services/ee/projects/gitlab_projects_import_service.rb
+++ ee/app/services/ee/projects/gitlab_projects_import_service.rb
@@ -11,7 +11,7 @@ module EE
         super.tap do |project|
           if project.saved? && custom_template
             custom_template.add_template_export_job(current_user: current_user,
-                                                    after_export_strategy: export_strategy(project))
+              after_export_strategy: export_strategy(project))
           end
         end
       end
