efe692f899360c4c8234be8251c12c1e37541bd5
Denys Mishunov <dmishunov@gitlab.com>
2024-12-17 16:00:40 +0000

    Updated the tests to not test internals
    Also re-named the buttons to not be bad/good
    
    As per review

---
+++ ee/app/assets/javascripts/ai/components/comment_temperature.vue
@@ -0,0 +1,206 @@
+<script>
+import { v4 as uuidv4 } from 'uuid';
+import { GlAlert, GlButton, GlSprintf, GlLink } from '@gitlab/ui';
+import aiActionMutation from 'ee/graphql_shared/mutations/ai_action.mutation.graphql';
+import aiResponseSubscription from 'ee/graphql_shared/subscriptions/ai_completion_response.subscription.graphql';
+import { convertToGraphQLId } from '~/graphql_shared/utils';
+import { scrollToElement } from '~/lib/utils/common_utils';
+import { TYPENAME_USER } from '~/graphql_shared/constants';
+import { fetchPolicies } from '~/lib/graphql';
+import { logError } from '~/lib/logger';
+import { createAlert } from '~/alert';
+import { __ } from '~/locale';
+
+const CLIENT_SUBSCRIPTION_ID = uuidv4();
+
+export default {
+  name: 'AiCommentTemperature',
+  components: {
+    GlAlert,
+    GlButton,
+    GlSprintf,
+    GlLink,
+  },
+  i18n: {
+    title: __('Proceed with caution.'),
+    warning: __(
+      'We have detected that your message might be composed against %{linkStart}our guidelines%{linkEnd}. Please review our findings below:',
+    ),
+    commentAnyway: __('Comment anyway'),
+    checkAgain: __('Updated. Check again'),
+  },
+  props: {
+    userId: {
+      type: Number,
+      required: true,
+    },
+    itemType: {
+      type: String,
+      required: true,
+    },
+    itemId: {
+      type: Number,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isMeasuring: false,
+      commentTemperatureIssues: [],
+      subscribedToTemperatureUpdates: false,
+    };
+  },
+  computed: {
+    resourceGid() {
+      return convertToGraphQLId(this.itemType, this.itemId);
+    },
+    userGid() {
+      return convertToGraphQLId(TYPENAME_USER, this.userId);
+    },
+  },
+  apollo: {
+    $subscribe: {
+      // eslint-disable-next-line @gitlab/vue-no-undef-apollo-properties
+      aiCompletionResponse: {
+        query: aiResponseSubscription,
+        fetchPolicy: fetchPolicies.NO_CACHE,
+        skip() {
+          return !this.subscribedToTemperatureUpdates; // We manually start and stop the subscription.
+        },
+        variables() {
+          return {
+            resourceId: this.resourceGid,
+            userId: this.userGid,
+            clientSubscriptionId: CLIENT_SUBSCRIPTION_ID,
+          };
+        },
+        result({ data: { aiCompletionResponse } }) {
+          if (!aiCompletionResponse) {
+            return;
+          }
+          const { content } = aiCompletionResponse;
+          if (content) {
+            const regex = /<temperature_rating>\s*(\{.*?\})\s*<\/temperature_rating>/s;
+            const match = content.match(regex);
+
+            this.isMeasuring = false;
+            if (match) {
+              try {
+                const { rating, issues } = JSON.parse(match[1]);
+                if (rating === 1) {
+                  this.save();
+                  return;
+                }
+                this.commentTemperatureIssues = issues;
+                scrollToElement(this.$el);
+              } catch (e) {
+                logError(e);
+                createAlert({
+                  message: __(
+                    'An error occured while parsing comment temperature. Please try again.',
+                  ),
+                });
+              }
+            }
+
+            this.subscribedToTemperatureUpdates = false;
+          }
+        },
+        error(e) {
+          logError(e);
+          createAlert({
+            message: __(
+              'An error occured when subscribing to the comment temperature updates. Please try again.',
+            ),
+          });
+          this.subscribedToTemperatureUpdates = false;
+        },
+      },
+    },
+  },
+  methods: {
+    resetCommentTemperature() {
+      this.commentTemperatureIssues = [];
+      this.subscribedToTemperatureUpdates = false;
+    },
+    save() {
+      this.resetCommentTemperature();
+      this.$emit('save');
+    },
+    measureCommentTemperature() {
+      this.isMeasuring = true;
+      this.$apollo
+        .mutate({
+          mutation: aiActionMutation,
+          variables: {
+            input: {
+              measureCommentTemperature: {
+                content: this.value,
+                resourceId: this.resourceGid,
+              },
+              clientSubscriptionId: CLIENT_SUBSCRIPTION_ID,
+            },
+          },
+        })
+        .then(({ data: { aiAction } }) => {
+          if (!aiAction.errors.length) {
+            this.subscribedToTemperatureUpdates = true;
+          }
+        })
+        .catch((e) => {
+          logError(e);
+          createAlert({
+            message: __('Failed to measure the comment temperature. Please try again.'),
+          });
+        });
+    },
+  },
+};
+</script>
+<template>
+  <div>
+    <gl-alert
+      v-if="commentTemperatureIssues.length"
+      variant="warning"
+      class="gl-my-5"
+      :dismissible="false"
+      :title="$options.i18n.title"
+      data-testid="comment-temperature-alert"
+    >
+      <p>
+        <gl-sprintf :message="$options.i18n.warning">
+          <template #link="{ content }">
+            <gl-link href="https://handbook.gitlab.com/handbook/communication/" target="_blank">{{
+              content
+            }}</gl-link>
+          </template>
+        </gl-sprintf>
+      </p>
+      <ul class="gl-mb-5 gl-pl-0">
+        <li v-for="(issue, index) in commentTemperatureIssues" :key="index">{{ issue }}</li>
+      </ul>
+      <template #actions>
+        <gl-button
+          data-testid="bad-button"
+          category="primary"
+          variant="danger"
+          class="gl-mr-3"
+          @click="save"
+          >{{ $options.i18n.commentAnyway }}</gl-button
+        >
+        <gl-button
+          data-testid="good-button"
+          category="primary"
+          variant="confirm"
+          :loading="isMeasuring"
+          @click="measureCommentTemperature"
+          >{{ $options.i18n.checkAgain }}</gl-button
+        >
+      </template>
+    </gl-alert>
+  </div>
+</template>
---
+++ ee/spec/frontend/ai/components/comment_temperature_spec.js
@@ -0,0 +1,204 @@
+import { shallowMount } from '@vue/test-utils';
+import Vue, { nextTick } from 'vue';
+import VueApollo from 'vue-apollo';
+import { GlAlert } from '@gitlab/ui';
+import { createMockSubscription } from 'mock-apollo-client';
+import CommentTemperature from 'ee/ai/components/comment_temperature.vue';
+import createMockApollo from 'helpers/mock_apollo_helper';
+import aiActionMutation from 'ee/graphql_shared/mutations/ai_action.mutation.graphql';
+import aiResponseSubscription from 'ee/graphql_shared/subscriptions/ai_completion_response.subscription.graphql';
+import waitForPromises from 'helpers/wait_for_promises';
+import { logError } from '~/lib/logger';
+import { createAlert } from '~/alert';
+
+Vue.use(VueApollo);
+
+jest.mock('~/lib/logger');
+jest.mock('~/alert');
+
+describe('AiCommentTemperature', () => {
+  let wrapper;
+  let aiActionMutationHandler;
+  let aiResponseSubscriptionHandler;
+  let mockSubscription;
+  const issues = ['Issue 1', 'Issue 2'];
+
+  const defaultProps = {
+    userId: 1,
+    itemType: 'Issue',
+    itemId: 123,
+    value: 'Test comment',
+  };
+
+  const createComponent = ({ props = {}, data = {} } = {}) => {
+    aiActionMutationHandler = jest.fn();
+    mockSubscription = createMockSubscription();
+    aiResponseSubscriptionHandler = jest.fn().mockReturnValue(mockSubscription);
+    const mockApollo = createMockApollo([[aiActionMutation, aiActionMutationHandler]]);
+    mockApollo.defaultClient.setRequestHandler(
+      aiResponseSubscription,
+      aiResponseSubscriptionHandler,
+    );
+
+    wrapper = shallowMount(CommentTemperature, {
+      propsData: { ...defaultProps, ...props },
+      apolloProvider: mockApollo,
+      data() {
+        return {
+          ...data,
+        };
+      },
+      stubs: {
+        GlAlert,
+      },
+    });
+  };
+
+  const findAlert = () => wrapper.findComponent(GlAlert);
+  const findIssues = () => findAlert().findAll('ul li');
+  const findCommentAnywayButton = () => wrapper.find('[data-testid="bad-button"]');
+  const findCheckAgainButton = () => wrapper.find('[data-testid="good-button"]');
+
+  describe('initial state', () => {
+    it('does not show the alert', () => {
+      createComponent();
+      expect(findAlert().exists()).toBe(false);
+    });
+  });
+
+  describe('when clicking', () => {
+    beforeEach(async () => {
+      createComponent({
+        data: {
+          commentTemperatureIssues: issues,
+        },
+      });
+      await nextTick();
+      aiActionMutationHandler.mockResolvedValue({ data: { aiAction: { errors: [] } } });
+    });
+
+    describe('the check-again button', () => {
+      it('calls the aiActionMutation with correct variables', () => {
+        findCheckAgainButton().vm.$emit('click');
+        expect(aiActionMutationHandler).toHaveBeenCalledWith({
+          input: {
+            measureCommentTemperature: {
+              content: defaultProps.value,
+              resourceId: expect.any(String),
+            },
+            clientSubscriptionId: expect.any(String),
+          },
+        });
+      });
+
+      it('triggers the loading state of the button', async () => {
+        const btn = findCheckAgainButton();
+        btn.vm.$emit('click');
+        await nextTick();
+        expect(btn.props('loading')).toBe(true);
+      });
+
+      it('subscribes to temperature updates on successful mutation', async () => {
+        findCheckAgainButton().vm.$emit('click');
+        await waitForPromises();
+        expect(aiResponseSubscriptionHandler).toHaveBeenCalled();
+      });
+
+      it('handles mutation errors', async () => {
+        aiActionMutationHandler.mockRejectedValue(new Error('Mutation error'));
+        findCheckAgainButton().vm.$emit('click');
+        await waitForPromises();
+        expect(logError).toHaveBeenCalledWith(new Error('Mutation error'));
+        expect(createAlert).toHaveBeenCalledWith({
+          message: 'Failed to measure the comment temperature. Please try again.',
+        });
+      });
+    });
+
+    describe('the comment-anyway button', () => {
+      it('does not run the mutation and saves the comment right away', () => {
+        findCommentAnywayButton().vm.$emit('click');
+        expect(aiActionMutationHandler).not.toHaveBeenCalled();
+        expect(wrapper.emitted('save')).toEqual([[]]);
+      });
+
+      it('resets the comment temperature issues', () => {
+        findCommentAnywayButton().vm.$emit('click');
+        expect(findIssues().length).toEqual(2);
+      });
+    });
+  });
+
+  describe('subscription handling', () => {
+    beforeEach(() => {
+      createComponent({
+        data: {
+          subscribedToTemperatureUpdates: true,
+        },
+      });
+    });
+
+    describe('correctly unwraps the data from the response', () => {
+      it.each([
+        `<temperature_rating>{"rating": 2, "issues": ${JSON.stringify(issues)}}</temperature_rating>`,
+        `<temperature_rating>{"rating": 2,"issues": ${JSON.stringify(issues)}}</temperature_rating>`,
+        `<temperature_rating>\n{"rating": 2, "issues": ${JSON.stringify(issues)}}\n</temperature_rating>`,
+        `<temperature_rating>\n{"rating": 2,"issues": ${JSON.stringify(issues)}}\n</temperature_rating>`,
+        `Foo bar <temperature_rating>{"rating": 2, "issues": ${JSON.stringify(issues)}}</temperature_rating> blah-blah`,
+        `Foo bar <temperature_rating>\n{"rating": 2, "issues": ${JSON.stringify(issues)}}\n</temperature_rating> blah-blah`,
+        `Foo bar\n<temperature_rating>{"rating": 2, "issues": ${JSON.stringify(issues)}}</temperature_rating>\nblah-blah`,
+        `Foo bar\n<temperature_rating>\n{"rating": 2, "issues": ${JSON.stringify(issues)}}\n</temperature_rating>\nblah-blah`,
+      ])('correctly unwraps the response data from "%s"', async (string) => {
+        mockSubscription.next({
+          data: {
+            aiCompletionResponse: {
+              content: string,
+            },
+          },
+        });
+        await nextTick();
+        expect(findAlert().exists()).toBe(true);
+        expect(findIssues().length).toBe(issues.length);
+      });
+    });
+
+    it('catches the errors when parsing JSON', async () => {
+      // In this test, we intentionally provide an invalid JSON object as an argument to JSON.parse.
+      mockSubscription.next({
+        data: {
+          aiCompletionResponse: {
+            content: `<temperature_rating>{rating: 2, issues: ${issues}}</temperature_rating>`,
+          },
+        },
+      });
+      await nextTick();
+      expect(logError).toHaveBeenCalled();
+      expect(createAlert).toHaveBeenCalledWith({
+        message: 'An error occured while parsing comment temperature. Please try again.',
+      });
+    });
+
+    it('emits `save` when rating is 1 (good) and does not render anything', async () => {
+      mockSubscription.next({
+        data: {
+          aiCompletionResponse: {
+            content: `<temperature_rating>${JSON.stringify({ rating: 1, issues: [] })}</temperature_rating>`,
+          },
+        },
+      });
+      await nextTick();
+      expect(findAlert().exists()).toBe(false);
+      expect(wrapper.emitted('save')).toEqual([[]]);
+    });
+
+    it('handles subscription errors', async () => {
+      mockSubscription.error(new Error('Subscription error'));
+      await nextTick();
+      expect(logError).toHaveBeenCalledWith(new Error('Subscription error'));
+      expect(createAlert).toHaveBeenCalledWith({
+        message:
+          'An error occured when subscribing to the comment temperature updates. Please try again.',
+      });
+    });
+  });
+});
