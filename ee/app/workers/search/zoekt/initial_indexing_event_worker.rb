9f084888bf20d6e4c0e4ddb0523f01f8e150ed64
Ravi Kumar <rkumar@gitlab.com>
2024-12-17 12:10:24 +0000

    Use the project_id_from and project_id_to in InitialIndexingEventWorker
    Use the project_id_from and project_id_to to avoid creating redundant
    indexing tasks for the projects of a namespace having multiple indices.
    
    Changelog: other
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/175844
    EE: true

--- ee/app/workers/search/zoekt/initial_indexing_event_worker.rb
+++ ee/app/workers/search/zoekt/initial_indexing_event_worker.rb
@@ -11,18 +11,64 @@ module Search
 
       # Create the pending zoekt_repositories and move the index to initializing
       def handle_event(event)
-        index = Index.find_by_id(event.data[:index_id])
+        index = find_index(event.data[:index_id])
         return if index.nil? || !index.pending?
 
-        namespace = ::Namespace.find_by_id(index.namespace_id)
+        namespace = find_namespace(index.namespace_id)
         return if namespace.nil?
 
-        ::Namespace.by_root_id(namespace.id).project_namespaces.each_batch do |project_namespaces_batch|
-          project_ids = ::Project.by_project_namespace(project_namespaces_batch.select(:id)).pluck_primary_key
-          data = project_ids.map { |p_id| { zoekt_index_id: index.id, project_id: p_id, project_identifier: p_id } }
-          Repository.insert_all(data)
-        end
+        create_repositories(index, namespace)
         index.initializing!
+      end
+
+      private
+
+      def find_index(index_id)
+        Index.find_by_id(index_id)
+      end
+
+      def find_namespace(namespace_id)
+        ::Namespace.find_by_id(namespace_id)
+      end
+
+      def create_repositories(index, namespace)
+        if index.metadata['project_id_from'].blank?
+          create_repositories_for_namespace(index, namespace)
+        else
+          create_repositories_for_project_range(index)
+        end
+      end
+
+      def create_repositories_for_namespace(index, namespace)
+        ::Namespace.by_root_id(namespace.id).project_namespaces.each_batch do |project_namespaces_batch|
+          project_ids = fetch_project_ids(project_namespaces_batch)
+          insert_repositories!(index, project_ids)
+        end
+      end
+
+      def create_repositories_for_project_range(index)
+        project_ids = determine_project_id_range(index)
+        ::Project.id_in(project_ids).each_batch do |batch|
+          project_ids = batch.pluck_primary_key
+          insert_repositories!(index, project_ids)
+        end
+      end
+
+      def fetch_project_ids(project_namespaces_batch)
+        ::Project.by_project_namespace(project_namespaces_batch.select(:id)).pluck_primary_key
+      end
+
+      def determine_project_id_range(index)
+        return (index.metadata['project_id_from']..) if index.metadata['project_id_to'].blank?
+
+        index.metadata['project_id_from']..index.metadata['project_id_to']
+      end
+
+      def insert_repositories!(index, project_ids)
+        data = project_ids.map do |p_id|
+          { zoekt_index_id: index.id, project_id: p_id, project_identifier: p_id }
+        end
+        Repository.insert_all(data)
       end
     end
   end
--- ee/spec/workers/search/zoekt/initial_indexing_event_worker_spec.rb
+++ ee/spec/workers/search/zoekt/initial_indexing_event_worker_spec.rb
@@ -15,18 +15,57 @@ RSpec.describe Search::Zoekt::InitialIndexingEventWorker, :zoekt_settings_enable
   end
 
   before do
-    [namespace, namespace.children.first].each { |n| create(:project, namespace: n) }
+    [namespace, namespace.children.first, namespace.children.first.children.first].each do |n|
+      create(:project, namespace: n)
+    end
   end
 
   it_behaves_like 'subscribes to event'
 
   it_behaves_like 'an idempotent worker' do
-    it 'creates pending zoekt_repositories for each project move the index to initializing' do
-      expect(zoekt_repositories_for_index(zoekt_index)).to be_empty
-      expect { consume_event(subscriber: described_class, event: event) }
-        .to change { zoekt_index.reload.state }.from('pending').to('initializing')
-      expect(zoekt_repositories_for_index(zoekt_index).count).to eq namespace.all_project_ids.count
-      expect(zoekt_repositories_for_index(zoekt_index).pluck(:state).uniq).to contain_exactly 'pending'
+    context 'when metadata does not have project_id_from and project_id_to' do
+      it 'creates pending zoekt_repositories for each project move the index to initializing' do
+        expect(zoekt_repositories_for_index(zoekt_index)).to be_empty
+        expect { consume_event(subscriber: described_class, event: event) }
+          .to change { zoekt_index.reload.state }.from('pending').to('initializing')
+        expect(zoekt_repositories_for_index(zoekt_index).count).to eq namespace.all_project_ids.count
+        expect(zoekt_repositories_for_index(zoekt_index).all?(&:pending?)).to be true
+      end
+    end
+
+    context 'when metadata has project_id_from and project_id_to' do
+      let(:project_id_from) { namespace.all_project_ids.first.id }
+      let(:project_id_to) { namespace.all_project_ids.second.id }
+      let(:expected_project_ids) { Project.where(id: project_id_from..project_id_to).pluck(:id) }
+
+      before do
+        zoekt_index.update!(metadata: { project_id_from: project_id_from, project_id_to: project_id_to })
+      end
+
+      it 'creates pending zoekt_repositories for project_ids of range project_id_from and project_id_to' do
+        expect(zoekt_repositories_for_index(zoekt_index)).to be_empty
+        expect { consume_event(subscriber: described_class, event: event) }
+          .to change { zoekt_index.reload.state }.from('pending').to('initializing')
+        expect(zoekt_repositories_for_index(zoekt_index).pluck(:project_id)).to match_array(expected_project_ids)
+        expect(zoekt_repositories_for_index(zoekt_index).all?(&:pending?)).to be true
+      end
+    end
+
+    context 'when metadata has only project_id_from' do
+      let(:project_id_from) { namespace.all_project_ids.second.id }
+      let(:expected_project_ids) { Project.where(id: project_id_from..).pluck(:id) }
+
+      before do
+        zoekt_index.update!(metadata: { project_id_from: project_id_from })
+      end
+
+      it 'creates pending zoekt_repositories for all project_ids from project_id_from' do
+        expect(zoekt_repositories_for_index(zoekt_index)).to be_empty
+        expect { consume_event(subscriber: described_class, event: event) }
+          .to change { zoekt_index.reload.state }.from('pending').to('initializing')
+        expect(zoekt_repositories_for_index(zoekt_index).pluck(:project_id)).to match_array(expected_project_ids)
+        expect(zoekt_repositories_for_index(zoekt_index).all?(&:pending?)).to be true
+      end
     end
 
     context 'when index is not in pending' do
