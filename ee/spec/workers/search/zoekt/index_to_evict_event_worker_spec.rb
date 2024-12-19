76d9174c509963a41205f89e61866a87d50ae847
Dmitry Gruzd <dgruzd@gitlab.com>
2024-12-19 11:33:11 +0100

    Fix index to evict event worker
    Changelog: fixed
    EE: true

--- ee/spec/workers/search/zoekt/index_to_evict_event_worker_spec.rb
+++ ee/spec/workers/search/zoekt/index_to_evict_event_worker_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe Search::Zoekt::IndexToEvictEventWorker, :zoekt_settings_enabled, feature_category: :global_search do
   let(:event) { Search::Zoekt::IndexToEvictEvent.new(data: data) }
 
-  let_it_be(:indices_in_event) { create_list(:zoekt_index, 3, watermark_level: :critical_watermark_exceeded) }
+  let_it_be(:indices_in_event) { create_list(:zoekt_index, 3, watermark_level: :healthy) }
   let_it_be(:index_ids) { indices_in_event.map(&:id) }
 
   let(:data) do
