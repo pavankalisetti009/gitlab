af38e768f23d31c881036b90fdef206698508bc8
Ravi Kumar <rkumar@gitlab.com>
2024-12-20 11:30:02 +0000

    Use the project_id_from and project_id_to in InitialIndexingEventWorker
    Use the project_id_from and project_id_to to avoid creating redundant
    indexing tasks for the projects of a namespace having multiple indices.
    
    Changelog: fixed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/176224
    EE: true

--- ee/app/workers/search/zoekt/initial_indexing_event_worker.rb
+++ ee/app/workers/search/zoekt/initial_indexing_event_worker.rb
@@ -11,18 +11,58 @@ module Search
 
       # Create the pending zoekt_repositories and move the index to initializing
       def handle_event(event)
-        index = Index.find_by_id(event.data[:index_id])
+        index = find_index(event.data[:index_id])
         return if index.nil? || !index.pending?
 
-        namespace = ::Namespace.find_by_id(index.namespace_id)
+        namespace = find_namespace(index.namespace_id)
         return if namespace.nil?
 
-        ::Namespace.by_root_id(namespace.id).project_namespaces.each_batch do |project_namespaces_batch|
-          project_ids = ::Project.by_project_namespace(project_namespaces_batch.select(:id)).pluck_primary_key
-          data = project_ids.map { |p_id| { zoekt_index_id: index.id, project_id: p_id, project_identifier: p_id } }
-          Repository.insert_all(data)
-        end
+        create_repositories(namespace: namespace, index: index)
         index.initializing!
+      end
+
+      private
+
+      def find_index(index_id)
+        Index.find_by_id(index_id)
+      end
+
+      def find_namespace(namespace_id)
+        ::Namespace.find_by_id(namespace_id)
+      end
+
+      def create_repositories(namespace:, index:)
+        if index.metadata['project_id_from'].blank?
+          create_repositories_with_scope(namespace: namespace, index: index)
+        else
+          create_repositories_for_project_range(namespace: namespace, index: index)
+        end
+      end
+
+      def create_repositories_for_project_range(namespace:, index:)
+        range_ids = determine_project_id_range(index)
+        create_repositories_with_scope(namespace: namespace, index: index) { |scope| scope.id_in(range_ids) }
+      end
+
+      def create_repositories_with_scope(namespace:, index:)
+        ::Namespace.by_root_id(namespace.id).project_namespaces.each_batch do |project_namespaces_batch|
+          scope = ::Project.by_project_namespace(project_namespaces_batch.select(:id))
+          scope = yield(scope) if block_given?
+          insert_repositories!(index: index, project_ids: scope.pluck_primary_key)
+        end
+      end
+
+      def determine_project_id_range(index)
+        return (index.metadata['project_id_from']..) if index.metadata['project_id_to'].blank?
+
+        index.metadata['project_id_from']..index.metadata['project_id_to']
+      end
+
+      def insert_repositories!(index:, project_ids:)
+        data = project_ids.map do |p_id|
+          { zoekt_index_id: index.id, project_id: p_id, project_identifier: p_id }
+        end
+        Repository.insert_all(data)
       end
     end
   end
