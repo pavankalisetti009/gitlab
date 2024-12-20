8b0d1ea6e6c7e2087f26be6187a1858e469ce4a5
Anna Vovchenko <avovchenko@gitlab.com>
2024-12-20 01:17:21 +0000

    Update Kubernetes agent registration
    - Remove agents selector;
    - Create form input for agent name;
    - Add success alert on agent creation.
    
    Changelog: changed
    EE: true

--- ee/spec/frontend/clusters_list/components/mock_data.js
+++ ee/spec/frontend/clusters_list/components/mock_data.js
@@ -3,12 +3,13 @@ import {
   treeListResponseData as treeListResponseDataCE,
   expectedAgentsList as expectedAgentsListCE,
 } from 'jest/clusters_list/components/mock_data';
+import { agent, tokens, connections } from 'jest/clusters_list/mocks/apollo';
 
 function extendAgentsWithReceptiveField() {
   const response = JSON.parse(JSON.stringify(clusterAgentsResponseCE));
   const { project } = response.data;
 
-  const addReceptiveField = (agent) => ({ ...agent, isReceptive: false });
+  const addReceptiveField = (agentNode) => ({ ...agentNode, isReceptive: false });
 
   const updateAgents = (agentList) =>
     agentList.map((item) =>
@@ -27,3 +28,48 @@ export const clusterAgentsResponse = extendAgentsWithReceptiveField();
 export const treeListResponseData = treeListResponseDataCE;
 
 export const expectedAgentsList = expectedAgentsListCE;
+
+export const createAgentResponse = {
+  data: {
+    createClusterAgent: {
+      clusterAgent: {
+        ...agent,
+        isReceptive: false,
+        connections,
+        tokens,
+      },
+      errors: [],
+    },
+  },
+};
+
+export const createAgentErrorResponse = {
+  data: {
+    createClusterAgent: {
+      clusterAgent: {
+        ...agent,
+        isReceptive: false,
+        connections,
+        tokens,
+      },
+      errors: ['could not create agent'],
+    },
+  },
+};
+
+export const getAgentResponse = {
+  data: {
+    project: {
+      __typename: 'Project',
+      id: 'project-1',
+      clusterAgents: { nodes: [{ ...agent, isReceptive: false, connections, tokens }] },
+      ciAccessAuthorizedAgents: { nodes: [] },
+      userAccessAuthorizedAgents: { nodes: [] },
+      repository: {
+        tree: {
+          trees: { nodes: [{ ...agent, path: null }] },
+        },
+      },
+    },
+  },
+};
