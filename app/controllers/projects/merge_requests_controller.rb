67b3cb5a12ab1d9211a9847cd8e2841434a7fb3b
Briley Sandlin <bsandlin@gitlab.com>
2025-01-02 17:39:01 -0600

    Remove ci_graphql_pipeline_mini_graph
    Removing feature flag and it's references
    
    Changelog: removed

--- app/controllers/projects/merge_requests_controller.rb
+++ app/controllers/projects/merge_requests_controller.rb
@@ -42,7 +42,6 @@ class Projects::MergeRequestsController < Projects::MergeRequests::ApplicationCo
     push_frontend_feature_flag(:core_security_mr_widget_counts, project)
     push_frontend_feature_flag(:mr_experience_survey, project)
     push_frontend_feature_flag(:mr_pipelines_graphql, project)
-    push_frontend_feature_flag(:ci_graphql_pipeline_mini_graph, project)
     push_frontend_feature_flag(:notifications_todos_buttons, current_user)
     push_frontend_feature_flag(:vulnerability_code_flow, project)
     push_frontend_feature_flag(:pipeline_vulnerability_code_flow, project)
