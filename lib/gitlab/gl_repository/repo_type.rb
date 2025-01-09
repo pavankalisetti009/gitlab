5c74aa6af2ada89ad816c68820942b7492946829
Gabriel Mazetto <gabriel@gitlab.com>
2025-01-09 20:27:57 -0300

    Extract `ContainerClassMismatchError` from `RepoType

--- lib/gitlab/gl_repository/repo_type.rb
+++ lib/gitlab/gl_repository/repo_type.rb
@@ -2,18 +2,6 @@
 
 module Gitlab
   class GlRepository
-    class ContainerClassMismatchError < StandardError
-      def initialize(container_class, repo_type)
-        @container_class = container_class
-        @repo_type = repo_type
-      end
-
-      def message
-        "Expected container class to be #{@repo_type.container_class} for " \
-          "repo type #{@repo_type.name}, but found #{@container_class} instead."
-      end
-    end
-
     class RepoType
       attr_reader :name,
         :access_checker_class,
