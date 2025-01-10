787f0a46432166ab9b6e0ce5fe9b8d7b2d2ba7d6
Miguel Rincon <mrincon@gitlab.com>
2025-01-10 15:37:31 +0000

    Yarn upgrade commands
    yarn upgrade @vue/compat@^3.5.13 @vue/compiler-sfc@^3.5.13
    yarn-deduplicate --strategy fewer yarn.lock
    yarn

--- ee/spec/frontend/invite_members/components/invite_modal_base_spec.js
+++ ee/spec/frontend/invite_members/components/invite_modal_base_spec.js
@@ -4,6 +4,7 @@ import VueApollo from 'vue-apollo';
 import waitForPromises from 'helpers/wait_for_promises';
 import { stubComponent } from 'helpers/stub_component';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import { stubTransition } from 'helpers/stub_transition';
 import ContentTransition from '~/invite_members/components/content_transition.vue';
 import CEInviteModalBase from '~/invite_members/components/invite_modal_base.vue';
 import EEInviteModalBase from 'ee/invite_members/components/invite_modal_base.vue';
@@ -91,6 +92,7 @@ describe('EEInviteModalBase', () => {
           template:
             '<div><slot name="modal-title"></slot><slot></slot><slot name="modal-footer"></slot></div>',
         }),
+        transition: stubTransition(),
       },
       listeners: {
         submit: (...args) => listenerSpy('submit', ...args),
