6525f1ba7459c1aae178113c8f5b88a011713f5a
Dominic Bauer <dbauer@gitlab.com>
2025-01-13 12:07:14 +0000

    Reject soft-deletion of groups containing security policy projects
    Changelog: changed
    EE: true

--- ee/app/models/group_deletion_schedule.rb
+++ ee/app/models/group_deletion_schedule.rb
@@ -5,4 +5,15 @@ class GroupDeletionSchedule < ApplicationRecord
   belongs_to :deleting_user, foreign_key: 'user_id', class_name: 'User'
 
   validates :marked_for_deletion_on, presence: true
+  validate :excludes_security_policy_projects, if: :group, on: :create
+
+  private
+
+  def excludes_security_policy_projects
+    return unless ::Feature.enabled?(:reject_security_policy_project_deletion_groups, group)
+    return unless group.licensed_feature_available?(:security_orchestration_policies)
+    return unless ::Security::OrchestrationPolicyConfiguration.for_management_project(group.all_project_ids).exists?
+
+    errors.add(:base, _('Group cannot be deleted because one of its projects is linked as Security Policy Project'))
+  end
 end
--- ee/spec/models/group_deletion_schedule_spec.rb
+++ ee/spec/models/group_deletion_schedule_spec.rb
@@ -10,5 +10,48 @@ RSpec.describe GroupDeletionSchedule do
 
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:marked_for_deletion_on) }
+
+    context 'when containing linked security policy project' do
+      subject(:deletion_schedule) { group.build_deletion_schedule.tap(&:validate) }
+
+      let_it_be(:group) { create(:group) }
+      let_it_be(:subgroup) { create(:group, parent: group) }
+      let_it_be(:policy_project) { create(:project, group: subgroup) }
+      let_it_be(:policy_configuration) do
+        create(
+          :security_orchestration_policy_configuration,
+          namespace: subgroup,
+          project: nil,
+          security_policy_management_project: policy_project
+        )
+      end
+
+      context 'with licensed feature' do
+        before do
+          stub_licensed_features(security_orchestration_policies: true)
+        end
+
+        specify do
+          expect(deletion_schedule.errors[:base])
+            .to include('Group cannot be deleted because one of its projects is linked as Security Policy Project')
+        end
+
+        context 'with feature disabled' do
+          before do
+            stub_feature_flags(reject_security_policy_project_deletion_groups: false)
+          end
+
+          specify do
+            expect(deletion_schedule.errors[:base]).to be_empty
+          end
+        end
+      end
+
+      context 'without licensed feature' do
+        specify do
+          expect(deletion_schedule.errors[:base]).to be_empty
+        end
+      end
+    end
   end
 end
