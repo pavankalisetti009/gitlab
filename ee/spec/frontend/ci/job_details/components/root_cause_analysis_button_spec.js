9fd2a7116022ab43c001a78c02439968470898b9
Serhii Yarynovskyi <syarynovskyi@gitlab.com>
2025-01-13 13:52:39 +0000

    Update 2 files
    - /ee/config/events/render_root_cause_analysis.yml
    - /ee/config/events/click_root_cause_analysis.yml

--- ee/spec/frontend/ci/job_details/components/root_cause_analysis_button_spec.js
+++ ee/spec/frontend/ci/job_details/components/root_cause_analysis_button_spec.js
@@ -2,6 +2,7 @@ import { GlButton } from '@gitlab/ui';
 import { sendDuoChatCommand } from 'ee/ai/utils';
 import RootCauseAnalysisButton from 'ee/ci/job_details/components/root_cause_analysis_button.vue';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import { useMockInternalEventsTracking } from 'helpers/tracking_internal_events_helper';
 
 jest.mock('ee/ai/utils', () => ({
   sendDuoChatCommand: jest.fn(),
@@ -74,6 +75,22 @@ describe('Root cause analysis button', () => {
         question: '/troubleshoot',
         resourceId: 'gid://gitlab/Ci::Build/11781',
       });
+    });
+  });
+
+  describe('with tracking', () => {
+    const { bindInternalEventDocument } = useMockInternalEventsTracking();
+
+    it('tracks button render and click', () => {
+      createComponent({ jobId: 123 });
+
+      const { trackEventSpy } = bindInternalEventDocument(wrapper.element);
+
+      expect(trackEventSpy).toHaveBeenCalledWith('render_root_cause_analysis', {}, undefined);
+
+      wrapper.findComponent(GlButton).vm.$emit('click');
+
+      expect(trackEventSpy).toHaveBeenCalledWith('click_root_cause_analysis', {}, undefined);
     });
   });
 });
