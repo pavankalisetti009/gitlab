315d101e1d08e8b5fe2b36c79acfef308c7be636
Brian Williams <bwilliams@gitlab.com>
2025-01-15 10:12:31 -0600

    Add `expires_at` column to `dependency_list_exports`
    To facilitate email delivery of exports, this column will allow us to
    expire the export after 7 days instead of immediately queuing up a
    deletion job.
    
    Relates to: https://gitlab.com/gitlab-org/gitlab/-/issues/513149
    
    Changelog: added

--- db/structure.sql
+++ db/structure.sql
@@ -12027,6 +12027,7 @@ CREATE TABLE dependency_list_exports (
     pipeline_id bigint,
     export_type smallint DEFAULT 0 NOT NULL,
     organization_id bigint,
+    expires_at timestamp with time zone,
     CONSTRAINT check_67a9c23e79 CHECK ((num_nonnulls(group_id, organization_id, project_id) > 0)),
     CONSTRAINT check_fff6fc9b2f CHECK ((char_length(file) <= 255))
 );
