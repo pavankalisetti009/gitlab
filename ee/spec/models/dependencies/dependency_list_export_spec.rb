3828806d593fdbcd310e791dcbbfb64cae20e4ac
Brian Williams <bwilliams@gitlab.com>
2025-01-16 13:50:07 -0600

    Add tests for export emails

--- ee/spec/models/dependencies/dependency_list_export_spec.rb
+++ ee/spec/models/dependencies/dependency_list_export_spec.rb
@@ -219,15 +219,96 @@ RSpec.describe Dependencies::DependencyListExport, feature_category: :dependency
     it { is_expected.to be_an_instance_of(Dependencies::Export::SegmentedExportService) }
   end
 
+  describe '#send_completion_email!' do
+    subject(:send_completion_email!) { export.send_completion_email! }
+
+    context 'when exportable is a project' do
+      let(:export) { create(:dependency_list_export, project: project) }
+
+      it 'passes project.group as second argument' do
+        expect(Notify).to receive(:dependency_export_completion_email).with(export, project.group)
+
+        send_completion_email!
+      end
+    end
+
+    context 'when exportable is a group' do
+      let(:export) { create(:dependency_list_export, group: group, project: nil) }
+
+      it 'passes group as second argument' do
+        expect(Notify).to receive(:dependency_export_completion_email).with(export, group)
+
+        send_completion_email!
+      end
+    end
+
+    context 'when exportable is a pipeline' do
+      let(:export) { create(:dependency_list_export, pipeline: pipeline, project: pipeline.project) }
+
+      it 'does not send email' do
+        expect(Notify).not_to receive(:dependency_export_completion_email)
+
+        send_completion_email!
+      end
+    end
+  end
+
   describe '#schedule_export_deletion' do
     let(:export) { create(:dependency_list_export) }
 
     subject(:schedule_export_deletion) { export.schedule_export_deletion }
 
-    it 'schedules the export deletion' do
-      expect(Dependencies::DestroyExportWorker).to receive(:perform_in).with(1.hour, export.id)
+    it 'sets `expires_at`', :freeze_time do
+      expect { schedule_export_deletion }.to change { export.reload.expires_at }
+        .from(nil).to(described_class::EXPIRES_AFTER.from_now)
+    end
 
-      schedule_export_deletion
+    context 'when email delivery is disabled' do
+      before do
+        allow(export).to receive(:email_delivery_enabled?).and_return(false)
+      end
+
+      it 'schedules the export deletion' do
+        expect(Dependencies::DestroyExportWorker).to receive(:perform_in).with(1.hour, export.id)
+
+        schedule_export_deletion
+      end
+    end
+  end
+
+  describe '#email_delivery_enabled?' do
+    subject(:email_delivery_enabled?) { export.email_delivery_enabled? }
+
+    context 'when exportable is a group' do
+      let(:export) { create(:dependency_list_export, group: group, project: nil) }
+
+      context 'when feature flag is enabled for group' do
+        it { is_expected.to be(true) }
+      end
+
+      context 'when feature flag is disabled for group' do
+        before do
+          stub_feature_flags(asynchronous_dependency_export_delivery_for_groups: false)
+        end
+
+        it { is_expected.to be(false) }
+      end
+    end
+
+    context 'when exportable is a project' do
+      let(:export) { create(:dependency_list_export, project: project) }
+
+      context 'when feature flag is enabled for project' do
+        it { is_expected.to be(true) }
+      end
+
+      context 'when feature flag is disabled for project' do
+        before do
+          stub_feature_flags(asynchronous_dependency_export_delivery_for_projects: false)
+        end
+
+        it { is_expected.to be(false) }
+      end
     end
   end
 
