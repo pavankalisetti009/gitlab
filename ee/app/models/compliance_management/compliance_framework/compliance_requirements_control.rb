39260bb79457f5f001445e2119dc88849814cdda
Sam Figueroa <sfigueroa@gitlab.com>
2025-01-16 18:15:15 +0100

    Add secret_token for compliance requirement controls
    - Adds external controL_type as well
    
    - Refs: https://gitlab.com/gitlab-org/gitlab/-/issues/513420
    
    Changelog: changed
    EE: true

--- ee/app/models/compliance_management/compliance_framework/compliance_requirements_control.rb
+++ ee/app/models/compliance_management/compliance_framework/compliance_requirements_control.rb
@@ -4,6 +4,12 @@ module ComplianceManagement
   module ComplianceFramework
     class ComplianceRequirementsControl < ApplicationRecord
       self.table_name = 'compliance_requirements_controls'
+      attr_encrypted :secret_token,
+        mode: :per_attribute_iv,
+        key: Settings.attr_encrypted_db_key_base_32,
+        algorithm: 'aes-256-gcm',
+        encode: false,
+        encode_iv: false
 
       MAX_COMPLIANCE_CONTROLS_PER_REQUIREMENT_COUNT = 5
       CONTROL_EXPRESSION_SCHEMA_PATH = 'ee/app/validators/json_schemas/compliance_requirements_control_expression.json'
@@ -26,17 +32,19 @@ module ComplianceManagement
       }
 
       enum control_type: {
-        internal: 0
+        internal: 0,
+        external: 1
       }
 
       validates_presence_of :name, :control_type, :namespace, :compliance_requirement
       validates_presence_of :expression, if: :internal?
+
       validates :expression, length: { maximum: 255 }
       validate :validate_internal_expression, if: :internal?
+      validate :controls_count_per_requirement
 
       validates :name, uniqueness: { scope: :compliance_requirement_id }
-
-      validate :controls_count_per_requirement
+      validates :secret_token, presence: true, if: :external?
 
       private
 
