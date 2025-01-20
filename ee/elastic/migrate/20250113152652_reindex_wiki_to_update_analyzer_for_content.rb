4ede9be11f2e0b659cbedb8408967ae0debfd884
路小鹿 <1551755561@qq.com>
2025-01-20 21:40:01 +0800

    Remove SaaS only limit

--- ee/elastic/migrate/20250113152652_reindex_wiki_to_update_analyzer_for_content.rb
+++ ee/elastic/migrate/20250113152652_reindex_wiki_to_update_analyzer_for_content.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ReindexWikiToUpdateAnalyzerForContent < Elastic::Migration
-  skip_if -> { !Gitlab::Saas.feature_available?(:advanced_search) }
-
   def migrate
     Search::Elastic::ReindexingTask.create!(targets: %w[Wiki], options: { skip_pending_migrations_check: true })
   end
