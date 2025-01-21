b8462258d998704242dba202ade83b838874a38c
Shane Maglangit <smaglangit@gitlab.com>
2025-01-21 14:23:14 +0800

    Add NOT NULL constraint on terraform_state_versions.project_id
    Changelog: other

--- db/structure.sql
+++ db/structure.sql
@@ -26272,6 +26272,9 @@ ALTER TABLE vulnerability_scanners
 ALTER TABLE p_ci_pipeline_variables
     ADD CONSTRAINT check_6e932dbabf CHECK ((project_id IS NOT NULL)) NOT VALID;
 
+ALTER TABLE terraform_state_versions
+    ADD CONSTRAINT check_84142902f6 CHECK ((project_id IS NOT NULL)) NOT VALID;
+
 ALTER TABLE sbom_occurrences_vulnerabilities
     ADD CONSTRAINT check_a02e48df9c CHECK ((project_id IS NOT NULL)) NOT VALID;
 
