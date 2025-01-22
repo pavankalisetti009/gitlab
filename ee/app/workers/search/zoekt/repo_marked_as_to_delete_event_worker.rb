eed152dcdf7ebeb005a3fcb6be162bc0c60f3493
John Mason <9717668-johnmason@users.noreply.gitlab.com>
2025-01-22 19:30:34 +0000

    Process repo_to_delete worker in a single batch
    Changelog: changed
    EE: true

--- ee/app/workers/search/zoekt/repo_marked_as_to_delete_event_worker.rb
+++ ee/app/workers/search/zoekt/repo_marked_as_to_delete_event_worker.rb
@@ -7,21 +7,24 @@ module Search
       include Search::Zoekt::EventWorker
       prepend ::Geo::SkipSecondary
 
+      deduplicate :until_executed
       idempotent!
 
       defer_on_database_health_signal :gitlab_main, [:zoekt_repositories, :zoekt_tasks], 10.minutes
 
-      def handle_event(event)
-        Search::Zoekt::Repository.where(id: event.data[:zoekt_repo_ids]).each_batch do |batch| # rubocop:disable CodeReuse/ActiveRecord -- Not relevant
-          batch.each do |repo|
-            # Note: this has checks in place that prevent creating duplicate tasks
-            Search::Zoekt::Repository.create_tasks(
-              project_id: repo.project_identifier,
-              zoekt_index: repo.zoekt_index,
-              task_type: :delete_repo,
-              perform_at: Time.current
-            )
-          end
+      BATCH_SIZE = 1000
+
+      def handle_event(_event)
+        repos = Repository.should_be_deleted.limit(BATCH_SIZE)
+
+        repos.each do |repo|
+          # Note: this has checks in place that prevent creating duplicate tasks
+          Repository.create_tasks(
+            project_id: repo.project_identifier,
+            zoekt_index: repo.zoekt_index,
+            task_type: :delete_repo,
+            perform_at: Time.current
+          )
         end
       end
     end
