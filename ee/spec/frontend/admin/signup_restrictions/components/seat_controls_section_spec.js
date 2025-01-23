3e65d3f04d279c091cbdb79c78a965bcb370686e
Angelo Gulina <agulina@gitlab.com>
2025-01-23 16:32:17 +0000

    Consolidate Seat Control moving to EE-only

--- ee/spec/frontend/admin/signup_restrictions/components/seat_controls_section_spec.js
+++ ee/spec/frontend/admin/signup_restrictions/components/seat_controls_section_spec.js
@@ -1,4 +1,5 @@
-import { GlSprintf } from '@gitlab/ui';
+import { nextTick } from 'vue';
+import { GlFormRadioGroup, GlSprintf } from '@gitlab/ui';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import SeatControlsMemberPromotionManagement from 'ee/pages/admin/application_settings/general/components/seat_controls_member_promotion_management.vue';
 import SeatControlsSection from 'ee/pages/admin/application_settings/general/components/seat_controls_section.vue';
@@ -8,17 +9,21 @@ describe('SeatControlsSection', () => {
   /** @type {import('helpers/vue_test_utils_helper').ExtendedWrapper} */
   let wrapper;
 
+  const findSeatControlSettings = () => wrapper.findComponent(GlFormRadioGroup);
   const findSeatControlsMemberPromotionManagement = () =>
     wrapper.findComponent(SeatControlsMemberPromotionManagement);
   const findUserCapInput = () => wrapper.findByTestId('user-cap-input');
   const findUserCapHiddenInput = () => wrapper.findByTestId('user-cap-input-hidden');
 
-  const mountComponent = ({ props = {}, provide = {} } = {}) => {
+  const mountComponent = ({ provide = {} } = {}) => {
     wrapper = shallowMountExtended(SeatControlsSection, {
-      propsData: { value: { newUserSignupsCap: '', seatControl: 0, ...props } },
       provide: {
         licensedUserCount: 0,
+        newUserSignupsCap: '',
         promotionManagementAvailable: false,
+        seatControl: SEAT_CONTROL.OFF,
+        // This actually refers to a licensed feature. See https://gitlab.com/gitlab-org/gitlab/-/issues/322460
+        glFeatures: { seatControl: true },
         ...provide,
       },
       stubs: { GlSprintf },
@@ -72,20 +77,127 @@ describe('SeatControlsSection', () => {
     });
   });
 
+  describe('should verify user auto approval', () => {
+    describe('when set to Seat Control > Open Access', () => {
+      beforeEach(() => {
+        mountComponent({ provide: { newUserSignupsCap: '', seatControl: SEAT_CONTROL.OFF } });
+      });
+
+      describe('when switching to Seat Control > Block Overages', () => {
+        it('emits false', async () => {
+          findSeatControlSettings().vm.$emit('change', SEAT_CONTROL.BLOCK_OVERAGES);
+
+          await nextTick();
+
+          expect(wrapper.emitted('checkUsersAutoApproval')[0]).toEqual([false]);
+        });
+      });
+
+      describe.each(['', 13])(
+        'when switching to Seat Control > User Cap (value: %d)',
+        (newUserCap) => {
+          it('emits false', async () => {
+            findUserCapInput().vm.$emit('input', newUserCap);
+            findSeatControlSettings().vm.$emit('change', SEAT_CONTROL.USER_CAP);
+
+            await nextTick();
+
+            expect(wrapper.emitted('checkUsersAutoApproval')[0]).toEqual([false]);
+          });
+        },
+      );
+    });
+
+    describe('when set to Seat Control > Block Overages', () => {
+      beforeEach(() => {
+        mountComponent({
+          provide: { newUserSignupsCap: '', seatControl: SEAT_CONTROL.BLOCK_OVERAGES },
+        });
+      });
+
+      describe('when switching to Seat Control > Open Access', () => {
+        it('emits false', async () => {
+          findSeatControlSettings().vm.$emit('change', SEAT_CONTROL.OFF);
+
+          await nextTick();
+
+          expect(wrapper.emitted('checkUsersAutoApproval')[0]).toEqual([false]);
+        });
+      });
+
+      describe.each(['', 13])(
+        'when switching to Seat Control > User Cap (value: %d)',
+        (newUserCap) => {
+          it('emits false', async () => {
+            findUserCapInput().vm.$emit('input', newUserCap);
+            findSeatControlSettings().vm.$emit('change', SEAT_CONTROL.USER_CAP);
+
+            await nextTick();
+
+            expect(wrapper.emitted('checkUsersAutoApproval')[0]).toEqual([false]);
+          });
+        },
+      );
+    });
+
+    describe('when set to Seat Control > User Cap', () => {
+      beforeEach(() => {
+        mountComponent({
+          provide: { newUserSignupsCap: 7, seatControl: SEAT_CONTROL.USER_CAP },
+        });
+      });
+
+      describe('when switching to Seat Control > Open Access', () => {
+        it('emits false', async () => {
+          findSeatControlSettings().vm.$emit('change', SEAT_CONTROL.OFF);
+
+          await nextTick();
+
+          expect(wrapper.emitted('checkUsersAutoApproval')[0]).toEqual([true]);
+        });
+      });
+
+      describe.each`
+        initialUserCap | userCap | expected
+        ${''}          | ${13}   | ${false}
+        ${13}          | ${16}   | ${true}
+        ${13}          | ${9}    | ${false}
+        ${13}          | ${''}   | ${true}
+      `(
+        'when changing the Seat Control > User Cap value to $userCap',
+        ({ initialUserCap, userCap, expected }) => {
+          beforeEach(() => {
+            mountComponent({
+              provide: { newUserSignupsCap: initialUserCap, seatControl: SEAT_CONTROL.USER_CAP },
+            });
+          });
+
+          it(`emits ${expected}`, async () => {
+            findUserCapInput().vm.$emit('input', userCap);
+
+            await nextTick();
+
+            expect(wrapper.emitted('checkUsersAutoApproval')[0]).toEqual([expected]);
+          });
+        },
+      );
+    });
+  });
+
   describe.each([
     [SEAT_CONTROL.OFF, ['true', undefined]],
     [SEAT_CONTROL.USER_CAP, [undefined, 'disabled']],
     [SEAT_CONTROL.BLOCK_OVERAGES, ['true', undefined]],
   ])('with seat control to value (%s)', (seatControl, [isDisabled, idHiddenDisabled]) => {
-    it(`sets the input value to ${isDisabled}`, () => {
-      mountComponent({ props: { seatControl } });
+    beforeEach(() => {
+      mountComponent({ provide: { seatControl } });
+    });
 
+    it(`sets the input value to ${isDisabled}`, () => {
       expect(findUserCapInput().attributes().disabled).toBe(isDisabled);
     });
 
     it(`sets the hidden input value to ${idHiddenDisabled}`, () => {
-      mountComponent({ props: { seatControl } });
-
       expect(findUserCapHiddenInput().attributes().disabled).toBe(idHiddenDisabled);
     });
   });
