a9709b6f0eda849fa3dd0f94267080135ec74327
Patrick Bajao <ebajao@gitlab.com>
2025-01-24 15:47:42 +0800

    Remove duo_code_review quick action
    We no longer need it as we already trigger Duo Code Review when
    we assign the MR to GitLab Duo.
    
    This feature is still behind `ai_review_merge_request` feature flag.

--- ee/lib/ee/gitlab/quick_actions/merge_request_actions.rb
+++ ee/lib/ee/gitlab/quick_actions/merge_request_actions.rb
@@ -21,26 +21,6 @@ module EE
           command :reassign_reviewer do |reassign_param|
             @updates[:reviewer_ids] = extract_users(reassign_param).map(&:id)
           end
-
-          desc { _('Requests a Duo Code Review') }
-          explanation { _('Requests a Duo Code Review') }
-          execution_message { _('Request for a Duo Code Review queued.') }
-          types MergeRequest
-          condition do
-            quick_action_target.ai_review_merge_request_allowed?(current_user)
-          end
-          command :duo_code_review do
-            duo_code_review_bot = ::Users::Internal.duo_code_review_bot
-
-            if quick_action_target.find_reviewer(duo_code_review_bot)
-              ::MergeRequests::RequestReviewService
-                .new(project: quick_action_target.project, current_user: current_user)
-                .execute(quick_action_target, duo_code_review_bot)
-            else
-              @updates[:reviewer_ids] ||= quick_action_target.reviewers.map(&:id)
-              @updates[:reviewer_ids] |= [duo_code_review_bot.id]
-            end
-          end
         end
       end
     end
