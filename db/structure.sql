94410f407af125bacf3762f5a892031d135a4c56
Shane Maglangit <smaglangit@gitlab.com>
2025-01-24 13:47:05 +0800

    Add NOT NULL constraint on project_relation_exports.project_id
    Add a NOT NULL constraint on the `project_id` column in the
    `project_relation_exports`, which will serve as the sharding
    key for the table. This column would help identify from which
    Organization does this item belong to.
    
    Changelog: other

--- db/structure.sql
+++ db/structure.sql
@@ -26530,6 +26530,9 @@ ALTER TABLE web_hook_logs
 ALTER TABLE vulnerability_finding_evidences
     ADD CONSTRAINT check_e8f37f70eb CHECK ((project_id IS NOT NULL)) NOT VALID;
 
+ALTER TABLE project_relation_exports
+    ADD CONSTRAINT check_f461e3537f CHECK ((project_id IS NOT NULL)) NOT VALID;
+
 ALTER TABLE approval_merge_request_rule_sources
     ADD CONSTRAINT check_f82666a937 CHECK ((project_id IS NOT NULL)) NOT VALID;
 
