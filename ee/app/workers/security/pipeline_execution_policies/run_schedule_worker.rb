6b45bc4310e20b2cbaa3e6a85deb9b840af0d134
Andy Schoenen <asoiron@gitlab.com>
2025-01-25 00:59:57 +0000

    Execute pipelines for PEP project schedules
    Changelog: changed
    EE: true

--- ee/app/workers/security/pipeline_execution_policies/run_schedule_worker.rb
+++ ee/app/workers/security/pipeline_execution_policies/run_schedule_worker.rb
@@ -10,8 +10,40 @@ module Security
       data_consistency :sticky
       feature_category :security_policy_management
 
+      PIPELINE_SOURCE = :pipeline_execution_policy_schedule
+      EVENT_KEY = 'scheduled_pipeline_execution_policy_failure'
+
       def perform(schedule_id)
-        # no-op, will be implemented as part of https://gitlab.com/gitlab-org/gitlab/-/issues/504091
+        schedule = Security::PipelineExecutionProjectSchedule.find_by_id(schedule_id) || return
+
+        return if Feature.disabled?(:scheduled_pipeline_execution_policies, schedule.project)
+
+        result = execute(schedule)
+
+        log_pipeline_creation_failure(result, schedule) if result.error?
+      end
+
+      private
+
+      def execute(schedule)
+        ci_content = schedule.ci_content.deep_stringify_keys.to_yaml
+
+        Ci::CreatePipelineService.new(
+          schedule.project,
+          schedule.project.security_policy_bot,
+          ref: schedule.project.default_branch_or_main
+        ).execute(PIPELINE_SOURCE, content: ci_content, ignore_skip_ci: true)
+      end
+
+      def log_pipeline_creation_failure(result, schedule)
+        Gitlab::AppJsonLogger.error(
+          build_structured_payload(
+            event: EVENT_KEY,
+            message: result.message,
+            reason: result.reason,
+            project_id: schedule.project_id,
+            schedule_id: schedule.id,
+            policy_id: schedule.security_policy.id))
       end
     end
   end
