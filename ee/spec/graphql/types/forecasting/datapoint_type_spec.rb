e9686da31ae79747287f6a7c1a8762d8281b9850
Pavel Shutsin <pshutsin@gitlab.com>
2025-02-07 16:41:43 +0100

    Remove buildForecast graphQL endpoint
    The feature was discontinued so we can
    safely remove the mutation and all its
    traces.
    
    Changelog: removed
    EE: true

--- ee/spec/graphql/types/forecasting/datapoint_type_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe GitlabSchema.types['ForecastDatapoint'], feature_category: :devops_reports do
-  it 'has expected fields' do
-    expect(described_class).to have_graphql_fields(*%i[datapoint value])
-  end
-end
--- ee/spec/graphql/types/forecasting/forecast_status_enum_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe GitlabSchema.types['ForecastStatus'], feature_category: :devops_reports do
-  it 'exposes all statuses' do
-    expect(described_class.values.keys).to include(*%w[READY UNAVAILABLE])
-  end
-end
