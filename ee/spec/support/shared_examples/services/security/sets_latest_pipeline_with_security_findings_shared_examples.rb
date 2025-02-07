583cb23ccbf1345818dc5c11e1ff93a6fd89c0d6
Oscar Tovar <otovar@gitlab.com>
2025-02-07 16:30:24 -0500

    Use latest pipeline per project when setting latest pipeline with
    * Update specs to consider a batch where a project has two pipelines for
      two different findings (two unique UUIDs).
    * If uniqueness is reverted to pipeline ID, the test will now catch the
      regression.
    
    See https://gitlab.com/gitlab-org/gitlab/-/issues/512128#note_2335598207
    for more details.

--- ee/spec/support/shared_examples/services/security/sets_latest_pipeline_with_security_findings_shared_examples.rb
+++ ee/spec/support/shared_examples/services/security/sets_latest_pipeline_with_security_findings_shared_examples.rb
@@ -3,6 +3,6 @@
 RSpec.shared_examples 'schedules synchronization of vulnerability statistic' do
   it 'sets the resolved vulnerabilities, latest pipeline ID and has_vulnerabilities flag' do
     expect { ingest_reports }.to change { project.reload.project_setting&.has_vulnerabilities }.to(true)
-      .and change { project.reload.vulnerability_statistic&.latest_pipeline_id }.to(pipeline.id)
+      .and change { project.reload.vulnerability_statistic&.latest_pipeline_id }.to(latest_pipeline.id)
   end
 end
