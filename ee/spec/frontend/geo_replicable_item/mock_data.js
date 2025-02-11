63c7afd55cb8f30edb915671cb8c2ff6af1a768a
Zack Cuddy <zcuddy@gitlab.com>
2025-02-11 15:40:47 +0000

    Geo Replicable Details - Hookup GraphQL API
    This change hooks up the GraphQL API to the
    new UI for Geo Replicable Details.

---
+++ ee/spec/frontend/geo_replicable_item/mock_data.js
@@ -0,0 +1,41 @@
+export const MOCK_REPLICABLE_CLASS = {
+  graphqlRegistryClass: 'gid://gitlab/Geo::MockRegistry',
+  graphqlFieldName: 'testGraphqlFieldName',
+  verificationEnabled: true,
+};
+
+export const MOCK_REPLICABLE_WITH_VERIFICATION = {
+  id: 'gid://gitlab/Geo::MockRegistry/1',
+  checksumMismatch: false,
+  createdAt: '2025-01-01',
+  forceToRedownload: false,
+  lastSyncFailure: null,
+  lastSyncedAt: '2025-01-01',
+  missingOnPrimary: false,
+  modelRecordId: 1,
+  retryAt: null,
+  retryCount: null,
+  state: 'SYNCED',
+  verificationChecksum: null,
+  verificationChecksumMismatched: false,
+  verificationFailure: null,
+  verificationRetryAt: null,
+  verificationRetryCount: null,
+  verificationStartedAt: null,
+  verificationState: 'SUCCEEDED',
+  verifiedAt: '2025-01-01',
+};
+
+export const MOCK_REPLICABLE_WITHOUT_VERIFICATION = {
+  id: 'gid://gitlab/Geo::MockRegistry/2',
+  checksumMismatch: false,
+  createdAt: '2025-01-01',
+  forceToRedownload: false,
+  lastSyncFailure: null,
+  lastSyncedAt: '2025-01-01',
+  missingOnPrimary: false,
+  modelRecordId: 2,
+  retryAt: null,
+  retryCount: null,
+  state: 'SYNCED',
+};
