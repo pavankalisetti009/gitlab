b805dd8e0d661a0bdda5980669b10c64a8575cf6
Xiao Di Guan <xdg@puxlit.net>
2025-02-12 04:48:02 +1100

    Update existing tests

--- spec/lib/banzai/filter/plantuml_filter_spec.rb
+++ spec/lib/banzai/filter/plantuml_filter_spec.rb
@@ -8,7 +8,12 @@ RSpec.describe Banzai::Filter::PlantumlFilter, feature_category: :markdown do
   it 'replaces plantuml pre tag with img tag' do
     stub_application_setting(plantuml_enabled: true, plantuml_url: "http://localhost:8080")
 
-    input = '<pre data-canonical-lang="plantuml"><code>Bob -> Sara : Hello</code></pre>'
+    # Input rendered from the following Markdown:
+    #
+    # ```plantuml
+    # Bob -> Sara : Hello
+    # ```
+    input = %(<pre data-canonical-lang="plantuml"><code>Bob -&gt; Sara : Hello\n</code></pre>)
     output = '<img class="plantuml" src="http://localhost:8080/png/U9npoazIqBLJ24uiIbImKl18pSd91m0rkGMq" data-diagram="plantuml" data-diagram-src="data:text/plain;base64,Qm9iIC0+IFNhcmEgOiBIZWxsbw==">'
     doc = filter(input)
 
@@ -18,7 +23,12 @@ RSpec.describe Banzai::Filter::PlantumlFilter, feature_category: :markdown do
   it 'allows the lang attribute on the code tag to support RST files processed by gitlab-markup gem' do
     stub_application_setting(plantuml_enabled: true, plantuml_url: "http://localhost:8080")
 
-    input = '<pre><code data-canonical-lang="plantuml">Bob -> Sara : Hello</code></pre>'
+    # Input rendered from the following Markdown:
+    #
+    # ```plantuml
+    # Bob -> Sara : Hello
+    # ```
+    input = %(<pre><code data-canonical-lang="plantuml">Bob -&gt; Sara : Hello\n</code></pre>)
     output = '<img class="plantuml" src="http://localhost:8080/png/U9npoazIqBLJ24uiIbImKl18pSd91m0rkGMq" data-diagram="plantuml" data-diagram-src="data:text/plain;base64,Qm9iIC0+IFNhcmEgOiBIZWxsbw==">'
     doc = filter(input)
 
@@ -28,21 +38,29 @@ RSpec.describe Banzai::Filter::PlantumlFilter, feature_category: :markdown do
   it 'does not replace plantuml pre tag with img tag if disabled' do
     stub_application_setting(plantuml_enabled: false)
 
-    input = '<pre data-canonical-lang="plantuml"><code>Bob -> Sara : Hello</code></pre>'
-    output = '<pre data-canonical-lang="plantuml"><code>Bob -&gt; Sara : Hello</code></pre>'
+    # Input rendered from the following Markdown:
+    #
+    # ```plantuml
+    # Bob -> Sara : Hello
+    # ```
+    input = %(<pre data-canonical-lang="plantuml"><code>Bob -&gt; Sara : Hello\n</code></pre>)
     doc = filter(input)
 
-    expect(doc.to_s).to eq output
+    expect(doc.to_s).to eq input
   end
 
   it 'does not replace plantuml pre tag with img tag if url is invalid' do
     stub_application_setting(plantuml_enabled: true, plantuml_url: "invalid")
 
-    input = '<pre data-canonical-lang="plantuml"><code>Bob -> Sara : Hello</code></pre>'
-    output = '<pre data-canonical-lang="plantuml"><code>Bob -&gt; Sara : Hello</code></pre>'
+    # Input rendered from the following Markdown:
+    #
+    # ```plantuml
+    # Bob -> Sara : Hello
+    # ```
+    input = %(<pre data-canonical-lang="plantuml"><code>Bob -&gt; Sara : Hello\n</code></pre>)
     doc = filter(input)
 
-    expect(doc.to_s).to eq output
+    expect(doc.to_s).to eq input
   end
 
   it_behaves_like 'pipeline timing check'
