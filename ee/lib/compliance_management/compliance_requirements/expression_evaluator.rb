a56f18e9eee736b60e61d32825c52f8f3689f4aa
Harsimar Sandhu <hsandhu@gitlab.com>
2025-02-13 17:41:21 +0000

    Query evaluator for expression control
    This commit adds a service to evaluate a control
    expression and stores the result in a status table
    
    EE: true
    Changelog: added

---
+++ ee/lib/compliance_management/compliance_requirements/expression_evaluator.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module ComplianceManagement
+  module ComplianceRequirements
+    class ExpressionEvaluator
+      include Gitlab::Utils::StrongMemoize
+
+      def initialize(control, project)
+        @control = control
+        @project = project
+      end
+
+      def evaluate
+        return if parsed_expression.nil?
+
+        ComparisonOperator.compare(
+          fetch_field_value,
+          parsed_expression[:value],
+          parsed_expression[:operator]
+        )
+      end
+
+      private
+
+      attr_reader :control, :project
+
+      def parsed_expression
+        control.expression_as_hash(symbolize_names: true)
+      end
+      strong_memoize_attr :parsed_expression
+
+      def fetch_field_value
+        ProjectFields.map_field(project, parsed_expression[:field])
+      end
+    end
+  end
+end
