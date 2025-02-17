144b070b5c5975d6a8ce83700fdf7eeb748056f4
Peter Hegman <phegman@gitlab.com>
2025-02-17 09:14:18 +0000

    Add `Shared groups` tab to group overview
    Allows users to see what groups the current group has been invited to.
    
    Changelog: added

--- app/assets/javascripts/groups/components/empty_states/shared_groups_empty_state.vue
+++ app/assets/javascripts/groups/components/empty_states/shared_groups_empty_state.vue
@@ -1,29 +1,30 @@
 <script>
 import { GlEmptyState, GlSprintf } from '@gitlab/ui';
+import groupsEmptyStateIllustration from '@gitlab/svgs/dist/illustrations/empty-state/empty-groups-md.svg?url';
 import HelpPageLink from '~/vue_shared/components/help_page_link/help_page_link.vue';
 
 import { s__ } from '~/locale';
 
 export default {
-  components: { GlEmptyState, HelpPageLink, GlSprintf },
+  components: { GlEmptyState, GlSprintf, HelpPageLink },
+  groupsEmptyStateIllustration,
   i18n: {
-    title: s__('GroupsEmptyState|This group has not been invited to any other projects.'),
+    title: s__('GroupsEmptyState|This group has not been invited to any other groups.'),
     description: s__(
-      'GroupsEmptyState|Projects this group has been %{linkStart}invited to%{linkEnd} will appear here.',
+      'GroupsEmptyState|Other groups this group has been %{linkStart}invited to%{linkEnd} will appear here.',
     ),
   },
-  inject: ['emptyProjectsIllustration'],
 };
 </script>
 
 <template>
-  <gl-empty-state :title="$options.i18n.title" :svg-path="emptyProjectsIllustration">
+  <gl-empty-state :title="$options.i18n.title" :svg-path="$options.groupsEmptyStateIllustration">
     <template #description>
       <gl-sprintf :message="$options.i18n.description">
         <template #link="{ content }">
           <help-page-link
             href="user/project/members/sharing_projects_groups"
-            anchor="invite-a-group-to-a-project"
+            anchor="invite-a-group-to-a-group"
             >{{ content }}</help-page-link
           >
         </template>
