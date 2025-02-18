048f5998001dcda45b2629a5a800f63d8481ebd2
Angelo Gulina <agulina@gitlab.com>
2025-02-18 16:51:17 +0100

    Update seats related notifications
    When Restricted Access is active, seats related notifications copy
    and interaction should change accordingly.
    
    Changelog: changed
    EE: true

--- ee/app/helpers/seat_count_alert_helper.rb
+++ ee/app/helpers/seat_count_alert_helper.rb
@@ -16,4 +16,20 @@ module SeatCountAlertHelper
   def namespace
     @seat_count_data[:namespace]
   end
+
+  def seat_count_text
+    if namespace.block_seat_overages?
+      return _('Once you reach the number of seats in your subscription, you can no longer ' \
+        'invite or add users to the namespace.')
+    end
+
+    _('Even if you reach the number of seats in your subscription, you can continue to add users, ' \
+      'and GitLab will bill you for the overage.')
+  end
+
+  def seat_count_help_page_link
+    return help_page_path('user/group/manage.md', anchor: 'turn-on-restricted-access') if namespace.block_seat_overages?
+
+    help_page_path('subscriptions/quarterly_reconciliation.md')
+  end
 end
