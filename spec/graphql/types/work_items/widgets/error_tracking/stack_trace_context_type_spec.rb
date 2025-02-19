5d848cf139d116004f57e599925adcb9f264b46a
Stefanos Xanthopoulos <sxanthopoulos@gitlab.com>
2025-02-19 11:32:24 +0000

    Expose error tracking stack trace though GraphQL
    Add error stack trace details for error tracking work item widget
    
    Changelog: added

--- spec/graphql/types/work_items/widgets/error_tracking/stack_trace_context_type_spec.rb
+++ spec/graphql/types/work_items/widgets/error_tracking/stack_trace_context_type_spec.rb
@@ -2,9 +2,9 @@
 
 require 'spec_helper'
 
-RSpec.describe Types::WorkItems::Widgets::ErrorTrackingType, feature_category: :team_planning do
+RSpec.describe Types::WorkItems::Widgets::ErrorTracking::StackTraceContextType, feature_category: :team_planning do
   it 'exposes the expected fields' do
-    expected_fields = %i[type identifier stack_trace status]
+    expected_fields = %i[line_number line]
 
     expect(described_class).to have_graphql_fields(*expected_fields)
   end
