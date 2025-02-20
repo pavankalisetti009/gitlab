010dc11145cd18c3a4e9dc27503a57cfabefe72a
Madelein van Niekerk <mvanniekerk@gitlab.com>
2025-02-20 11:09:25 +0000

    ActiveContext Find partition by serializing collection
    Changelog: added

--- ee/app/models/ai/active_context/collection.rb
+++ ee/app/models/ai/active_context/collection.rb
@@ -8,6 +8,10 @@ module Ai
       validates :name, presence: true, length: { maximum: 255 }
       validates :metadata, json_schema: { filename: 'ai_active_context_collection_metadata' }
       validates :number_of_partitions, presence: true, numericality: { greater_than_or_equal_to: 1, only_integer: true }
+
+      def partition_for(routing_value)
+        ::ActiveContext::Hash.consistent_hash(number_of_partitions, routing_value)
+      end
     end
   end
 end
