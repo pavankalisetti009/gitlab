f75414f0787901a7ca9565f8bf36d6ee34c7aaaa
Coung Ngo <cngo@gitlab.com>
2025-02-23 12:55:30 +0000

    Add comment summarization to work items
    Changelog: added
    EE: true

--- ee/app/assets/javascripts/notes/components/note_actions/ai_summarize_notes.vue
+++ ee/app/assets/javascripts/notes/components/note_actions/ai_summarize_notes.vue
@@ -1,41 +1,31 @@
 <script>
 import { GlButton, GlTooltipDirective } from '@gitlab/ui';
-import { s__, __ } from '~/locale';
-import { createAlert } from '~/alert';
-import glFeatureFlagsMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 import { sendDuoChatCommand } from 'ee/ai/utils';
 import { BV_HIDE_TOOLTIP } from '~/lib/utils/constants';
 
 export default {
+  name: 'AiSummarizeNotes',
   components: {
     GlButton,
   },
   directives: {
     GlTooltip: GlTooltipDirective,
   },
-  mixins: [glFeatureFlagsMixin()],
-  inject: ['summarizeClientSubscriptionId'],
   props: {
-    resourceGlobalId: {
-      type: String,
-      required: true,
-    },
     loading: {
       type: Boolean,
       default: false,
       required: false,
     },
-  },
-  data() {
-    return {
-      errorAlert: null,
-      aiCompletionResponse: {},
-    };
-  },
-  destroyed() {
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-    }
+    resourceGlobalId: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: 'medium',
+    },
   },
   methods: {
     onClick() {
@@ -55,19 +45,6 @@ export default {
         this.$root.$emit(BV_HIDE_TOOLTIP);
       });
     },
-    handleError(error) {
-      this.hideTooltips();
-      const alertOptions = error ? { captureError: true, error } : {};
-      this.errorAlert = createAlert({
-        message: error ? error.message : __('Something went wrong'),
-        ...alertOptions,
-      });
-      this.$parent.$emit('set-ai-loading', false);
-    },
-  },
-  i18n: {
-    button: s__('AISummary|View summary'),
-    tooltip: s__('AISummary|Generates a summary of this issue'),
   },
 };
 </script>
@@ -78,11 +55,12 @@ export default {
     icon="duo-chat"
     :disabled="loading"
     :loading="loading"
-    :title="$options.i18n.tooltip"
-    :aria-label="$options.i18n.tooltip"
+    :size="size"
+    :title="s__('AISummary|Generates a summary of this issue')"
+    :aria-label="s__('AISummary|Generates a summary of this issue')"
     @click="onClick"
     @mouseout="hideTooltips"
   >
-    {{ $options.i18n.button }}
+    {{ s__('AISummary|View summary') }}
   </gl-button>
 </template>
