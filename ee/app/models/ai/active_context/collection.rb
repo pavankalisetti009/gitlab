b02468dab2f8df0e8a08351bd184752b289d6d5b
Madelein van Niekerk <mvanniekerk@gitlab.com>
2025-02-24 12:58:17 +0000

    Update active context Collections table
    Adding FK for connection and unique index on name
    
    Changelog: changed

--- ee/app/models/ai/active_context/collection.rb
+++ ee/app/models/ai/active_context/collection.rb
@@ -5,9 +5,13 @@ module Ai
     class Collection < ApplicationRecord
       self.table_name = :ai_active_context_collections
 
+      belongs_to :connection, class_name: 'Ai::ActiveContext::Connection'
+
       validates :name, presence: true, length: { maximum: 255 }
+      validates :name, uniqueness: { scope: :connection_id }
       validates :metadata, json_schema: { filename: 'ai_active_context_collection_metadata' }
       validates :number_of_partitions, presence: true, numericality: { greater_than_or_equal_to: 1, only_integer: true }
+      validates :connection_id, presence: true
 
       def partition_for(routing_value)
         ::ActiveContext::Hash.consistent_hash(number_of_partitions, routing_value)
