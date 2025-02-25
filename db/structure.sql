00b2cc0f4e3c24ba40dc42fb72ebd63424445982
Shane Maglangit <smaglangit@gitlab.com>
2025-02-25 12:39:08 +0800

    Add NOT NULL constraint on evidences.project_id
    Changelog: other

--- db/structure.sql
+++ db/structure.sql
@@ -13615,7 +13615,8 @@ CREATE TABLE evidences (
     updated_at timestamp with time zone NOT NULL,
     summary_sha bytea,
     summary jsonb DEFAULT '{}'::jsonb NOT NULL,
-    project_id bigint
+    project_id bigint,
+    CONSTRAINT check_32e833c325 CHECK ((project_id IS NOT NULL))
 );
 
 CREATE SEQUENCE evidences_id_seq
