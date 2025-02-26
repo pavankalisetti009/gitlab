aeed634595dc6683c1d978f144954d7f388f311a
Smriti Garg <sgarg@gitlab.com>
2025-02-26 19:04:21 +0000

    Removed worker from scheduling and commented out the code for calling service

--- ee/app/policies/compliance_management/pipl_user_policy.rb
+++ ee/app/policies/compliance_management/pipl_user_policy.rb
@@ -6,9 +6,17 @@ module ComplianceManagement
       Feature.enabled?(:enforce_pipl_compliance, @subject)
     end
 
+    condition(:disable_delete_pipl_user) do
+      Feature.disabled?(:delete_pipl_non_compliant_users, @subject)
+    end
+
     rule { admin & enforce_pipl_compliance }.policy do
       enable :block_pipl_user
       enable :delete_pipl_user
+    end
+
+    rule { disable_delete_pipl_user }.policy do
+      prevent :delete_pipl_user
     end
   end
 end
