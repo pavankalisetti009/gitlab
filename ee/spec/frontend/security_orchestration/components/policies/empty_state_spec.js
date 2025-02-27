ddea06abe00be73318ac19ce49005c901aa7d220
Alexander Turinske <aturinske@gitlab.com>
2025-02-26 19:42:46 -0800

    Simplify empty page tests
    - make them more maintainable
    
    Changelog: changed
    
    EE: true

--- ee/spec/frontend/security_orchestration/components/policies/empty_state_spec.js
+++ ee/spec/frontend/security_orchestration/components/policies/empty_state_spec.js
@@ -1,5 +1,4 @@
 import { GlSprintf } from '@gitlab/ui';
-import { nextTick } from 'vue';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import { NAMESPACE_TYPES } from 'ee/security_orchestration/constants';
 import EmptyState from 'ee/security_orchestration/components/policies/empty_state.vue';
@@ -10,7 +9,7 @@ describe('EmptyState component', () => {
   const findEmptyFilterState = () => wrapper.findByTestId('empty-filter-state');
   const findEmptyListState = () => wrapper.findByTestId('empty-list-state');
 
-  const factory = ({
+  const createComponent = ({
     disableScanPolicyUpdate = false,
     hasExistingPolicies = false,
     hasPolicyProject = false,
@@ -33,28 +32,25 @@ describe('EmptyState component', () => {
   };
 
   it.each`
-    title                                        | findComponent           | state    | factoryFn
-    ${'does not display the empty filter state'} | ${findEmptyFilterState} | ${false} | ${factory}
-    ${'does display the empty list state'}       | ${findEmptyListState}   | ${true}  | ${factory}
-    ${'does display the empty filter state'}     | ${findEmptyFilterState} | ${true}  | ${() => factory({ hasExistingPolicies: true })}
-    ${'does not display the empty list state'}   | ${findEmptyListState}   | ${false} | ${() => factory({ hasExistingPolicies: true })}
-  `('$title', async ({ factoryFn, findComponent, state }) => {
-    factoryFn();
-    await nextTick();
+    title                                        | findComponent           | state    | hasExistingPolicies
+    ${'does not display the empty filter state'} | ${findEmptyFilterState} | ${false} | ${false}
+    ${'does display the empty list state'}       | ${findEmptyListState}   | ${true}  | ${false}
+    ${'does display the empty filter state'}     | ${findEmptyFilterState} | ${true}  | ${true}
+    ${'does not display the empty list state'}   | ${findEmptyListState}   | ${false} | ${true}
+  `('$title', ({ hasExistingPolicies, findComponent, state }) => {
+    createComponent({ hasExistingPolicies });
     expect(findComponent().exists()).toBe(state);
   });
 
-  it('displays the correct empty list state when there is not a policy project', async () => {
-    factory();
-    await nextTick();
+  it('displays the correct empty list state when there is not a policy project', () => {
+    createComponent();
     expect(findEmptyListState().text()).toContain(
       'This project is not linked to a security policy project. Either link it to an existing project or create a new policy, which will create a new project that you can use as a security policy project. For help, see',
     );
   });
 
-  it('displays the correct empty list state when there is a policy project', async () => {
-    factory({ hasPolicyProject: true });
-    await nextTick();
+  it('displays the correct empty list state when there is a policy project', () => {
+    createComponent({ hasPolicyProject: true });
     expect(findEmptyListState().text()).toContain(
       'This project does not contain any security policies.',
     );
@@ -64,21 +60,18 @@ describe('EmptyState component', () => {
     title                                                   | namespaceType
     ${'does display the correct description for a project'} | ${NAMESPACE_TYPES.PROJECT}
     ${'does display the correct description for a group'}   | ${NAMESPACE_TYPES.GROUP}
-  `('$title', async ({ namespaceType }) => {
-    factory({ namespaceType });
-    await nextTick();
+  `('$title', ({ namespaceType }) => {
+    createComponent({ namespaceType });
     expect(findEmptyListState().text()).toContain(namespaceType);
   });
 
-  it('does display the "New policy" button for non-owners', async () => {
-    factory();
-    await nextTick();
+  it('does display the "New policy" button for non-owners', () => {
+    createComponent();
     expect(findEmptyListState().attributes('primarybuttontext')).toBe('New policy');
   });
 
-  it('does not display the "New policy" button for non-owners', async () => {
-    factory({ disableScanPolicyUpdate: true });
-    await nextTick();
+  it('does not display the "New policy" button for non-owners', () => {
+    createComponent({ disableScanPolicyUpdate: true });
     expect(findEmptyListState().attributes('primarybuttontext')).toBe('');
   });
 });
