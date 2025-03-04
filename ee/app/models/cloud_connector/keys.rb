54a43ecebdea34e1667589302f9074212bace19a
Matthias Käppler <mkaeppler@gitlab.com>
2025-03-04 12:27:18 +0000

    Cloud Connector: add key rotation rake tasks
    This adds new rake tasks as well as key model
    functions supporting these tasks to perform
    Cloud Connector key rotations.
    
    Changelog: added
    EE: true

--- ee/app/models/cloud_connector/keys.rb
+++ ee/app/models/cloud_connector/keys.rb
@@ -8,21 +8,59 @@ module CloudConnector
     validates :secret_key, rsa_key: true, allow_nil: true
 
     scope :valid, -> { where.not(secret_key: nil) }
+    scope :ordered_by_date, -> { valid.order(created_at: :asc) }
 
     class << self
       def current
-        valid.order(created_at: :asc).first
+        ordered_by_date.first
       end
 
       def current_as_jwk
-        current&.secret_key&.then do |key_data|
-          ::JWT::JWK.new(OpenSSL::PKey::RSA.new(key_data), kid_generator: ::JWT::JWK::Thumbprint)
-        end
+        current&.secret_key&.then { |keydata| pem_to_jwk(keydata) }
       end
 
       def all_as_pem
         valid.map(&:secret_key)
       end
+
+      def create_new_key!
+        create!(secret_key: new_private_key.to_pem)
+      end
+
+      def rotate!
+        keys = ordered_by_date
+        key_count = keys.count
+
+        raise "Key rotation requires exactly 2 keys, found #{key_count}" if key_count != 2
+
+        current_key, next_key = *keys
+
+        transaction do
+          current_key_data = current_key.secret_key
+          current_key.update!(secret_key: next_key.secret_key)
+          next_key.update!(secret_key: current_key_data)
+        end
+      end
+
+      def trim!
+        raise 'Refusing to remove single key, as it is in use' if valid.count == 1
+
+        ordered_by_date.last&.destroy
+      end
+
+      private
+
+      def new_private_key
+        OpenSSL::PKey::RSA.new(2048)
+      end
+
+      def pem_to_jwk(key_data)
+        ::JWT::JWK.new(OpenSSL::PKey::RSA.new(key_data), kid_generator: ::JWT::JWK::Thumbprint)
+      end
+    end
+
+    def truncated_pem
+      secret_key&.truncate(90)
     end
   end
 end
--- ee/spec/models/cloud_connector/keys_spec.rb
+++ ee/spec/models/cloud_connector/keys_spec.rb
@@ -44,13 +44,22 @@ RSpec.describe CloudConnector::Keys, :models, feature_category: :cloud_connector
   end
 
   describe '.valid' do
-    subject(:jwks) { described_class.valid }
+    subject(:keys) { described_class.valid }
 
     include_examples 'serving valid keys'
   end
 
+  describe '.ordered_by_date' do
+    subject(:keys) { described_class.ordered_by_date }
+
+    let_it_be(:first_key) { create(:cloud_connector_keys, created_at: Time.current - 1.minute) }
+    let_it_be(:second_key) { create(:cloud_connector_keys) }
+
+    it { is_expected.to eq([first_key, second_key]) }
+  end
+
   describe '.all_as_pem' do
-    subject(:jwks) { described_class.all_as_pem }
+    subject(:keys) { described_class.all_as_pem }
 
     include_examples 'serving valid keys'
 
@@ -130,6 +139,72 @@ RSpec.describe CloudConnector::Keys, :models, feature_category: :cloud_connector
           expect(queries.count).to eq(1)
         end
       end
+    end
+  end
+
+  describe '.create_new_key!' do
+    it 'creates a new valid key' do
+      expect { described_class.create_new_key! }.to change { described_class.valid.count }.from(0).to(1)
+    end
+  end
+
+  describe '.rotate!' do
+    subject(:rotate) { described_class.rotate! }
+
+    let_it_be(:first_key) { create(:cloud_connector_keys, created_at: Time.current - 1.minute) }
+
+    context 'when fewer than 2 keys exist' do
+      it { expect { rotate }.to raise_error(StandardError, 'Key rotation requires exactly 2 keys, found 1') }
+    end
+
+    context 'when more than 2 keys exist' do
+      before do
+        create_list(:cloud_connector_keys, 2)
+      end
+
+      it { expect { rotate }.to raise_error(StandardError, 'Key rotation requires exactly 2 keys, found 3') }
+    end
+
+    context 'when exactly 2 keys exist' do
+      let_it_be(:second_key) { create(:cloud_connector_keys) }
+
+      it 'swaps the keys' do
+        expect { rotate }.to change { described_class.valid.pluck(:secret_key) }
+          .from([first_key.secret_key, second_key.secret_key])
+          .to([second_key.secret_key, first_key.secret_key])
+      end
+    end
+  end
+
+  describe '.trim!' do
+    subject(:trimmed_key) { described_class.trim! }
+
+    context 'with no keys' do
+      it { is_expected.to be_nil }
+    end
+
+    context 'with a single key' do
+      let_it_be(:key) { create(:cloud_connector_keys) }
+
+      it { expect { trimmed_key }.to raise_error(StandardError, 'Refusing to remove single key, as it is in use') }
+    end
+
+    context 'with more than 1 key' do
+      let_it_be(:first_key) { create(:cloud_connector_keys, created_at: Time.current - 1.minute) }
+      let_it_be(:second_key) { create(:cloud_connector_keys) }
+
+      it 'removes the newest key' do
+        expect(trimmed_key).to eq(second_key)
+        expect(described_class.all).to contain_exactly(first_key)
+      end
+    end
+  end
+
+  describe '#truncated_pem' do
+    subject(:short_pem) { create(:cloud_connector_keys).truncated_pem }
+
+    it 'truncates the PEM string to 90 characters' do
+      expect(short_pem.length).to eq(90)
     end
   end
 end
