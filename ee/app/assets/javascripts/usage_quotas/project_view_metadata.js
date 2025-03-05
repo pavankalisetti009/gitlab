9741b421d910efcd0702589c9b70439f3fa5250b
Kos Palchyk <kpalchyk@gitlab.com>
2025-03-05 15:59:05 +0200

    Split Usage Quotas mounting functions

--- ee/app/assets/javascripts/usage_quotas/project_view_metadata.js
+++ ee/app/assets/javascripts/usage_quotas/project_view_metadata.js
@@ -1,12 +1,15 @@
 import { PROJECT_VIEW_TYPE } from '~/usage_quotas/constants';
 import { getStorageTabMetadata } from '~/usage_quotas/storage/tab_metadata';
+import { mountUsageQuotasApp } from '~/usage_quotas/utils';
 import { getTransferTabMetadata } from './transfer/tab_metadata';
 import { getObservabilityTabMetadata } from './observability/tab_metadata';
 import { getPagesTabMetadata } from './pages/tab_metadata';
 
-export const usageQuotasTabsMetadata = [
+const usageQuotasTabsMetadata = [
   getStorageTabMetadata({ viewType: PROJECT_VIEW_TYPE }),
   getTransferTabMetadata({ viewType: PROJECT_VIEW_TYPE }),
   getObservabilityTabMetadata(),
   getPagesTabMetadata({ viewType: PROJECT_VIEW_TYPE }),
 ].filter(Boolean);
+
+export default () => mountUsageQuotasApp(usageQuotasTabsMetadata);
