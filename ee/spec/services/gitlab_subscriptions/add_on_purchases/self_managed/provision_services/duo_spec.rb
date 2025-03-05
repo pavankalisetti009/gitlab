445de297eb3a4b2d226f60d6ed3354d8515c3d0c
Lukas Wanko <lwanko@gitlab.com>
2025-03-05 12:13:36 +0100

    Add Duo with Amazon Q support to SM/Dedicated provisioning
    Changelog: added
    EE: true

--- ee/spec/services/gitlab_subscriptions/add_on_purchases/self_managed/provision_services/duo_spec.rb
+++ ee/spec/services/gitlab_subscriptions/add_on_purchases/self_managed/provision_services/duo_spec.rb
@@ -9,19 +9,21 @@ RSpec.describe GitlabSubscriptions::AddOnPurchases::SelfManaged::ProvisionServic
 
     let_it_be(:add_on_duo_pro) { create(:gitlab_subscription_add_on, :code_suggestions) }
     let_it_be(:add_on_duo_enterprise) { create(:gitlab_subscription_add_on, :duo_enterprise) }
+    let_it_be(:add_on_duo_amazon_q) { create(:gitlab_subscription_add_on, :duo_amazon_q) }
 
     let_it_be(:organization) { create(:organization) }
+    let_it_be(:started_at) { Date.current }
 
-    let(:quantity_duo_pro) { 0 }
-    let(:quantity_duo_enterprise) { 0 }
+    let(:add_ons) { [] }
     let(:namespace) { nil }
+    let(:trial) { false }
+    let(:quantity) { 1 }
 
-    let(:started_at) { Date.current }
     let!(:current_license) do
       create_current_license(
         cloud_licensing_enabled: true,
         restrictions: {
-          add_on_products: add_on_products(started_at: started_at),
+          add_on_products: add_on_products(add_ons: add_ons, started_at: started_at, quantity: quantity, trial: trial),
           subscription_name: 'A-S00000001'
         }
       )
@@ -43,30 +45,20 @@ RSpec.describe GitlabSubscriptions::AddOnPurchases::SelfManaged::ProvisionServic
       end
     end
 
-    context 'with Duo Pro' do
-      let(:quantity_duo_pro) { 1 }
-
+    shared_examples 'provision duo add-on purchase' do
       context 'with a trial' do
-        let!(:current_license) do
-          create_current_license(
-            cloud_licensing_enabled: true,
-            restrictions: {
-              add_on_products: add_on_products(started_at: started_at, trial: true),
-              subscription_name: 'A-S00000001'
-            }
-          )
-        end
+        let(:trial) { true }
 
         it 'creates a new Duo Pro add-on purchase' do
           expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(1)
 
           expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
           expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
-            subscription_add_on_id: add_on_duo_pro.id,
-            quantity: quantity_duo_pro,
+            subscription_add_on_id: expected_add_on.id,
+            quantity: 1,
             started_at: started_at,
             expires_on: started_at + 1.year,
-            purchase_xid: 'duo_pro_purchase',
+            purchase_xid: '123456789',
             trial: true
           )
         end
@@ -77,184 +69,156 @@ RSpec.describe GitlabSubscriptions::AddOnPurchases::SelfManaged::ProvisionServic
 
         expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
         expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
-          subscription_add_on_id: add_on_duo_pro.id,
-          quantity: quantity_duo_pro,
+          subscription_add_on_id: expected_add_on.id,
+          quantity: 1,
           started_at: started_at,
           expires_on: started_at + 1.year,
-          purchase_xid: 'duo_pro_purchase',
+          purchase_xid: '123456789',
           trial: false
         )
       end
+    end
+
+    shared_examples 'seat increase provision' do
+      it 'updates quantity of existing add-on purchase' do
+        expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(0)
+
+        expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
+        expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
+          subscription_add_on_id: add_on.id,
+          quantity: quantity,
+          started_at: started_at,
+          expires_on: started_at + 1.year,
+          purchase_xid: '123456789',
+          trial: trial
+        )
+      end
+    end
+
+    shared_examples 'seat increase provision for trials and non-trials' do
+      let!(:add_on_purchase_duo_enterprise) do
+        create(
+          :gitlab_subscription_add_on_purchase,
+          add_on: add_on,
+          quantity: 1,
+          namespace: namespace
+        )
+      end
+
+      it_behaves_like 'seat increase provision'
+
+      context 'with a trial' do
+        let(:trial) { true }
+
+        it_behaves_like 'seat increase provision'
+      end
+    end
+
+    shared_examples 'update existing Duo add-on purchase' do
+      let!(:add_on_purchase_duo_enterprise) do
+        create(
+          :gitlab_subscription_add_on_purchase,
+          add_on: initial_add_on,
+          quantity: 1,
+          namespace: namespace
+        )
+      end
+
+      it 'updates the add-on' do
+        expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(0)
+
+        expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
+        expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
+          subscription_add_on_id: expected_add_on.id,
+          quantity: 1,
+          started_at: started_at,
+          expires_on: started_at + 1.year,
+          purchase_xid: '123456789',
+          trial: false
+        )
+      end
+    end
+
+    context 'with Duo Pro' do
+      let(:add_ons) { %i[duo_pro] }
+      let(:expected_add_on) { add_on_duo_pro }
+
+      it_behaves_like 'provision duo add-on purchase'
     end
 
     context 'with existing Duo Pro and seat count increase' do
-      let!(:add_on_purchase_duo_pro) do
-        create(
-          :gitlab_subscription_add_on_purchase,
-          add_on: add_on_duo_pro,
-          quantity: 1,
-          namespace: namespace
-        )
-      end
+      let(:add_on) { add_on_duo_pro }
+      let(:add_ons) { %i[duo_pro] }
+      let(:quantity) { 2 }
 
-      let(:quantity_duo_pro) { 2 }
-
-      context 'with a trial' do
-        let!(:current_license) do
-          create_current_license(
-            cloud_licensing_enabled: true,
-            restrictions: {
-              add_on_products: add_on_products(started_at: started_at, trial: true),
-              subscription_name: 'A-S00000001'
-            }
-          )
-        end
-
-        it 'updates quantity of existing add-on purchase' do
-          expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(0)
-
-          expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
-          expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
-            subscription_add_on_id: add_on_duo_pro.id,
-            quantity: quantity_duo_pro,
-            started_at: started_at,
-            expires_on: started_at + 1.year,
-            purchase_xid: 'duo_pro_purchase',
-            trial: true
-          )
-        end
-      end
-
-      it 'updates quantity of existing add-on purchase' do
-        expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(0)
-
-        expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
-        expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
-          subscription_add_on_id: add_on_duo_pro.id,
-          quantity: quantity_duo_pro,
-          started_at: started_at,
-          expires_on: started_at + 1.year,
-          purchase_xid: 'duo_pro_purchase',
-          trial: false
-        )
-      end
+      it_behaves_like 'seat increase provision for trials and non-trials'
     end
 
     context 'with existing Duo Pro and additional purchase of Duo Enterprise' do
-      let!(:add_on_purchase_duo_pro) do
-        create(
-          :gitlab_subscription_add_on_purchase,
-          add_on: add_on_duo_pro,
-          quantity: quantity_duo_pro,
-          namespace: namespace
-        )
-      end
+      let(:add_ons) { %i[duo_pro duo_enterprise] }
+      let(:initial_add_on) { add_on_duo_pro }
+      let(:expected_add_on) { add_on_duo_enterprise }
 
-      let(:quantity_duo_enterprise) { 1 }
-      let(:quantity_duo_pro) { 1 }
-
-      it 'upgrade to Duo Enterprise' do
-        expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(0)
-
-        expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
-        expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
-          subscription_add_on_id: add_on_duo_enterprise.id,
-          quantity: quantity_duo_enterprise,
-          started_at: started_at,
-          expires_on: started_at + 1.year,
-          purchase_xid: 'duo_enterprise_purchase',
-          trial: false
-        )
-      end
+      it_behaves_like 'update existing Duo add-on purchase'
     end
 
     context 'with Duo Enterprise' do
-      let(:quantity_duo_enterprise) { 1 }
+      let(:add_ons) { %i[duo_enterprise] }
+      let(:expected_add_on) { add_on_duo_enterprise }
 
-      it 'creates a new Duo Enterprise add-on purchase' do
-        expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(1)
-
-        expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
-        expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
-          subscription_add_on_id: add_on_duo_enterprise.id,
-          quantity: quantity_duo_enterprise,
-          started_at: started_at,
-          expires_on: started_at + 1.year,
-          purchase_xid: 'duo_enterprise_purchase',
-          trial: false
-        )
-      end
+      it_behaves_like 'provision duo add-on purchase'
     end
 
     context 'with existing Duo Enterprise and seat count increase' do
-      let!(:add_on_purchase_duo_enterprise) do
-        create(
-          :gitlab_subscription_add_on_purchase,
-          add_on: add_on_duo_enterprise,
-          quantity: 1,
-          namespace: namespace
-        )
-      end
+      let(:add_on) { add_on_duo_enterprise }
+      let(:add_ons) { %i[duo_enterprise] }
+      let(:quantity) { 2 }
 
-      let(:quantity_duo_enterprise) { 2 }
-
-      it 'updates quantity of existing add-on purchase' do
-        expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(0)
-
-        expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
-        expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
-          subscription_add_on_id: add_on_duo_enterprise.id,
-          quantity: quantity_duo_enterprise,
-          started_at: started_at,
-          expires_on: started_at + 1.year,
-          purchase_xid: 'duo_enterprise_purchase',
-          trial: false
-        )
-      end
+      it_behaves_like 'seat increase provision for trials and non-trials'
     end
 
     context 'with existing Duo Enterprise and downgrade to Duo Pro' do
-      let!(:add_on_purchase_duo_enterprise) do
-        create(
-          :gitlab_subscription_add_on_purchase,
-          add_on: add_on_duo_enterprise,
-          quantity: 1,
-          namespace: namespace
-        )
-      end
+      let(:add_ons) { %i[duo_pro] }
+      let(:initial_add_on) { add_on_duo_enterprise }
+      let(:expected_add_on) { add_on_duo_pro }
 
-      let(:quantity_duo_enterprise) { 0 }
-      let(:quantity_duo_pro) { 1 }
+      it_behaves_like 'update existing Duo add-on purchase'
+    end
 
-      it 'downgrades to Duo Pro' do
-        expect { provision_service.execute }.to change { GitlabSubscriptions::AddOnPurchase.count }.by(0)
+    context 'with Duo with Amazon Q' do
+      let(:add_ons) { %i[duo_amazon_q] }
+      let(:expected_add_on) { add_on_duo_amazon_q }
 
-        expect(GitlabSubscriptions::AddOnPurchase.count).to eq(1)
-        expect(GitlabSubscriptions::AddOnPurchase.first).to have_attributes(
-          subscription_add_on_id: add_on_duo_pro.id,
-          quantity: quantity_duo_pro,
-          started_at: started_at,
-          expires_on: started_at + 1.year,
-          purchase_xid: 'duo_pro_purchase',
-          trial: false
-        )
-      end
+      it_behaves_like 'provision duo add-on purchase'
+    end
+
+    context 'with existing Duo Enterprise and additional purchase of Duo with Amazon Q' do
+      let(:add_ons) { %i[duo_enterprise duo_amazon_q] }
+      let(:initial_add_on) { add_on_duo_enterprise }
+      let(:expected_add_on) { add_on_duo_amazon_q }
+
+      it_behaves_like 'update existing Duo add-on purchase'
+    end
+
+    context 'with existing Duo Amazon Q and downgrade to Duo Enterprise' do
+      let(:add_ons) { %i[duo_enterprise] }
+      let(:initial_add_on) { add_on_duo_amazon_q }
+      let(:expected_add_on) { add_on_duo_enterprise }
+
+      it_behaves_like 'update existing Duo add-on purchase'
     end
   end
 
   private
 
-  def add_on_products(started_at:, trial: false)
-    [:duo_pro, :duo_enterprise].each_with_object({}) do |add_on_name, products|
-      quantity = send(:"quantity_#{add_on_name}")
-      next products if quantity <= 0
-
-      products[add_on_name] = [
+  def add_on_products(add_ons:, started_at:, quantity:, trial:)
+    add_ons.index_with({}) do
+      [
         {
           "quantity" => quantity,
           "started_on" => started_at.to_s,
           "expires_on" => (started_at + 1.year).to_s,
-          "purchase_xid" => "#{add_on_name}_purchase",
+          "purchase_xid" => "123456789",
           "trial" => trial
         }
       ]
