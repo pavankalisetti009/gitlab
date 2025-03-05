4f005d444b3b24fb23ce3a0cf5308b1193bf8964
SAM FIGUEROA <sfigueroa@gitlab.com>
2025-03-05 22:53:38 +0100

    Add awareness banner for new adhernece report
    - Still needs input on final wording from techinical writing staff.
    
    - Refs: https://gitlab.com/gitlab-org/gitlab/-/issues/515163
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/compliance_dashboard/components/standards_adherence_report/report.vue
+++ ee/app/assets/javascripts/compliance_dashboard/components/standards_adherence_report/report.vue
@@ -1,14 +1,23 @@
 <script>
+import { GlSprintf, GlAlert, GlLink } from '@gitlab/ui';
 import Tracking from '~/tracking';
+import {
+  FEEDBACK_ISSUE_URL,
+  STANDARDS_ADHERENCE_DOCS_URL,
+} from 'ee/compliance_dashboard/constants';
+import { s__ } from '~/locale';
 import ComplianceStandardsAdherenceTable from './standards_adherence_table.vue';
 
 export default {
   name: 'ComplianceStandardsAdherenceReport',
   components: {
     ComplianceStandardsAdherenceTable,
+    GlAlert,
+    GlLink,
+    GlSprintf,
   },
   mixins: [Tracking.mixin()],
-  inject: ['activeComplianceFrameworks'],
+  inject: ['activeComplianceFrameworks', 'adherenceV2Enabled'],
   props: {
     groupPath: {
       type: String,
@@ -21,14 +30,51 @@ export default {
       default: null,
     },
   },
+  data() {
+    return {
+      showBanner: this.adherenceV2Enabled,
+    };
+  },
   mounted() {
     this.track('visit_standards_adherence', {
       property: this.activeComplianceFrameworks ? 'with_active_compliance_frameworks' : '',
     });
   },
+  i18n: {
+    feedbackTitle: s__(
+      "AdherenceReport|We've updated the Adherence Report with new features to enhance your compliance workflow.",
+    ),
+    learnMoreDocsText: s__(
+      'AdherenceReport|Learn more about the changes in our %{linkStart}documentation%{linkEnd}.',
+    ),
+    feedbackText: s__(
+      'AdherenceReport|Have questions or thoughts on the new improvements we made? %{linkStart}Please provide feedback on your experience%{linkEnd}.',
+    ),
+  },
+  FEEDBACK_ISSUE_URL,
+  STANDARDS_ADHERENCE_DOCS_URL,
 };
 </script>
 
 <template>
-  <compliance-standards-adherence-table :group-path="groupPath" :project-path="projectPath" />
+  <div>
+    <gl-alert v-if="showBanner" variant="info" dismissible @dismiss="showBanner = false">
+      <div>
+        {{ $options.i18n.feedbackTitle }}
+        <gl-sprintf :message="$options.i18n.learnMoreDocsText">
+          <template #link="{ content }">
+            <gl-link :href="$options.STANDARDS_ADHERENCE_DOCS_URL" target="_blank">{{
+              content
+            }}</gl-link>
+          </template>
+        </gl-sprintf>
+        <gl-sprintf :message="$options.i18n.feedbackText">
+          <template #link="{ content }">
+            <gl-link :href="$options.FEEDBACK_ISSUE_URL" target="_blank">{{ content }}</gl-link>
+          </template>
+        </gl-sprintf>
+      </div>
+    </gl-alert>
+    <compliance-standards-adherence-table :group-path="groupPath" :project-path="projectPath" />
+  </div>
 </template>
--- ee/app/assets/javascripts/compliance_dashboard/constants.js
+++ ee/app/assets/javascripts/compliance_dashboard/constants.js
@@ -82,6 +82,8 @@ export const POLICY_SCOPES_DOCS_URL = `${DOCS_URL_IN_EE_DIR}/user/application_se
 
 export const CREATE_FRAMEWORKS_DOCS_URL = `${DOCS_URL_IN_EE_DIR}/user/group/compliance_frameworks.html#prerequisites`;
 
+export const STANDARDS_ADHERENCE_DOCS_URL = `${DOCS_URL_IN_EE_DIR}/user/compliance/compliance_center/compliance_standards_adherence_dashboard/`;
+
 export const FEEDBACK_ISSUE_URL = 'https://gitlab.com/gitlab-org/gitlab/-/issues/481586';
 
 export const GRAPHQL_FIELD_MISSING_ERROR_MESSAGE = __(
