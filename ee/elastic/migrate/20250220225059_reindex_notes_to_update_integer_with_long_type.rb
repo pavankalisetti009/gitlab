38044f5b0af8633b9794a9a45194cd38e5502896
Arturo Herrero <arturo.herrero@gmail.com>
2025-03-06 12:00:47 +0100

    Add migration reindex task helper
    EE: true

--- ee/elastic/migrate/20250220225059_reindex_notes_to_update_integer_with_long_type.rb
+++ ee/elastic/migrate/20250220225059_reindex_notes_to_update_integer_with_long_type.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class ReindexNotesToUpdateIntegerWithLongType < Elastic::Migration
-  def migrate
-    Search::Elastic::ReindexingTask.create!(targets: %w[Note], options: { skip_pending_migrations_check: true })
-  end
+  include ::Search::Elastic::MigrationReindexTaskHelper
 
-  def completed?
-    true
+  def targets
+    %w[Note]
   end
 end
