54dc18e98b571fcd85bb4f5db763774cae17cacc
Siddharth Dungarwal <sdungarwal@gitlab.com>
2025-03-06 20:17:53 +0530

    Use other scope as we are stopping use of Issue class for search

--- ee/spec/lib/gem_extensions/elasticsearch/model/adapter/active_record/records_spec.rb
+++ ee/spec/lib/gem_extensions/elasticsearch/model/adapter/active_record/records_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Elasticsearch::Model::Adapter::ActiveRecord::Records, :elastic do
+RSpec.describe Elasticsearch::Model::Adapter::ActiveRecord::Records, :elastic, feature_category: :global_search do
   before do
     stub_ee_application_setting(elasticsearch_indexing: true)
   end
@@ -10,16 +10,16 @@ RSpec.describe Elasticsearch::Model::Adapter::ActiveRecord::Records, :elastic do
   describe '#records' do
     let(:user) { create(:user) }
     let(:search_options) { { options: { search_level: 'global', current_user: user, project_ids: :any, order_by: 'created_at', sort: 'desc' } } }
-    let(:results) { Issue.elastic_search('*', **search_options).records.to_a }
+    let(:results) { MergeRequest.elastic_search('*', **search_options).records.to_a }
 
-    let!(:new_issue) { create(:issue) }
-    let!(:recent_issue) { create(:issue, created_at: 1.hour.ago) }
-    let!(:old_issue) { create(:issue, created_at: 7.days.ago) }
+    let!(:new_merge_request) { create(:merge_request) }
+    let!(:recent_merge_request) { create(:merge_request, created_at: 1.hour.ago) }
+    let!(:old_merge_request) { create(:merge_request, created_at: 7.days.ago) }
 
     it 'returns results in the same sorted order as they come back from Elasticsearch' do
       ensure_elasticsearch_index!
 
-      expect(results).to eq([new_issue, recent_issue, old_issue])
+      expect(results).to eq([new_merge_request, recent_merge_request, old_merge_request])
     end
   end
 end
--- ee/spec/lib/gem_extensions/elasticsearch/model/adapter/multiple/records_spec.rb
+++ ee/spec/lib/gem_extensions/elasticsearch/model/adapter/multiple/records_spec.rb
@@ -10,15 +10,15 @@ RSpec.describe Elasticsearch::Model::Adapter::Multiple::Records, :elastic, featu
   describe '#records' do
     let(:user) { create(:user) }
     let(:search_options) { { options: { current_user: user, project_ids: :any } } }
-    let(:records) { Elasticsearch::Model.search('*', [Issue, MergeRequest]).records.to_a }
+    let(:records) { Elasticsearch::Model.search('*', [MergeRequest, Note]).records.to_a }
 
-    let!(:issue) { create(:issue) }
+    let!(:note) { create(:note) }
     let!(:merge_request) { create(:merge_request) }
 
     it 'returns results from both classes in different Elasticsearch indexes' do
       ensure_elasticsearch_index!
 
-      expect(records).to match_array([issue, merge_request])
+      expect(records).to match_array([merge_request, note])
     end
   end
 end
