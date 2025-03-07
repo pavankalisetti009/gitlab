1c2f01ab5263ce4e0e349b204f78d7741535e569
Martin Čavoj <mcavoj@gitlab.com>
2025-03-07 15:30:22 +0100

    Update policy_tuning wording to account for pipeline execution policies
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/security_orchestration/components/policy_editor/scan_result/advanced_settings/constants.js
+++ ee/app/assets/javascripts/security_orchestration/components/policy_editor/scan_result/advanced_settings/constants.js
@@ -7,5 +7,5 @@ export const OPEN = 'open';
 export const UNBLOCK_RULES_KEY = 'unblock_rules_using_execution_policies';
 
 export const UNBLOCK_RULES_TEXT = s__(
-  'ScanResultPolicy|Make approval rules optional using scan execution policies',
+  'ScanResultPolicy|Make approval rules optional using execution policies',
 );
