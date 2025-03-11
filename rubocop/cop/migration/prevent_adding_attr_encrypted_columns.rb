b4951acb1e364c759f13796d5aacbbd8c2d1261e
Rémy Coutable <remy@rymai.me>
2025-03-11 10:10:29 +0100

    Simplify PreventAddingAttrEncryptedColumns cop and address feedback
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- rubocop/cop/migration/prevent_adding_attr_encrypted_columns.rb
+++ rubocop/cop/migration/prevent_adding_attr_encrypted_columns.rb
@@ -6,103 +6,105 @@ module RuboCop
   module Cop
     module Migration
       # Cop that prevents introducing `encrypted_*` columns (used by the `attr_encrypted` gem).
+      #
+      # @examples
+      #
+      #   # bad
+      #   class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+      #     def change
+      #       create_table :audit_events_instance_amazon_s3_configurations do |t|
+      #         t.binary :encrypted_secret_access_key
+      #         t.binary :encrypted_secret_access_key_iv
+      #       end
+      #     end
+      #   end
+      #
+      #   # good
+      #   class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+      #     def change
+      #       create_table :audit_events_instance_amazon_s3_configurations do |t|
+      #         t.jsonb :secret_access_key
+      #       end
+      #     end
+      #   end
+      #
+      #   # bad
+      #   class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+      #     def change
+      #       add_column :audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key, :binary
+      #       add_column :audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key_iv, :binary
+      #     end
+      #   end
+      #
+      #   # good
+      #   class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+      #     def change
+      #       add_column :audit_events_instance_amazon_s3_configurations, :secret_access_key, :jsonb
+      #     end
+      #   end
       class PreventAddingAttrEncryptedColumns < RuboCop::Cop::Base
+        extend AutoCorrector
         include MigrationHelpers
 
-        MSG = 'Do not introduce `%<wrong_column>s` (`attr_encrypted` column), introduce a single ' \
-          '`%<correct_column>s` column with type `:jsonb` instead.'
+        MSG = "Do not introduce `%<wrong_column>s` (`attr_encrypted` column), introduce a single " \
+          "`%<correct_column>s` column with type `:jsonb` instead. " \
+          "See https://docs.gitlab.com/development/migration_style_guide/#encrypted-attributes"
 
-        def_node_matcher :reverting?, <<~PATTERN
-          (def :down ...)
+        ADD_COLUMN_PATTERN = <<~PATTERN
+          `$(send nil? {:add_column :change_column_type_concurrently} sym $({sym|const nil?} _column_name) (sym :binary))
+        PATTERN
+
+        CREATE_TABLE_PATTERN = <<~PATTERN
+          `(block
+            (send _ :create_table sym)
+            args
+            `$(send lvar :binary $({sym|const nil?} _column_name) ...)
+          )
+        PATTERN
+
+        def_node_matcher :attr_encrypted_columns, <<~PATTERN
+          (def !:down args {#{ADD_COLUMN_PATTERN} | #{CREATE_TABLE_PATTERN}})
         PATTERN
 
         def on_def(node)
-          return unless should_check_node?(node)
+          attr_encrypted_columns(node) do |send_node, wrong_column|
+            wrong_column = resolve_constant(wrong_column)
+            next unless attr_encrypted_column?(wrong_column)
 
-          check_encrypted_columns(node)
-        end
-
-        private
-
-        def should_check_node?(node)
-          in_migration?(node) && !reverting?(node)
-        end
-
-        def check_encrypted_columns(node)
-          node.each_descendant(:send) do |send_node|
-            process_encrypted_column(send_node)
+            correct_column = derive_correct_column_name(wrong_column)
+            register_offense(send_node, wrong_column, correct_column)
           end
         end
 
-        def process_encrypted_column(send_node)
-          wrong_column = attr_encrypted_wrong_column(send_node)
-          return unless wrong_column
-
-          correct_column = derive_correct_column_name(wrong_column)
-          register_offense(send_node, wrong_column, correct_column)
-        end
+        private
 
         def derive_correct_column_name(wrong_column)
           wrong_column.to_s.delete_prefix('encrypted_').delete_suffix('_iv')
         end
 
-        def register_offense(send_node, wrong_column, correct_column)
+        def register_offense(node, wrong_column, correct_column)
           add_offense(
-            send_node.loc.selector,
+            node.loc.selector,
             message: format(MSG, wrong_column: wrong_column, correct_column: correct_column)
-          )
-        end
+          ) do |corrector|
+            replaced_content = node.source
+            constant = node.children.find { |child| valid_node?(child) && child.const_type? }
+            replaced_content = node.source.sub(constant.short_name.to_s, ":#{correct_column}") if constant
+            replaced_content = replaced_content.sub(wrong_column.to_s, correct_column.to_s).sub('binary', 'jsonb')
 
-        def attr_encrypted_wrong_column(node)
-          table_ref = node.children[0]
-          column_name = node.children[2]
-
-          if new_column_in_create_table_block?(table_ref, column_name)
-            return handle_create_table_column(table_ref, column_name)
+            corrector.replace(node, replaced_content)
           end
-
-          handle_add_column(node, table_ref) if add_column_method?(node)
-        end
-
-        def new_column_in_create_table_block?(table_ref, column_name)
-          table_ref&.type == :lvar && %i[sym string].include?(column_name&.type)
-        end
-
-        def handle_create_table_column(table_ref, column_name)
-          return unless new_column_in_create_table_block?(table_ref, column_name)
-
-          column_value = column_name.value
-          column_name.value if attr_encrypted_column?(column_value)
         end
 
         def attr_encrypted_column?(column_name)
           column_name.start_with?('encrypted_')
         end
 
-        def add_column_method?(node)
-          ADD_COLUMN_METHODS.include?(node.children[1])
-        end
+        def resolve_constant(node)
+          return node.value unless node.type == :const
 
-        def handle_add_column(node, table_ref)
-          return unless table_ref.nil?
-
-          column_name = extract_column_name(node)
-          column_name if attr_encrypted_column?(column_name)
-        end
-
-        def extract_column_name(node)
-          column_def = node.children[3]
-
-          if column_def.type == :const
-            resolve_constant_value(node, column_def.short_name)
-          else
-            column_def.value
-          end
-        end
-
-        def resolve_constant_value(node, const_name)
           node.ancestors.each do |ancestor|
-            constant = find_constant(ancestor, const_name)
+            constant = find_constant(ancestor, node.short_name)
             return constant.expression.value if constant
           end
         end
--- spec/rubocop/cop/migration/prevent_adding_attr_encrypted_columns_spec.rb
+++ spec/rubocop/cop/migration/prevent_adding_attr_encrypted_columns_spec.rb
@@ -4,134 +4,136 @@ require 'rubocop_spec_helper'
 require_relative '../../../../rubocop/cop/migration/prevent_adding_attr_encrypted_columns'
 
 RSpec.describe RuboCop::Cop::Migration::PreventAddingAttrEncryptedColumns, feature_category: :database do
-  context 'when in migration' do
-    before do
-      allow(cop).to receive(:in_migration?).and_return(true)
-    end
+  context 'when `encrypted_*` columns are introduced in create_table' do
+    it 'registers offenses' do
+      expect_offense(<<~RUBY)
+        class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+          def change
+            create_table :audit_events_instance_amazon_s3_configurations do |t|
+              t.binary :encrypted_secret_access_key
+                ^^^^^^ Do not introduce `encrypted_secret_access_key` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
+            end
+          end
+        end
+      RUBY
 
-    context 'when `encrypted_*` columns are introduced in create_table' do
+      expect_correction(<<~RUBY)
+        class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+          def change
+            create_table :audit_events_instance_amazon_s3_configurations do |t|
+              t.jsonb :secret_access_key
+            end
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'when `encrypted_*` columns are introduced in create_table as a constant' do
+    it 'registers offenses' do
+      expect_offense(<<~RUBY)
+        class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+          COLUMN_NAME = :encrypted_secret_access_key
+          COLUMN_NAME_IV = :encrypted_secret_access_key_iv
+
+          def change
+            create_table :audit_events_instance_amazon_s3_configurations do |t|
+              t.binary COLUMN_NAME
+                ^^^^^^ Do not introduce `encrypted_secret_access_key` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
+              t.binary COLUMN_NAME_IV
+            end
+          end
+        end
+      RUBY
+
+      expect_correction(<<~RUBY)
+        class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+          COLUMN_NAME = :encrypted_secret_access_key
+          COLUMN_NAME_IV = :encrypted_secret_access_key_iv
+
+          def change
+            create_table :audit_events_instance_amazon_s3_configurations do |t|
+              t.jsonb :secret_access_key
+              t.jsonb :secret_access_key
+            end
+          end
+        end
+      RUBY
+    end
+  end
+
+  described_class::ADD_COLUMN_METHODS.each do |add_column_method|
+    context "when `encrypted_*` columns are introduced with `#{add_column_method}`" do
       it 'registers offenses' do
-        expect_offense(<<~RUBY)
+        expect_offense(<<~RUBY, add_column_method: add_column_method)
           class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
             def change
-              create_table :audit_events_instance_amazon_s3_configurations do |t|
-                t.binary :encrypted_secret_access_key, null: false
-                  ^^^^^^ Do not introduce `encrypted_secret_access_key` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
-                t.binary :encrypted_secret_access_key_iv, null: false
-                  ^^^^^^ Do not introduce `encrypted_secret_access_key_iv` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
-              end
+              %{add_column_method}(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key, :binary)
+              ^{add_column_method} Do not introduce `encrypted_secret_access_key` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
+              %{add_column_method}(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key_iv, :binary)
             end
           end
         RUBY
       end
-    end
 
-    described_class::ADD_COLUMN_METHODS.each do |add_column_method|
-      context "when `encrypted_*` columns are introduced with `#{add_column_method}`" do
+      context "and column name is a constant" do
         it 'registers offenses' do
           expect_offense(<<~RUBY, add_column_method: add_column_method)
             class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+              COLUMN_NAME = :encrypted_secret_access_key
+
               def change
-                %{add_column_method}(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key, :binary)
+                %{add_column_method}(:audit_events_instance_amazon_s3_configurations, COLUMN_NAME, :binary)
                 ^{add_column_method} Do not introduce `encrypted_secret_access_key` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
-                %{add_column_method}(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key_iv, :binary)
-                ^{add_column_method} Do not introduce `encrypted_secret_access_key_iv` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
               end
             end
           RUBY
         end
+      end
+    end
+  end
 
-        context "and column name is a constant" do
-          it 'registers offenses' do
-            expect_offense(<<~RUBY, add_column_method: add_column_method)
-              class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
-                COLUMN_NAME = :encrypted_secret_access_key
-
-                def change
-                  %{add_column_method}(:audit_events_instance_amazon_s3_configurations, COLUMN_NAME, :binary)
-                  ^{add_column_method} Do not introduce `encrypted_secret_access_key` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
-                end
-              end
-            RUBY
+  context "when `encrypted_*` columns are introduced with `:add_column`" do
+    it 'registers offenses' do
+      expect_offense(<<~RUBY)
+        class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+          def change
+            add_column(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key, :binary)
+            ^^^^^^^^^^ Do not introduce `encrypted_secret_access_key` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
+            add_column(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key_iv, :binary)
           end
         end
-      end
+      RUBY
+
+      expect_correction(<<~RUBY)
+        class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+          def change
+            add_column(:audit_events_instance_amazon_s3_configurations, :secret_access_key, :jsonb)
+            add_column(:audit_events_instance_amazon_s3_configurations, :secret_access_key, :jsonb)
+          end
+        end
+      RUBY
     end
 
-    context 'when no `encrypted_*` columns are introduced' do
-      it 'registers no offense' do
-        expect_no_offenses(<<~RUBY)
-          class Users < ActiveRecord::Migration[6.0]
-            def up
-              create_table :users do |t|
-                t.jsonb :my_secret_field, null: false
-                t.timestamps_with_timezone null: true
-              end
+    context "and column name is a constant" do
+      it 'registers offenses' do
+        expect_offense(<<~RUBY)
+          class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+            COLUMN_NAME = :encrypted_secret_access_key
 
-              add_column(:users, :not_a_string, :integer)
+            def change
+              add_column(:audit_events_instance_amazon_s3_configurations, COLUMN_NAME, :binary)
+              ^^^^^^^^^^ Do not introduce `encrypted_secret_access_key` (`attr_encrypted` column), introduce a single `secret_access_key` column with type `:jsonb` instead.[...]
             end
           end
         RUBY
-      end
-    end
 
-    context 'when no `encrypted_*` columns are introduced but constant is used as column name' do
-      it 'registers no offense' do
-        expect_no_offenses(<<~RUBY)
-          class Users < ActiveRecord::Migration[6.0]
-            COLUMN_NAME = :container_scanning_for_registry_enabled
+        expect_correction(<<~RUBY)
+          class CreateAuditEventsInstanceAmazonS3Configurations < ActiveRecord::Migration[6.0]
+            COLUMN_NAME = :encrypted_secret_access_key
 
-            def up
-              add_column TABLE_NAME, COLUMN_NAME, :boolean, null: false,
-                default: false
-            end
-
-            def down
-              remove_column TABLE_NAME, COLUMN_NAME
-            end
-          end
-        RUBY
-      end
-    end
-
-    context 'when the string data type is used for arrays' do
-      it 'registers no offense' do
-        expect_no_offenses(<<~RUBY)
-          class TestStringArrays < ActiveRecord::Migration[6.0]
-            def up
-              create_table :test_string_arrays, id: false do |t|
-                t.integer :test_id, null: false
-                t.string :name, array: true, default: [], null: false
-              end
-
-              add_column :test_string_arrays, :email, :string, array: true
-              add_column :test_string_arrays, :role, :string, default: [], array: true
-              change_column_type_concurrently :test_string_arrays, :test_id, :string, array: true
-            end
-          end
-        RUBY
-      end
-    end
-
-    context 'when using down method' do
-      it 'registers no offense' do
-        expect_no_offenses(<<~RUBY)
-          class Users < ActiveRecord::Migration[6.0]
-            def up
-              remove_column :audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key
-              remove_column :audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key_iv
-
-              drop_table :audit_events_instance_amazon_s3_configurations
-            end
-
-            def down
-              create_table :audit_events_instance_amazon_s3_configurations, id: false do |t|
-                t.integer :test_id, null: false
-                t.string :name
-              end
-
-              add_column(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key, :binary)
-              add_column(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key_iv, :binary)
+            def change
+              add_column(:audit_events_instance_amazon_s3_configurations, :secret_access_key, :jsonb)
             end
           end
         RUBY
@@ -139,15 +141,61 @@ RSpec.describe RuboCop::Cop::Migration::PreventAddingAttrEncryptedColumns, featu
     end
   end
 
-  context 'when outside of migration' do
+  context 'when no `encrypted_*` columns are introduced' do
     it 'registers no offense' do
       expect_no_offenses(<<~RUBY)
-        class CreateAuditEventsInstanceAmazonS3Configurations < Gitlab::Database::Migration[2.2]
-          def change
-            create_table :audit_events_instance_amazon_s3_configurations do |t|
-              t.binary :encrypted_secret_access_key, null: false
-              t.binary :encrypted_secret_access_key_iv, null: false
+        class Users < ActiveRecord::Migration[6.0]
+          def up
+            create_table :users do |t|
+              t.jsonb :my_secret_field
+              t.timestamps_with_timezone null: true
             end
+
+            add_column(:users, :not_a_string, :integer)
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'when no `encrypted_*` columns are introduced but constant is used as column name' do
+    it 'registers no offense' do
+      expect_no_offenses(<<~RUBY)
+        class Users < ActiveRecord::Migration[6.0]
+          COLUMN_NAME = :container_scanning_for_registry_enabled
+
+          def up
+            add_column TABLE_NAME, COLUMN_NAME, :boolean,
+              default: false
+          end
+
+          def down
+            remove_column TABLE_NAME, COLUMN_NAME
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'when using down method' do
+    it 'registers no offense' do
+      expect_no_offenses(<<~RUBY)
+        class Users < ActiveRecord::Migration[6.0]
+          def up
+            remove_column :audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key
+            remove_column :audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key_iv
+
+            drop_table :audit_events_instance_amazon_s3_configurations
+          end
+
+          def down
+            create_table :audit_events_instance_amazon_s3_configurations, id: false do |t|
+              t.integer :test_id
+              t.string :name
+            end
+
+            add_column(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key, :binary)
+            add_column(:audit_events_instance_amazon_s3_configurations, :encrypted_secret_access_key_iv, :binary)
           end
         end
       RUBY
