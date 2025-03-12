f0add57ea58328e618f3e18fa84649c11280036e
Heinrich Lee Yu <heinrich@gitlab.com>
2025-03-12 15:53:47 +0800

    Change workItemTypeIds argument to be singular
    We do not need to support multiple values for filtering so we restrict
    the GraphQL API argument.
    
    This field is experimental and not yet used by any frontend query so we
    can change it without deprecating.

--- ee/app/graphql/resolvers/issuables/custom_fields_resolver.rb
+++ ee/app/graphql/resolvers/issuables/custom_fields_resolver.rb
@@ -4,6 +4,7 @@ module Resolvers
   module Issuables
     class CustomFieldsResolver < BaseResolver
       include LooksAhead
+      include ::Issuables::CustomFields::LookAheadPreloads
 
       type Types::Issuables::CustomFieldType.connection_type, null: true
 
@@ -20,14 +21,15 @@ module Resolvers
         required: false,
         description: 'Search query for custom field name.'
 
-      argument :work_item_type_ids, [Types::GlobalIDType[::WorkItems::Type]],
+      argument :work_item_type_id, Types::GlobalIDType[::WorkItems::Type],
         required: false,
-        description: 'Filter custom fields associated to any of the given work item types. ' \
-          'If empty, returns custom fields not associated to any work item type.',
-        prepare: ->(global_ids, _ctx) { global_ids.map(&:model_id) }
+        description: 'Filter custom fields associated to the given work item type.',
+        prepare: ->(work_item_typ_id, _ctx) { work_item_typ_id&.model_id }
 
-      def resolve_with_lookahead(active: nil, field_type: nil, search: nil, work_item_type_ids: nil)
-        work_item_type_ids = work_item_type_ids_from(work_item_type_ids) unless work_item_type_ids.nil?
+      def resolve_with_lookahead(active: nil, field_type: nil, search: nil, work_item_type_id: nil)
+        unless work_item_type_id.nil?
+          work_item_type_ids = [::WorkItems::Type.find_by_id_with_fallback(work_item_type_id)&.id]
+        end
 
         custom_fields = ::Issuables::CustomFieldsFinder.new(
           current_user,
@@ -41,25 +43,6 @@ module Resolvers
         offset_pagination(
           apply_lookahead(custom_fields)
         )
-      end
-
-      def unconditional_includes
-        [:namespace]
-      end
-
-      def preloads
-        {
-          created_by: [:created_by],
-          updated_by: [:updated_by],
-          select_options: [:select_options],
-          work_item_types: [:work_item_types]
-        }
-      end
-
-      private
-
-      def work_item_type_ids_from(request_ids)
-        ::WorkItems::Type.with_id_and_fallback(request_ids).map(&:id)
       end
     end
   end
