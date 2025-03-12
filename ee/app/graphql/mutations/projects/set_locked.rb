c6ec619e1350d72acb7a588bc12661897f42d475
Joe Woodward <j@joewoodward.me>
2025-03-12 10:55:50 +0000

    Update path lock endpoints
    Cleans up the path lock controller and set path mutation.
    
    Uses new logic from path lock and lfs lock services to remove complexity
    from controller and mutation.
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/512677

--- ee/app/graphql/mutations/projects/set_locked.rb
+++ ee/app/graphql/mutations/projects/set_locked.rb
@@ -28,67 +28,23 @@ module Mutations
       attr_reader :project
 
       def resolve(project_path:, file_path:, lock:)
-        @project = authorized_find!(project_path)
+        project = authorized_find!(project_path)
 
-        unless allowed?
+        unless project.licensed_feature_available?(:file_locks)
           raise Gitlab::Graphql::Errors::ResourceNotAvailable, 'FileLocks feature disabled'
         end
 
-        if lock
-          lock_path(file_path)
-        else
-          unlock_path(file_path)
+        path_lock = project.path_locks.for_path(file_path)
+
+        if lock && !path_lock
+          PathLocks::LockService.new(project, current_user).execute(file_path)
+        elsif !lock && path_lock
+          PathLocks::UnlockService.new(project, current_user).execute(path_lock)
         end
 
-        {
-          project: project,
-          errors: []
-        }
-      rescue PathLocks::UnlockService::AccessDenied => e
-        {
-          project: nil,
-          errors: [e.message]
-        }
-      end
-
-      private
-
-      delegate :repository, to: :project
-
-      def fetch_path_lock(file_path)
-        project.path_locks.find_by(path: file_path) # rubocop: disable CodeReuse/ActiveRecord
-      end
-
-      def lock_path(file_path)
-        return if fetch_path_lock(file_path)
-
-        PathLocks::LockService.new(project, current_user).execute(file_path)
-      end
-
-      def unlock_path(file_path)
-        path_lock = fetch_path_lock(file_path)
-
-        return unless path_lock
-
-        PathLocks::UnlockService.new(project, current_user).execute(path_lock)
-      end
-
-      def sync_with_lfs?(file_path)
-        project.lfs_enabled? && lfs_file?(file_path)
-      end
-
-      def lfs_file?(file_path)
-        blob = repository.blob_at_branch(repository.root_ref, file_path)
-
-        return false unless blob
-
-        lfs_blob_ids = LfsPointersFinder.new(repository, file_path).execute
-
-        lfs_blob_ids.include?(blob.id)
-      end
-
-      def allowed?
-        project.licensed_feature_available?(:file_locks)
+        { project: project, errors: [] }
+      rescue PathLocks::UnlockService::AccessDenied, PathLocks::LockService::AccessDenied => error
+        { project: nil, errors: [error.message] }
       end
     end
   end
