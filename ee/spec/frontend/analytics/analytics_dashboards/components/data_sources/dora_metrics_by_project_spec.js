dd5c9c41baf40a2c2942832b13bc3280b5a57592
Alex Pennells <apennells@gitlab.com>
2025-03-14 10:30:29 -0400

    Remove `dora_projects_comparison_subgroups` feature flag
    Removes the derisk feature flag now that the solution
    has been validated.
    
    Changelog: removed
    EE: true

--- ee/spec/frontend/analytics/analytics_dashboards/components/data_sources/dora_metrics_by_project_spec.js
+++ ee/spec/frontend/analytics/analytics_dashboards/components/data_sources/dora_metrics_by_project_spec.js
@@ -1,8 +1,5 @@
 import doraMetricsByProject from 'ee/analytics/analytics_dashboards/data_sources/dora_metrics_by_project';
-import {
-  mockDataSourceResponses,
-  mockLegacyDataSourceResponses,
-} from 'ee_jest/analytics/dashboards/dora_projects_comparison/mock_data';
+import { mockDataSourceResponses } from 'ee_jest/analytics/dashboards/dora_projects_comparison/mock_data';
 import { defaultClient } from 'ee/analytics/analytics_dashboards/graphql/client';
 
 describe('Dora Metrics by project Data Source', () => {
@@ -20,47 +17,37 @@ describe('Dora Metrics by project Data Source', () => {
   };
 
   describe('for group', () => {
-    describe.each`
-      doraProjectsComparisonSubgroups | responses
-      ${true}                         | ${mockDataSourceResponses}
-      ${false}                        | ${mockLegacyDataSourceResponses}
-    `(
-      'when doraProjectsComparisonSubgroups=$doraProjectsComparisonSubgroups',
-      ({ doraProjectsComparisonSubgroups, responses }) => {
-        beforeEach(() => {
-          gon.features = { doraProjectsComparisonSubgroups };
-          jest
-            .spyOn(defaultClient, 'query')
-            .mockResolvedValueOnce(responses[0])
-            .mockResolvedValueOnce(responses[1]);
-          return fetch();
-        });
+    beforeEach(() => {
+      jest
+        .spyOn(defaultClient, 'query')
+        .mockResolvedValueOnce(mockDataSourceResponses[0])
+        .mockResolvedValueOnce(mockDataSourceResponses[1]);
+      return fetch();
+    });
 
-        it('requests data until pagination completes', () => {
-          const variables = {
-            startDate: '2020-05-01',
-            endDate: '2020-06-30',
-            fullPath: 'cool namespace',
-            interval: 'MONTHLY',
-          };
+    it('requests data until pagination completes', () => {
+      const variables = {
+        startDate: '2020-05-01',
+        endDate: '2020-06-30',
+        fullPath: 'cool namespace',
+        interval: 'MONTHLY',
+      };
 
-          expect(defaultClient.query).toHaveBeenCalledTimes(2);
-          expect(defaultClient.query).toHaveBeenCalledWith(expect.objectContaining({ variables }));
-          expect(defaultClient.query).toHaveBeenCalledWith(
-            expect.objectContaining({
-              variables: {
-                ...variables,
-                after: 'page1',
-              },
-            }),
-          );
-        });
+      expect(defaultClient.query).toHaveBeenCalledTimes(2);
+      expect(defaultClient.query).toHaveBeenCalledWith(expect.objectContaining({ variables }));
+      expect(defaultClient.query).toHaveBeenCalledWith(
+        expect.objectContaining({
+          variables: {
+            ...variables,
+            after: 'page1',
+          },
+        }),
+      );
+    });
 
-        it('formats the DORA metrics for the list of projects', () => {
-          expect(res).toMatchSnapshot();
-        });
-      },
-    );
+    it('formats the DORA metrics for the list of projects', () => {
+      expect(res).toMatchSnapshot();
+    });
   });
 
   describe('for project', () => {
