96b915148dc71b22fa9d2e5276c0002224edcca9
Igor Drozdov <idrozdov@gitlab.com>
2025-03-17 14:30:14 +0100

    Pass scoped user to q web hook event
    When a merge request or pipeline is created:
    
    - If the author is not a composite identity - pass it as-is
    - If the author is a composite identity - pass the scoped user

--- ee/app/models/concerns/integrations/base/amazon_q.rb
+++ ee/app/models/concerns/integrations/base/amazon_q.rb
@@ -25,38 +25,6 @@ module Integrations
         attribute :wiki_page_events, default: false
       end
 
-      def execute(data)
-        return unless Feature.enabled?(:amazon_q_chat_and_code_suggestions, :instance)
-
-        user = ::User.find_by_id(data.dig(:user, :id))
-        return unless user
-
-        client = ::Gitlab::Llm::QAi::Client.new(user)
-
-        client.create_event(
-          payload: { source: :web_hook, data: data },
-          role_arn: ::Ai::Setting.instance.amazon_q_role_arn,
-          event_id: WebHookService.hook_to_event("#{data[:object_kind]}_hooks")
-        )
-      end
-
-      def sections
-        [{
-          type: 'amazon_q',
-          title: s_('AmazonQ|Configure GitLab Duo with Amazon Q'),
-          description: help,
-          plan: 'ultimate'
-        }]
-      end
-
-      def editable?
-        false
-      end
-
-      def manual_activation?
-        false
-      end
-
       class_methods do
         def title
           s_('AmazonQ|Amazon Q')
@@ -81,6 +49,53 @@ module Integrations
         def supported_events
           %w[merge_request pipeline]
         end
+      end
+
+      def sections
+        [{
+          type: 'amazon_q',
+          title: s_('AmazonQ|Configure GitLab Duo with Amazon Q'),
+          description: help,
+          plan: 'ultimate'
+        }]
+      end
+
+      def editable?
+        false
+      end
+
+      def manual_activation?
+        false
+      end
+
+      def execute(data)
+        return unless Feature.enabled?(:amazon_q_chat_and_code_suggestions, :instance)
+
+        user = user_from_web_hook_data(data)
+        return unless user
+
+        client = ::Gitlab::Llm::QAi::Client.new(user)
+
+        client.create_event(
+          payload: { source: :web_hook, data: data },
+          role_arn: ::Ai::Setting.instance.amazon_q_role_arn,
+          event_id: WebHookService.hook_to_event("#{data[:object_kind]}_hooks")
+        )
+      end
+
+      private
+
+      def user_from_web_hook_data(data)
+        user = ::User.find_by_id(data.dig(:user, :id))
+        composite_identity = ::Gitlab::Auth::Identity.fabricate(user)
+
+        return user unless composite_identity&.composite?
+
+        # If a composite identity was propagated through Sidekiq and the user is service account
+        # that requires composite identity, we can use the scoped user.
+        return unless composite_identity.valid?
+
+        composite_identity.scoped_user
       end
     end
   end
