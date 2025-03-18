ashierj/superdemo

commit 0a71dc3f33e809198d522d3cf2a28781aeac5809

Derive Project Namespace Organization from Project

app/models/namespaces/project_namespace.rb
@@ -35,9 +35,11 @@ module Namespaces
     end
 
     def sync_attributes_from_project(project)
+      attribute_list = %w[name path namespace_id namespace visibility_level shared_runners_enabled organization_id]
+
       attributes_to_sync = project
                              .changes
-                             .slice(*%w[name path namespace_id namespace visibility_level shared_runners_enabled])
+                             .slice(*attribute_list)
                              .transform_values { |val| val[1] }
 
       # if visibility_level is not set explicitly for project, it defaults to 0,

spec/factories/namespaces/project_namespaces.rb
@@ -9,5 +9,6 @@ FactoryBot.define do
     path { project.path }
     type { Namespaces::ProjectNamespace.sti_name }
     owner { nil }
+    association :organization
   end
 end

spec/models/namespaces/project_namespace_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe Namespaces::ProjectNamespace, type: :model do
+  let_it_be(:organization) { create(:organization) }
+
   describe 'relationships' do
     it { is_expected.to have_one(:project).inverse_of(:project_namespace) }
 
@@ -46,7 +48,7 @@ RSpec.describe Namespaces::ProjectNamespace, type: :model do
     end
 
     context 'for new record when namespace exists' do
-      let(:project) { build(:project) }
+      let(:project) { build(:project, organization: organization) }
       let(:project_namespace) { project.project_namespace }
 
       it 'syncs the project attributes to project namespace' do
@@ -55,6 +57,7 @@ RSpec.describe Namespaces::ProjectNamespace, type: :model do
 
         described_class.create_from_project!(project)
         expect(project.project_namespace.name).to eq(project_name)
+        expect(project.project_namespace.organization_id).to eq(project.organization_id)
       end
 
       context 'when project has an unsaved project namespace' do
@@ -69,7 +72,7 @@ RSpec.describe Namespaces::ProjectNamespace, type: :model do
 
   describe '#sync_attributes_from_project' do
     context 'with existing project' do
-      let(:project) { create(:project) }
+      let(:project) { build(:project, organization: organization) }
       let(:project_namespace) { project.project_namespace }
       let(:project_new_namespace) { create(:namespace) }
       let(:project_new_path) { 'project-new-path' }
@@ -94,6 +97,7 @@ RSpec.describe Namespaces::ProjectNamespace, type: :model do
         expect(project_namespace.namespace).to eq(project_new_namespace)
         expect(project_namespace.namespace_id).to eq(project_new_namespace.id)
         expect(project_namespace.shared_runners_enabled).to eq(project_shared_runners_enabled)
+        expect(project_namespace.organization_id).to eq(project.organization_id)
       end
     end
 

pavankalisetti009/gitlab

commit 20d7eeee84a1c2bafe77d96104091d5b513be0bc

Detect organization_id change to be synced in project callback
This fixes an edge-case when changing the organization_id of a project,
it wouldn't be synced to its project_namespace.

The bug was introduced by 0a71dc3f33e809198d522d3cf2a28781aeac5809.

Changelog: fixed
Signed-off-by: Rémy Coutable <remy@rymai.me>

app/models/namespaces/project_namespace.rb
@@ -4,6 +4,16 @@ module Namespaces
   class ProjectNamespace < Namespace
     self.allow_legacy_sti_class = true
 
+    SYNCED_ATTRIBUTES = %w[
+      name
+      path
+      namespace_id
+      namespace
+      visibility_level
+      shared_runners_enabled
+      organization_id
+    ].freeze
+
     # These aliases are added to make it easier to sync parent/parent_id attribute with
     # project.namespace/project.namespace_id attribute.
     #
@@ -37,11 +47,9 @@ module Namespaces
     end
 
     def sync_attributes_from_project(project)
-      attribute_list = %w[name path namespace_id namespace visibility_level shared_runners_enabled organization_id]
-
       attributes_to_sync = project
                              .changes
-                             .slice(*attribute_list)
+                             .slice(*SYNCED_ATTRIBUTES)
                              .transform_values { |val| val[1] }
 
       # if visibility_level is not set explicitly for project, it defaults to 0,

app/models/project.rb
@@ -3716,7 +3716,7 @@ class Project < ApplicationRecord
   end
 
   def sync_project_namespace?
-    (changes.keys & %w[name path namespace_id namespace visibility_level shared_runners_enabled]).any? && project_namespace.present?
+    (changes.keys & Namespaces::ProjectNamespace::SYNCED_ATTRIBUTES).any? && project_namespace.present?
   end
 
   def reload_project_namespace_details

spec/models/project_spec.rb
@@ -361,25 +361,21 @@ RSpec.describe Project, factory_default: :keep, feature_category: :groups_and_pr
       end
 
       context 'with project namespaces' do
-        shared_examples 'creates project namespace' do
-          it 'automatically creates a project namespace' do
-            project = build(:project, path: 'hopefully-valid-path1')
-            project.save!
+        it 'automatically creates a project namespace' do
+          project = build(:project, path: 'hopefully-valid-path1')
+          project.save!
 
-            expect(project).to be_persisted
-            expect(project.project_namespace).to be_persisted
-            expect(project.project_namespace).to be_in_sync_with_project(project)
-            expect(project.reload.project_namespace.traversal_ids).to eq([project.namespace.traversal_ids, project.project_namespace.id].flatten.compact)
-          end
+          expect(project).to be_persisted
+          expect(project.project_namespace).to be_persisted
+          expect(project.project_namespace).to be_in_sync_with_project(project)
+          expect(project.reload.project_namespace.traversal_ids).to match_array([project.namespace.traversal_ids, project.project_namespace.id].flatten.compact)
         end
-
-        it_behaves_like 'creates project namespace'
       end
     end
 
     context 'updating a project' do
       let_it_be(:project_namespace) { create(:project_namespace) }
-      let_it_be(:project) { project_namespace.project }
+      let_it_be(:project, reload: true) { project_namespace.project }
 
       context 'when project has an associated project namespace' do
         # when FF is disabled creating a project does not create a project_namespace, so we create one
@@ -396,19 +392,20 @@ RSpec.describe Project, factory_default: :keep, feature_category: :groups_and_pr
           expect(project.reload.project_namespace).to be_in_sync_with_project(project)
         end
 
-        context 'when same project is being updated in 2 instances' do
-          it 'syncs only changed attributes' do
-            project1 = described_class.last
-            project2 = described_class.last
+        it 'syncs changed attributes' do
+          project.update!(name: "New project name", path: "new_project_path")
 
-            project_name = project1.name
-            project_path = project1.path
+          expect(project.reload.project_namespace).to be_in_sync_with_project(project)
+        end
 
-            project1.update!(name: project_name + "-1")
-            project2.update!(path: project_path + "-1")
+        # Regression test for edge-case introduced by 0a71dc3f33e809198d522d3cf2a28781aeac5809
+        it 'syncs organization_id even when it is the only change' do
+          new_org = create(:organization)
 
-            expect(project.reload.project_namespace).to be_in_sync_with_project(project)
-          end
+          project.parent.update_column(:organization_id, new_org.id)
+          project.update!(organization_id: new_org.id)
+
+          expect(project.reload.project_namespace).to be_in_sync_with_project(project)
         end
       end
     end

spec/support/matchers/project_namespace_matcher.rb
@@ -8,11 +8,9 @@ RSpec::Matchers.define :be_in_sync_with_project do |project|
     break true if project.new_record? && !project_namespace.present?
 
     project_namespace.present? &&
-      project.name == project_namespace.name &&
-      project.path == project_namespace.path &&
-      project.namespace_id == project_namespace.parent_id &&
-      project.visibility_level == project_namespace.visibility_level &&
-      project.shared_runners_enabled == project_namespace.shared_runners_enabled
+      Namespaces::ProjectNamespace::SYNCED_ATTRIBUTES.all? do |attribute|
+        project[attribute] == project_namespace[attribute]
+      end
   end
 
   failure_message_when_negated do |project_namespace|