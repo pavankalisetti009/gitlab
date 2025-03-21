a338bc736c82b53e3b55221097cc03cd060d394a
David Fernandez <dfernandez@gitlab.com>
2025-03-21 09:51:13 +0100

    Complete the virtual registry cleanup worker rename
    Remove the old unused class
    Remove the related feature flag
    
    Changelog: other

--- ee/app/workers/ee/dependency_proxy/cleanup_dependency_proxy_worker.rb
+++ ee/app/workers/ee/dependency_proxy/cleanup_dependency_proxy_worker.rb
@@ -16,11 +16,7 @@ module EE
       def enqueue_vreg_packages_cache_entry_cleanup_job
         [::VirtualRegistries::Packages::Maven::Cache::Entry].each do |klass|
           if klass.pending_destruction.any?
-            if ::Feature.enabled?(:virtual_registry_maven_cleanup_new_worker_class, ::Feature.current_request)
-              ::VirtualRegistries::Packages::Cache::DestroyOrphanEntriesWorker.perform_with_capacity(klass.name)
-            else
-              ::VirtualRegistries::Packages::DestroyOrphanCachedResponsesWorker.perform_with_capacity(klass.name)
-            end
+            ::VirtualRegistries::Packages::Cache::DestroyOrphanEntriesWorker.perform_with_capacity(klass.name)
           end
         end
       end
