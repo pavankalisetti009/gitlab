9d24b8b50f7edc71b5a2f20563e58f28f737a844
Shinya Maeda <shinya@gitlab.com>
2025-03-25 16:08:33 +0900

    Cleanup Duo Chat Claude 3.7 feature flags
    This commit cleans up Duo Chat feature flags
    for gitlab.com derisk.
    
    Changelog: changed

--- ee/lib/gitlab/llm/completions/chat.rb
+++ ee/lib/gitlab/llm/completions/chat.rb
@@ -160,9 +160,6 @@ module Gitlab
             Gitlab::AiGateway.push_feature_flag(:enable_anthropic_prompt_caching, user)
           end
 
-          Gitlab::AiGateway.push_feature_flag(:duo_chat_react_agent_claude_3_7, user)
-          Gitlab::AiGateway.push_feature_flag(:duo_chat_docs_qa_claude_3_7, user)
-
           return if ::CloudConnector.self_managed_cloud_connected?
 
           Gitlab::AiGateway.push_feature_flag(:expanded_ai_logging, user)
