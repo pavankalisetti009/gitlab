028507f0b4e6171d74dee1ef4db6ff6eb665daa3
Pavel Shutsin <pshutsin@gitlab.com>
2025-03-26 14:51:12 +0100

    Add backfill worker for CodeSuggestionsUsage
    The worker will copy all events from PG
    to CH when analytics in CH toggle
    is switched to enabled state.

--- ee/spec/factories/ai/code_suggestion_events.rb
+++ ee/spec/factories/ai/code_suggestion_events.rb
@@ -4,7 +4,9 @@ FactoryBot.define do
   factory :ai_code_suggestion_event, class: '::Ai::CodeSuggestionEvent' do
     event { 'code_suggestion_shown_in_ide' }
     user
-    payload { { language: 'ruby', suggestion_size: 1, unique_tracking_id: SecureRandom.hex.slice(0, 20) } }
+    payload do
+      { language: 'ruby', suggestion_size: 1, unique_tracking_id: SecureRandom.hex.slice(0, 20), branch_name: 'main' }
+    end
 
     trait :shown do
       event { 'code_suggestion_shown_in_ide' }
