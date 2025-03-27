8c90978c281cdb536a436a582c72652992947f69
Artur Fedorov <afedorov@gitlab.com>
2025-03-27 12:22:32 +0000

    Vue 3 spec fix
    Add missing stubs
    
    EE: true

--- ee/spec/frontend/vulnerabilities/generic_report/report_item_graphql_spec.js
+++ ee/spec/frontend/vulnerabilities/generic_report/report_item_graphql_spec.js
@@ -1,39 +1,44 @@
-import { shallowMount } from '@vue/test-utils';
+import MockAdapter from 'axios-mock-adapter';
 import ReportItem, {
   GRAPHQL_TYPENAMES,
 } from 'ee/vulnerabilities/components/generic_report/report_item_graphql.vue';
 import { vulnerabilityDetails } from 'ee_jest/security_dashboard/components/pipeline/mock_data';
-import { extendedWrapper } from 'helpers/vue_test_utils_helper';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import axios from '~/lib/utils/axios_utils';
 
 describe('ee/vulnerabilities/components/generic_report/report_item_graphql.vue', () => {
   let wrapper;
+  let mock;
 
   const createWrapper = ({ props } = {}) =>
-    extendedWrapper(
-      shallowMount(ReportItem, {
-        propsData: {
-          item: {},
-          ...props,
-        },
-        // manual stubbing is needed because the components are dynamically imported
-        stubs: GRAPHQL_TYPENAMES,
-      }),
-    );
+    shallowMountExtended(ReportItem, {
+      propsData: {
+        item: {},
+        ...props,
+      },
+      provide: {
+        commitPathTemplate: 'commitPathTemplate',
+      },
+      stubs: {
+        VulnerabilityDetailTable: true,
+      },
+    });
 
-  const findReportComponent = () => wrapper.findComponentByTestId('report-component');
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    mock.restore();
+  });
 
   describe.each(GRAPHQL_TYPENAMES)('with report type "%s"', (reportType) => {
     const testData = Object.values(vulnerabilityDetails).find((item) => item.type === reportType);
     const reportItem = { type: reportType, ...testData };
 
-    beforeEach(() => {
-      wrapper = createWrapper({ props: { item: reportItem } });
-    });
-
     it('passes the report data as props', () => {
-      expect(findReportComponent().props()).toMatchObject({
-        item: reportItem,
-      });
+      wrapper = createWrapper({ props: { item: reportItem } });
+      expect(wrapper.props('item')).toEqual(reportItem);
     });
   });
 });
