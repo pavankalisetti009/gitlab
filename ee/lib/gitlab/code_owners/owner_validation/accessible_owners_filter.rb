8ebfb60f0d39a5ac3dd43fca755271825380e501
Joe Woodward <j@joewoodward.me>
2025-03-28 17:03:12 +0000

    Refactor code owner validation filters
    We have some differences in the filters e.g. supplied input users or
    groups vs none supplied, handling groups and users in the same filter
    vs only users or groups.
    
    Originally we implemented a base class, however, as we have these
    variances we have decided to remove the base class.
    
    We have also split the accessors into their own methods instead of
    having an execute method. This allows the filters to behave more like
    true objects.
    
    We have also removed the need to define a constant for the error
    message. As symbols are immutable in ruby there's not much benefit of
    adding constants here. The old implementation was fairly abstract so
    harder to grok. This new implementation makes it very clear what is
    happening without needing to flick between the base and derived classes.
    
    Closes https://gitlab.com/gitlab-org/gitlab/-/issues/526521+
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/429261+
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/521151+

--- ee/lib/gitlab/code_owners/owner_validation/accessible_owners_filter.rb
+++ ee/lib/gitlab/code_owners/owner_validation/accessible_owners_filter.rb
@@ -3,21 +3,62 @@
 module Gitlab
   module CodeOwners
     module OwnerValidation
-      class AccessibleOwnersFilter < BaseFilter
-        ERROR_MESSAGE = :inaccessible_owner
+      class AccessibleOwnersFilter
+        include ::Gitlab::Utils::StrongMemoize
 
-        def execute
-          self.output_groups = GroupsLoader.new(project, names: input_names).groups
-          self.valid_group_names = output_groups.map(&:full_path)
-
-          remaining_names = input_names - valid_group_names
-          self.output_users = UsersLoader.new(project, emails: input_emails, names: remaining_names).members
-
-          self.invalid_names = remaining_names - output_users.map(&:username)
-          self.invalid_emails = input_emails - output_users.flat_map(&:verified_emails)
-          self.valid_usernames = input_names - invalid_names - valid_group_names
-          self.valid_emails = input_emails - invalid_emails
+        def initialize(project, names:, emails:)
+          @project = project
+          @input_names = names
+          @input_emails = emails
         end
+
+        def error_message
+          :inaccessible_owner
+        end
+
+        def output_groups
+          GroupsLoader.new(project, names: input_names).groups
+        end
+        strong_memoize_attr :output_groups
+
+        def output_users
+          UsersLoader.new(project, emails: input_emails, names: group_filtered_input_names).members
+        end
+        strong_memoize_attr :output_users
+
+        def valid_group_names
+          output_groups.map(&:full_path)
+        end
+        strong_memoize_attr :valid_group_names
+
+        def valid_usernames
+          group_filtered_input_names - invalid_names
+        end
+        strong_memoize_attr :valid_usernames
+
+        def invalid_names
+          group_filtered_input_names - output_users.map(&:username)
+        end
+        strong_memoize_attr :invalid_names
+
+        def invalid_emails
+          input_emails - output_users.flat_map(&:verified_emails)
+        end
+        strong_memoize_attr :invalid_emails
+
+        def valid_emails
+          input_emails - invalid_emails
+        end
+        strong_memoize_attr :valid_emails
+
+        private
+
+        attr_reader :project, :input_names, :input_emails
+
+        def group_filtered_input_names
+          input_names - valid_group_names
+        end
+        strong_memoize_attr :group_filtered_input_names
       end
     end
   end
--- ee/spec/lib/gitlab/code_owners/owner_validation/accessible_owners_filter_spec.rb
+++ ee/spec/lib/gitlab/code_owners/owner_validation/accessible_owners_filter_spec.rb
@@ -4,86 +4,188 @@ require 'spec_helper'
 
 RSpec.describe Gitlab::CodeOwners::OwnerValidation::AccessibleOwnersFilter, feature_category: :source_code_management do
   let_it_be(:project) { create(:project, :in_subgroup) }
+  let_it_be(:invalid_name) { 'not_a_user' }
+  let_it_be(:non_member) { create(:user) }
   let_it_be(:guest) { create(:user, guest_of: project) }
   let_it_be(:developer) { create(:user, developer_of: project) }
   let_it_be(:maintainer) { create(:user, maintainer_of: project) }
-  let_it_be(:non_member) { create(:user) }
-  let_it_be(:maintainer_email) { create(:email, :confirmed, :skip_validate, user: maintainer).email }
-  let_it_be(:non_member_email) { create(:email, :confirmed, :skip_validate, user: non_member).email }
+  let_it_be(:owner) { create(:user, owner_of: project) }
+  let_it_be(:invalid_email) { 'not_a_user@mail.com' }
+  let_it_be(:non_member_extra_email) { create(:email, :confirmed, :skip_validate, user: non_member).email }
+  let_it_be(:maintainer_extra_email) { create(:email, :confirmed, :skip_validate, user: maintainer).email }
+  let_it_be(:owner_unconfirmed_extra_email) { create(:email, :skip_validate, user: owner).email }
   let_it_be(:external_group) { create(:group) }
   let_it_be(:invited_group) { create(:project_group_link, project: project).group }
   let_it_be(:project_group) { project.group }
   let_it_be(:parent_group) { project_group.parent }
-  let_it_be(:invalid_names) { ['not_a_user', non_member.username, external_group.full_path] }
-  let_it_be(:valid_usernames) { [guest.username, developer.username, maintainer.username] }
-  let_it_be(:valid_group_names) { [invited_group.full_path, project_group.full_path, parent_group.full_path] }
-  let_it_be(:input_names) { invalid_names + valid_usernames + valid_group_names }
-  let_it_be(:invalid_emails) { ['not_a_user@mail.com', non_member.private_commit_email, non_member_email] }
-  let_it_be(:valid_emails) { [guest.email, developer.email, maintainer.private_commit_email, maintainer_email] }
-  let_it_be(:input_emails) { invalid_emails + valid_emails }
-  let_it_be(:output_users) { [guest, developer, maintainer] }
-  let_it_be(:output_groups) { [invited_group, project_group, parent_group] }
 
-  subject(:filter) do
-    described_class.new(project, names: input_names, emails: input_emails).tap(&:execute)
+  let_it_be(:filter) do
+    names = [
+      invalid_name,
+      non_member.username,
+      external_group.full_path,
+      developer.username,
+      maintainer.username,
+      invited_group.full_path,
+      project_group.full_path,
+      parent_group.full_path
+    ]
+
+    emails = [
+      invalid_email,
+      non_member.private_commit_email,
+      non_member_extra_email,
+      owner_unconfirmed_extra_email,
+      guest.email,
+      developer.email,
+      maintainer.private_commit_email,
+      maintainer_extra_email
+    ]
+    described_class.new(project, names: names, emails: emails)
   end
 
-  it 'finds all of the users who are project members assigns them to output_users' do
-    expect(filter.output_users).to match_array(output_users)
-  end
-
-  it 'finds all groups which are invited or ancestors of the project and assigns them to output_groups' do
-    expect(filter.output_groups).to match_array(output_groups)
-  end
-
-  it 'maps all of the invalid input_names to invalid_names' do
-    expect(filter.invalid_names).to match_array(invalid_names)
-  end
-
-  it 'maps all of the valid input_names matching users to valid_usernames' do
-    expect(filter.valid_usernames).to match_array(valid_usernames)
-  end
-
-  it 'maps all of the valid input_names matching groups to valid_group_names' do
-    expect(filter.valid_group_names).to match_array(valid_group_names)
-  end
-
-  it 'maps all of the invalid input_names to invalid_names' do
-    expect(filter.invalid_emails).to match_array(invalid_emails)
-  end
-
-  it 'maps all of the valid input_emails to valid_emails' do
-    expect(filter.valid_emails).to match_array(valid_emails)
-  end
-
-  it 'does not perform N+1 queries', :request_store, :use_sql_query_cache do
-    project_id = project.id
-    project = Project.find(project_id)
-
-    control = ActiveRecord::QueryRecorder.new(skip_cached: false) do
-      described_class.new(project, names: input_names, emails: input_emails).tap(&:execute)
+  describe '#output_groups' do
+    it 'returns invited and ancestral groups' do
+      expect(filter.output_groups).to contain_exactly(
+        invited_group,
+        project_group,
+        parent_group
+      )
     end
+  end
 
-    create(:email, :confirmed, :skip_validate, user: create(:user))
-    create(:email, :confirmed, :skip_validate, user: create(:user, guest_of: project))
-    extra_developer = create(:user, developer_of: project)
-    extra_developer_email = create(:email, :skip_validate, user: extra_developer).email
-    create(:group)
-    create(:project_group_link, project: project)
-    extra_group = create(:project_group_link, project: project).group
+  describe '#output_users' do
+    it 'returns project members' do
+      expect(filter.output_users).to contain_exactly(
+        guest,
+        developer,
+        maintainer
+      )
+    end
+  end
 
-    project = Project.find(project_id)
-    names = input_names + ['foo', extra_developer.username, extra_group.full_path]
-    emails = input_emails + ['foo@mail.com', extra_developer_email]
+  describe '#invalid_names' do
+    it 'returns all names that do not match an accessible group or user' do
+      expect(filter.invalid_names).to contain_exactly(
+        invalid_name,
+        non_member.username,
+        external_group.full_path
+      )
+    end
+  end
 
-    expect do
-      described_class.new(project, names: names, emails: emails).tap(&:execute)
-    end.to issue_same_number_of_queries_as(control)
+  describe '#invalid_emails' do
+    it 'returns all emails that do not match an accessible user or are not verified' do
+      expect(filter.invalid_emails).to contain_exactly(
+        invalid_email,
+        non_member.private_commit_email,
+        non_member_extra_email,
+        owner_unconfirmed_extra_email
+      )
+    end
+  end
+
+  describe '#valid_group_names' do
+    it 'returns all names that match an accessible group' do
+      expect(filter.valid_group_names).to contain_exactly(
+        invited_group.full_path,
+        project_group.full_path,
+        parent_group.full_path
+      )
+    end
+  end
+
+  describe '#valid_usernames' do
+    it 'returns all names that match an accessible user' do
+      expect(filter.valid_usernames).to contain_exactly(
+        developer.username,
+        maintainer.username
+      )
+    end
+  end
+
+  describe '#valid_emails' do
+    it 'returns all verified emails that match an accessible user' do
+      expect(filter.valid_emails).to contain_exactly(
+        guest.email,
+        developer.email,
+        maintainer.private_commit_email,
+        maintainer_extra_email
+      )
+    end
   end
 
   describe '#error_message' do
-    subject(:error_message) { described_class.new(project).error_message }
+    it 'returns an error message key to be applied to invalid entries' do
+      expect(filter.error_message).to eq(:inaccessible_owner)
+    end
+  end
 
-    it { is_expected.to eq(:inaccessible_owner) }
+  it 'avoids N+1 queries', :request_store, :use_sql_query_cache do
+    # Reload the project manually, outside of the control
+    project_id = project.id
+    project = Project.find(project_id)
+
+    names = [
+      invalid_name,
+      non_member.username,
+      external_group.full_path,
+      guest.username,
+      developer.username,
+      maintainer.username,
+      invited_group.full_path,
+      project_group.full_path,
+      parent_group.full_path
+    ]
+
+    emails = [
+      invalid_email,
+      non_member.private_commit_email,
+      non_member_extra_email,
+      guest.email,
+      developer.email,
+      maintainer.private_commit_email,
+      maintainer_extra_email
+    ]
+
+    control = ActiveRecord::QueryRecorder.new(skip_cached: false) do
+      filter = described_class.new(project, names: names, emails: emails)
+      filter.output_groups
+      filter.output_users
+      filter.valid_group_names
+      filter.valid_usernames
+      filter.valid_emails
+      filter.invalid_emails
+      filter.invalid_names
+    end
+
+    extra_developer = create(:user, developer_of: project)
+    extra_developer_email = create(:email, :skip_validate, user: extra_developer).email
+    extra_group = create(:project_group_link, project: project).group
+
+    names += ['foo', extra_developer.username, extra_group.full_path]
+    emails += ['foo@mail.com', extra_developer_email]
+
+    create(:email, :confirmed, :skip_validate, user: create(:user))
+    create(:email, :confirmed, :skip_validate, user: create(:user, guest_of: project))
+    create(:group)
+    create(:project_group_link, project: project)
+
+    # Clear the RequestStore to ensure we do not have a warm cache
+    RequestStore.clear!
+
+    # Refind the project to reset the associations
+    project = Project.find(project_id)
+
+    expect do
+      filter = described_class.new(project, names: names, emails: emails)
+      filter.output_groups
+      filter.output_users
+      filter.valid_group_names
+      filter.valid_usernames
+      filter.valid_emails
+      filter.invalid_names
+      filter.invalid_emails
+    end.to issue_same_number_of_queries_as(control)
   end
 end
