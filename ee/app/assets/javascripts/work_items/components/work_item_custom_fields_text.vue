537396622de3e0fd00c3f6799df4f2c3d42d0235
Coung Ngo <cngo@gitlab.com>
2025-04-01 11:15:53 +0000

    Migrate WorkItemCustomFieldsText to use WorkItemSidebarWidget
    Update WorkItemCustomFieldsText to use WorkItemSidebarWidget
    for maintainability and consistency.

--- ee/app/assets/javascripts/work_items/components/work_item_custom_fields_text.vue
+++ ee/app/assets/javascripts/work_items/components/work_item_custom_fields_text.vue
@@ -1,37 +1,37 @@
 <script>
 import {
   GlButton,
-  GlForm,
+  GlFormGroup,
   GlFormInput,
-  GlLoadingIcon,
   GlLink,
   GlTooltipDirective,
   GlTruncate,
 } from '@gitlab/ui';
+import { n__ } from '~/locale';
 import * as Sentry from '~/sentry/sentry_browser_wrapper';
 import {
   CUSTOM_FIELDS_TYPE_TEXT,
-  sprintfWorkItem,
   I18N_WORK_ITEM_ERROR_UPDATING,
+  sprintfWorkItem,
 } from '~/work_items/constants';
 import updateWorkItemCustomFieldsMutation from 'ee/work_items/graphql/update_work_item_custom_fields.mutation.graphql';
 import { isValidURL } from '~/lib/utils/url_utility';
+import WorkItemSidebarWidget from '~/work_items/components/shared/work_item_sidebar_widget.vue';
 
 export const CHARACTER_LIMIT = 1024;
 
 export default {
   CHARACTER_LIMIT,
-  inputId: 'custom-field-text-input',
   directives: {
     GlTooltip: GlTooltipDirective,
   },
   components: {
     GlButton,
-    GlForm,
+    GlFormGroup,
     GlFormInput,
-    GlLoadingIcon,
     GlLink,
     GlTruncate,
+    WorkItemSidebarWidget,
   },
   props: {
     canUpdate: {
@@ -61,12 +61,8 @@ export default {
   },
   data() {
     return {
-      isEditing: false,
       isUpdating: false,
-      clickingClearButton: false,
       value: this.customField.value,
-      charsLeft: null,
-      displayCharsLeft: false,
     };
   },
   computed: {
@@ -76,11 +72,25 @@ export default {
     label() {
       return this.customField.customField?.name;
     },
+    charactersRemaining() {
+      return this.value == null || this.isValueEmpty
+        ? CHARACTER_LIMIT
+        : CHARACTER_LIMIT - this.value.length;
+    },
     hasValue() {
       if (this.isValueValid) {
         return !this.isValueEmpty;
       }
       return false;
+    },
+    inputWarning() {
+      // only display warning if we're over 90% the characters limit
+      return this.charactersRemaining <= CHARACTER_LIMIT * 0.1
+        ? n__('%d character remaining.', '%d characters remaining.', this.charactersRemaining)
+        : undefined;
+    },
+    isLink() {
+      return isValidURL(this.customField.value);
     },
     isValueValid() {
       return this.value !== null && typeof this.value === 'string';
@@ -103,50 +113,28 @@ export default {
         this.value = customField.value;
       },
     },
-    // Need this check to manage the characters limit warning
-    value: {
-      immediate: true,
-      handler() {
-        if (this.isValueValid) {
-          this.checkDisplayCharsLeft();
-        }
-      },
-    },
   },
   methods: {
-    blurInput() {
-      this.$refs.input.$el.blur();
+    cancelEditing(stopEditing) {
+      this.resetText();
+      stopEditing();
     },
-    handleFocus() {
-      this.isEditing = true;
+    clearText(stopEditing) {
+      this.value = '';
+      stopEditing();
+      this.updateText();
     },
-    isLink(value) {
-      return isValidURL(value);
+    resetText() {
+      this.value = this.customField.value;
     },
-    getCharsLeft() {
-      if (this.value == null || this.isValueEmpty) {
-        return CHARACTER_LIMIT;
-      }
-      return CHARACTER_LIMIT - this.value.length;
-    },
-    checkDisplayCharsLeft() {
-      this.charsLeft = this.getCharsLeft();
+    updateText() {
+      if (!this.canUpdate) return;
 
-      // only display warning if we're over 90% the characters limit
-      this.displayCharsLeft = this.charsLeft <= CHARACTER_LIMIT * 0.1;
-    },
-    updateTextFromInput() {
-      if (this.value?.trim() === '') {
-        this.updateText(null);
+      const textValue = this.value?.trim() === '' ? null : this.value;
+
+      if (textValue === this.customField.value) {
         return;
       }
-
-      const value = String(this.value);
-      this.updateText(value);
-    },
-    updateText(text) {
-      if (this.clickingClearButton) return;
-      if (!this.canUpdate) return;
 
       this.isUpdating = true;
 
@@ -159,7 +147,7 @@ export default {
               customFieldsWidget: [
                 {
                   customFieldId: this.customFieldId,
-                  textValue: text,
+                  textValue,
                 },
               ],
             },
@@ -171,13 +159,12 @@ export default {
           }
         })
         .catch((error) => {
-          const msg = sprintfWorkItem(I18N_WORK_ITEM_ERROR_UPDATING, this.workItemType);
-          this.$emit('error', msg);
+          this.resetText();
+          this.$emit('error', sprintfWorkItem(I18N_WORK_ITEM_ERROR_UPDATING, this.workItemType));
           Sentry.captureException(error);
         })
         .finally(() => {
           this.isUpdating = false;
-          this.isEditing = false;
         });
     },
   },
@@ -185,91 +172,69 @@ export default {
 </script>
 
 <template>
-  <div v-if="displayWidget">
-    <div class="gl-flex gl-items-center gl-justify-between">
-      <!-- hide header when editing, since we then have a form label. Keep it reachable for screenreader nav  -->
-      <h3 :class="{ 'gl-sr-only': isEditing }" class="gl-heading-5 !gl-mb-0">
-        {{ label }}
-      </h3>
-      <gl-button
-        v-if="canUpdate && !isEditing"
-        data-testid="edit-text"
-        class="flex-shrink-0"
-        category="tertiary"
-        size="small"
-        @click="isEditing = true"
-        >{{ __('Edit') }}</gl-button
-      >
-    </div>
-    <gl-form v-if="isEditing" @submit.prevent="blurInput">
-      <div class="gl-flex gl-items-center">
-        <label :for="$options.inputId" class="gl-mb-0">{{ label }}</label>
-        <gl-loading-icon v-if="isUpdating" size="sm" inline class="gl-ml-3" />
-        <gl-button
-          data-testid="apply-text"
-          category="tertiary"
-          size="small"
-          class="gl-ml-auto"
-          :disabled="isUpdating"
-          @click="isEditing = false"
-          >{{ __('Apply') }}</gl-button
-        >
-      </div>
-      <!-- wrapper for the form input so the borders fit inside the sidebar -->
-      <div class="gl-relative gl-px-2">
-        <gl-form-input
-          :id="$options.inputId"
-          ref="input"
-          v-model="value"
-          :maxlength="$options.CHARACTER_LIMIT"
-          class="hide-unfocused-input-decoration gl-block"
-          :disabled="isUpdating"
-          :placeholder="__('Enter text')"
-          autofocus
-          @blur="updateTextFromInput"
-        />
-        <gl-button
-          v-if="showRemoveValue"
-          v-gl-tooltip
-          category="tertiary"
-          size="small"
-          icon="clear"
-          class="gl-clear-icon-button gl-absolute gl-right-3 gl-top-2"
-          :title="__('Remove text')"
-          :aria-label="__('Remove text')"
-          @mousedown="clickingClearButton = true"
-          @mouseup="clickingClearButton = false"
-          @click="updateText(null)"
-        />
-      </div>
-      <span v-if="displayCharsLeft" class="gl-text-subtle">{{
-        n__('%d character remaining.', '%d characters remaining.', charsLeft)
-      }}</span>
-    </gl-form>
-    <template v-else-if="hasValue">
-      <div v-if="isLink(value)" class="gl-flex">
-        <gl-link
-          v-gl-tooltip
-          is-unsafe-link
-          target="_blank"
-          class="gl-truncate"
-          :href="value"
-          :title="value"
-          data-testid="custom-field-value"
-        >
-          {{ value }}
-        </gl-link>
-      </div>
-      <template v-else>
-        <span v-gl-tooltip :title="value">
+  <work-item-sidebar-widget
+    v-if="displayWidget"
+    :can-update="canUpdate"
+    :is-updating="isUpdating"
+    @stopEditing="updateText"
+  >
+    <template #title>
+      {{ label }}
+    </template>
+    <template #content>
+      <template v-if="hasValue">
+        <div v-if="isLink" class="gl-flex">
+          <gl-link
+            v-gl-tooltip
+            is-unsafe-link
+            target="_blank"
+            class="gl-truncate"
+            :href="value"
+            :title="value"
+            data-testid="custom-field-value"
+          >
+            {{ value }}
+          </gl-link>
+        </div>
+        <span v-else v-gl-tooltip :title="value">
           <gl-truncate :text="value" data-testid="custom-field-value" />
         </span>
       </template>
+      <div v-else class="gl-text-subtle" data-testid="custom-field-value">{{ __('None') }}</div>
     </template>
-    <template v-else>
-      <div class="gl-text-subtle" data-testid="custom-field-value">{{ __('None') }}</div>
+    <template #editing-content="{ stopEditing }">
+      <div class="gl-relative gl-px-2">
+        <gl-form-group
+          :description="inputWarning"
+          :label="label"
+          label-for="custom-field-text-input"
+          label-sr-only
+        >
+          <gl-form-input
+            id="custom-field-text-input"
+            v-model="value"
+            autofocus
+            :disabled="isUpdating"
+            :maxlength="$options.CHARACTER_LIMIT"
+            :placeholder="__('Enter text')"
+            @keydown.enter="stopEditing"
+            @keydown.exact.esc.stop="cancelEditing(stopEditing)"
+          />
+        </gl-form-group>
+        <gl-button
+          v-if="showRemoveValue"
+          v-gl-tooltip
+          class="gl-absolute gl-right-3 gl-top-2"
+          category="tertiary"
+          icon="clear"
+          size="small"
+          :title="__('Remove text')"
+          :aria-label="__('Remove text')"
+          @click="clearText(stopEditing)"
+        />
+      </div>
     </template>
-  </div>
+  </work-item-sidebar-widget>
 </template>
 
 <style scoped>
