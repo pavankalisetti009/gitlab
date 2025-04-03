4cd4a4254cde1324baf2e39ed5693dd1949dd6a8
Nikola Milojevic <nmilojevic@gitlab.com>
2025-04-03 14:26:56 +0200

    Bump gitlab-cloud-connector version to 1.6

--- ee/spec/lib/cloud_connector/self_signed/access_data_reader_spec.rb
+++ ee/spec/lib/cloud_connector/self_signed/access_data_reader_spec.rb
@@ -4,7 +4,28 @@ require 'spec_helper'
 
 RSpec.describe CloudConnector::SelfSigned::AccessDataReader, feature_category: :cloud_connector do
   describe '#read_available_services' do
-    subject(:available_services) { described_class.new.read_available_services }
+    let(:access_data_reader) { described_class.new }
+    let(:access_data) do
+      {
+        services: {
+          duo_chat: {
+            cut_off_date: '2024-02-15 00:00:00 UTC',
+            bundled_with: {
+              add_on_1: {
+                unit_primitives: %w[duo_chat]
+              }
+            },
+            backend: 'ai_gateway'
+          }
+        }
+      }.deep_stringify_keys
+    end
+
+    subject(:available_services) { access_data_reader.read_available_services }
+
+    before do
+      allow(access_data_reader).to receive(:access_record_data).and_return(access_data)
+    end
 
     it 'parses the service data correctly' do
       expect(available_services).to include({
@@ -14,8 +35,8 @@ RSpec.describe CloudConnector::SelfSigned::AccessDataReader, feature_category: :
 
     it 'configures AvailableServiceData objects correctly' do
       expect(available_services[:duo_chat].name).to eq(:duo_chat)
-      expect(available_services[:duo_chat].cut_off_date).to eq(Time.zone.parse("2024-10-17T00:00:00Z"))
-      expect(available_services[:duo_chat].add_on_names).to match_array(%w[code_suggestions duo_enterprise])
+      expect(available_services[:duo_chat].cut_off_date).to eq(Time.zone.parse("2024-02-15 00:00:00 UTC"))
+      expect(available_services[:duo_chat].add_on_names).to match_array(%w[add_on_1])
     end
   end
 end
