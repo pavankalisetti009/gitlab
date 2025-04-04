156b03050e9284db828ba5ba01ecfcba5addbaa6
Payton Burdette <pburdette@gitlab.com>
2025-04-04 12:18:21 -0400

    Add polling to last commit
    Add 30 second polling interval to last commit
    to get fresh pipeline IDs.
    
    Changelog: changed

--- app/assets/javascripts/repository/components/last_commit.vue
+++ app/assets/javascripts/repository/components/last_commit.vue
@@ -18,6 +18,8 @@ import CommitInfo from './commit_info.vue';
 import CollapsibleCommitInfo from './collapsible_commit_info.vue';
 
 const trackingMixin = InternalEvents.mixin();
+const POLL_INTERVAL = 30000;
+
 export default {
   components: {
     CiIcon,
@@ -60,6 +62,7 @@ export default {
       error(error) {
         throw error;
       },
+      pollInterval: POLL_INTERVAL,
       subscribeToMore: {
         document() {
           return pipelineCiStatusUpdatedSubscription;
