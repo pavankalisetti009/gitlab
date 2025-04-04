223114eb4b2aacb4ffada29a0d215eb2c4297a37
Phil Hughes <me@iamphill.com>
2025-04-04 16:48:11 +0100

    Filter out merge requests in lists with Duo bot reviewers
    For the returned to you list we will only return merge requests where there are reviews
    left and that the only review left is not from the bot user.
    For the assigned to you list we will return merge requests that have no reviewer
    or if the only reviewer is the Duo bot user.
    
    https://gitlab.com/gitlab-org/gitlab/-/issues/521381

--- app/helpers/merge_requests_helper.rb
+++ app/helpers/merge_requests_helper.rb
@@ -586,7 +586,11 @@ module MergeRequestsHelper
                 helpContent: _('Reviewers left feedback, or requested changes from you, on these merge requests.'),
                 query: is_author_or_assignee ? 'authorOrAssigneeMergeRequests' : 'assignedMergeRequests',
                 variables: {
-                  reviewStates: %w[REVIEWED REQUESTED_CHANGES]
+                  reviewStates: %w[REVIEWED REQUESTED_CHANGES],
+                  not: {
+                    onlyReviewer: true,
+                    reviewerUsername: 'GitlabDuo'
+                  }
                 }
               },
               {
@@ -611,7 +615,10 @@ module MergeRequestsHelper
 
                 query: is_author_or_assignee ? 'authorOrAssigneeMergeRequests' : 'assignedMergeRequests',
                 variables: {
-                  reviewerWildcardId: 'NONE'
+                  or: {
+                    reviewerWildcard: 'NONE',
+                    onlyReviewerUsername: 'GitlabDuo'
+                  }
                 }
               }
             ],
