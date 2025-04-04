33e9a2c0c11c5c77484d5bb43ad5d79636d29e90
Brian Williams <bwilliams@gitlab.com>
2025-04-04 09:52:55 -0500

    Ensure files are deleted from storage alongside dependency list exports
    Changelog: fixed
    EE: true

--- ee/spec/workers/sbom/delete_expired_exports_worker_spec.rb
+++ ee/spec/workers/sbom/delete_expired_exports_worker_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe Sbom::DeleteExpiredExportsWorker, feature_category: :dependency_management do
   describe '#perform' do
     let!(:expired_now) { create(:dependency_list_export, expires_at: Time.zone.now) }
-    let!(:expired_recently) { create(:dependency_list_export, expires_at: 1.hour.ago) }
+    let!(:expired_recently) { create(:dependency_list_export, :with_file, expires_at: 1.hour.ago) }
     let!(:expiring_soon) { create(:dependency_list_export, expires_at: 1.hour.from_now) }
     let!(:not_set) { create(:dependency_list_export, expires_at: nil) }
 
@@ -20,6 +20,12 @@ RSpec.describe Sbom::DeleteExpiredExportsWorker, feature_category: :dependency_m
         .and change { exists?(expired_recently) }.from(true).to(false)
         .and not_change { exists?(expiring_soon) }
         .and not_change { exists?(not_set) }
+    end
+
+    it 'deletes the file from the file storage', :sidekiq_inline do
+      files = Upload.all.map(&:absolute_path)
+
+      expect { perform }.to change { files.map { |f| File.exist?(f) }.uniq }.from([true]).to([false])
     end
   end
 end
