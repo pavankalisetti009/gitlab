2dcf896c15640dd66e5d85616149c177f9c02fc5
Suraj Tripathi <stripathi@gitlab.com>
2025-04-04 15:24:45 +0530

    DRY'd member_eligible_for_admin_event?

--- ee/app/services/ee/members/create_service.rb
+++ ee/app/services/ee/members/create_service.rb
@@ -257,16 +257,6 @@ module EE
           current_user&.can_admin_all_resources? &&
           at_least_one_member_created?
       end
-
-      def member_eligible_for_admin_event?(member)
-        member.persisted? &&
-          member.errors.empty? &&
-          member.user_id.present? &&
-          promotion_management_required_for_role?(
-            new_access_level: member.access_level,
-            member_role_id: member.member_role_id
-          )
-      end
     end
   end
 end
