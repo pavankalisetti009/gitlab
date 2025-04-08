f26b98f7e1aa0a9207032bc852e4064c2f94fce2
Vijay Hawoldar <vhawoldar@gitlab.com>
2025-04-08 09:28:15 +0100

    Removes organization_id default value
    GitlabSubscription::SeatAssignment recently added the organization_id
    column with a default value of 1, this removes that default value and
    relies on application code to populate the NOT NULL column instead
    
    Changelog: changed

--- ee/app/models/gitlab_subscriptions/seat_assignment.rb
+++ ee/app/models/gitlab_subscriptions/seat_assignment.rb
@@ -2,6 +2,10 @@
 
 module GitlabSubscriptions
   class SeatAssignment < ApplicationRecord
+    include SafelyChangeColumnDefault
+
+    columns_changing_default :organization_id
+
     belongs_to :namespace, optional: false
     belongs_to :user, optional: false
     belongs_to :organization, class_name: 'Organizations::Organization'
