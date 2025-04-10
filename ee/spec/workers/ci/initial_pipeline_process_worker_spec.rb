dc1279d63e401b79bf754a5cbd93f58a66ea7f30
Tianwen Chen <tchen@gitlab.com>
2025-04-10 14:54:03 +1000

    Only ensure_persistent_ref on StartPipelineService

--- ee/spec/workers/ci/initial_pipeline_process_worker_spec.rb
+++ ee/spec/workers/ci/initial_pipeline_process_worker_spec.rb
@@ -4,9 +4,10 @@ require 'spec_helper'
 
 RSpec.describe Ci::InitialPipelineProcessWorker, '#perform', :freeze_time, feature_category: :continuous_integration do
   let_it_be(:namespace) { create(:namespace, :with_used_build_minutes_limit) }
-  let_it_be(:project) { create(:project, namespace: namespace) }
+  let_it_be(:project) { create(:project, :repository, namespace: namespace) }
+  let_it_be(:sha) { project.repository.commit.sha }
   let_it_be_with_reload(:pipeline) do
-    create(:ci_pipeline, :with_job, project: project, status: :created)
+    create(:ci_pipeline, :with_job, sha: sha, project: project, status: :created)
   end
 
   let_it_be(:instance_runner) { create(:ci_runner, :instance, :online) }
