0baaf19e30062504877d4cc183859aa72313b8ef
Peter Hegman <phegman@gitlab.com>
2025-04-10 11:25:45 -0700

    Hook up show children button to API request
    On `Your work` -> `Groups`

--- ee/app/assets/javascripts/groups/your_work/graphql/utils.js
+++ ee/app/assets/javascripts/groups/your_work/graphql/utils.js
@@ -2,6 +2,8 @@ import { formatGroup as CeFormatGroup } from '~/groups/your_work/graphql/utils';
 
 export const formatGroup = (group) => ({
   ...CeFormatGroup(group),
+  // Overwrite CE `children` key to avoid circular references
+  children: group.children?.length ? group.children.map(formatGroup) : [],
   // Properties below are hard coded for now until API has been
   // updated to support these fields.
   markedForDeletionOn: null,
