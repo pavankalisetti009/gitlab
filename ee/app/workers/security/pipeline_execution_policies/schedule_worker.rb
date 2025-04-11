d17c87d18802f9dd7b54ac09058cb39783f1a8f0
Dominic Bauer <dbauer@gitlab.com>
2025-04-11 10:41:02 +0200

    Add Prometheus histogram for scheduled pipeline execution scheduling
    Changelog: changed
    EE: true

--- ee/app/workers/security/pipeline_execution_policies/schedule_worker.rb
+++ ee/app/workers/security/pipeline_execution_policies/schedule_worker.rb
@@ -10,6 +10,7 @@ module Security
 
       LEASE_KEY = 'security_pipeline_execution_policies_schedule'
       LEASE_TIMEOUT = 5.minutes
+      HISTOGRAM = :gitlab_security_policies_pipeline_execution_policy_scheduling_duration_seconds
 
       idempotent!
 
@@ -18,32 +19,36 @@ module Security
 
       def perform
         try_obtain_lease do
-          scope = Security::PipelineExecutionProjectSchedule
-            .runnable_schedules
-            .including_security_policy_and_project
-            .ordered_by_next_run_at
+          measure(HISTOGRAM) do
+            scope = Security::PipelineExecutionProjectSchedule
+                      .runnable_schedules
+                      .including_security_policy_and_project
+                      .ordered_by_next_run_at
 
-          iterator = Gitlab::Pagination::Keyset::Iterator.new(scope: scope)
+            iterator = Gitlab::Pagination::Keyset::Iterator.new(scope: scope)
 
-          iterator.each_batch(of: 1000) do |schedules|
-            schedules.each do |schedule|
-              next unless Feature.enabled?(:scheduled_pipeline_execution_policies, schedule.project)
+            iterator.each_batch(of: 1000) do |schedules|
+              schedules.each do |schedule|
+                next unless Feature.enabled?(:scheduled_pipeline_execution_policies, schedule.project)
 
-              unless valid_cadence?(schedule.cron)
-                log_invalid_cadence_error(schedule.project_id, schedule.cron)
+                unless valid_cadence?(schedule.cron)
+                  log_invalid_cadence_error(schedule.project_id, schedule.cron)
 
-                next
+                  next
+                end
+
+                enqueue_within_time_window(schedule)
+
+                schedule.schedule_next_run!
               end
-
-              enqueue_within_time_window(schedule)
-
-              schedule.schedule_next_run!
             end
           end
         end
       end
 
       private
+
+      delegate :measure, to: ::Security::SecurityOrchestrationPolicies::ObserveHistogramsService
 
       def enqueue_within_time_window(schedule)
         return if schedule.snoozed?
