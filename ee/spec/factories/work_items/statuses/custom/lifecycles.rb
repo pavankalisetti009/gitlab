f6cca638c3ec8f25b782f9ad52b4b710cbb6fc9f
Agnes Slota <aslota@gitlab.com>
2025-04-15 20:10:15 +1000

    Ensure default statuses are included in lifecycle
    Add a callback to automatically include default
    statuses in custom work item lifecycles.
    
    This ensures that:
    - Default open, closed, and duplicate statuses
    are always part of the lifecycle
    - Statuses are added during validation to maintain
    data integrity
    - No duplicate statuses are created when
    updating default statuses

--- ee/spec/factories/work_items/statuses/custom/lifecycles.rb
+++ ee/spec/factories/work_items/statuses/custom/lifecycles.rb
@@ -3,9 +3,9 @@
 FactoryBot.define do
   factory :work_item_custom_lifecycle, class: 'WorkItems::Statuses::Custom::Lifecycle' do
     association :namespace
-    association :default_open_status, factory: [:work_item_custom_status, :open]
-    association :default_closed_status, factory: [:work_item_custom_status, :closed]
-    association :default_duplicate_status, factory: [:work_item_custom_status, :duplicate]
+    default_open_status { association :work_item_custom_status, :open, namespace: namespace }
+    default_closed_status { association :work_item_custom_status, :closed, namespace: namespace }
+    default_duplicate_status { association :work_item_custom_status, :duplicate, namespace: namespace }
     sequence(:name) { |n| "Custom Lifecycle #{n}" }
   end
 end
--- ee/spec/models/work_items/statuses/custom/lifecycle_spec.rb
+++ ee/spec/models/work_items/statuses/custom/lifecycle_spec.rb
@@ -3,7 +3,19 @@
 require 'spec_helper'
 
 RSpec.describe WorkItems::Statuses::Custom::Lifecycle, feature_category: :team_planning do
-  subject(:custom_lifecycle) { build_stubbed(:work_item_custom_lifecycle) }
+  let_it_be(:namespace) { create(:group) }
+  let_it_be(:open_status) { create(:work_item_custom_status, :open, namespace: namespace) }
+  let_it_be(:closed_status) { create(:work_item_custom_status, :closed, namespace: namespace) }
+  let_it_be(:duplicate_status) { create(:work_item_custom_status, :duplicate, namespace: namespace) }
+
+  subject(:custom_lifecycle) do
+    build(:work_item_custom_lifecycle,
+      namespace: namespace,
+      default_open_status: open_status,
+      default_closed_status: closed_status,
+      default_duplicate_status: duplicate_status
+    )
+  end
 
   describe 'associations' do
     it { is_expected.to belong_to(:namespace) }
@@ -21,6 +33,9 @@ RSpec.describe WorkItems::Statuses::Custom::Lifecycle, feature_category: :team_p
     it { is_expected.to validate_presence_of(:namespace) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(255) }
+    it { is_expected.to validate_presence_of(:default_open_status) }
+    it { is_expected.to validate_presence_of(:default_closed_status) }
+    it { is_expected.to validate_presence_of(:default_duplicate_status) }
 
     context 'with uniqueness validations' do
       subject(:custom_lifecycle) { create(:work_item_custom_lifecycle) }
@@ -28,8 +43,85 @@ RSpec.describe WorkItems::Statuses::Custom::Lifecycle, feature_category: :team_p
       it { is_expected.to validate_uniqueness_of(:name).scoped_to(:namespace_id) }
     end
 
-    it { is_expected.to validate_presence_of(:default_open_status) }
-    it { is_expected.to validate_presence_of(:default_closed_status) }
-    it { is_expected.to validate_presence_of(:default_duplicate_status) }
+    describe '#validate_default_status_categories' do
+      context 'with invalid category combinations' do
+        it 'is invalid when default_open_status has wrong category' do
+          custom_lifecycle = build(:work_item_custom_lifecycle,
+            namespace: namespace,
+            default_open_status: closed_status,
+            default_closed_status: closed_status,
+            default_duplicate_status: duplicate_status
+          )
+
+          expect(custom_lifecycle).to be_invalid
+          expect(custom_lifecycle.errors[:default_open_status])
+            .to include(/must be of category triage or to_do or in_progress/)
+        end
+
+        it 'is invalid when default_closed_status has wrong category' do
+          custom_lifecycle = build(:work_item_custom_lifecycle,
+            namespace: namespace,
+            default_open_status: open_status,
+            default_closed_status: open_status,
+            default_duplicate_status: duplicate_status
+          )
+
+          expect(custom_lifecycle).to be_invalid
+          expect(custom_lifecycle.errors[:default_closed_status]).to include(/must be of category done or cancelled/)
+        end
+
+        it 'is invalid when default_duplicate_status has wrong category' do
+          custom_lifecycle = build(:work_item_custom_lifecycle,
+            namespace: namespace,
+            default_open_status: open_status,
+            default_closed_status: closed_status,
+            default_duplicate_status: open_status
+          )
+
+          expect(custom_lifecycle).to be_invalid
+          expect(custom_lifecycle.errors[:default_duplicate_status]).to include(/must be of category done or cancelled/)
+        end
+      end
+    end
+  end
+
+  describe 'callbacks' do
+    describe '#ensure_default_statuses_in_lifecycle' do
+      context 'when creating a new lifecycle' do
+        it 'automatically adds default statuses to the lifecycle statuses' do
+          expect { custom_lifecycle.save! }.to change { custom_lifecycle.statuses.count }.from(0).to(3)
+
+          expect(custom_lifecycle.statuses).to include(open_status)
+          expect(custom_lifecycle.statuses).to include(closed_status)
+          expect(custom_lifecycle.statuses).to include(duplicate_status)
+        end
+      end
+
+      context 'when updating an existing lifecycle' do
+        let_it_be(:new_open_status) do
+          create(:work_item_custom_status, :open, name: "Ready for development", namespace: namespace)
+        end
+
+        before do
+          custom_lifecycle.save!
+        end
+
+        it 'adds new default statuses to the lifecycle statuses' do
+          expect do
+            custom_lifecycle.update!(default_open_status: new_open_status)
+          end.to change { custom_lifecycle.statuses.count }.by(1)
+
+          expect(custom_lifecycle.statuses).to include(new_open_status)
+          expect(custom_lifecycle.statuses).to include(closed_status)
+          expect(custom_lifecycle.statuses).to include(duplicate_status)
+        end
+
+        it 'does not duplicate statuses already in the collection' do
+          expect do
+            custom_lifecycle.update!(default_open_status: open_status)
+          end.not_to change { custom_lifecycle.statuses.count }
+        end
+      end
+    end
   end
 end
