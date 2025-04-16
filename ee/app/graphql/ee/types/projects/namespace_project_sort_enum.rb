ae9fed7232b64f544fe8506edab88d14d214bfec
Sheldon Led <sheldonled@gitlab.com>
2025-04-16 11:11:23 +0100

    Remove GraphQL `STORAGE` enum in NamespaceProjectSortEnum
    We no longer support `STORAGE` enum in NamespaceProjectSortEnum
    Replace its usage with `EXCESS_REPO_STORAGE_SIZE_DESC` or any other
    available enum
    
    EE: true
    Changelog: removed

--- ee/app/graphql/ee/types/projects/namespace_project_sort_enum.rb
+++ ee/app/graphql/ee/types/projects/namespace_project_sort_enum.rb
@@ -7,12 +7,6 @@ module EE
         extend ActiveSupport::Concern
 
         prepended do
-          value 'STORAGE', 'Sort by excess repository storage size, descending order.',
-            value: :excess_repo_storage_size_desc,
-            deprecated: {
-              reason: 'Please use EXCESS_REPO_STORAGE_SIZE_DESC',
-              milestone: '16.9'
-            }
           value 'EXCESS_REPO_STORAGE_SIZE_DESC', 'Sort by excess repository storage size, descending order.',
             value: :excess_repo_storage_size_desc
         end
