34b061b9fb78a5ba9eb628be92f48349759383dd
Ayush Billore <abillore@gitlab.com>
2025-04-18 23:46:49 +0530

    Move delayed deletion filters to CE in groups API

--- ee/spec/requests/api/groups_spec.rb
+++ ee/spec/requests/api/groups_spec.rb
@@ -187,35 +187,6 @@ RSpec.describe API::Groups, :with_current_organization, :aggregate_failures, fea
       end
     end
 
-    context 'when using the marked_for_deletion_on filter' do
-      let_it_be(:group_with_deletion_on) { create(:group_with_deletion_schedule, name: "group_with_deletion_on", marked_for_deletion_on: Date.parse('2024-01-01'), owners: user) }
-      let_it_be(:group_without_deletion) { create(:group, name: "group_without_deletion", owners: user) }
-      let(:response_groups) { json_response.map { |group| group['id'] } }
-
-      before do
-        stub_licensed_features(adjourned_deletion_for_projects_and_groups: true)
-      end
-
-      it 'returns groups marked for deletion on the specified date' do
-        get api("/groups", user), params: { marked_for_deletion_on: Date.parse('2024-01-01') }
-
-        expect(response).to have_gitlab_http_status(:ok)
-        expect(response).to include_pagination_headers
-        expect(json_response).to be_an Array
-        expect(response_groups).to contain_exactly(group_with_deletion_on.id)
-        expect(json_response.map { |group| group["marked_for_deletion_on"] }).to contain_exactly(Date.parse('2024-01-01').iso8601)
-      end
-
-      it 'returns all groups when marked_for_deletion_on is not specified' do
-        get api("/groups", user)
-
-        expect(response).to have_gitlab_http_status(:ok)
-        expect(response).to include_pagination_headers
-        expect(json_response).to be_an Array
-        expect(response_groups).to contain_exactly(group.id, group_with_deletion_on.id, group_without_deletion.id)
-      end
-    end
-
     context 'file_template_project_id is a private project' do
       let_it_be(:private_project) { create(:project, :private, group: group) }
 
