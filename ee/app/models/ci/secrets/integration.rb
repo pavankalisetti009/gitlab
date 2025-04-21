cc34dd4770561976f3215e886672b2f2686d7dc6
Jayakrishnan Mallissery <jmallissery@gitlab.com>
2025-04-21 16:16:21 +0200

    Refactor & Optimise Ci::Secrets::Integration class
    Prior to this change, in the
    Ci::Secrets::Integration class, the
    Ci::Variables::Collection instance was converted to a hash and then
    to check for a specific value, we were searching for the value
    in the hash. This is completely unnecessary and inefficient as the
    Ci::Variables::Collection class already has a method to fetch a value
    by key. By converting to hash, we are wasting memory and CPU cycles.
    
    This change fixes the issue by reusing the
    existing method thereby avoiding converting it into a hash and then
    searching the hash.
    
    Issue
    https://gitlab.com/gitlab-org/gitlab/-/issues/535399

--- ee/app/models/ci/secrets/integration.rb
+++ ee/app/models/ci/secrets/integration.rb
@@ -36,38 +36,28 @@ module Ci
         end
       end
 
-      def variable_value(key, default = nil)
-        variables_hash.fetch(key, default)
-      end
-
       private
 
       attr_reader :variables
 
-      def variables_hash
-        @variables_hash ||= variables.to_h do |variable|
-          [variable[:key], variable[:value]]
-        end
-      end
-
       def gcp_secret_manager?
-        variable_value('GCP_PROJECT_NUMBER').present? &&
-          variable_value('GCP_WORKLOAD_IDENTITY_FEDERATION_POOL_ID').present? &&
-          variable_value('GCP_WORKLOAD_IDENTITY_FEDERATION_PROVIDER_ID').present?
+        variables['GCP_PROJECT_NUMBER']&.value.present? &&
+          variables['GCP_WORKLOAD_IDENTITY_FEDERATION_POOL_ID']&.value.present? &&
+          variables['GCP_WORKLOAD_IDENTITY_FEDERATION_PROVIDER_ID']&.value.present?
       end
 
       def azure_key_vault?
-        variable_value('AZURE_KEY_VAULT_SERVER_URL').present? &&
-          variable_value('AZURE_CLIENT_ID').present? &&
-          variable_value('AZURE_TENANT_ID').present?
+        variables['AZURE_KEY_VAULT_SERVER_URL']&.value.present? &&
+          variables['AZURE_CLIENT_ID']&.value.present? &&
+          variables['AZURE_TENANT_ID']&.value.present?
       end
 
       def hashicorp_vault?
-        variable_value('VAULT_SERVER_URL').present?
+        variables['VAULT_SERVER_URL']&.value.present?
       end
 
       def akeyless?
-        variable_value('AKEYLESS_ACCESS_ID').present?
+        variables['AKEYLESS_ACCESS_ID']&.value.present?
       end
 
       def gitlab_secrets_manager?
