d90b6011e9b7ecd4dfe93af8d1b49b0eabd8f558
Chad Lavimoniere <clavimoniere@gitlab.com>
2025-04-22 15:13:24 -0400

    Move work item confidentiality loading spinner to actions dropdown
    - Remove update in progress loading spinner from work item metadata and
      work item sticky header version of metadata
    - Add loading spinner to work item actions confidentiality toggle item
    - Add logic to wait for confidentiality update to resolve before closing
      dropdown and showing toast
    
    Changelog: changed

--- app/assets/javascripts/work_items/components/work_item_created_updated.vue
+++ app/assets/javascripts/work_items/components/work_item_created_updated.vue
@@ -31,11 +31,6 @@ export default {
       required: false,
       default: null,
     },
-    updateInProgress: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
   },
   computed: {
     createdAt() {
@@ -106,7 +101,6 @@ export default {
       :moved-to-work-item-url="workItemMovedToWorkItemUrl"
       :promoted-to-epic-url="workItemPromotedToEpicUrl"
     />
-    <gl-loading-icon v-if="updateInProgress" inline />
     <confidentiality-badge
       v-if="isWorkItemConfidential"
       class="gl-align-middle"
