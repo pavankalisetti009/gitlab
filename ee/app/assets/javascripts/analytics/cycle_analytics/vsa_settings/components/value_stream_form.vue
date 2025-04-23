08c024643c12f149f41177fe17a925081cae49a8
Alex Pennells <apennells@gitlab.com>
2025-04-23 15:56:18 -0400

    Remove Vuex from the VSA settings page app
    Load the edit page VSA stages within the form component and
    remove the final references of Vuex from the app.

--- ee/app/assets/javascripts/analytics/cycle_analytics/vsa_settings/components/value_stream_form.vue
+++ ee/app/assets/javascripts/analytics/cycle_analytics/vsa_settings/components/value_stream_form.vue
@@ -1,7 +1,8 @@
 <script>
-import { GlLoadingIcon } from '@gitlab/ui';
-// eslint-disable-next-line no-restricted-imports
-import { mapState } from 'vuex';
+import { GlLoadingIcon, GlAlert } from '@gitlab/ui';
+import { I18N_VSA_ERROR_STAGES } from '~/analytics/cycle_analytics/constants';
+import { getStagesAndEvents } from 'ee/api/analytics_api';
+import { transformRawStages } from '../../utils';
 import { generateInitialStageData } from '../utils';
 import ValueStreamFormContent from './value_stream_form_content.vue';
 
@@ -10,8 +11,9 @@ export default {
   components: {
     ValueStreamFormContent,
     GlLoadingIcon,
+    GlAlert,
   },
-  inject: ['valueStream', 'defaultStages'],
+  inject: ['valueStream', 'defaultStages', 'namespaceFullPath'],
   props: {
     isEditing: {
       type: Boolean,
@@ -19,8 +21,14 @@ export default {
       default: false,
     },
   },
+  data() {
+    return {
+      isLoading: false,
+      showError: false,
+      stages: [],
+    };
+  },
   computed: {
-    ...mapState(['stages', 'isFetchingGroupStages', 'isLoading']),
     initialData() {
       return this.isEditing
         ? {
@@ -33,13 +41,40 @@ export default {
           };
     },
   },
+  mounted() {
+    if (this.isEditing) {
+      this.fetchStages();
+    }
+  },
+  methods: {
+    async fetchStages() {
+      this.isLoading = true;
+
+      try {
+        const {
+          data: { stages = [] },
+        } = await getStagesAndEvents({
+          namespacePath: this.namespaceFullPath,
+          valueStreamId: this.valueStream.id,
+        });
+
+        this.stages = transformRawStages(stages);
+      } catch {
+        this.showError = true;
+      }
+
+      this.isLoading = false;
+    },
+  },
+  I18N_VSA_ERROR_STAGES,
 };
 </script>
 <template>
   <div>
-    <div v-if="isLoading || isFetchingGroupStages" class="gl-pt-7 gl-text-center">
-      <gl-loading-icon size="lg" />
-    </div>
+    <gl-loading-icon v-if="isLoading" class="gl-pt-7" size="lg" />
+    <gl-alert v-else-if="showError" variant="danger" :dismissible="false">
+      {{ $options.I18N_VSA_ERROR_STAGES }}
+    </gl-alert>
     <value-stream-form-content v-else :initial-data="initialData" :is-editing="isEditing" />
   </div>
 </template>
