eb384a428fb36581d33e0901bd75a46ce923f274
Jeff Park <jpark@gitlab.com>
2025-04-23 11:02:59 -0400

    Feat: add feature flag to gate previous workflow tool

--- ee/lib/api/helpers/duo_workflow_helpers.rb
+++ ee/lib/api/helpers/duo_workflow_helpers.rb
@@ -14,6 +14,7 @@ module API
       private
 
       def push_feature_flags
+        Gitlab::AiGateway.push_feature_flag(:duo_workflow_previous_workflow_tool, current_user)
         Gitlab::AiGateway.push_feature_flag(:batch_duo_workflow_planner_tasks, current_user)
         Gitlab::AiGateway.push_feature_flag(:duo_workflow_claude_3_7, current_user)
       end
