54e41f6ef00369d4cb27fd592c056c0ea55beb64
Brian Williams <bwilliams@gitlab.com>
2025-04-24 10:51:52 -0500

    Expose vulnerabilities fields to Jira integration API
    Changelog: changed
    EE: true

--- app/models/integrations/jira.rb
+++ app/models/integrations/jira.rb
@@ -180,13 +180,23 @@ module Integrations
         'which Jira projects to view issues from in GitLab.')
       }
 
+    field :vulnerabilities_enabled,
+      required: false,
+      type: :checkbox,
+      api_only: true,
+      description: -> { s_('JiraIntegration|Enable creating Jira issues for GitLab vulnerabilities.') }
+
+    field :vulnerabilities_issuetype,
+      required: false,
+      type: :select,
+      api_only: true,
+      description: -> { s_('JiraIntegration|Jira issue type to use when creating issues from vulnerabilities.') }
+
     # TODO: we can probably just delegate as part of
     # https://gitlab.com/gitlab-org/gitlab/issues/29404
     # These fields are API only, so no field definition is required.
     data_field :jira_issue_transition_automatic
     data_field :project_key
-    data_field :vulnerabilities_enabled
-    data_field :vulnerabilities_issuetype
     data_field :customize_jira_issue_enabled
 
     # When these are false GitLab does not create cross reference
--- spec/models/integrations/jira_spec.rb
+++ spec/models/integrations/jira_spec.rb
@@ -223,7 +223,22 @@ RSpec.describe Integrations::Jira, feature_category: :integrations do
     subject(:fields) { integration.fields }
 
     it 'returns custom fields' do
-      expect(fields.pluck(:name)).to eq(%w[url api_url jira_auth_type username password jira_issue_regex jira_issue_prefix jira_issue_transition_id issues_enabled project_keys])
+      expect(fields.pluck(:name)).to eq(
+        %w[
+          url
+          api_url
+          jira_auth_type
+          username
+          password
+          jira_issue_regex
+          jira_issue_prefix
+          jira_issue_transition_id
+          issues_enabled
+          project_keys
+          vulnerabilities_enabled
+          vulnerabilities_issuetype
+        ]
+      )
     end
   end
 
