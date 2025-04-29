b494c329fd407358e430bcaeb9c20e1dfd445f19
Hunter Stewart <hustewart@gitlab.com>
2025-04-29 16:37:01 -0400

    Reuse existing ApprovalMergeRequestRulePolicy
    For now, we only want to replicate the existing policy behavior. Rather
    than keeping two sources of truth for this, we can declare a policy in
    the new class that points to the existing policy.
    
    Once we have a need that diverges from the existing policy, we can
    create a new policy class.

--- ee/spec/policies/approval_merge_request_rule_policy_spec.rb
+++ ee/spec/policies/approval_merge_request_rule_policy_spec.rb
@@ -2,39 +2,81 @@
 
 require 'spec_helper'
 
-RSpec.describe ApprovalMergeRequestRulePolicy do
-  let(:merge_request) { create(:merge_request) }
-  let(:approval_rule) { create(:approval_merge_request_rule, merge_request: merge_request) }
+RSpec.describe ApprovalMergeRequestRulePolicy, feature_category: :source_code_management do
+  let_it_be(:merge_request) { create(:merge_request) }
 
   def permissions(user, approval_rule)
     described_class.new(user, approval_rule)
   end
 
-  context 'when user can update merge request' do
-    it 'allows updating approval rule' do
-      expect(permissions(merge_request.author, approval_rule)).to be_allowed(:edit_approval_rule)
+  context 'when v2 approval rule' do
+    let_it_be(:project) { create(:project) }
+    let_it_be(:approval_rule) do
+      create(:merge_requests_approval_rule, merge_request: merge_request, project_id: project.id)
     end
 
-    context 'when rule is any-approval' do
-      let(:approval_rule) { build(:any_approver_rule, merge_request: merge_request) }
-
+    context 'when user can update merge request' do
       it 'allows updating approval rule' do
         expect(permissions(merge_request.author, approval_rule)).to be_allowed(:edit_approval_rule)
       end
+
+      context 'when rule is any-approval' do
+        let(:approval_rule) do
+          build(:merge_requests_approval_rule, rule_type: :any_approver, merge_request: merge_request)
+        end
+
+        it 'allows updating approval rule' do
+          expect(permissions(merge_request.author, approval_rule)).to be_allowed(:edit_approval_rule)
+        end
+      end
+
+      context 'when rule is not user editable' do
+        let(:approval_rule) do
+          build(:merge_requests_approval_rule, rule_type: :code_owner, merge_request: merge_request)
+        end
+
+        it 'disallows updating approval rule' do
+          expect(permissions(merge_request.author, approval_rule)).to be_disallowed(:edit_approval_rule)
+        end
+      end
     end
 
-    context 'when rule is not user editable' do
-      let(:approval_rule) { create(:code_owner_rule, merge_request: merge_request) }
-
+    context 'when user cannot update merge request' do
       it 'disallows updating approval rule' do
-        expect(permissions(merge_request.author, approval_rule)).to be_disallowed(:edit_approval_rule)
+        expect(permissions(create(:user), approval_rule)).to be_disallowed(:edit_approval_rule)
       end
     end
   end
 
-  context 'when user cannot update merge request' do
-    it 'disallows updating approval rule' do
-      expect(permissions(create(:user), approval_rule)).to be_disallowed(:edit_approval_rule)
+  context 'when v1 approval rule' do
+    let_it_be(:approval_rule) { create(:approval_merge_request_rule, merge_request: merge_request) }
+
+    context 'when user can update merge request' do
+      it 'allows updating approval rule' do
+        expect(permissions(merge_request.author, approval_rule)).to be_allowed(:edit_approval_rule)
+      end
+
+      context 'when rule is any-approval' do
+        let(:approval_rule) { build(:any_approver_rule, merge_request: merge_request) }
+
+        it 'allows updating approval rule' do
+          expect(permissions(merge_request.author, approval_rule)).to be_allowed(:edit_approval_rule)
+        end
+      end
+
+      context 'when rule is not user editable' do
+        let(:approval_rule) { create(:code_owner_rule, merge_request: merge_request) }
+
+        it 'disallows updating approval rule' do
+          expect(permissions(merge_request.author, approval_rule)).to be_disallowed(:edit_approval_rule)
+        end
+      end
+    end
+
+    context 'when user cannot update merge request' do
+      it 'disallows updating approval rule' do
+        expect(permissions(create(:user), approval_rule)).to be_disallowed(:edit_approval_rule)
+      end
     end
   end
 end
