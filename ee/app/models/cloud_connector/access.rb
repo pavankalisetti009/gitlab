4b14f02463e6e88e6d31953a5bc77a93e5eb3426
Aleksei Lipniagov <alipniagov@gitlab.com>
2025-04-30 13:01:02 +0200

    Save cc catalog in DB during seat link sync
    Store cloud connector catalog.

--- ee/app/models/cloud_connector/access.rb
+++ ee/app/models/cloud_connector/access.rb
@@ -7,9 +7,19 @@ module CloudConnector
     STALE_PERIOD = 3.days
 
     self.table_name = 'cloud_connector_access'
-    validates :data, json_schema: { filename: "cloud_connector_access" }
-    validates :data, presence: true
-
+    validates :data, json_schema: { filename: "cloud_connector_access" }, allow_nil: true
     validates :catalog, json_schema: { filename: "cloud_connector_access_catalog" }, allow_nil: true
+    validate :data_or_catalog_present
+
+    scope :with_data, -> { where.not(data: nil) }
+    scope :with_catalog, -> { where.not(catalog: nil) }
+
+    private
+
+    def data_or_catalog_present
+      return unless data.blank? && catalog.blank?
+
+      errors.add(:base, "Either valid data or catalog must be present")
+    end
   end
 end
