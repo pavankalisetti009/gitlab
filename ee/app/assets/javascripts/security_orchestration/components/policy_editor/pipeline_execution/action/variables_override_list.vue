b208d2d120f448bf27f6989998d5a93eb83a5ff9
Artur Fedorov <afedorov@gitlab.com>
2025-04-30 21:51:27 +0000

    Add default state and validation
    Variable configuration now has
    validation and default state
    
    Changelog: added
    EE: true

--- ee/app/assets/javascripts/security_orchestration/components/policy_editor/pipeline_execution/action/variables_override_list.vue
+++ ee/app/assets/javascripts/security_orchestration/components/policy_editor/pipeline_execution/action/variables_override_list.vue
@@ -1,11 +1,12 @@
 <script>
-import { isEqual } from 'lodash';
+import { isEmpty } from 'lodash';
 import {
   GlAccordion,
   GlAccordionItem,
   GlButton,
   GlCollapsibleListbox,
   GlLink,
+  GlIcon,
   GlSprintf,
 } from '@gitlab/ui';
 import { n__, s__, sprintf } from '~/locale';
@@ -17,6 +18,7 @@ import {
   DENY,
 } from 'ee/security_orchestration/components/policy_editor/pipeline_execution/constants';
 import { helpPagePath } from '~/helpers/help_page_helper';
+import { doesVariablesOverrideHasValidStructure } from 'ee/security_orchestration/components/policy_editor/pipeline_execution/action/utils';
 import VariablesOverrideModal from './variables_override_modal.vue';
 
 export default {
@@ -32,6 +34,9 @@ export default {
       'SecurityOrchestration|%{listType} attempts from %{linkStart}other settings%{linkEnd} to override variables when the policy runs, except the variables defined in the %{list}.',
     ),
     listTypeDefaultText: s__('SecurityOrchestration|Select list type'),
+    validationErrorMessage: s__(
+      'SecurityOrchestration|Variables override configuration has invalid structure.',
+    ),
   },
   name: 'VariablesOverrideList',
   components: {
@@ -39,6 +44,7 @@ export default {
     GlAccordionItem,
     GlButton,
     GlCollapsibleListbox,
+    GlIcon,
     GlLink,
     GlSprintf,
     VariablesOverrideModal,
@@ -47,15 +53,25 @@ export default {
     variablesOverride: {
       type: Object,
       required: false,
-      default: () => DEFAULT_VARIABLES_OVERRIDE_STATE,
+      default: () => ({}),
     },
   },
   data() {
     return {
-      accordionVisible: !isEqual(this.variablesOverride, DEFAULT_VARIABLES_OVERRIDE_STATE),
+      accordionVisible: !isEmpty(this.variablesOverride),
     };
   },
   computed: {
+    isDefaultState() {
+      return isEmpty(this.variablesOverride);
+    },
+    doesVariablesOverrideHasValidStructure() {
+      if (this.isDefaultState) {
+        return true;
+      }
+
+      return doesVariablesOverrideHasValidStructure(this.variablesOverride);
+    },
     buttonText() {
       const { denyListText, allowListText } = this.$options.i18n;
       const message = this.isVariablesOverrideAllowed ? denyListText : allowListText;
@@ -73,11 +89,11 @@ export default {
         : this.$options.i18n.allowList;
     },
     isVariablesOverrideAllowed() {
-      return this.variablesOverride.allowed;
+      return Boolean(this.variablesOverride.allowed);
     },
     selectedExceptions() {
       const { exceptions = [] } = this.variablesOverride || {};
-      return exceptions.length > 0 ? exceptions : [''];
+      return Array.isArray(exceptions) && exceptions.length > 0 ? exceptions : [''];
     },
     allowedKey() {
       return this.isVariablesOverrideAllowed ? ALLOW : DENY;
@@ -99,44 +115,66 @@ export default {
     selectListType(type) {
       this.emitChange({ allowed: type !== DENY, exceptions: [] });
     },
+    toggleAccordion(visible) {
+      if (this.isDefaultState && visible) {
+        this.emitChange(DEFAULT_VARIABLES_OVERRIDE_STATE);
+      }
+
+      this.accordionVisible = visible;
+    },
   },
 };
 </script>
 
 <template>
-  <gl-accordion :header-level="3">
-    <gl-accordion-item :title="$options.i18n.header" :visible="accordionVisible">
-      <p class="gl-my-4">
-        <gl-sprintf :message="$options.i18n.message">
-          <template #listType>
-            <gl-collapsible-listbox
-              :selected="allowedKey"
-              :items="$options.ALLOW_DENY_LISTBOX_ITEMS"
-              :toggle-text="toggleText"
-              @select="selectListType"
-            />
-          </template>
-          <template #link="{ content }">
-            <gl-link :href="$options.HELP_PAGE_LINK" target="_blank">
-              {{ content }}
-            </gl-link>
-          </template>
-          <template #list>
-            <span>{{ listName }}</span>
-          </template>
-        </gl-sprintf>
-      </p>
+  <div>
+    <gl-accordion :header-level="3">
+      <gl-accordion-item
+        :title="$options.i18n.header"
+        :visible="accordionVisible"
+        @input="toggleAccordion"
+      >
+        <p class="gl-my-4">
+          <gl-sprintf :message="$options.i18n.message">
+            <template #listType>
+              <gl-collapsible-listbox
+                :selected="allowedKey"
+                :items="$options.ALLOW_DENY_LISTBOX_ITEMS"
+                :toggle-text="toggleText"
+                @select="selectListType"
+              />
+            </template>
+            <template #link="{ content }">
+              <gl-link :href="$options.HELP_PAGE_LINK" target="_blank">
+                {{ content }}
+              </gl-link>
+            </template>
+            <template #list>
+              <span>{{ listName }}</span>
+            </template>
+          </gl-sprintf>
+        </p>
 
-      <gl-button category="primary" variant="link" @click="showModal">
-        {{ buttonText }}
-      </gl-button>
+        <gl-button category="primary" variant="link" @click="showModal">
+          {{ buttonText }}
+        </gl-button>
 
-      <variables-override-modal
-        ref="modal"
-        :exceptions="selectedExceptions"
-        :is-variables-override-allowed="isVariablesOverrideAllowed"
-        @select-exceptions="selectExceptions"
-      />
-    </gl-accordion-item>
-  </gl-accordion>
+        <variables-override-modal
+          ref="modal"
+          :exceptions="selectedExceptions"
+          :is-variables-override-allowed="isVariablesOverrideAllowed"
+          @select-exceptions="selectExceptions"
+        />
+      </gl-accordion-item>
+    </gl-accordion>
+
+    <div
+      v-if="!doesVariablesOverrideHasValidStructure"
+      class="gl-mt-3 gl-flex gl-items-center gl-gap-2"
+      data-testid="validation-message"
+    >
+      <gl-icon name="error" variant="danger" />
+      <p class="gl-m-0 gl-text-danger">{{ $options.i18n.validationErrorMessage }}</p>
+    </div>
+  </div>
 </template>
