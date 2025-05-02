8f3eee87e5d9622819c8ffb80aa8cde0fae12620
David Fernandez <dfernandez@gitlab.com>
2025-05-02 15:53:11 +0200

    Fix sbt authentication in Maven virtual registries
    When the related feature flag is partially rolled out, sbt clients
    can't authenticate because their anonymous request will receive a
    404 Not Found response instead of a 401 Unauthorized.
    
    Maven virtual registries will now return a 401 Unauthorized when the
    related feature flag is not enabled.

--- ee/lib/api/concerns/virtual_registries/packages/maven/shared_setup.rb
+++ ee/lib/api/concerns/virtual_registries/packages/maven/shared_setup.rb
@@ -20,7 +20,7 @@ module API
               end
 
               after_validation do
-                not_found! unless ::Feature.enabled?(:virtual_registry_maven, current_user)
+                unauthorized! unless ::Feature.enabled?(:virtual_registry_maven, current_user)
                 not_found! unless ::Gitlab.config.dependency_proxy.enabled
                 not_found! unless target_group.licensed_feature_available?(:packages_virtual_registry)
 
