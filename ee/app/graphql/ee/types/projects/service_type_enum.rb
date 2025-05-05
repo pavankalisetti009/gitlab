0195cb3d13fd79f0f0937608d04fbe34a15a67e9
Luke Duncalfe <lduncalfe@gitlab.com>
2025-05-06 02:34:15 +1200

    Document SaaS-only GraphQL schema
    Previously SaaS-only GraphQL `ServiceType` enum values were absent from
    our documentation, although they would be present in the actual GraphQL
    API on GitLab.com.
    
    This change allows SaaS-only schema to be present for when we compile
    the documentation, but not present in the actual GraphQL schema.
    
    The Saas-only `ServiceType` enum values are described as
    "(GitLab.com only)".
    
    Changelog: changed

--- ee/app/graphql/ee/types/projects/service_type_enum.rb
+++ ee/app/graphql/ee/types/projects/service_type_enum.rb
@@ -6,6 +6,13 @@ module EE
       module ServiceTypeEnum
         extend ActiveSupport::Concern
 
+        # This list is only used for GraphQL documentation purposes, to be able to
+        # append (SaaS only) to the ServiceType enum description.
+        SAAS_ONLY_INTEGRATION_NAMES = [
+          ::Integrations::GoogleCloudPlatform::WorkloadIdentityFederation.to_param,
+          ::Integrations::GoogleCloudPlatform::ArtifactRegistry.to_param
+        ].freeze
+
         class_methods do
           extend ::Gitlab::Utils::Override
 
@@ -19,6 +26,13 @@ module EE
               include_disabled: true,
               include_blocked_by_settings: true
             )
+          end
+
+          override :value_description
+          def value_description(name)
+            description = super
+            description += " (SaaS only)" if name.in?(SAAS_ONLY_INTEGRATION_NAMES)
+            description
           end
         end
       end
