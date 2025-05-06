803ab95a2f20364ecdfdf4a74461bcb9db5edc61
Keeyan Nejad <knejad@gitlab.com>
2025-05-06 09:56:11 +0100

    Improve Octokit::ResponseValidation middleware and add rate limits
    Merge branch 'security-improve-response-validation-middleware' into 'master'
    
    See merge request gitlab-org/security/gitlab!4850
    
    Changelog: security

--- db/structure.sql
+++ db/structure.sql
@@ -8966,6 +8966,7 @@ CREATE TABLE application_settings (
     group_settings jsonb DEFAULT '{}'::jsonb NOT NULL,
     model_prompt_cache_enabled boolean DEFAULT true NOT NULL,
     lock_model_prompt_cache_enabled boolean DEFAULT false NOT NULL,
+    response_limits jsonb DEFAULT '{}'::jsonb NOT NULL,
     CONSTRAINT app_settings_container_reg_cleanup_tags_max_list_size_positive CHECK ((container_registry_cleanup_tags_service_max_list_size >= 0)),
     CONSTRAINT app_settings_dep_proxy_ttl_policies_worker_capacity_positive CHECK ((dependency_proxy_ttl_group_policy_worker_capacity >= 0)),
     CONSTRAINT app_settings_ext_pipeline_validation_service_url_text_limit CHECK ((char_length(external_pipeline_validation_service_url) <= 255)),
