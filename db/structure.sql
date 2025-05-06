530c4779174bb25a2e085ab61f72cab5efcb38b0
Mayra Cabrera <mcabrera@gitlab.com>
2025-05-06 10:53:45 -0600

    Fixes conflicts on db/structure.sql
    Conflicts were caused since
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/189144/diffs and https://gitlab.com/gitlab-org/security/gitlab/-/merge_requests/4850/ target the same table (`application_settings`)

--- db/structure.sql
+++ db/structure.sql
@@ -8966,12 +8966,9 @@ CREATE TABLE application_settings (
     group_settings jsonb DEFAULT '{}'::jsonb NOT NULL,
     model_prompt_cache_enabled boolean DEFAULT true NOT NULL,
     lock_model_prompt_cache_enabled boolean DEFAULT false NOT NULL,
-<<<<<<< HEAD
     response_limits jsonb DEFAULT '{}'::jsonb NOT NULL,
-=======
     web_based_commit_signing_enabled boolean DEFAULT false NOT NULL,
     lock_web_based_commit_signing_enabled boolean DEFAULT false NOT NULL,
->>>>>>> source-project/master
     CONSTRAINT app_settings_container_reg_cleanup_tags_max_list_size_positive CHECK ((container_registry_cleanup_tags_service_max_list_size >= 0)),
     CONSTRAINT app_settings_dep_proxy_ttl_policies_worker_capacity_positive CHECK ((dependency_proxy_ttl_group_policy_worker_capacity >= 0)),
     CONSTRAINT app_settings_ext_pipeline_validation_service_url_text_limit CHECK ((char_length(external_pipeline_validation_service_url) <= 255)),
