5dd7b6053771df8004885aafc336a2965acb9e29
Terri Chu <tchu@gitlab.com>
2025-05-07 13:57:25 -0400

    Send duration_s in blob search response

--- ee/app/graphql/types/search/blob/blob_search_type.rb
+++ ee/app/graphql/types/search/blob/blob_search_type.rb
@@ -8,6 +8,8 @@ module Types
         graphql_name 'BlobSearch'
         description 'Full JSON structure of multi-match results in a single file'
 
+        field :duration_s, ::GraphQL::Types::Float, null: true, experiment: { milestone: '18.0' },
+          description: 'Duration of the request in seconds.'
         field :file_count, GraphQL::Types::Int, null: true, experiment: { milestone: '17.2' },
           description: 'Total number of files with matches.'
         field :files, [::Types::Search::Blob::FileType], null: true, experiment: { milestone: '17.2' },
