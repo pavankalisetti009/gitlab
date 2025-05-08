3ee09a8a489f8d3a06fae2d0d681345603daefcc
Miranda Fluharty <mfluharty@gitlab.com>
2025-05-08 11:19:59 -0600

    Fix expandable group spec for Vue 3

--- ee/spec/frontend/security_inventory/components/sidebar/expandable_group_spec.js
+++ ee/spec/frontend/security_inventory/components/sidebar/expandable_group_spec.js
@@ -1,11 +1,15 @@
 import { GlBadge, GlButton, GlIcon } from '@gitlab/ui';
 import { nextTick } from 'vue';
-import { mountExtended, shallowMountExtended } from 'helpers/vue_test_utils_helper';
-import waitForPromises from 'helpers/wait_for_promises';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import { stubComponent } from 'helpers/stub_component';
+import waitForPromises from 'helpers/wait_for_promises';
 import ExpandableGroup from 'ee/security_inventory/components/sidebar/expandable_group.vue';
 import GroupList from 'ee/security_inventory/components/sidebar/group_list.vue';
 import { groupWithSubgroups } from '../../mock_data';
+
+jest.mock('ee/security_inventory/components/sidebar/group_list.vue', () => ({
+  name: 'GroupList',
+}));
 
 describe('ExpandableGroup', () => {
   let wrapper;
@@ -25,15 +29,18 @@ describe('ExpandableGroup', () => {
     group = { ...subgroups[0].node },
     activeFullPath = 'a-group',
     indentation = 0,
-    mountFn = shallowMountExtended,
   } = {}) => {
-    wrapper = mountFn(ExpandableGroup, {
+    wrapper = shallowMountExtended(ExpandableGroup, {
       propsData: {
         group,
         activeFullPath,
         indentation,
       },
-      stubs: { GroupList: stubComponent(GroupList) },
+      stubs: {
+        GroupList: stubComponent(GroupList, {
+          props: ['activeFullPath', 'groupFullPath', 'indentation'],
+        }),
+      },
     });
   };
 
@@ -78,7 +85,7 @@ describe('ExpandableGroup', () => {
 
   describe('events', () => {
     beforeEach(() => {
-      createComponent({ mountFn: mountExtended });
+      createComponent();
     });
 
     it('on group click, emits selectSubgroup event', () => {
