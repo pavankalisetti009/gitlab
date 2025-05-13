57860ff27b5408f961e56d86366979d3eda170bd
Kos Palchyk <kpalchyk@gitlab.com>
2025-05-13 15:59:15 +0300

    Make the CSP headers addition more strict

--- ee/spec/features/users/google_syndication_csp_spec.rb
+++ ee/spec/features/users/google_syndication_csp_spec.rb
@@ -5,45 +5,38 @@ require 'spec_helper'
 RSpec.describe 'Google Syndication content security policy', feature_category: :subscription_management do
   include ContentSecurityPolicyHelpers
 
-  let_it_be(:connect_src) { 'https://other-cdn.test' }
+  let(:gtm_enabled) { true }
+  let(:gtm_id) { 'GTM-ID' }
+  let(:connect_src) { ['*.google.com/pagead/landing', 'pagead2.googlesyndication.com/pagead/landing'] }
+  let(:csp) { ActionDispatch::ContentSecurityPolicy.new { |p| p.default_src '' } }
 
-  let_it_be(:allowed_src) do
-    '*.google.com/pagead/landing pagead2.googlesyndication.com/pagead/landing'
-  end
-
-  let(:extra) { { google_tag_manager_nonce_id: 'google_tag_manager_nonce_id' } }
-
-  let(:csp) do
-    ActionDispatch::ContentSecurityPolicy.new do |p|
-      p.connect_src(*connect_src.split)
-    end
-  end
-
-  subject { response_headers['Content-Security-Policy'] }
+  subject(:csp_header) { response_headers['Content-Security-Policy'] }
 
   before do
-    stub_csp_for_controller(SessionsController, csp)
-    stub_config(extra: extra)
-    visit new_user_session_path
+    stub_config(extra: { google_tag_manager_nonce_id: gtm_id })
+    stub_saas_features(marketing_google_tag_manager: gtm_enabled)
+    stub_csp_for_controller(RegistrationsController, csp)
+
+    visit new_user_registration_path
   end
 
-  context 'when self-hosted' do
-    context 'when there is no CSP config' do
-      let(:extra) { {} }
-      let(:csp) { ActionDispatch::ContentSecurityPolicy.new }
+  it 'includes the relevant connect-src CSP' do
+    expect(find_csp_directive('connect-src', header: csp_header)).to include(*connect_src)
+  end
 
-      it { is_expected.to be_blank }
-    end
+  context 'when disabled' do
+    let(:gtm_enabled) { false }
 
-    context 'when connect-src CSP config exists' do
-      it { is_expected.to include("connect-src #{connect_src}") }
-      it { is_expected.not_to include(allowed_src) }
+    it 'does not include the relevant connect-src CSP headers' do
+      expect(csp_header).not_to include(*connect_src)
     end
   end
 
-  context 'when SaaS', :saas do
-    context 'when connect-src CSP config exists' do
-      it { is_expected.to match(/connect-src [^;]*#{Regexp.escape(allowed_src)}/) }
+  context 'when CSP is absent' do
+    let(:csp) { ActionDispatch::ContentSecurityPolicy.new }
+
+    it 'does not have Content Security Policy headers' do
+      expect(csp_header).not_to include(*connect_src)
     end
   end
 end
