0eab08b84d22abec47264ed2e6f3a29e2d704f8c
Rahul Chanila <rchanila@gitlab.com>
2025-05-13 10:05:06 +1000

    Setup routing for virtual registry maven upstream
    Adds the show & edit routes, controller actions
    and view templates.
    
    In both cases, looks up the maven upstream from
    the database.
    
    EE: true

--- ee/config/routes/virtual_registries.rb
+++ ee/config/routes/virtual_registries.rb
@@ -3,6 +3,8 @@
 resources :virtual_registries, only: [:index]
 namespace :virtual_registries do
   namespace :maven do
-    resources :registries, path: '', only: [:new, :index]
+    resources :registries, path: '', only: [:new, :index] do
+      resources :upstreams, only: [:show, :edit], shallow: true
+    end
   end
 end
