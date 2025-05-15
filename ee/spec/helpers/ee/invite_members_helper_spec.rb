d789201f150ac9fbcde2b3faa5f53a76d3ae59f4
Bogdan Denkovych <bdenkovych@gitlab.com>
2025-05-15 17:28:18 +0300

    Clean up `new_implementation_of_invite_members_search` FF and old code
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/540306
    
    Changelog: changed

--- ee/spec/helpers/ee/invite_members_helper_spec.rb
+++ ee/spec/helpers/ee/invite_members_helper_spec.rb
@@ -227,51 +227,6 @@ RSpec.describe EE::InviteMembersHelper, feature_category: :groups_and_projects d
     end
   end
 
-  describe '#users_filter_data' do
-    let_it_be(:group) { create(:group, :private) }
-    let_it_be(:saml_provider) { create(:saml_provider, group: group) }
-
-    let!(:group2) { create(:group) }
-
-    context 'when the group has enforced sso' do
-      before do
-        allow(group).to receive(:enforced_sso?).and_return(true)
-      end
-
-      context 'when there is a group with a saml provider' do
-        it 'returns user filter data' do
-          expected = { users_filter: 'saml_provider_id', filter_id: saml_provider.id }
-
-          expect(helper.users_filter_data(group)).to eq expected
-        end
-      end
-
-      context 'when there is a group without a saml provider' do
-        it 'does not return user filter data' do
-          expect(helper.users_filter_data(group2)).to eq({})
-        end
-      end
-    end
-
-    context 'when group has enforced sso disabled' do
-      before do
-        allow(group).to receive(:enforced_sso?).and_return(false)
-      end
-
-      context 'when there is a group with a saml provider' do
-        it 'does not return user filter data' do
-          expect(helper.users_filter_data(group)).to eq({})
-        end
-      end
-
-      context 'when there is a group without a saml provider' do
-        it 'does not return user filter data' do
-          expect(helper.users_filter_data(group2)).to eq({})
-        end
-      end
-    end
-  end
-
   describe '#overage_members_modal_available' do
     context 'when SaaS' do
       before do
