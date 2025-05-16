5660ec3a27383ae274e915c2fca76a8d008f2a94
Rémy Coutable <remy@rymai.me>
2025-05-16 15:46:25 +0200

    Use migrate_to_encrypts :asset_proxy_secret_key in ApplicationSetting
    Changelog: other
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- db/structure.sql
+++ db/structure.sql
@@ -9124,6 +9124,7 @@ CREATE TABLE application_settings (
     response_limits jsonb DEFAULT '{}'::jsonb NOT NULL,
     web_based_commit_signing_enabled boolean DEFAULT false NOT NULL,
     lock_web_based_commit_signing_enabled boolean DEFAULT false NOT NULL,
+    tmp_asset_proxy_secret_key jsonb,
     CONSTRAINT app_settings_container_reg_cleanup_tags_max_list_size_positive CHECK ((container_registry_cleanup_tags_service_max_list_size >= 0)),
     CONSTRAINT app_settings_dep_proxy_ttl_policies_worker_capacity_positive CHECK ((dependency_proxy_ttl_group_policy_worker_capacity >= 0)),
     CONSTRAINT app_settings_ext_pipeline_validation_service_url_text_limit CHECK ((char_length(external_pipeline_validation_service_url) <= 255)),
