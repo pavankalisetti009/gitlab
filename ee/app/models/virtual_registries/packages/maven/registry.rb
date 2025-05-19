2c9ee6deb80e3a4c8648f9ab2f3fd7f82232aa73
moaz-khalifa <mkhalifa@gitlab.com>
2025-05-19 20:12:20 +0200

    Maven virtual registry: support multiple registries
    Update the unique index on virtual_registries_packages_maven_registries group_id to be a composite one on group_id and name
    
    Changelog: other
    EE: true

--- ee/app/models/virtual_registries/packages/maven/registry.rb
+++ ee/app/models/virtual_registries/packages/maven/registry.rb
@@ -4,6 +4,8 @@ module VirtualRegistries
   module Packages
     module Maven
       class Registry < ApplicationRecord
+        MAX_REGISTRY_COUNT = 20
+
         belongs_to :group
         has_many :registry_upstreams,
           -> { order(position: :asc) },
@@ -13,15 +15,27 @@ module VirtualRegistries
           class_name: 'VirtualRegistries::Packages::Maven::Upstream',
           through: :registry_upstreams
 
-        validates :group, top_level_group: true, presence: true, uniqueness: true
+        validates :group, top_level_group: true, presence: true
         validates :name, presence: true, length: { maximum: 255 }
         validates :description, length: { maximum: 1024 }
+        validates :group_id, uniqueness: { scope: :name }
+
+        validate :max_per_group, on: :create
 
         scope :for_group, ->(group) { where(group: group) }
 
         before_destroy :delete_upstreams
 
         private
+
+        def max_per_group
+          return if self.class.for_group(group).size < MAX_REGISTRY_COUNT
+
+          errors.add(
+            :group,
+            format(_('%{count} registries is the maximum allowed per group.'), count: MAX_REGISTRY_COUNT)
+          )
+        end
 
         def delete_upstreams
           VirtualRegistries::Packages::Maven::Upstream
