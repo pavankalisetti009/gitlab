ed4fbbdc1ec7ec25a8218f91abcfbc41b41fc06a
rkumar555 <rkumar@gitlab.com>
2025-05-20 11:49:33 +0200

    Improve the test

--- ee/spec/requests/custom_roles/read_code/request_spec.rb
+++ ee/spec/requests/custom_roles/read_code/request_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe 'User with read_code custom role', feature_category: :permissions do
   let_it_be(:user) { create(:user) }
-  let_it_be(:project) { create(:project, :repository, :in_group) }
+  let_it_be(:project) { create(:project, :small_repo, :in_group) }
   let_it_be(:role) { create(:member_role, :guest, :read_code, namespace: project.group) }
   let_it_be(:member) { create(:group_member, :guest, member_role: role, user: user, source: project.group) }
 
@@ -18,32 +18,19 @@ RSpec.describe 'User with read_code custom role', feature_category: :permissions
     end
 
     describe '#show' do
-      let(:source_code) do
-        "Mailer.deliver(to: 'bob@example.com', from: 'us@example.com', subject: 'Important message', body: source.text)"
-      end
-
       context 'with elasticsearch', :elastic, :sidekiq_inline do
         before do
-          stub_ee_application_setting(
-            elasticsearch_indexing: true,
-            elasticsearch_search: true
-          )
+          stub_ee_application_setting(elasticsearch_indexing: true, elasticsearch_search: true)
           project.repository.index_commits_and_blobs
           ensure_elasticsearch_index!
         end
 
         context 'when searching a group' do
           it 'allows access via a custom role' do
-            get search_path, params: {
-              group_id: project.group.id,
-              scope: 'blobs',
-              search: 'Mailer.deliver'
-            }
+            get search_path, params: { group_id: project.group.id, scope: 'blobs', search: 'test' }
 
             expect(response).to have_gitlab_http_status(:ok)
-            expect(response.body).not_to include('We couldn&#39;t find any code results matching')
-            expect(response.body).to include('/files/markdown/ruby-style-guide.md#L452')
-            expect(response.body).to include(source_code)
+            expect(response.body).to include('test.txt#L1')
           end
 
           context 'when saas', :saas do
@@ -59,41 +46,23 @@ RSpec.describe 'User with read_code custom role', feature_category: :permissions
               )
             end
 
-            it 'allows access via a custom role' do
-              get search_path, params: {
-                group_id: project.group.id,
-                scope: 'blobs',
-                search: 'Mailer.deliver'
-              }
-
-              expect(response).to have_gitlab_http_status(:ok)
-              expect(response.body).not_to include('We couldn&#39;t find any code results matching')
-              expect(response.body).to include('/files/markdown/ruby-style-guide.md#L452')
-              expect(response.body).to include(source_code)
-            end
-
             it 'avoids N+1 queries' do
-              get search_path, params: { group_id: project.group.id, scope: 'blobs', search: 'Mailer.deliver' } # warmup
-              expect(response.body).to include(source_code)
+              get search_path, params: { group_id: project.group.id, scope: 'blobs', search: 'test' } # warmup
+
+              expect(response.body).to include('test.txt#L1')
 
               control = ActiveRecord::QueryRecorder.new(skip_cached: false) do
-                get search_path, params: {
-                  group_id: project.group.id,
-                  scope: 'blobs',
-                  search: 'Mailer.deliver'
-                }
-                expect(response.body).to include(source_code)
+                get search_path, params: { group_id: project.group.id, scope: 'blobs', search: 'test' }
+
+                expect(response.body).to include('test.txt#L1')
               end
 
               create(:project, :private, :repository, group: create(:group, parent: project.group))
 
               expect do
-                get search_path, params: {
-                  group_id: project.group.id,
-                  scope: 'blobs',
-                  search: 'Mailer.deliver'
-                }
-                expect(response.body).to include(source_code)
+                get search_path, params: { group_id: project.group.id, scope: 'blobs', search: 'test' }
+
+                expect(response.body).to include('test.txt#L1')
               end.to issue_same_number_of_queries_as(control).or_fewer
             end
           end
@@ -101,60 +70,10 @@ RSpec.describe 'User with read_code custom role', feature_category: :permissions
 
         context 'when searching a project' do
           it 'allows access via a custom role' do
-            get search_path, params: {
-              project_id: project.id,
-              search_code: true,
-              scope: 'blobs',
-              search: 'Mailer.deliver'
-            }
+            get search_path, params: { project_id: project.id, search_code: true, scope: 'blobs', search: 'test' }
 
             expect(response).to have_gitlab_http_status(:ok)
-            expect(response.body).not_to include('We couldn&#39;t find any code results matching')
-            expect(response.body).to include('/files/markdown/ruby-style-guide.md#L452')
-            expect(response.body).to include(source_code)
-          end
-        end
-      end
-
-      context 'with Zoekt', :zoekt, :zoekt_settings_enabled do
-        before do
-          zoekt_ensure_project_indexed!(project)
-        end
-
-        context 'when searching a group' do
-          before do
-            project.group.zoekt_enabled_namespace.replicas.update_all(state: :ready)
-          end
-
-          it 'allows access via a custom role' do
-            # Temporary: There is no results in the current version
-            # WIP
-
-            get search_path, params: {
-              group_id: project.group.id,
-              scope: 'blobs',
-              search: 'Mailer.deliver'
-            }
-
-            expect(response).to have_gitlab_http_status(:ok)
-            expect(response.body).not_to include('No results found')
-          end
-        end
-
-        context 'when searching a project' do
-          it 'allows access via a custom role' do
-            # Temporary: There is no results in the current version
-            # WIP
-
-            get search_path, params: {
-              project_id: project.id,
-              search_code: true,
-              scope: 'blobs',
-              search: 'Mailer.deliver'
-            }
-
-            expect(response).to have_gitlab_http_status(:ok)
-            expect(response.body).not_to include('No results found')
+            expect(response.body).to include('test.txt#L1')
           end
         end
       end
