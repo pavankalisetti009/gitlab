a506573b2f3aa883039441ca13fa94d900b0e0b4
Deepika Guliani <dguliani@gitlab.com>
2025-05-26 12:31:07 +1000

    Small UI changes for long status names
    Changelog: added
    EE: true

--- app/assets/javascripts/boards/components/board_card_inner.vue
+++ app/assets/javascripts/boards/components/board_card_inner.vue
@@ -376,7 +376,7 @@ export default {
           </span>
         </span>
       </div>
-      <div class="gl-flex gl-flex-1 gl-items-center gl-justify-end gl-gap-3">
+      <div class="gl-flex gl-flex-1 gl-flex-wrap gl-items-center gl-justify-end gl-gap-3">
         <div class="board-card-assignee gl-flex">
           <user-avatar-link
             v-for="assignee in cappedAssignees"
@@ -413,12 +413,14 @@ export default {
           :work-item-web-url="item.webUrl"
           :target-id="targetId"
         />
-        <work-item-status-badge
-          v-if="showStatus"
-          :name="item.status.name"
-          :icon-name="item.status.iconName"
-          :color="item.status.color"
-        />
+        <div class="gl-max-w-20">
+          <work-item-status-badge
+            v-if="showStatus"
+            :name="item.status.name"
+            :icon-name="item.status.iconName"
+            :color="item.status.color"
+          />
+        </div>
       </div>
     </div>
   </div>
