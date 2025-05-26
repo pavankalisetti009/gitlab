e31d36cbf7fc85260f87dcc2792cbcd00eb2d920
adruid <ataha@gitlab.com>
2025-05-26 11:37:57 +0300

    Add UserAddonAssignmentSyncStrategy

--- ee/app/workers/click_house/user_add_on_assignments_sync_worker.rb
+++ ee/app/workers/click_house/user_add_on_assignments_sync_worker.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# Deprecation: This class is going to be removed as
+# `ee/app/workers/click_house/user_addon_assignment_versions_sync_worker.rb`
+# will take precedence.
+# This change is added in %18.1, this class shall be removed in the next 1-3 milestones
+# See the following issues for more context:
+# - https://gitlab.com/gitlab-org/gitlab/-/issues/545321
+# - https://gitlab.com/gitlab-org/gitlab/-/issues/540267
 module ClickHouse # rubocop:disable Gitlab/BoundedContexts -- Context already present in other files
   class UserAddOnAssignmentsSyncWorker
     include ApplicationWorker
