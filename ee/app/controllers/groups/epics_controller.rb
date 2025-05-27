72f18c75ca22c52da70d2d35f1b449cf30b43268
Kassio Borges <kborges@gitlab.com>
2025-05-27 16:50:50 +0200

    Change work_item_epics_list feature flag to root level actor
    Related to:
    - https://gitlab.com/gitlab-org/gitlab/-/issues/545928
    - https://gitlab.com/gitlab-org/gitlab/-/issues/473073

--- ee/app/controllers/groups/epics_controller.rb
+++ ee/app/controllers/groups/epics_controller.rb
@@ -28,7 +28,7 @@ class Groups::EpicsController < Groups::ApplicationController
     push_force_frontend_feature_flag(:glql_integration, !!@group&.glql_integration_feature_flag_enabled?)
     push_force_frontend_feature_flag(:glql_load_on_click, !!@group&.glql_load_on_click_feature_flag_enabled?)
     push_force_frontend_feature_flag(:continue_indented_text, !!@group&.continue_indented_text_feature_flag_enabled?)
-    push_frontend_feature_flag(:work_item_epics_list, @group)
+    push_force_frontend_feature_flag(:work_item_epics_list, !!@group.work_item_epics_list_enabled?)
     push_force_frontend_feature_flag(:work_items_alpha, !!group.work_items_alpha_feature_flag_enabled?)
     push_frontend_feature_flag(:epics_list_drawer, @group)
   end
@@ -57,7 +57,7 @@ class Groups::EpicsController < Groups::ApplicationController
   end
 
   def index
-    if Feature.enabled?(:work_item_epics_list, @group)
+    if @group.work_item_epics_list_enabled?
       render 'work_items_index'
     else
       render 'index'
@@ -107,7 +107,7 @@ class Groups::EpicsController < Groups::ApplicationController
       .with_work_item_type
       .find_by_iid(epic.iid)
 
-    if Feature.enabled?(:work_item_epics_list, @group) && epic_work_items_enabled?
+    if @group.work_item_epics_list_enabled? && epic_work_items_enabled?
       render 'work_items_index'
     else
       render 'groups/work_items/show'
