8afb11895266740d3ee17d6004333cef69cf181e
Agnes Slota <aslota@gitlab.com>
2025-06-02 22:32:17 +1000

    Use authorize shorthand instead of Ability.allowed
    Use a shorthand for checking the permission
    to read lifecycles.
    
    Changelog: changed
    EE: true

--- ee/app/graphql/resolvers/work_items/lifecycles_resolver.rb
+++ ee/app/graphql/resolvers/work_items/lifecycles_resolver.rb
@@ -3,15 +3,21 @@
 module Resolvers
   module WorkItems
     class LifecyclesResolver < Resolvers::WorkItems::BaseResolver
+      include ::WorkItems::Lifecycles::LookAheadPreloads
+
       type Types::WorkItems::LifecycleType.connection_type, null: true
+      authorize :read_lifecycle
 
       alias_method :namespace, :object
 
-      def resolve
+      # Only apply preloading to custom lifecycles (ActiveRecord model)
+      # Return system-defined lifecycles (Fixed model) as is
+      def resolve_with_lookahead
         return unless work_item_status_feature_available?
-        return unless Ability.allowed?(current_user, :read_lifecycle, namespace)
 
-        namespace.lifecycles
+        lifecycles = namespace.lifecycles
+
+        lifecycles.is_a?(ActiveRecord::Relation) ? apply_lookahead(lifecycles) : lifecycles
       end
 
       private
--- ee/spec/requests/api/graphql/namespace/lifecycles_spec.rb
+++ ee/spec/requests/api/graphql/namespace/lifecycles_spec.rb
@@ -50,28 +50,49 @@ RSpec.describe 'Namespace.lifecycles', feature_category: :team_planning do
     end
   end
 
-  context 'with system-defined lifecycles' do
-    let(:expected_lifecycles) do
-      WorkItems::Statuses::SystemDefined::Lifecycle.all.map { |lifecycle| format_lifecycle(lifecycle) }
+  context 'when user has permission to read lifecycles' do
+    context 'with system-defined lifecycles' do
+      let(:expected_lifecycles) do
+        WorkItems::Statuses::SystemDefined::Lifecycle.all.map { |lifecycle| format_lifecycle(lifecycle) }
+      end
+
+      it_behaves_like 'returns lifecycles'
     end
 
-    it_behaves_like 'returns lifecycles'
-  end
+    context 'with custom lifecycles' do
+      let!(:custom_lifecycle) { create(:work_item_custom_lifecycle, namespace: namespace) }
+      let(:expected_lifecycles) { [format_lifecycle(custom_lifecycle)] }
 
-  context 'with custom lifecycles' do
-    let!(:custom_lifecycle) { create(:work_item_custom_lifecycle, namespace: namespace) }
-    let(:expected_lifecycles) { [format_lifecycle(custom_lifecycle)] }
+      it_behaves_like 'returns lifecycles'
 
-    it_behaves_like 'returns lifecycles'
-  end
+      it 'avoids N+1 queries when fetching multiple lifecycles' do
+        post_graphql(query, current_user: guest)
 
-  context 'when feature is not available' do
-    before do
-      stub_licensed_features(work_item_status: false)
+        control = ActiveRecord::QueryRecorder.new { post_graphql(query, current_user: guest) }
+
+        create_list(:work_item_custom_lifecycle, 2, namespace: namespace)
+
+        expect { post_graphql(query, current_user: guest) }.not_to exceed_query_limit(control)
+      end
     end
 
+    context 'when feature is not available' do
+      before do
+        stub_licensed_features(work_item_status: false)
+      end
+
+      it 'does not return lifecycles' do
+        post_graphql(query, current_user: guest)
+
+        expect(response).to have_gitlab_http_status(:ok)
+        expect(graphql_data_at(:namespace, :lifecycles, :nodes)).to be_blank
+      end
+    end
+  end
+
+  context 'when user does not have permission to read lifecycles' do
     it 'does not return lifecycles' do
-      post_graphql(query, current_user: guest)
+      post_graphql(query, current_user: create(:user))
 
       expect(response).to have_gitlab_http_status(:ok)
       expect(graphql_data_at(:namespace, :lifecycles, :nodes)).to be_blank
