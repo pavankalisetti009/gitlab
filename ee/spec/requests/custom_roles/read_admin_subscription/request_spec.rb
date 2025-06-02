e535d32e3e22483d1b6ea24ce0920e48633f61d6
Jarka Košanová <jarka@gitlab.com>
2025-06-02 16:31:05 +0200

    Add feature specs for admin custom abilities
    - add specs for read_admin_cicd
    - add cases testing access to admin dashboard for all admin abilities

--- ee/spec/requests/custom_roles/read_admin_subscription/request_spec.rb
+++ ee/spec/requests/custom_roles/read_admin_subscription/request_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe 'User with read_admin_subscription custom role', feature_category: :system_access do
+RSpec.describe 'User with read_admin_subscription custom role', :enable_admin_mode, feature_category: :system_access do
   let_it_be(:user) { create(:user) }
   let_it_be(:role) { create(:member_role, :read_admin_subscription) }
   let_it_be(:user_member_role) { create(:user_member_role, member_role: role, user: user) }
@@ -15,11 +15,22 @@ RSpec.describe 'User with read_admin_subscription custom role', feature_category
 
   describe Admin::SubscriptionsController do
     describe "#show" do
-      it 'user has access via a custom role', :enable_admin_mode do
+      it 'user has access via a custom role' do
         get admin_subscription_path
 
         expect(response).to have_gitlab_http_status(:ok)
         expect(response).to render_template(:show)
+      end
+    end
+  end
+
+  describe Admin::DashboardController do
+    describe "#index" do
+      it 'user has access via a custom role' do
+        get admin_root_path
+
+        expect(response).to have_gitlab_http_status(:ok)
+        expect(response).to render_template(:index)
       end
     end
   end
