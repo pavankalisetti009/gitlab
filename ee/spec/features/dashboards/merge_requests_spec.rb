425c4f0b73f70eb9d9c0f653ef93dd57e0096ee0
Marc Shaw <mshaw@gitlab.com>
2025-06-04 11:18:51 +0200

    Wrap the test in context for quarantine

--- ee/spec/features/dashboards/merge_requests_spec.rb
+++ ee/spec/features/dashboards/merge_requests_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe 'Dashboard merge requests', feature_category: :code_review_workfl
   let_it_be(:user) { create(:user) }
   let_it_be(:page_path) { merge_requests_dashboard_path(assignee_username: [user.username]) }
 
-  it_behaves_like 'dashboard ultimate trial callout', quarantine: "https://gitlab.com/gitlab-org/gitlab/-/issues/512586"
+  context 'when quarantined test', quarantine: "https://gitlab.com/gitlab-org/gitlab/-/issues/512586" do
+    it_behaves_like 'dashboard ultimate trial callout'
+  end
 
   it_behaves_like 'dashboard SAML reauthentication banner' do
     let_it_be(:match_filter_params) { true }
