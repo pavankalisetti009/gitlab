f28472d2d6df999d616e3ad8a3136ec81992fe1e
Savas Vedova <svedova@gitlab.com>
2025-06-04 10:37:40 +0200

    Remove tool token from codebase

--- ee/app/assets/javascripts/security_dashboard/components/shared/filtered_search_v2/tokens/constants.js
+++ ee/app/assets/javascripts/security_dashboard/components/shared/filtered_search_v2/tokens/constants.js
@@ -2,7 +2,6 @@ import { OPERATORS_OR, OPERATORS_IS } from '~/vue_shared/components/filtered_sea
 import StatusToken from './status_token.vue';
 import ActivityToken from './activity_token.vue';
 import SeverityToken from './severity_token.vue';
-import ToolToken from './tool_token.vue';
 import ImageToken from './image_token.vue';
 import ClusterToken from './cluster_token.vue';
 import ProjectToken from './project_token.vue';
@@ -33,15 +32,6 @@ export const ACTIVITY_TOKEN_DEFINITION = {
   multiSelect: true,
   unique: true,
   token: ActivityToken,
-  operators: OPERATORS_OR,
-};
-
-export const TOOL_VENDOR_TOKEN_DEFINITION = {
-  type: 'scanner',
-  title: ToolToken.i18n.label,
-  multiSelect: true,
-  unique: true,
-  token: ToolToken,
   operators: OPERATORS_OR,
 };
 
