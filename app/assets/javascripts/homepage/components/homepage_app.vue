106375a17517b77f08dcbb83a3d01328d467e9f9
Thomas Hutterer <thutterer@gitlab.com>
2025-06-05 12:26:56 +0200

    Init basic Vue app for new homepage

---
+++ app/assets/javascripts/homepage/components/homepage_app.vue
@@ -0,0 +1,14 @@
+<script>
+import TodosWidget from './todos_widget.vue';
+
+export default {
+  components: { TodosWidget },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ __("Today's highlights") }}</h1>
+    <todos-widget />
+  </div>
+</template>
