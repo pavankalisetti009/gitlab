d39479a9c9526e87d85ff72e10dfbb2ba12cae6f
Angelo Gulina <agulina@gitlab.com>
2025-06-06 11:44:25 +0200

    Remove references to Ramon

--- ee/app/assets/javascripts/billings/constants.js
+++ ee/app/assets/javascripts/billings/constants.js
@@ -3,7 +3,6 @@ import { s__ } from '~/locale';
 export const TABLE_TYPE_DEFAULT = 'default';
 export const TABLE_TYPE_FREE = 'free';
 export const TABLE_TYPE_TRIAL = 'trial';
-export const TABLE_TYPE_RAMON = 'ramon';
 export const TEMPORARY_EXTENSION_LABEL = s__(
   'SubscriptionTableRow|*Access temporarily extended until %{temporaryExtensionEndDate}',
 );
