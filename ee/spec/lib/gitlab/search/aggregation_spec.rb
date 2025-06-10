d6d2695cad8a383a8a23906d3c690340bb39a9fc
Schmil Monderer <smonderer@gitlab.com>
2025-06-10 20:30:33 +0000

    Add vulnerability_elastic_base_finder.rb

--- ee/spec/lib/gitlab/search/aggregation_spec.rb
+++ ee/spec/lib/gitlab/search/aggregation_spec.rb
@@ -16,6 +16,68 @@ RSpec.describe Gitlab::Search::Aggregation do
       end
     end
 
+    context 'when elasticsearch nested buckets are provided' do
+      let(:aggregation_buckets) do
+        [
+          {
+            key_as_string: "2025-04-11T00:00:00.000Z",
+            key: 1744243200000,
+            doc_count: 16676,
+            by_severity: {
+              buckets: [
+                {
+                  key: 5,
+                  doc_count: 9780
+                },
+                {
+                  key: 4,
+                  doc_count: 6896
+                }
+              ]
+            }
+          },
+          {
+            key_as_string: "2025-04-12T00:00:00.000Z",
+            key: 1744416000000,
+            doc_count: 3269,
+            by_severity: {
+              buckets: [
+                {
+                  key: 5,
+                  doc_count: 1948
+                },
+                {
+                  key: 4,
+                  doc_count: 1321
+                }
+              ]
+            }
+          }
+        ].map(&:with_indifferent_access)
+      end
+
+      it 'parses the results' do
+        expected = [
+          {
+            key: "2025-04-11T00:00:00.000Z",
+            count: 16676,
+            buckets: {
+              by_severity: [{ key: 5, count: 9780 }, { key: 4, count: 6896 }]
+            }
+          },
+          {
+            key: "2025-04-12T00:00:00.000Z",
+            count: 3269,
+            buckets: {
+              by_severity: [{ key: 5, count: 1948 }, { key: 4, count: 1321 }]
+            }
+          }
+        ]
+
+        expect(subject.buckets).to match_array(expected)
+      end
+    end
+
     context 'when extra is provided' do
       let(:aggregation_buckets) do
         [
