87e12824917c2624cf4c1f1ed8770823bb50576c
Simon Knox <simon@gitlab.com>
2025-06-11 15:38:25 +1000

    Add custom status settings page

--- ee/app/controllers/groups/settings/work_items_controller.rb
+++ ee/app/controllers/groups/settings/work_items_controller.rb
@@ -11,16 +11,21 @@ module Groups
       feature_category :team_planning
       urgency :low
 
+      before_action do
+        push_frontend_feature_flag(:work_item_status_feature_flag, group)
+      end
+
       def show; end
 
       private
 
       def check_feature_availability
-        render_404 unless group.licensed_feature_available?(:custom_fields)
+        render_404 unless group.licensed_feature_available?(:custom_fields) || group.work_item_status_feature_available?
       end
 
       def authorize_admin_work_item_settings
-        render_404 unless can?(current_user, :admin_custom_field, group)
+        render_404 unless can?(current_user, :admin_custom_field,
+          group) || can?(current_user, :admin_work_item, group)
       end
     end
   end
