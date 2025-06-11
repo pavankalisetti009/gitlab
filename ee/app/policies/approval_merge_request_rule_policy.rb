529bf673cafb2a22245b925cfd2ae3825d59d772
Marc Shaw <mshaw@gitlab.com>
2025-06-11 21:22:51 +0200

    Ensure the editting of approval rules is consistent
    MR: gitlab.com/gitlab-org/gitlab/-/merge_requests/189771

--- ee/app/policies/approval_merge_request_rule_policy.rb
+++ ee/app/policies/approval_merge_request_rule_policy.rb
@@ -4,7 +4,12 @@ class ApprovalMergeRequestRulePolicy < BasePolicy
   delegate { @subject.merge_request }
 
   condition(:editable) do
-    can?(:update_merge_request, @subject.merge_request) && @subject.user_defined?
+    if Feature.enabled?(:ensure_consistent_editing_rule, @subject.merge_request.project)
+      @subject.editable_by_user?(@user) &&
+        can?(:update_merge_request, @subject.merge_request)
+    else
+      can?(:update_merge_request, @subject.merge_request) && @subject.user_defined?
+    end
   end
 
   rule { editable }.enable :edit_approval_rule
