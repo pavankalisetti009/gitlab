1ee6ec8a0f4b7dae29b3d8cb4ac6c5dec6fde2e4
Coung Ngo <cngo@gitlab.com>
2025-06-11 14:24:23 +0100

    Update issue feature specs to new issue look
    Now that the new issue look (based on the work items framework)
    is enabled by default, the feature specs should be updated
    accordingly

--- ee/spec/features/issues/form_spec.rb
+++ ee/spec/features/issues/form_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'New/edit issue', :js, feature_category: :team_planning do
   include GitlabRoutingHelper
   include ActionView::Helpers::JavaScriptHelper
   include FormHelper
+  include ListboxHelpers
 
   let!(:project)   { create(:project) }
   let!(:user)      { create(:user) }
@@ -19,6 +20,7 @@ RSpec.describe 'New/edit issue', :js, feature_category: :team_planning do
     project.add_maintainer(user)
     project.add_maintainer(user2)
 
+    stub_feature_flags(work_item_view_for_issues: true)
     stub_licensed_features(multiple_issue_assignees: true)
     gitlab_sign_in(user)
   end
@@ -28,157 +30,27 @@ RSpec.describe 'New/edit issue', :js, feature_category: :team_planning do
       visit new_project_issue_path(project)
     end
 
-    describe 'shorten users API pagination limit' do
-      before do
-        # Using `allow_any_instance_of`/`and_wrap_original`, `original` would
-        # somehow refer to the very block we defined to _wrap_ that method, instead of
-        # the original method, resulting in infinite recursion when called.
-        # This is likely a bug with helper modules included into dynamically generated view classes.
-        # To work around this, we have to hold on to and call to the original implementation manually.
-        original_issue_dropdown_options = FormHelper.instance_method(:assignees_dropdown_options)
-        allow_any_instance_of(FormHelper).to receive(:assignees_dropdown_options).and_wrap_original do |original, *args|
-          options = original_issue_dropdown_options.bind_call(original.receiver, *args)
-          options[:data][:per_page] = 2
-
-          options
-        end
-
-        visit new_project_issue_path(project)
-
-        click_button 'Unassigned'
-      end
-
-      it 'displays selected users even if they are not part of the original API call' do
-        fill_in 'Search users', with: user2.name
-        click_link user2.name
-
-        page.within '.dropdown-menu-user' do
-          expect(page).to have_content user.name
-          expect(find('.dropdown-menu-user a.is-active').first(:xpath, '..')['data-user-id']).to eq(user2.id.to_s)
-        end
-      end
-    end
-
     describe 'multiple assignees' do
-      before do
-        click_button 'Unassigned'
-      end
-
       it 'unselects other assignees when unassigned is selected' do
-        click_link user2.name
-        click_link 'Unassigned'
+        within_testid('work-item-assignees') do
+          expect(page).to have_button 'assign yourself'
 
-        expect(find('input[name="issue[assignee_ids][]"]', visible: false).value).to match('0')
-      end
+          click_button 'Edit'
+          select_listbox_item(user.name)
+          select_listbox_item(user2.name)
+          click_button 'Apply'
 
-      it 'toggles assign to me when current user is selected and unselected' do
-        click_link user.name
+          expect(page).to have_link user.name
+          expect(page).to have_link user2.name
+          expect(page).not_to have_button 'assign yourself'
 
-        expect(page).not_to have_link 'Assign to me'
+          click_button 'Edit'
+          click_button 'Clear'
 
-        click_link user.name
-
-        expect(page).to have_link 'Assign to me'
+          expect(page).to have_text 'None'
+          expect(page).to have_button 'assign yourself'
+        end
       end
     end
-
-    it 'allows user to create new issue' do
-      fill_in 'Title (required)', with: 'title'
-      fill_in 'Description', with: 'title'
-
-      expect(page).to have_link 'Assign to me'
-
-      within_testid('merge-request-assignee') do
-        click_button 'Unassigned'
-        click_link user2.name
-
-        expect(find('input[name="issue[assignee_ids][]"]', visible: false).value).to match(user2.id.to_s)
-        expect(page).to have_button user2.name
-
-        click_button 'Close'
-        click_link 'Assign to me'
-
-        assignee_ids = page.all('input[name="issue[assignee_ids][]"]', visible: false)
-        expect(assignee_ids[0].value).to match(user2.id.to_s)
-        expect(assignee_ids[1].value).to match(user.id.to_s)
-
-        expect(page).to have_button "#{user2.name} + 1 more"
-        expect(page).not_to have_link 'Assign to me'
-      end
-
-      click_button 'Select milestone'
-      click_button milestone.title
-
-      expect(find('input[name="issue[milestone_id]"]', visible: false).value).to match(milestone.id.to_s)
-      expect(page).to have_button milestone.title
-
-      click_button _('Select label')
-      wait_for_all_requests
-      within_testid('sidebar-labels') do
-        click_button label.title
-        click_button label2.title
-        click_button _('Close')
-        wait_for_requests
-      end
-
-      expect(page).to have_button label.title
-      within_testid('embedded-labels-list') do
-        expect(page).to have_content(label.title)
-        expect(page).to have_content(label2.title)
-      end
-
-      fill_in 'issue_weight', with: '1'
-      click_button 'Create issue'
-
-      page.within '.issuable-sidebar' do
-        page.within '.assignee' do
-          expect(page).to have_text "2 Assignees"
-        end
-
-        page.within '.milestone' do
-          expect(page).to have_text milestone.title
-        end
-
-        page.within '.labels' do
-          expect(page).to have_text label.title
-          expect(page).to have_text label2.title
-        end
-
-        page.within '.weight' do
-          expect(page).to have_text '1'
-        end
-      end
-
-      within_testid('breadcrumb-links') do
-        issue = Issue.find_by(title: 'title')
-
-        expect(page).to have_text("Issues #{issue.to_reference}")
-      end
-    end
-
-    it 'correctly updates the selected user when changing assignee' do
-      click_button 'Unassigned'
-      click_link user.name
-
-      expect(page).to have_button(user.name)
-
-      click_link user2.name
-
-      expect(page.all('input[name="issue[assignee_ids][]"]', visible: false)[0].value).to match(user.id.to_s)
-      expect(page.all('input[name="issue[assignee_ids][]"]', visible: false)[1].value).to match(user2.id.to_s)
-      expect(page.all('.dropdown-menu-user a.is-active').length).to eq(2)
-      expect(page.all('.dropdown-menu-user a.is-active')[0].first(:xpath, '..')['data-user-id']).to eq(user.id.to_s)
-      expect(page.all('.dropdown-menu-user a.is-active')[1].first(:xpath, '..')['data-user-id']).to eq(user2.id.to_s)
-    end
-  end
-
-  def before_for_selector(selector)
-    js = <<-JS.strip_heredoc
-      (function(selector) {
-        var el = document.querySelector(selector);
-        return window.getComputedStyle(el, '::before').getPropertyValue('content');
-      })("#{escape_javascript(selector)}")
-    JS
-    page.evaluate_script(js)
   end
 end
