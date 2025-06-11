73388fe5ba6aad5bb394422e603cde5d345db469
Arturo Herrero <arturo.herrero@gmail.com>
2025-06-11 17:16:38 +0200

    Fix mispaced EE search specs
    EE: true

--- ee/spec/services/ee/search/snippet_service_spec.rb
+++ ee/spec/services/ee/search/snippet_service_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe Search::SnippetService, feature_category: :global_search do
   include AdminModeHelper
   using RSpec::Parameterized::TableSyntax
 
-  it_behaves_like 'EE search service shared examples', ::Gitlab::SnippetSearchResults, ::Gitlab::Elastic::SnippetSearchResults do
+  it_behaves_like 'EE search service shared examples', ::Gitlab::SnippetSearchResults,
+    ::Gitlab::Elastic::SnippetSearchResults do
     let_it_be(:user) { create(:user) }
     let(:scope) { nil }
     let(:service) { described_class.new(user, params) }
@@ -21,7 +22,7 @@ RSpec.describe Search::SnippetService, feature_category: :global_search do
       stub_ee_application_setting(elasticsearch_search: true, elasticsearch_indexing: true)
     end
 
-    context 'visibility', :elastic_delete_by_query, :clean_gitlab_redis_shared_state, :sidekiq_inline do
+    context 'for visibility', :elastic_delete_by_query, :clean_gitlab_redis_shared_state, :sidekiq_inline do
       include_context 'ProjectPolicyTable context'
       include ProjectHelpers
 
@@ -31,22 +32,31 @@ RSpec.describe Search::SnippetService, feature_category: :global_search do
       let_it_be(:guest)          { create(:user) }
       let_it_be(:snippet_author) { create(:user) }
 
-      context 'project snippet' do
+      context 'for project snippet' do
         let(:pendings) do
-          # TODO: Ignore some spec cases, non-members regular users or non-member admins without admin mode should see snippets if:
+          # TODO: Ignore some spec cases, non-members regular users or non-member admins without admin mode
+          # should see snippets if:
           #   - feature access level is enabled, and
           #   - project access level is public or internal, and
           #   - snippet access level is equal or more open than the project access level
           # See https://gitlab.com/gitlab-org/gitlab/-/merge_requests/45988#note_436009204
           [
-            { snippet_level: :public, project_level: :public, feature_access_level: :enabled, membership: :admin, admin_mode: false, expected_count: 1 },
-            { snippet_level: :public, project_level: :internal, feature_access_level: :enabled, membership: :admin, admin_mode: false, expected_count: 1 },
-            { snippet_level: :internal, project_level: :public, feature_access_level: :enabled, membership: :admin, admin_mode: false, expected_count: 1 },
-            { snippet_level: :internal, project_level: :internal, feature_access_level: :enabled, membership: :admin, admin_mode: false, expected_count: 1 },
-            { snippet_level: :public, project_level: :public, feature_access_level: :enabled, membership: :non_member, admin_mode: nil, expected_count: 1 },
-            { snippet_level: :public, project_level: :internal, feature_access_level: :enabled, membership: :non_member, admin_mode: nil, expected_count: 1 },
-            { snippet_level: :internal, project_level: :public, feature_access_level: :enabled, membership: :non_member, admin_mode: nil, expected_count: 1 },
-            { snippet_level: :internal, project_level: :internal, feature_access_level: :enabled, membership: :non_member, admin_mode: nil, expected_count: 1 }
+            { snippet_level: :public, project_level: :public, feature_access_level: :enabled, membership: :admin,
+              admin_mode: false, expected_count: 1 },
+            { snippet_level: :public, project_level: :internal, feature_access_level: :enabled, membership: :admin,
+              admin_mode: false, expected_count: 1 },
+            { snippet_level: :internal, project_level: :public, feature_access_level: :enabled, membership: :admin,
+              admin_mode: false, expected_count: 1 },
+            { snippet_level: :internal, project_level: :internal, feature_access_level: :enabled, membership: :admin,
+              admin_mode: false, expected_count: 1 },
+            { snippet_level: :public, project_level: :public, feature_access_level: :enabled, membership: :non_member,
+              admin_mode: nil, expected_count: 1 },
+            { snippet_level: :public, project_level: :internal, feature_access_level: :enabled,
+              membership: :non_member, admin_mode: nil, expected_count: 1 },
+            { snippet_level: :internal, project_level: :public, feature_access_level: :enabled,
+              membership: :non_member, admin_mode: nil, expected_count: 1 },
+            { snippet_level: :internal, project_level: :internal, feature_access_level: :enabled,
+              membership: :non_member, admin_mode: nil, expected_count: 1 }
           ]
         end
 
@@ -71,7 +81,9 @@ RSpec.describe Search::SnippetService, feature_category: :global_search do
           end
         end
 
-        let_it_be(:snippet) { create(:project_snippet, :public, project: project, author: snippet_author, title: 'foobar') }
+        let_it_be(:snippet) do
+          create(:project_snippet, :public, project: project, author: snippet_author, title: 'foobar')
+        end
 
         where(:snippet_level, :project_level, :feature_access_level, :membership, :admin_mode, :expected_count) do
           permission_table_for_project_snippet_access
@@ -79,7 +91,8 @@ RSpec.describe Search::SnippetService, feature_category: :global_search do
 
         with_them do
           it 'respects visibility' do
-            project.update!(visibility_level: Gitlab::VisibilityLevel.level_value(project_level.to_s), snippets_access_level: feature_access_level)
+            project.update!(visibility_level: Gitlab::VisibilityLevel.level_value(project_level.to_s),
+              snippets_access_level: feature_access_level)
             snippet.update!(visibility_level: Gitlab::VisibilityLevel.level_value(snippet_level.to_s))
             ensure_elasticsearch_index!
 
@@ -88,17 +101,26 @@ RSpec.describe Search::SnippetService, feature_category: :global_search do
             search_user = user_from_membership(membership)
             enable_admin_mode!(search_user) if admin_mode
 
-            expect_search_results(search_user, 'snippet_titles', expected_objects: expected_objects, pending: pending?) do |user|
+            expect_search_results(search_user, 'snippet_titles', expected_objects: expected_objects,
+              pending: pending?) do |user|
               described_class.new(user, search: snippet.title).execute
             end
           end
         end
       end
 
-      context 'personal snippet' do
-        let_it_be(:public_snippet)   { create(:personal_snippet, :public, title: 'This is a public snippet', author: snippet_author) }
-        let_it_be(:private_snippet)  { create(:personal_snippet, :private, title: 'This is a private snippet', author: snippet_author) }
-        let_it_be(:internal_snippet) { create(:personal_snippet, :internal, title: 'This is a internal snippet', author: snippet_author) }
+      context 'for personal snippet' do
+        let_it_be(:public_snippet) do
+          create(:personal_snippet, :public, title: 'This is a public snippet', author: snippet_author)
+        end
+
+        let_it_be(:private_snippet) do
+          create(:personal_snippet, :private, title: 'This is a private snippet', author: snippet_author)
+        end
+
+        let_it_be(:internal_snippet) do
+          create(:personal_snippet, :internal, title: 'This is a internal snippet', author: snippet_author)
+        end
 
         let(:snippets) do
           {
