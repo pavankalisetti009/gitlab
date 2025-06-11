745db3e2755349aeb71aeb5d5218662cb569c8fa
Coung Ngo <cngo@gitlab.com>
2025-06-11 23:13:52 +0000

    Add group /work_items/new page
    Add /work_items/new route at the group level without feature flag
    that allows user to select project and project-level work item to
    create

--- ee/app/assets/javascripts/pages/groups/epics/new/index.js
+++ ee/app/assets/javascripts/pages/groups/epics/new/index.js
@@ -1,4 +1,5 @@
 import { WORKSPACE_GROUP } from '~/issues/constants';
 import { initWorkItemsRoot } from '~/work_items';
+import { WORK_ITEM_TYPE_NAME_EPIC } from '~/work_items/constants';
 
-initWorkItemsRoot({ workspaceType: WORKSPACE_GROUP });
+initWorkItemsRoot({ workItemType: WORK_ITEM_TYPE_NAME_EPIC, workspaceType: WORKSPACE_GROUP });
