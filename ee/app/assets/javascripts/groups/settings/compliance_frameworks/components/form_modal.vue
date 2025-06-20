cb95da9643355ad7fbcc9cab600a411716ffa180
Sahil Jawale <sahiljawale54@gmail.com>
2025-06-20 19:19:13 +0530

    Handled eslint error in form_modal

--- ee/app/assets/javascripts/groups/settings/compliance_frameworks/components/form_modal.vue
+++ ee/app/assets/javascripts/groups/settings/compliance_frameworks/components/form_modal.vue
@@ -27,6 +27,7 @@ export default {
     },
   },
   methods: {
+    // eslint-disable-next-line vue/no-unused-properties -- show() is part of the component's public API.
     show() {
       this.$refs.modal.show();
     },
