0ed64f3156c2d3308517f5fedc257c7bc616b931
Illya Klymov <iklymov@gitlab.com>
2025-06-20 14:15:29 +0300

    Improve code readability

--- ee/app/finders/compliance_management/compliance_framework/project_requirement_status_finder.rb
+++ ee/app/finders/compliance_management/compliance_framework/project_requirement_status_finder.rb
@@ -30,8 +30,9 @@ module ComplianceManagement
       attr_reader :group, :current_user, :params
 
       def allowed?
-        (project.present? && Ability.allowed?(current_user, :read_compliance_adherence_report, project)) ||
-          Ability.allowed?(current_user, :read_compliance_adherence_report, group)
+        [project, group].any? do |subject|
+          Ability.allowed?(current_user, :read_compliance_adherence_report, subject)
+        end
       end
 
       def model
