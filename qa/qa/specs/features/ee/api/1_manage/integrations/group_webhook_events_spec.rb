0adf702407ce2269961db260e88e024b0484fc86
tim_beauchamp <tbeauchamp@gitlab.com>
2025-06-24 19:16:43 -0300

    Change product group name to remove and_integrate

--- qa/qa/specs/features/ee/api/1_manage/integrations/group_webhook_events_spec.rb
+++ qa/qa/specs/features/ee/api/1_manage/integrations/group_webhook_events_spec.rb
@@ -7,7 +7,7 @@ module QA
       :requires_admin,
       :integrations,
       :orchestrated,
-      product_group: :import_and_integrate
+      product_group: :import
     ) do
       before(:context) do
         toggle_local_requests(true)
