ed2b0470183566e18d9aeeba8fd36f1c007bb12d
Jose Ivan Vargas <jvargas@gitlab.com>
2025-06-24 00:39:14 -0600

    Add RegistryUpstream type for Maven upstreams
    EE: true

--- ee/spec/graphql/ee/types/virtual_registries/packages/maven/maven_upstream_type_spec.rb
+++ ee/spec/graphql/ee/types/virtual_registries/packages/maven/maven_upstream_type_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe GitlabSchema.types['MavenUpstream'], feature_category: :virtual_r
 
   subject { described_class }
 
-  let_it_be(:fields) { %i[id name description url cacheValidityHours username password] }
+  let_it_be(:fields) { %i[id name description url cacheValidityHours username password registry_upstreams] }
 
   it { is_expected.to require_graphql_authorizations(:read_virtual_registry) }
   it { is_expected.to have_graphql_fields(fields) }
