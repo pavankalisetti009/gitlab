a5de8af6d78b60f72ef2d201516f0d89297ab15d
Pavel Shutsin <pshutsin@gitlab.com>
2025-06-24 11:50:05 +0200

    Track AI usage to new Ai::UsageEvent model
    Tracking is hidden behind `unified_ai_events_write`
    feature flag. New tracking stores all events
    in `ai_usage_events` table.
    
    Changelog: added
    EE: true

--- ee/lib/gitlab/tracking/ai_tracking.rb
+++ ee/lib/gitlab/tracking/ai_tracking.rb
@@ -30,7 +30,13 @@ module Gitlab
 
       class << self
         def track_event(event_name, **context_hash)
-          OldApproach.track_event(event_name, **context_hash)
+          tracked = OldApproach.track_event(event_name, **context_hash)
+
+          if Feature.enabled?(:unified_ai_events_write, context_hash[:user])
+            tracked = UnifiedApproach.track_event(event_name, **context_hash) || tracked
+          end
+
+          track_user_activity(context_hash[:user]) if tracked
         end
 
         def track_user_activity(user)
--- ee/spec/lib/gitlab/tracking/ai_tracking_spec.rb
+++ ee/spec/lib/gitlab/tracking/ai_tracking_spec.rb
@@ -3,51 +3,15 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Tracking::AiTracking, feature_category: :value_stream_management do
-  context 'for code suggestion events' do
-    let(:expected_context) do
-      { unique_tracking_id: '123', suggestion_size: 100, language: 'ruby', branch_name: 'feature' }
-    end
+  # .track_event is tested in _old_approach_spec and _unified_approach_spec separately for now.
 
-    let(:sample_context) do
-      expected_context.merge(something_extra: '123')
-    end
+  describe '.track_user_activity' do
+    let(:current_user) { create(:user) }
 
-    it 'has `shown` event registered' do
-      is_expected.to have_ai_event_registered(code_suggestion_shown_in_ide: 2)
-                       .with_transformation(sample_context => expected_context)
-    end
+    it 'refreshes user metrics for last activity' do
+      expect(Ai::UserMetrics).to receive(:refresh_last_activity_on).with(current_user).and_call_original
 
-    it 'has `accepted` event registered' do
-      is_expected.to have_ai_event_registered(code_suggestion_accepted_in_ide: 3)
-                       .with_transformation(sample_context => expected_context)
-    end
-
-    it 'has `rejected` event registered' do
-      is_expected.to have_ai_event_registered(code_suggestion_rejected_in_ide: 4)
-                       .with_transformation(sample_context => expected_context)
-    end
-  end
-
-  context 'for duo chat events' do
-    it { is_expected.to have_ai_event_registered(request_duo_chat_response: 6).with_no_transformations }
-  end
-
-  context 'for troubleshoot job events' do
-    let_it_be(:merge_request) { create(:merge_request) }
-    let_it_be(:pipeline) { create(:ci_pipeline, merge_request: merge_request) }
-    let_it_be(:job) { create(:ci_build, pipeline: pipeline) }
-
-    let(:sample_context) do
-      { job: job }
-    end
-
-    let(:expected_context) do
-      { job_id: job.id, project_id: job.project_id, pipeline_id: pipeline.id, merge_request_id: merge_request.id }
-    end
-
-    it 'has `troubleshoot_job` event registered' do
-      is_expected.to have_ai_event_registered(troubleshoot_job: 7)
-                       .with_transformation(sample_context => expected_context)
+      described_class.track_user_activity(current_user)
     end
   end
 end
