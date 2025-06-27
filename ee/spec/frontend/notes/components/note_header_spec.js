2cd2024662a08cb106b85a0526f4e6b5c6586f9c
Stanislav Lashmanov <slashmanov@gitlab.com>
2025-06-27 18:44:29 +0400

    Migrate more notes components to Pinia

--- ee/spec/frontend/notes/components/note_header_spec.js
+++ ee/spec/frontend/notes/components/note_header_spec.js
@@ -1,20 +1,30 @@
 import Vue from 'vue';
-// eslint-disable-next-line no-restricted-imports
-import Vuex from 'vuex';
+import { PiniaVuePlugin } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import NoteHeader from '~/notes/components/note_header.vue';
+import { globalAccessorPlugin } from '~/pinia/plugins';
+import { useLegacyDiffs } from '~/diffs/stores/legacy_diffs';
+import { useNotes } from '~/notes/store/legacy_notes';
 
-Vue.use(Vuex);
+Vue.use(PiniaVuePlugin);
 
 describe('NoteHeader component', () => {
   let wrapper;
+  let pinia;
 
   const createComponent = (props) => {
     wrapper = shallowMountExtended(NoteHeader, {
-      store: new Vuex.Store(),
+      pinia,
       propsData: { ...props },
     });
   };
+
+  beforeEach(() => {
+    pinia = createTestingPinia({ plugins: [globalAccessorPlugin] });
+    useLegacyDiffs();
+    useNotes();
+  });
 
   it('shows internal note badge tooltip for group context when isInternalNote is true for epics', () => {
     createComponent({ isInternalNote: true, noteableType: 'epic' });
