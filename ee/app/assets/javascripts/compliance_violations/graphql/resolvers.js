0b88e2b0b8ebeb0b1d739e390b5b0a2a921e675e
Scott Hampton <shampton@gitlab.com>
2025-06-27 08:37:05 -0700

    Add audit event section
    Added a simple audit event section
    that will be able to call a drawer in the
    next iteration.

--- ee/app/assets/javascripts/compliance_violations/graphql/resolvers.js
+++ ee/app/assets/javascripts/compliance_violations/graphql/resolvers.js
@@ -1,16 +1,43 @@
+/* eslint-disable @gitlab/require-i18n-strings */
 import produce from 'immer';
 import { uniqueId } from 'lodash';
 import complianceViolationQuery from './compliance_violation.query.graphql';
 
 const getMockViolationData = (id) => ({
-  id,
-  status: 'in_review',
+  id: `gid://gitlab/ComplianceManagement::Projects::ComplianceViolation/${id}`,
+  status: 'IN_REVIEW',
+  createdAt: '2025-06-16T02:20:41Z',
   project: {
-    id: 2,
+    id: 'gid://gitlab/Project/2',
     nameWithNamespace: 'GitLab.org / GitLab Test',
-    fullPath: '/gitlab/org/gitlab-test',
+    fullPath: 'gitlab-org/gitlab-test',
     webUrl: 'https://localhost:3000/gitlab/org/gitlab-test',
     __typename: 'Project',
+  },
+  auditEvent: {
+    id: 'gid://gitlab/AuditEvents::ProjectAuditEvent/467',
+    eventName: 'merge_request_merged',
+    targetId: '2',
+    details: '{}',
+    ipAddress: '123.1.1.9',
+    entityPath: 'gitlab-org/gitlab-test',
+    entityId: '2',
+    entityType: 'Project',
+    author: {
+      id: 'gid://gitlab/User/1',
+      name: 'John Doe',
+    },
+    project: {
+      id: 'gid://gitlab/Project/2',
+      name: 'Test project',
+      fullPath: 'gitlab-org/gitlab-test',
+      webUrl: 'https://localhost:3000/gitlab/org/gitlab-test',
+    },
+    group: null,
+    user: {
+      id: 'gid://gitlab/User/1',
+      name: 'John Doe',
+    },
   },
   __typename: 'ComplianceViolation',
 });
