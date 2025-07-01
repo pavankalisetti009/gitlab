8067d099b1aff23b9593dfe2f35e42b1946fab07
smriti <sgarg@gitlab.com>
2025-07-01 14:58:41 +0530

    Removal of FF delete_pipl_non_compliant_users
    This Feature flag has been set to true in production since quite some time now.
    No major errors have been reported so far and hence the same can be removed
    from the codebase.
    
    Changelog: removed
    EE: true

--- ee/app/policies/compliance_management/pipl_user_policy.rb
+++ ee/app/policies/compliance_management/pipl_user_policy.rb
@@ -6,17 +6,9 @@ module ComplianceManagement
       ::Gitlab::CurrentSettings.enforce_pipl_compliance?
     end
 
-    condition(:disable_delete_pipl_user) do
-      Feature.disabled?(:delete_pipl_non_compliant_users, @subject)
-    end
-
     rule { admin & enforce_pipl_compliance }.policy do
       enable :block_pipl_user
       enable :delete_pipl_user
-    end
-
-    rule { disable_delete_pipl_user }.policy do
-      prevent :delete_pipl_user
     end
   end
 end
