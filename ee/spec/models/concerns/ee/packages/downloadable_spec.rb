1cf6e7ed8363daebd50f460b7f7ba595dbf36a6f
Dzmitry Meshcharakou <12459192-dmeshcharakou@users.noreply.gitlab.com>
2025-07-02 12:47:58 +0200

    Add class method to update last_downloaded_at
    There might be the case when it's needed to invoke the update of
    last_downloaded_at indirectly: only object's id is known, but no
    instance is loaded.
    
    Changelog: added

--- ee/spec/models/concerns/ee/packages/downloadable_spec.rb
+++ ee/spec/models/concerns/ee/packages/downloadable_spec.rb
@@ -4,30 +4,40 @@ require 'spec_helper'
 
 RSpec.describe EE::Packages::Downloadable, feature_category: :package_registry do
   context 'with a package' do
-    let_it_be(:package) { create(:generic_package) }
+    let_it_be_with_reload(:package) { create(:generic_package) }
 
-    describe '#touch_last_downloaded_at' do
-      subject { package.touch_last_downloaded_at }
+    shared_examples 'updating the last_downloaded_at column' do
+      before do
+        allow(::Gitlab::Geo).to receive(:secondary?).and_return(secondary)
+      end
 
       context 'when not on a geo secondary' do
-        before do
-          allow(::Gitlab::Geo).to receive(:secondary?).and_return(false)
-        end
+        let(:secondary) { false }
 
         it 'updates the last_downloaded_at column' do
-          expect { subject }.to change { package.last_downloaded_at }
+          expect { execute }.to change { package.reload.last_downloaded_at }
         end
       end
 
       context 'when on a geo secondary' do
-        before do
-          allow(::Gitlab::Geo).to receive(:secondary?).and_return(true)
-        end
+        let(:secondary) { true }
 
         it 'does not update the last_downloaded_at column' do
-          expect { subject }.not_to change { package.last_downloaded_at }
+          expect { execute }.not_to change { package.reload.last_downloaded_at }
         end
       end
+    end
+
+    describe '#touch_last_downloaded_at' do
+      subject(:execute) { package.touch_last_downloaded_at }
+
+      it_behaves_like 'updating the last_downloaded_at column'
+    end
+
+    describe '.touch_last_downloaded_at' do
+      subject(:execute) { ::Packages::Generic::Package.touch_last_downloaded_at(package.id) }
+
+      it_behaves_like 'updating the last_downloaded_at column'
     end
   end
 end
