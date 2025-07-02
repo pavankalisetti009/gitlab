dd0fcfd0615ecd5917909211a6b8b33483f3d338
Gal Katz <gkatz@gitlab.com>
2025-07-02 16:04:50 +0000

    Fix symbol parameter being passed to sidekiq
    Changelog: fixed
    EE: true

--- ee/app/models/project_security_setting.rb
+++ ee/app/models/project_security_setting.rb
@@ -10,10 +10,10 @@ class ProjectSecuritySetting < ApplicationRecord
   ignore_column :pre_receive_secret_detection_enabled, remove_with: '17.9', remove_after: '2025-02-15'
 
   # saved_change_to_project_id? will return true on creating a new instance as project_id is the primary key
-  after_commit -> { schedule_analyzer_status_update_worker_for_type(:container_scanning) },
+  after_commit -> { schedule_analyzer_status_update_worker_for_type('container_scanning') },
     if: -> { saved_change_to_container_scanning_for_registry_enabled? || saved_change_to_project_id? }
 
-  after_commit -> { schedule_analyzer_status_update_worker_for_type(:secret_detection) },
+  after_commit -> { schedule_analyzer_status_update_worker_for_type('secret_detection') },
     if: -> { saved_change_to_secret_push_protection_enabled? || saved_change_to_project_id? }
 
   def set_continuous_vulnerability_scans!(enabled:)
