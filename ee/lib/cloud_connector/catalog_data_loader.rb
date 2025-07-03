73badeb3c2401f821838dd2b52408626c89655a5
Nikola Milojevic <nmilojevic@gitlab.com>
2025-07-03 15:33:02 +0200

    Make sure to load correct loader on every request
    - Fix caching issue
    - Udate catalog data loader specs

--- ee/lib/cloud_connector/catalog_data_loader.rb
+++ ee/lib/cloud_connector/catalog_data_loader.rb
@@ -2,43 +2,42 @@
 
 module CloudConnector
   class CatalogDataLoader < Gitlab::CloudConnector::DataModel::AbstractDataLoader
-    class << self
-      def data_loader_class
-        @data_loader_class ||= if use_yaml_data_loader?
-                                 ::Gitlab::CloudConnector::DataModel::YamlDataLoader
-                               else
-                                 ::CloudConnector::DatabaseDataLoader
-                               end
-      end
-
-      private
-
-      def use_yaml_data_loader?
-        return true if ::Gitlab::Saas.feature_available?(:cloud_connector_static_catalog)
-        return true if License.current.nil? || License.current.offline_cloud_license?
-
-        # This is a temporary fix for self_hosted_models issue:
-        # https://gitlab.com/gitlab-org/gitlab/-/issues/552318
-        # CloudConnector::AvailableServicesGenerator should always
-        # use YamlDataLoader, otherwise it would return empty hash.
-        # This can be removed once we have a better way to handle self_hosted_models.
-        return true if ::Ai::Setting.self_hosted?
-
-        Gitlab::Utils.to_boolean(ENV['CLOUD_CONNECTOR_SELF_SIGN_TOKENS'])
-      end
-    end
-    def initialize(model_class)
-      super
-
-      @loader = self.class.data_loader_class.new(model_class)
-    end
+    include Gitlab::Utils::StrongMemoize
 
     def load!
       loader.load!
     end
 
+    def loader
+      use_yaml_data_loader? ? yaml_data_loader : database_data_loader
+    end
+
     private
 
-    attr_reader :loader
+    def use_yaml_data_loader?
+      return true if ::Gitlab::Saas.feature_available?(:cloud_connector_static_catalog)
+      return true if License.current.nil? || License.current.offline_cloud_license?
+
+      # This is a temporary fix for self_hosted_models issue:
+      # https://gitlab.com/gitlab-org/gitlab/-/issues/552318
+      # CloudConnector::AvailableServicesGenerator should always
+      # use YamlDataLoader, otherwise it would return empty hash.
+      # This can be removed once we have a better way to handle self_hosted_models.
+      return true if ::Ai::Setting.self_hosted?
+
+      Gitlab::Utils.to_boolean(ENV['CLOUD_CONNECTOR_SELF_SIGN_TOKENS'])
+    end
+
+    def yaml_data_loader
+      strong_memoize_with(:yaml_data_loader, model_class) do
+        ::Gitlab::CloudConnector::DataModel::YamlDataLoader.new(model_class)
+      end
+    end
+
+    def database_data_loader
+      strong_memoize_with(:database_data_loader, model_class) do
+        ::CloudConnector::DatabaseDataLoader.new(model_class)
+      end
+    end
   end
 end
