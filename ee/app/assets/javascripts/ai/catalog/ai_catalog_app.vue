c620133c97e81f579a99857a2cde664d4ab6778d
James Rushford <jrushford@gitlab.com>
2025-07-04 02:52:13 +0200

    Create new agent route and link

--- ee/app/assets/javascripts/ai/catalog/ai_catalog_app.vue
+++ ee/app/assets/javascripts/ai/catalog/ai_catalog_app.vue
@@ -1,5 +1,7 @@
 <script>
+import { GlButton } from '@gitlab/ui';
 import PageHeading from '~/vue_shared/components/page_heading.vue';
+import { AI_CATALOG_AGENTS_NEW_ROUTE } from './router/constants';
 import AiCatalogNavTabs from './components/ai_catalog_nav_tabs.vue';
 
 export default {
@@ -7,13 +9,21 @@ export default {
   components: {
     AiCatalogNavTabs,
     PageHeading,
+    GlButton,
   },
+  newAgentRoute: { name: AI_CATALOG_AGENTS_NEW_ROUTE },
 };
 </script>
 
 <template>
   <div>
-    <page-heading :heading="s__('AICatalog|AI Catalog')" />
+    <page-heading :heading="s__('AICatalog|AI Catalog')">
+      <template #actions>
+        <gl-button :to="$options.newAgentRoute" variant="confirm">
+          {{ s__('AICatalog|New agent') }}
+        </gl-button>
+      </template>
+    </page-heading>
     <ai-catalog-nav-tabs />
     <router-view />
   </div>
