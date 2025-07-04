86e93a5a57eebaaa37c3801d4e8f73262b49b41e
Gal Katz <gkatz@gitlab.com>
2025-07-04 13:54:30 +0300

    Add defer_on_database_health_signal to setting change update worker
    Changelog: fixed
    EE: true

--- ee/app/workers/security/analyzers_status/setting_changed_update_worker.rb
+++ ee/app/workers/security/analyzers_status/setting_changed_update_worker.rb
@@ -8,6 +8,7 @@ module Security
       idempotent!
       data_consistency :sticky
       feature_category :security_asset_inventories
+      defer_on_database_health_signal :gitlab_sec, [:analyzer_namespace_statuses], 30.seconds
 
       def perform(project_ids, analyzer_type)
         return unless project_ids.present? && analyzer_type.present?
