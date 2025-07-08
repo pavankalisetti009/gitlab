73db8b6f38346b0b6c8f02329ef430383109973a
Frédéric Caplette <fcaplette@gitlab.com>
2025-07-08 10:47:14 -0400

    Keep Agent index page alive
    When navigating between the show page
    and the index, we were losing current
    page position because we were unmounting
    the index page, therefore loosing pagination.
    There are several ways to fix this, but the
    simplest is to simply not unmount the list
    so that it is always available.

--- ee/spec/frontend/ai/duo_agents_platform/duo_agents_platform_app_spec.js
+++ ee/spec/frontend/ai/duo_agents_platform/duo_agents_platform_app_spec.js
@@ -19,11 +19,22 @@ describe('DuoAgentsPlatformApp', () => {
       },
       stubs: {
         RouterView: true,
+        KeepAlive: true,
       },
     });
   };
 
   const findAppContainer = () => wrapper.find('#agents-platform-app');
+  const findKeepAliveComponentVue2 = () => wrapper.find('keepalive-stub');
+  const findKeepAliveComponentVue3 = () => wrapper.find('keep-alive-stub');
+
+  const findKeepAliveComponent = () => {
+    if (findKeepAliveComponentVue2().exists()) {
+      return findKeepAliveComponentVue2();
+    }
+
+    return findKeepAliveComponentVue3();
+  };
 
   describe('when component is mounted', () => {
     beforeEach(() => {
@@ -36,6 +47,11 @@ describe('DuoAgentsPlatformApp', () => {
 
     it('renders the router-view component', () => {
       expect(wrapper.html()).toContain('routerview');
+    });
+
+    it('renders the keep-alive component with list of components to include', () => {
+      expect(findKeepAliveComponent().exists()).toBe(true);
+      expect(findKeepAliveComponent().props().include).toEqual(['DuoAgentPlatformIndex']);
     });
   });
 });
