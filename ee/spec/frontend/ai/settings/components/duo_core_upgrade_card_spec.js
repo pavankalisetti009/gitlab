6726bfa0ba2b0d102304eac819cb7bfa1cc6a6ad
Divya Mahadevan <dmahadevan@gitlab.com>
2025-07-11 14:09:56 +0200

    Add tracking methods duo core upgrade card

--- ee/spec/frontend/ai/settings/components/duo_core_upgrade_card_spec.js
+++ ee/spec/frontend/ai/settings/components/duo_core_upgrade_card_spec.js
@@ -1,7 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
-import { GlButton } from '@gitlab/ui';
+import { GlButton, GlIntersectionObserver } from '@gitlab/ui';
 import DuoCoreUpgradeCard from 'ee/ai/settings/components/duo_core_upgrade_card.vue';
 import { SUPPORT_URL } from '~/sessions/new/constants';
+import { mockTracking } from 'helpers/tracking_helper';
 
 describe('DuoCoreUpgradeCard', () => {
   let wrapper;
@@ -15,6 +16,8 @@ describe('DuoCoreUpgradeCard', () => {
   };
 
   const findButtons = () => wrapper.findAllComponents(GlButton);
+  const findUpgradeBtn = () => findButtons().at(0);
+  const findContactSalesBtn = () => findButtons().at(1);
 
   describe('component rendering', () => {
     beforeEach(() => {
@@ -29,12 +32,52 @@ describe('DuoCoreUpgradeCard', () => {
       );
     });
 
-    it('renders a button for upgrading', () => {
+    it('renders a button for upgrading to duo pro', () => {
       expect(findButtons()).toHaveLength(2);
-      expect(findButtons().at(0).attributes('href')).toBe('https://customers.gitlab.com');
-      expect(findButtons().at(0).text()).toBe('Purchase Duo Pro seats');
-      expect(findButtons().at(1).attributes('href')).toBe(SUPPORT_URL);
-      expect(findButtons().at(1).text()).toBe('Contact sales for Duo Enterprise');
+      expect(findUpgradeBtn().attributes('href')).toBe('https://customers.gitlab.com');
+      expect(findUpgradeBtn().text()).toBe('Purchase Duo Pro seats');
+    });
+
+    it('renders a button for contacting sales about duo enterprise', () => {
+      expect(findContactSalesBtn().attributes('href')).toBe(SUPPORT_URL);
+      expect(findContactSalesBtn().text()).toBe('Contact sales for Duo Enterprise');
+    });
+  });
+
+  describe('tracking', () => {
+    let trackingSpy;
+    const glIntersectionObserver = () => wrapper.findComponent(GlIntersectionObserver);
+
+    beforeEach(() => {
+      createComponent();
+      trackingSpy = mockTracking(undefined, wrapper.element, jest.spyOn);
+    });
+
+    it('tracks page view on load', () => {
+      glIntersectionObserver().vm.$emit('appear');
+      expect(trackingSpy).toHaveBeenCalledWith(undefined, 'pageview', {
+        label: 'duo_core_add_on_upgrade_card',
+      });
+    });
+
+    describe('when duo pro upgrade button is clicked', () => {
+      it(`tracks the "duo_pro_purchase_seats" event`, async () => {
+        await findUpgradeBtn().vm.$emit('click');
+
+        expect(trackingSpy).toHaveBeenCalledWith(undefined, 'click_button', {
+          label: `duo_pro_purchase_seats`,
+        });
+      });
+    });
+
+    describe('when contact sales button is clicked', () => {
+      it(`tracks the "duo_enterprise_contact_sales" event`, async () => {
+        await findContactSalesBtn().vm.$emit('click');
+
+        expect(trackingSpy).toHaveBeenCalledWith(undefined, 'click_button', {
+          label: `duo_enterprise_contact_sales`,
+        });
+      });
     });
   });
 });
