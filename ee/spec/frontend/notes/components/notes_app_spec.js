adf16f35ee77b429530b51c687276f10cd40dd2e
Stanislav Lashmanov <slashmanov@gitlab.com>
2025-07-14 14:56:27 +0400

    Migrate the Notes Vue app to a Pinia store

--- ee/spec/frontend/notes/components/notes_app_spec.js
+++ ee/spec/frontend/notes/components/notes_app_spec.js
@@ -1,10 +1,16 @@
 import { shallowMount } from '@vue/test-utils';
 import { v4 as uuidv4 } from 'uuid';
+import Vue from 'vue';
+import { PiniaVuePlugin } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
 import waitForPromises from 'helpers/wait_for_promises';
 import NotesApp from '~/notes/components/notes_app.vue';
 import AiSummary from 'ee_component/notes/components/ai_summary.vue';
-import createStore from '~/notes/stores';
 // TODO: use generated fixture (https://gitlab.com/gitlab-org/gitlab-foss/issues/62491)
+import { globalAccessorPlugin } from '~/pinia/plugins';
+import { useLegacyDiffs } from '~/diffs/stores/legacy_diffs';
+import { useNotes } from '~/notes/store/legacy_notes';
+import { useBatchComments } from '~/batch_comments/store';
 import * as mockData from '../../../../../spec/frontend/notes/mock_data';
 
 jest.mock('~/behaviors/markdown/render_gfm');
@@ -16,17 +22,24 @@ const propsData = {
   notesFilters: mockData.notesFilters,
 };
 
+Vue.use(PiniaVuePlugin);
+
 describe('note_app', () => {
   let mountComponent;
   let wrapper;
-  let store;
+  let pinia;
 
   beforeEach(() => {
-    store = createStore();
-
+    pinia = createTestingPinia({
+      plugins: [globalAccessorPlugin],
+      stubActions: false,
+    });
+    useLegacyDiffs();
+    useNotes();
+    useBatchComments();
     mountComponent = () => {
       return shallowMount(NotesApp, {
-        store,
+        pinia,
         propsData,
         provide: {
           resourceGlobalId: 'gid://Gitlab/Issue/1',
