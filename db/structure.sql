1382013211237cbaeff6cdc191065770ca2d43a9
Gary Holtz <gholtz@gitlab.com>
2025-07-14 14:53:47 -0500

    Adding a temporary project_id column for diff commits epic
    Changelog: performance

--- db/structure.sql
+++ db/structure.sql
@@ -17448,7 +17448,8 @@ CREATE TABLE merge_request_diff_commits (
     trailers jsonb DEFAULT '{}'::jsonb,
     commit_author_id bigint,
     committer_id bigint,
-    merge_request_commits_metadata_id bigint
+    merge_request_commits_metadata_id bigint,
+    project_id bigint
 );
 
 CREATE TABLE merge_request_diff_details (
