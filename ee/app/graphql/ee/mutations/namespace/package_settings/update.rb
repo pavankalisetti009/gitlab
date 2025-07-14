a2203b63af9306abb314ee7a4a899b27705b6367
Moaz Khalifa <mkhalifa@gitlab.com>
2025-07-14 17:26:26 +0200

    Cleanup package_registry_audit_events FF
    Add the Package Registry to the list of audited events. The feature was behind the package_registry_audit_events feature flag which has been enabled on gitlab.com in 17.10 milestone. Now we cleanup the FF and release the feature.
    
    Changelog: added

--- ee/app/graphql/ee/mutations/namespace/package_settings/update.rb
+++ ee/app/graphql/ee/mutations/namespace/package_settings/update.rb
@@ -6,23 +6,12 @@ module EE
       module PackageSettings
         module Update
           extend ActiveSupport::Concern
-          extend ::Gitlab::Utils::Override
 
           prepended do
             argument :audit_events_enabled,
               ::GraphQL::Types::Boolean,
               required: false,
-              experiment: { milestone: '17.10' },
               description: copy_field_description(::Types::Namespace::PackageSettingsType, :audit_events_enabled)
-          end
-
-          override :resolve
-          def resolve(namespace_path:, **args)
-            if ::Feature.disabled?(:package_registry_audit_events, ::Feature.current_request)
-              args.delete(:audit_events_enabled)
-            end
-
-            super
           end
         end
       end
