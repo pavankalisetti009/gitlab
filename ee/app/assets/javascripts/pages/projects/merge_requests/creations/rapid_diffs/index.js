b6f725681773d19d42e4f6a0e6fa7485f6ac4867
Stanislav Lashmanov <slashmanov@gitlab.com>
2025-07-16 11:28:26 +0400

    Remove rapid_diffs_on_mr_creation feature flag
    Changelog: added

--- ee/app/assets/javascripts/pages/projects/merge_requests/creations/rapid_diffs/index.js
+++
@@ -1,7 +0,0 @@
-import '~/pages/projects/merge_requests/creations/rapid_diffs';
-import UserCallout from '~/user_callout';
-import initForm from '../../shared/init_form';
-
-initForm();
-// eslint-disable-next-line no-new
-new UserCallout();
