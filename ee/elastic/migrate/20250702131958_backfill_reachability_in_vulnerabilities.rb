b459f536b1e6cf6b7b7b795f9b1f639d3765df2f
Dmitry Gruzd <dgruzd@gitlab.com>
2025-07-17 21:07:00 +0200

    Advanced Search: Replace the vulnerabilities migration

--- ee/elastic/migrate/20250702131958_backfill_reachability_in_vulnerabilities.rb
+++ ee/elastic/migrate/20250702131958_backfill_reachability_in_vulnerabilities.rb
@@ -1,17 +1,27 @@
 # frozen_string_literal: true
 
-class BackfillReachabilityInVulnerability < Elastic::Migration
-  include ::Search::Elastic::MigrationReindexBasedOnSchemaVersion
+class BackfillReachabilityInVulnerabilities < Elastic::Migration
+  include ::Search::Elastic::MigrationDatabaseBackfillHelper
 
   skip_if -> { !(sass_with_es? || dedicated_with_es?) }
 
-  batch_size 10_000
+  batch_size 30_000
   batched!
   throttle_delay 30.seconds
   retry_on_failure
 
-  DOCUMENT_TYPE = Vulnerability
-  NEW_SCHEMA_VERSION = 25_26
+  QUEUE_THRESHOLD = 30_000
+  DOCUMENT_TYPE = ::Vulnerabilities::Read
+
+  # We do not honour this setting for SASS and Dedicated.
+  # For Self-managed decision is pending and tracked in https://gitlab.com/gitlab-org/gitlab/-/issues/525484.
+  def respect_limited_indexing?
+    false
+  end
+
+  def item_to_preload
+    { project: :namespace }
+  end
 
   class << self
     def sass_with_es?
