00ae4acf732cb98e8430eeabee489fc7289e11c0
Jonston Chan <jonstonchan1@gmail.com>
2025-07-17 01:24:04 +0000

    Fix RSpec/ClassStructure offenses
    Fixes RSpec/ClassStructure offenses
    
    Changelog: other

--- app/models/ci/runner_manager.rb
+++ app/models/ci/runner_manager.rb
@@ -142,6 +142,17 @@ module Ci
       exists?(ip_address:)
     end
 
+    def self.version_regex_expression_for_version(version)
+      case version
+      when /\d+\.\d+\.\d+/
+        '^\d+\.\d+\.\d+'
+      when /\d+\.\d+(\.)?/
+        '^\d+\.\d+\.'
+      else
+        '^\d+\.'
+      end
+    end
+
     def uncached_contacted_at
       read_attribute(:contacted_at)
     end
@@ -199,17 +210,6 @@ module Ci
 
     def no_organization_id
       errors.add(:runner_manager, 'cannot have organization_id assigned') if organization_id
-    end
-
-    def self.version_regex_expression_for_version(version)
-      case version
-      when /\d+\.\d+\.\d+/
-        '^\d+\.\d+\.\d+'
-      when /\d+\.\d+(\.)?/
-        '^\d+\.\d+\.'
-      else
-        '^\d+\.'
-      end
     end
   end
 end
