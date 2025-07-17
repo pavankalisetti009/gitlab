6815b7cdf9fbaab2d9fb43834aa9b42049125051
Rajan Mistry <rmistry@gitlab.com>
2025-07-17 16:25:49 +0530

    Support fullPath in Work Item Bulk Edit Mutation

--- ee/spec/frontend/work_items/list/components/work_item_bulk_edit_sidebar_spec.js
+++ ee/spec/frontend/work_items/list/components/work_item_bulk_edit_sidebar_spec.js
@@ -6,17 +6,16 @@ import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import { stubComponent } from 'helpers/stub_component';
 import waitForPromises from 'helpers/wait_for_promises';
 import workItemBulkUpdateMutation from '~/work_items/graphql/list/work_item_bulk_update.mutation.graphql';
-import workItemParentQuery from '~/work_items/graphql/list//work_item_parent.query.graphql';
 import getAvailableBulkEditWidgets from '~/work_items/graphql/list/get_available_bulk_edit_widgets.query.graphql';
 import WorkItemBulkEditSidebar from '~/work_items/components/work_item_bulk_edit/work_item_bulk_edit_sidebar.vue';
+import WorkItemBulkEditLabels from '~/work_items/components/work_item_bulk_edit/work_item_bulk_edit_labels.vue';
+import { createAlert } from '~/alert';
 import WorkItemBulkEditIteration from 'ee_component/work_items/components/list/work_item_bulk_edit_iteration.vue';
 import { WIDGET_TYPE_ITERATION } from '~/work_items/constants';
-import {
-  workItemParentQueryResponse,
-  availableBulkEditWidgetsQueryResponse,
-} from '../../mock_data';
+import { availableBulkEditWidgetsQueryResponse } from '../../mock_data';
 
 Vue.use(VueApollo);
+jest.mock('~/alert');
 
 const availableWidgetsWithout = (widgetToExclude) => {
   const widgetNames = availableBulkEditWidgetsQueryResponse.data.namespace.workItemsWidgets.filter(
@@ -53,7 +52,6 @@ describe('WorkItemBulkEditSidebar component EE', () => {
     },
   ];
 
-  const workItemParentQueryHandler = jest.fn().mockResolvedValue(workItemParentQueryResponse);
   const workItemBulkUpdateHandler = jest
     .fn()
     .mockResolvedValue({ data: { workItemBulkUpdate: { updatedWorkItemCount: 1 } } });
@@ -69,7 +67,6 @@ describe('WorkItemBulkEditSidebar component EE', () => {
   } = {}) => {
     wrapper = shallowMountExtended(WorkItemBulkEditSidebar, {
       apolloProvider: createMockApollo([
-        [workItemParentQuery, workItemParentQueryHandler],
         [workItemBulkUpdateMutation, mutationHandler],
         [getAvailableBulkEditWidgets, availableWidgetsHandler],
       ]),
@@ -92,6 +89,52 @@ describe('WorkItemBulkEditSidebar component EE', () => {
 
   const findForm = () => wrapper.findComponent(GlForm);
   const findIterationComponent = () => wrapper.findComponent(WorkItemBulkEditIteration);
+  const findAddLabelsComponent = () => wrapper.findAllComponents(WorkItemBulkEditLabels).at(0);
+  const findRemoveLabelsComponent = () => wrapper.findAllComponents(WorkItemBulkEditLabels).at(1);
+
+  describe('when epics list', () => {
+    it('calls mutation to bulk edit', async () => {
+      const addLabelIds = ['gid://gitlab/Label/1'];
+      const removeLabelIds = ['gid://gitlab/Label/2'];
+      createComponent({
+        props: { isEpicsList: true, fullPath: 'group/subgroup', isGroup: true },
+      });
+      await waitForPromises();
+
+      findAddLabelsComponent().vm.$emit('select', addLabelIds);
+      findRemoveLabelsComponent().vm.$emit('select', removeLabelIds);
+      findForm().vm.$emit('submit', { preventDefault: () => {} });
+
+      expect(workItemBulkUpdateHandler).toHaveBeenCalledWith({
+        input: {
+          fullPath: 'group/subgroup',
+          ids: ['gid://gitlab/WorkItem/11', 'gid://gitlab/WorkItem/22'],
+          labelsWidget: {
+            addLabelIds,
+            removeLabelIds,
+          },
+        },
+      });
+      expect(findAddLabelsComponent().props('selectedLabelsIds')).toEqual([]);
+      expect(findRemoveLabelsComponent().props('selectedLabelsIds')).toEqual([]);
+    });
+
+    it('renders error when there is a mutation error', async () => {
+      createComponent({
+        props: { isEpicsList: true },
+        mutationHandler: jest.fn().mockRejectedValue(new Error('oh no')),
+      });
+
+      findForm().vm.$emit('submit', { preventDefault: () => {} });
+      await waitForPromises();
+
+      expect(createAlert).toHaveBeenCalledWith({
+        captureError: true,
+        error: new Error('oh no'),
+        message: 'Something went wrong while bulk editing.',
+      });
+    });
+  });
 
   describe('when work_items_bulk_edit is enabled', () => {
     it('calls mutation to bulk edit ee attributes', async () => {
@@ -110,11 +153,19 @@ describe('WorkItemBulkEditSidebar component EE', () => {
 
       expect(workItemBulkUpdateHandler).toHaveBeenCalledWith({
         input: {
-          parentId: 'gid://gitlab/Group/1',
+          fullPath: 'group/project',
           ids: ['gid://gitlab/WorkItem/11', 'gid://gitlab/WorkItem/22'],
           iterationWidget: {
             iterationId: 'gid://gitlab/Iteration/1215',
           },
+          assigneesWidget: undefined,
+          confidential: undefined,
+          healthStatusWidget: undefined,
+          hierarchyWidget: undefined,
+          labelsWidget: undefined,
+          milestoneWidget: undefined,
+          stateEvent: undefined,
+          subscriptionEvent: undefined,
         },
       });
     });
