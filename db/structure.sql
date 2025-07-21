658d53dd5d94f559f52b68bc725bfb26b2681b36
Max Woolf <mwoolf@gitlab.com>
2025-07-21 13:06:30 +0100

    Add ability to register OAuth applications by API
    Implements OAuth 2.0 dynamic client registration tied to
    a user authenticated using a PAT.
    
    Implements features behind user-actor feature flag.

--- db/structure.sql
+++ db/structure.sql
@@ -18866,7 +18866,8 @@ CREATE TABLE oauth_applications (
     trusted boolean DEFAULT false NOT NULL,
     confidential boolean DEFAULT true NOT NULL,
     expire_access_tokens boolean DEFAULT false NOT NULL,
-    ropc_enabled boolean DEFAULT true NOT NULL
+    ropc_enabled boolean DEFAULT true NOT NULL,
+    dynamic boolean DEFAULT false NOT NULL
 );
 
 CREATE SEQUENCE oauth_applications_id_seq
