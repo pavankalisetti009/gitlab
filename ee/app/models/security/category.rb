95f1cec1d8d2dd991b886c7c3c71335a0a33d42f
Gal Katz <gkatz@gitlab.com>
2025-07-22 12:00:20 +0000

    Add security category table
    Changelog: added

---
+++ ee/app/models/security/category.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Security
+  class Category < ::SecApplicationRecord
+    self.table_name = 'security_categories'
+
+    belongs_to :namespace, optional: false
+
+    enum :editable_state, {
+      locked: 0,
+      partially_editable: 10,
+      editable: 20
+    }
+
+    enum :template_type, {
+      business_impact: 0,
+      business_unit: 1,
+      application: 2,
+      location: 3,
+      exposure: 4
+    }
+
+    validates :name, presence: true
+    validates :editable_state, presence: true
+    validates :multiple_selection, inclusion: { in: [true, false] }
+    validates :name, uniqueness: { scope: :namespace_id }, length: { maximum: 255 }
+    validates :description, length: { maximum: 255 }, allow_blank: true
+    validate :valid_namespace
+
+    private
+
+    def valid_namespace
+      return if namespace&.root? && namespace.group_namespace?
+
+      errors.add(:namespace, _('must be a root group.'))
+    end
+  end
+end
