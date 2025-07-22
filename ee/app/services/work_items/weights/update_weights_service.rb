e2c9baba690f4f3e0732c276e7ac3d475c28a865
Siddharth Dungarwal <sdungarwal@gitlab.com>
2025-07-22 12:49:12 +0530

    partial commit

--- ee/app/services/work_items/weights/update_weights_service.rb
+++ ee/app/services/work_items/weights/update_weights_service.rb
@@ -3,21 +3,13 @@
 module WorkItems
   module Weights
     class UpdateWeightsService
+      attr_reader :work_items
+
       def initialize(work_items)
         @work_items = Array.wrap(work_items)
       end
 
       def execute
-        return unless Feature.enabled?(:update_rolled_up_weights, :instance)
-
-        process_work_items_with_ancestors
-      end
-
-      private
-
-      attr_reader :work_items
-
-      def process_work_items_with_ancestors
         work_items.each do |work_item|
           WorkItems::WeightsSource.upsert_rolled_up_weights_for(work_item)
 
