6e95e16a26f7162410707ac0661922c41fdc83b0
Imam Hossain <ihossain@gitlab.com>
2025-07-25 13:26:49 +0200

    Trigger webhook event after transaction commit

--- ee/spec/workers/vulnerabilities/process_bulk_dismissed_events_worker_spec.rb
+++ ee/spec/workers/vulnerabilities/process_bulk_dismissed_events_worker_spec.rb
@@ -7,15 +7,19 @@ RSpec.describe Vulnerabilities::ProcessBulkDismissedEventsWorker, feature_catego
   let_it_be(:namespace) { create(:namespace) }
   let_it_be(:project) { create(:project, namespace: namespace) }
   let_it_be(:vulnerabilities) do
-    create_list(:vulnerability, 3, :with_findings, :detected, :high_severity, project: project)
+    create_list(:vulnerability, 2, :with_findings, :detected, :high_severity, project: project)
   end
-
-  let_it_be(:vulnerability_ids) { vulnerabilities.map(&:id) }
 
   let(:comment) { "i prefer lowercase." }
   let(:dismissal_reason) { 'used_in_tests' }
+  let(:note_with_comment) do
+    "changed vulnerability status to Dismissed: " \
+      "Used In Tests with the following comment: \"#{comment}\""
+  end
 
-  let(:bulk_dismissed_event) do
+  let(:bulk_dismissed_event) { create_bulk_dismissed_event(vulnerabilities) }
+
+  def create_bulk_dismissed_event(vulnerabilities)
     ::Vulnerabilities::BulkDismissedEvent.new(data: {
       vulnerabilities: vulnerabilities.map do |vulnerability|
         {
@@ -30,59 +34,63 @@ RSpec.describe Vulnerabilities::ProcessBulkDismissedEventsWorker, feature_catego
     })
   end
 
-  subject(:use_event) { consume_event(subscriber: described_class, event: bulk_dismissed_event) }
+  subject(:use_event) do
+    ->(event) { consume_event(subscriber: described_class, event: event) }
+  end
 
   it_behaves_like 'worker with data consistency', described_class, data_consistency: :always
 
   it 'inserts a system note for each vulnerability' do
-    use_event
+    expect { use_event.call(bulk_dismissed_event) }
+      .to change { Note.count }.by(2)
+      .and change { SystemNoteMetadata.count }.by(2)
 
-    notes = Note.last(3)
+    notes = Note.where(project: project, namespace: project.project_namespace).order(:id)
+    metadata = SystemNoteMetadata.where(note: notes).order(:note_id)
 
-    expect(notes[0]).to have_attributes(
-      noteable: vulnerabilities[0],
-      author: user,
-      project: project,
-      namespace_id: project.project_namespace_id,
-      note: "changed vulnerability status to Dismissed: " \
-        "Used In Tests with the following comment: \"#{comment}\"")
-    expect(notes[1]).to have_attributes(
-      noteable: vulnerabilities[1],
-      author: user,
-      project: project,
-      namespace_id: project.project_namespace_id,
-      note: "changed vulnerability status to Dismissed: " \
-        "Used In Tests with the following comment: \"#{comment}\"")
-    expect(notes[2]).to have_attributes(
-      noteable: vulnerabilities[2],
-      author: user,
-      project: project,
-      namespace_id: project.project_namespace_id,
-      note: "changed vulnerability status to Dismissed: " \
-        "Used In Tests with the following comment: \"#{comment}\"")
+    expect(notes.count).to eq(2)
+    expect(metadata.count).to eq(2)
 
-    system_note_metadata = SystemNoteMetadata.last(3)
-    expect(system_note_metadata[0]).to have_attributes(
-      note_id: notes[0].id,
-      action: "vulnerability_dismissed"
-    )
-    expect(system_note_metadata[1]).to have_attributes(
-      note_id: notes[1].id,
-      action: "vulnerability_dismissed"
-    )
-    expect(system_note_metadata[2]).to have_attributes(
-      note_id: notes[2].id,
-      action: "vulnerability_dismissed"
-    )
+    aggregate_failures 'note attributes' do
+      notes.zip(vulnerabilities).each do |note, vulnerability|
+        expect(note).to have_attributes(
+          noteable: vulnerability,
+          author: user,
+          project: project,
+          namespace_id: project.project_namespace_id,
+          note: note_with_comment
+        )
+      end
+    end
+
+    aggregate_failures 'system note metadata attributes' do
+      metadata.zip(notes).each do |meta, note|
+        expect(meta).to have_attributes(
+          note_id: note.id,
+          action: "vulnerability_dismissed"
+        )
+      end
+    end
   end
 
   it 'triggers webhook events for each vulnerability' do
-    3.times do
+    2.times do
       expect_next_found_instance_of(Vulnerability) do |found_vulnerability|
         expect(found_vulnerability).to receive(:trigger_webhook_event)
       end
     end
 
-    use_event
+    use_event.call(bulk_dismissed_event)
+  end
+
+  it 'avoids N+1 queries', :use_sql_query_cache do
+    control_event = create_bulk_dismissed_event(vulnerabilities)
+    control = ActiveRecord::QueryRecorder.new(skip_cached: false) { use_event.call(control_event) }
+
+    additional_vulnerabilities = create_list(:vulnerability, 2, :with_findings, :detected, :high_severity,
+      project: project)
+    test_event = create_bulk_dismissed_event(vulnerabilities + additional_vulnerabilities)
+
+    expect { use_event.call(test_event) }.to issue_same_number_of_queries_as(control)
   end
 end
