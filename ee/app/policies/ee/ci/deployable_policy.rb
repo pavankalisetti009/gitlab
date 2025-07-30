9ba32e2b07a99c7e45f7c86827a1bfddc844d480
Fabio Pitino <fpitino@gitlab.com>
2025-07-30 11:59:46 +0100

    Enable and prevent group of CI permissions
    This changes allows us to decouple granular dependencies from the ambiguous update_build policy

--- ee/app/policies/ee/ci/deployable_policy.rb
+++ ee/app/policies/ee/ci/deployable_policy.rb
@@ -20,17 +20,16 @@ module EE
         # See https://gitlab.com/gitlab-org/gitlab/-/issues/225482
         rule { reporter_has_access_to_protected_environment }.policy do
           enable :jailbreak
-          enable :update_build
           enable :cancel_build
+          ::ProjectPolicy::UPDATE_JOB_PERMISSIONS.each { |perm| enable(perm) }
         end
 
         # Authorizing the user to access to protected entities.
         # There is a "jailbreak" mode to exceptionally bypass the authorization,
         # however, you should NEVER allow it, rather suspect it's a wrong feature/product design.
         rule { ~can?(:jailbreak) & protected_environment }.policy do
-          prevent :update_build
-          prevent :cancel_build
-          prevent :erase_build
+          ::ProjectPolicy::UPDATE_JOB_PERMISSIONS.each { |perm| prevent(perm) }
+          ::ProjectPolicy::CLEANUP_JOB_PERMISSIONS.each { |perm| prevent(perm) }
         end
       end
     end
