e1a976cc2010d85df335aa9951908cb184d5b786
Zack Cuddy <zcuddy@gitlab.com>
2025-07-30 16:24:10 -0500

    Maintenance Mode: Remove Vuex
    This change removes the Vuex
    state library in favor of a local
    state approach.

--- ee/app/assets/javascripts/maintenance_mode_settings/components/app.vue
+++ ee/app/assets/javascripts/maintenance_mode_settings/components/app.vue
@@ -1,9 +1,8 @@
 <script>
 import { GlForm, GlToggle, GlFormGroup, GlFormTextarea, GlButton, GlLoadingIcon } from '@gitlab/ui';
-// eslint-disable-next-line no-restricted-imports
-import { mapActions, mapState } from 'vuex';
-import { __ } from '~/locale';
-import { mapComputed } from '~/vuex_shared/bindings';
+import { updateApplicationSettings } from '~/rest_api';
+import { createAlert } from '~/alert';
+import { __, s__ } from '~/locale';
 
 export default {
   name: 'MaintenanceModeSettingsApp',
@@ -24,25 +23,60 @@ export default {
     GlButton,
     GlLoadingIcon,
   },
-  computed: {
-    ...mapState(['loading']),
-    ...mapComputed([
-      { key: 'maintenanceEnabled', updateFn: 'setMaintenanceEnabled' },
-      { key: 'bannerMessage', updateFn: 'setBannerMessage' },
-    ]),
+  props: {
+    initialBannerMessage: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    initialMaintenanceEnabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      isLoading: false,
+      bannerMessage: this.initialBannerMessage,
+      isMaintenanceEnabled: this.initialMaintenanceEnabled,
+    };
   },
   methods: {
-    ...mapActions(['updateMaintenanceModeSettings']),
+    async updateMaintenanceModeSettings() {
+      try {
+        this.isLoading = true;
+
+        const {
+          data: {
+            maintenance_mode: maintenanceMode,
+            maintenance_mode_message: maintenanceModeMessage,
+          },
+        } = await updateApplicationSettings({
+          maintenance_mode: this.isMaintenanceEnabled,
+          maintenance_mode_message: this.bannerMessage,
+        });
+
+        this.isMaintenanceEnabled = Boolean(maintenanceMode);
+        this.bannerMessage = maintenanceModeMessage || '';
+      } catch {
+        createAlert({
+          message: s__('MaintenanceMode|There was an error updating the Maintenance Mode Settings'),
+        });
+      } finally {
+        this.isLoading = false;
+      }
+    },
   },
 };
 </script>
 <template>
   <section>
-    <gl-loading-icon v-if="loading" size="xl" />
+    <gl-loading-icon v-if="isLoading" size="xl" />
     <gl-form v-else @submit.prevent="updateMaintenanceModeSettings">
       <div class="gl-mb-4 gl-flex gl-items-center">
         <gl-toggle
-          v-model="maintenanceEnabled"
+          v-model="isMaintenanceEnabled"
           :label="$options.i18n.toggleLabel"
           label-position="hidden"
         />
--- ee/app/assets/javascripts/maintenance_mode_settings/store/state.js
+++
@@ -1,5 +0,0 @@
-export const createState = ({ maintenanceEnabled, bannerMessage }) => ({
-  loading: false,
-  maintenanceEnabled,
-  bannerMessage,
-});
