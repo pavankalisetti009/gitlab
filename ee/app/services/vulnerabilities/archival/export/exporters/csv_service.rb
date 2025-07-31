a029204bf43569c2a97ce514e9e7bfffb587b014
Patrick He <he.patrick2005@gmail.com>
2025-07-31 07:46:01 -0400

    Add Vulnerability ID to archival export
    Changelog: added
    EE: true

--- ee/app/services/vulnerabilities/archival/export/exporters/csv_service.rb
+++ ee/app/services/vulnerabilities/archival/export/exporters/csv_service.rb
@@ -25,44 +25,53 @@ module Vulnerabilities
 
           def mapping
             {
-              'Tool' => 'report_type',
-              'Scanner Name' => 'scanner',
-              'Status' => 'state',
-              'Vulnerability' => 'title',
-              'Details' => 'description',
-              'Severity' => 'severity',
-              'CVE' => 'cve_value',
-              'CWE' => 'cwe_value',
+              'Tool' => define_accessor(:report_type),
+              'Scanner Name' => define_accessor(:scanner),
+              'Status' => define_accessor(:state),
+              'Vulnerability' => define_accessor(:title),
+              'Details' => define_accessor(:description),
+              'Severity' => define_accessor(:severity),
+              'CVE' => define_accessor(:cve_value),
+              'CWE' => define_accessor(:cwe_value),
               'Other Identifiers' => method(:identifier_formatter),
-              'Dismissed At' => 'dismissed_at',
-              'Dismissed By' => 'dismissed_by',
-              'Confirmed At' => 'confirmed_at',
-              'Confirmed By' => 'confirmed_by',
-              'Resolved At' => 'resolved_at',
-              'Resolved By' => 'resolved_by',
-              'Detected At' => 'created_at',
-              'Location' => 'location',
-              'Issues' => 'related_issues',
-              'Merge Requests' => 'related_mrs',
-              'Activity' => 'resolved_on_default_branch',
-              'Comments' => 'notes_summary',
-              'Full Path' => 'full_path',
+              'Dismissed At' => define_accessor(:dismissed_at),
+              'Dismissed By' => define_accessor(:dismissed_by),
+              'Confirmed At' => define_accessor(:confirmed_at),
+              'Confirmed By' => define_accessor(:confirmed_by),
+              'Resolved At' => define_accessor(:resolved_at),
+              'Resolved By' => define_accessor(:resolved_by),
+              'Detected At' => define_accessor(:created_at),
+              'Location' => define_accessor(:location),
+              'Issues' => define_accessor(:related_issues),
+              'Merge Requests' => define_accessor(:related_mrs),
+              'Activity' => define_accessor(:resolved_on_default_branch),
+              'Comments' => define_accessor(:notes_summary),
+              'Full Path' => define_accessor(:full_path),
               'CVSS Vectors' => method(:cvss_formatter),
-              'Dismissal Reason' => method(:dismissal_formatter)
+              'Dismissal Reason' => method(:dismissal_formatter),
+              'Vulnerability ID' => method(:vulnerability_identifier)
             }
           end
 
-          def identifier_formatter(data)
-            data['other_identifiers'].to_csv(col_sep: CSV_DELIMITER, row_sep: '')
+          def define_accessor(attribute)
+            proc { |archived_record| archived_record.data[attribute.to_s] }
           end
 
-          def dismissal_formatter(data)
-            data['dismissal_reason']&.humanize
+          def identifier_formatter(archived_record)
+            archived_record.data['other_identifiers'].to_csv(col_sep: CSV_DELIMITER, row_sep: '')
           end
 
-          def cvss_formatter(data)
-            data['cvss'].map { |cvss| "#{cvss['vendor']}=#{cvss['vector']}" }
+          def dismissal_formatter(archived_record)
+            archived_record.data['dismissal_reason']&.humanize
+          end
+
+          def cvss_formatter(archived_record)
+            archived_record.data['cvss'].map { |cvss| "#{cvss['vendor']}=#{cvss['vector']}" }
                         .to_csv(col_sep: CSV_DELIMITER, row_sep: '')
+          end
+
+          def vulnerability_identifier(archived_record)
+            archived_record.vulnerability_identifier
           end
         end
       end
