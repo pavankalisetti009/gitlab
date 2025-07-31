4c478e5449d3f2f328d0b79a32f09b40c56a521a
Johannes Bauer <jobauer@gitlab.com>
2025-07-31 09:00:50 +0200

    Database Implementation

---
+++ ee/app/assets/javascripts/integrations/edit/components/sections/jira_verification.vue
@@ -0,0 +1,25 @@
+<script>
+// eslint-disable-next-line no-restricted-imports
+import { mapGetters } from 'vuex';
+
+import JiraVerificationFields from '../jira_verification_fields.vue';
+
+export default {
+  name: 'IntegrationSectionJiraVerification',
+  components: {
+    JiraVerificationFields,
+  },
+  computed: {
+    ...mapGetters(['currentKey', 'propsSource']),
+  },
+};
+</script>
+
+<template>
+  <div>
+    <jira-verification-fields
+      :key="`${currentKey}-jira-verification-fields`"
+      v-bind="propsSource.jiraVerificationProps"
+    />
+  </div>
+</template>
