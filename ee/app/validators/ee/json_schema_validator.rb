f3c6a2b29664ca2a3afb91ea0df48f17b489803c
Pavel Shutsin <pshutsin@gitlab.com>
2025-07-31 13:10:07 +0200

    Change schema priority for json validator
    EE schema should be preferred if exists
    over CE schema.

--- ee/app/validators/ee/json_schema_validator.rb
+++ ee/app/validators/ee/json_schema_validator.rb
@@ -5,11 +5,14 @@ module EE
     private
 
     def schema_path
-      if File.exist?(super)
-        super
-      else
-        Rails.root.join('ee', *base_directory, filename_with_extension).to_s
-      end
+      ce_path = super
+      ee_path = Rails.root.join('ee', *base_directory, filename_with_extension)
+
+      existing_path = [ee_path, ce_path].detect { |path| File.exist?(path) }
+
+      raise "No json validation schema `#{filename_with_extension}` found" unless existing_path
+
+      existing_path
     end
   end
 end
