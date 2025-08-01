b109cb18722aee7f439d0cd1effc9a70043d03a8
Naman Jagdish Gala <ngala@gitlab.com>
2025-08-01 11:25:37 -0400

    Fix incorrect redirect url post group SAML SSO
    Replaced usage of `user_return_to` with new parameter `user_sso_return_to` and deleting session parameter after its usage
    
    Related: https://gitlab.com/gitlab-org/gitlab/-/issues/557259
    
    Changelog: fixed

--- ee/spec/lib/ee/gitlab/auth/o_auth/oauth_resource_owner_redirect_resolver_spec.rb
+++ ee/spec/lib/ee/gitlab/auth/o_auth/oauth_resource_owner_redirect_resolver_spec.rb
@@ -3,8 +3,9 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_category: :system_access do
-  let(:resolver) { described_class.new(namespace_id) }
-  let(:namespace_id) { nil }
+  let(:resolver) { described_class.new(request, session) }
+  let(:request) { instance_double(ActionDispatch::Request) }
+  let(:session) { {} }
   let(:saml_provider) { create(:saml_provider, enforced_sso: true) }
   let(:group) { saml_provider.group }
   let(:child_group) { create(:group, parent: group) }
@@ -12,13 +13,16 @@ RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_
   before do
     allow(Gitlab).to receive(:ee?).and_return(true)
     allow(resolver).to receive(:new_user_session_url).and_return('/login')
+    allow(request).to receive(:fullpath).and_return('/oauth/authorize?client_id=123')
   end
 
   describe '#resolve_redirect_url' do
     subject(:resolve_redirect_url) { resolver.resolve_redirect_url }
 
     context 'when root_namespace_id is blank' do
-      let(:namespace_id) { nil }
+      before do
+        allow(request).to receive(:query_parameters).and_return({ 'root_namespace_id' => nil })
+      end
 
       it 'returns new_user_session_url' do
         expect(resolve_redirect_url).to eq('/login')
@@ -28,10 +32,17 @@ RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_
         expect(::Group).not_to receive(:find_by_id)
         resolve_redirect_url
       end
+
+      it 'does not set user_sso_return_to in session' do
+        resolve_redirect_url
+        expect(session[:user_sso_return_to]).to be_nil
+      end
     end
 
     context 'when namespace is found' do
-      let(:namespace_id) { group.id }
+      before do
+        allow(request).to receive(:query_parameters).and_return({ 'root_namespace_id' => group.id.to_s })
+      end
 
       context 'when found namespace is a Group' do
         context 'when feature flag is enabled' do
@@ -48,6 +59,11 @@ RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_
               expect(resolver).to receive(:build_sso_redirect_url).with(group)
               expect(resolve_redirect_url).to eq(sso_url)
             end
+
+            it 'sets user_sso_return_to in session' do
+              resolve_redirect_url
+              expect(session[:user_sso_return_to]).to eq('/oauth/authorize?client_id=123')
+            end
           end
 
           context 'when SSO URL is nil' do
@@ -59,6 +75,11 @@ RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_
 
             it 'returns new_user_session_url' do
               expect(resolve_redirect_url).to eq('/login')
+            end
+
+            it 'does not set user_sso_return_to in session' do
+              resolve_redirect_url
+              expect(session[:user_sso_return_to]).to be_nil
             end
           end
 
@@ -72,6 +93,11 @@ RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_
             it 'returns new_user_session_url' do
               expect(resolve_redirect_url).to eq('/login')
             end
+
+            it 'does not set user_sso_return_to in session' do
+              resolve_redirect_url
+              expect(session[:user_sso_return_to]).to be_nil
+            end
           end
         end
 
@@ -84,31 +110,50 @@ RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_
             expect(resolver).not_to receive(:build_sso_redirect_url)
             expect(resolve_redirect_url).to eq('/login')
           end
+
+          it 'does not set user_sso_return_to in session' do
+            resolve_redirect_url
+            expect(session[:user_sso_return_to]).to be_nil
+          end
         end
       end
 
       context 'when found namespace is not a Group' do
         let(:user_namespace) { create(:user_namespace) }
-        let(:namespace_id) { user_namespace.id }
+
+        before do
+          allow(request).to receive(:query_parameters).and_return({ 'root_namespace_id' => user_namespace.id.to_s })
+        end
 
         it 'returns new_user_session_url' do
           expect(resolve_redirect_url).to eq('/login')
+        end
+
+        it 'does not set user_sso_return_to in session' do
+          resolve_redirect_url
+          expect(session[:user_sso_return_to]).to be_nil
         end
       end
     end
 
     context 'when namespace is not found' do
-      let(:namespace_id) { 999999 }
+      before do
+        allow(request).to receive(:query_parameters).and_return({ 'root_namespace_id' => '999999' })
+      end
 
       it 'returns new_user_session_url' do
         expect(resolve_redirect_url).to eq('/login')
       end
+
+      it 'does not set user_sso_return_to in session' do
+        resolve_redirect_url
+        expect(session[:user_sso_return_to]).to be_nil
+      end
     end
 
     context 'with child namespace path' do
-      let(:namespace_id) { child_group.id }
-
       before do
+        allow(request).to receive(:query_parameters).and_return({ 'root_namespace_id' => child_group.id.to_s })
         allow(resolver).to receive(:build_sso_redirect_url)
           .with(child_group)
           .and_return('/sso-url')
@@ -116,6 +161,11 @@ RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_
 
       it 'works with nested groups' do
         expect(resolve_redirect_url).to eq('/sso-url')
+      end
+
+      it 'sets user_sso_return_to in session' do
+        resolve_redirect_url
+        expect(session[:user_sso_return_to]).to eq('/oauth/authorize?client_id=123')
       end
     end
   end
@@ -127,6 +177,7 @@ RSpec.describe Gitlab::Auth::OAuth::OauthResourceOwnerRedirectResolver, feature_
     let(:expected_url) { "/groups/#{group.full_path}/-/saml/sso" }
 
     before do
+      allow(request).to receive(:query_parameters).and_return({ 'root_namespace_id' => group.id.to_s })
       allow(::RoutableActions::SsoEnforcementRedirect)
         .to receive(:new)
         .with(group, group.full_path)
