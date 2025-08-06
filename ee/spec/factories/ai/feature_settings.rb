bf6780fe0aa5ec67992a9838585ac90e1adc1a79
Manoj M J <mmj@gitlab.com>
2025-08-06 12:43:57 +0200

    Track internal event when a self-hosted AI feature switches to vendored model

--- ee/spec/factories/ai/feature_settings.rb
+++ ee/spec/factories/ai/feature_settings.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :ai_feature_setting, class: '::Ai::FeatureSetting' do
     add_attribute(:feature) { :code_generations }
     provider { :self_hosted }
-    self_hosted_model factory: :ai_self_hosted_model
+    self_hosted_model { provider.to_sym == :self_hosted ? association(:ai_self_hosted_model) : nil }
 
     trait :duo_agent_platform do
       add_attribute(:feature) { :duo_agent_platform }
