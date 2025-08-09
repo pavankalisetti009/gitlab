3b45cc83e65bf5c32860f9608a13501ca5e896a2
Lee Tickett <ltickett@gitlab.com>
2025-08-09 23:59:10 +0100

    Add unit tests for AI catalog mutations

---
+++ ee/spec/graphql/mutations/ai/catalog/flow/update_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Mutations::Ai::Catalog::Flow::Update, feature_category: :workflow_catalog do
+  include GraphqlHelpers
+
+  subject(:mutation) { described_class }
+
+  it { is_expected.to have_graphql_name('AiCatalogFlowUpdate') }
+
+  it { expect(described_class).to require_graphql_authorizations(:admin_ai_catalog_item) }
+
+  it { is_expected.to have_graphql_fields(:item, :errors, :client_mutation_id) }
+
+  it 'has expected arguments' do
+    is_expected.to have_graphql_arguments(
+      :id,
+      :description,
+      :name,
+      :public,
+      :client_mutation_id
+    )
+  end
+end
