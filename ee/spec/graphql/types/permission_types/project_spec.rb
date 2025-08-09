1659456ad9547905549494707738394aaef27680
Lee Tickett <ltickett@gitlab.com>
2025-08-10 00:14:27 +0100

    Expose project AI catalog permissions in GraphQL

--- ee/spec/graphql/types/permission_types/project_spec.rb
+++ ee/spec/graphql/types/permission_types/project_spec.rb
@@ -4,8 +4,18 @@ require 'spec_helper'
 
 RSpec.describe Types::PermissionTypes::Project do
   specify do
-    expected_permissions = %i[create_path_lock read_path_locks admin_path_locks generate_description
-      admin_work_item_lifecycle manage_ai_flow_triggers]
+    expected_permissions = %i[
+      create_path_lock
+      read_path_locks
+      admin_path_locks
+      generate_description
+      admin_work_item_lifecycle
+      manage_ai_flow_triggers
+      read_ai_catalog_items
+      read_ai_catalog_item_consumers
+      manage_ai_catalog_items
+      manage_ai_catalog_item_consumers
+    ]
 
     expected_permissions.each do |permission|
       expect(described_class).to have_graphql_field(permission)
