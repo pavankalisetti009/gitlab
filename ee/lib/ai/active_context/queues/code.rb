dee0797702b3e6fbf4e5b1169c23e857f7d1b892
Pam Artiaga <partiaga@gitlab.com>
2025-08-11 22:54:11 +0800

    Allow flexible throughput adjustment for ActiveContext processing
    Changelog: changed
    EE: true

--- ee/lib/ai/active_context/queues/code.rb
+++ ee/lib/ai/active_context/queues/code.rb
@@ -6,10 +6,20 @@ module Ai
       class Code
         include ::ActiveContext::Concerns::Queue
 
+        COLLECTION_CLASS = ::Ai::ActiveContext::Collections::Code
+
+        DEFAULT_SHARD_LIMIT = 1000
+
+        # having 1 shard means we have absolute control over the amount of embeddings we generate in one go
+        DEFAULT_SHARD_COUNT = 1
+
         class << self
-          # having a single shard means we have absolute control over the amount of embeddings we generate in one go
           def number_of_shards
-            1
+            COLLECTION_CLASS.collection_record&.queue_shard_count || DEFAULT_SHARD_COUNT
+          end
+
+          def shard_limit
+            COLLECTION_CLASS.collection_record&.queue_shard_limit || DEFAULT_SHARD_LIMIT
           end
         end
       end
--- ee/spec/lib/ai/active_context/queues/code_spec.rb
+++ ee/spec/lib/ai/active_context/queues/code_spec.rb
@@ -3,9 +3,35 @@
 require 'spec_helper'
 
 RSpec.describe Ai::ActiveContext::Queues::Code, feature_category: :code_suggestions do
-  describe '.number_of_shards' do
-    it 'returns 1' do
+  describe 'queue processing properties' do
+    it 'returns default values' do
       expect(described_class.number_of_shards).to eq(1)
+      expect(described_class.shard_limit).to eq(1000)
+    end
+
+    context 'when a Code collection record exists' do
+      let_it_be(:collection) { create(:ai_active_context_collection, :code_embeddings_with_versions) }
+
+      it 'returns default values' do
+        expect(described_class.number_of_shards).to eq(1)
+        expect(described_class.shard_limit).to eq(1000)
+      end
+
+      context 'when the collection queue-related options are set' do
+        before do
+          collection.update!(
+            options: {
+              queue_shard_count: 4,
+              queue_shard_limit: 250
+            }
+          )
+        end
+
+        it 'returns the values defined in the collection options' do
+          expect(described_class.number_of_shards).to eq(4)
+          expect(described_class.shard_limit).to eq(250)
+        end
+      end
     end
   end
 
