84512d1dcd02e45678fcda6d980fd6ec869c6d73
Andrejs Cunskis <acunskis@gitlab.com>
2025-08-11 08:39:02 +0300

    Quarantine broken search related e2e tests

--- qa/qa/specs/features/ee/api/17_foundations/elasticsearch/index_tests/issue_index_spec.rb
+++ qa/qa/specs/features/ee/api/17_foundations/elasticsearch/index_tests/issue_index_spec.rb
@@ -22,7 +22,12 @@ module QA
 
       it(
         'finds issue that matches description',
-        testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347635'
+        testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347635',
+        quarantine: {
+          issue: 'https://gitlab.com/gitlab-org/quality/e2e-test-issues/-/issues/929',
+          type: :investigating,
+          only: { pipeline: :staging }
+        }
       ) do
         QA::Support::Retrier.retry_on_exception(
           max_attempts: Runtime::Search::RETRY_MAX_ITERATION,
