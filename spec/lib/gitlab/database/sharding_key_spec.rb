86241347824c7d2b0032b5b1e73561313c442cbb
Tomasz Skorupa <tskorupa@gitlab.com>
2025-08-13 15:06:51 -0400

    Add NOT NULL on adas#namespace_id
    Add NOT NULL to analytics_devops_adoption_segments#namespace_id
    
    Addresses https://gitlab.com/gitlab-org/gitlab/-/issues/555251
    
    Follows up on https://gitlab.com/gitlab-org/gitlab/-/commit/cc6cb9bd1b9b2ff48cdababaa5d9b6ae6d781e5d
    
    There are no rows with analytics_devops_adoption_segments#namespace_id IS NULL on gitlab.com (https://postgres.ai/console/gitlab/gitlab-production-main/sessions/42394/commands/129936)
    
    The attribute is already enforced in the model (https://gitlab.com/gitlab-org/gitlab/-/blob/11d074d349a6f538ec6513ce6ac034832ae7ed91/ee/app/models/analytics/devops_adoption/enabled_namespace.rb\#L13).

--- spec/lib/gitlab/database/sharding_key_spec.rb
+++ spec/lib/gitlab/database/sharding_key_spec.rb
@@ -19,7 +19,6 @@ RSpec.describe 'new tables missing sharding_key', feature_category: :organizatio
   # the table name to remove this once a decision has been made.
   let(:allowed_to_be_missing_not_null) do
     [
-      'analytics_devops_adoption_segments.namespace_id',
       *['badges.project_id', 'badges.group_id'],
       'ci_sources_pipelines.project_id',
       'member_roles.namespace_id', # https://gitlab.com/gitlab-org/gitlab/-/issues/444161
