53863c40eac002fc349f70da669c945eacc07c83
Allen Cook <acook@gitlab.com>
2025-08-14 15:23:52 -0400

    Remove some CS feature flags by default for development

--- ee/spec/lib/gitlab/duo/developments/feature_flag_enabler_spec.rb
+++ ee/spec/lib/gitlab/duo/developments/feature_flag_enabler_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe Gitlab::Duo::Developments::FeatureFlagEnabler, feature_category: 
 
   it 'enables feature flags by group code creation' do
     expect(Feature::Definition).to receive(:definitions)
-      .and_return({ test_f: Feature::Definition.new(nil, group: 'group::code creation', name: 'test_f') })
+      .and_return({ test_f: Feature::Definition.new(nil, group: 'group::code creation',
+        name: 'test_f') })
     expect(Feature).to receive(:enable).with(:test_f)
 
     described_class.execute
@@ -39,6 +40,22 @@ RSpec.describe Gitlab::Duo::Developments::FeatureFlagEnabler, feature_category: 
     expect(Feature::Definition).to receive(:definitions)
       .and_return({ test_f: Feature::Definition.new(nil, group: 'group::custom models', name: 'test_f') })
     expect(Feature).to receive(:enable).with(:test_f)
+
+    described_class.execute
+  end
+
+  it 'excludes feature flags listed in EXCLUDED_FEATURE_FLAGS' do
+    excluded_flag = described_class::EXCLUDED_FEATURE_FLAGS.first
+    regular_flag = :regular_duo_flag
+
+    expect(Feature::Definition).to receive(:definitions)
+      .and_return({
+        excluded_flag => Feature::Definition.new(nil, group: 'group::ai framework', name: excluded_flag.to_s),
+        regular_flag => Feature::Definition.new(nil, group: 'group::ai framework', name: regular_flag.to_s)
+      })
+
+    expect(Feature).to receive(:enable).with(regular_flag)
+    expect(Feature).not_to receive(:enable).with(excluded_flag)
 
     described_class.execute
   end
