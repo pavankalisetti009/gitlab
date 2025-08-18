1c5a2cb4697832885168d17a53c11bccc82b9532
Ryan Cobb <rcobb@gitlab.com>
2025-08-18 17:09:40 +0000

    Restrict namespace API billing fields based on user permissions
    Restrict namespace API billing fields based on user permissions. This
    change is behind a feature flag. When enabled, fields like
    billable_members_count, max_seats_used, seats_in_use and end_date are
    only returned to users with billing access permissions.
    
    Changelog: security
    EE: true

--- ee/lib/ee/api/entities/namespace.rb
+++ ee/lib/ee/api/entities/namespace.rb
@@ -16,18 +16,28 @@ module EE
           expose :additional_purchased_storage_size, documentation: { type: 'integer', example: 1000 }, if: can_update_limits
           expose :additional_purchased_storage_ends_on, documentation: { type: 'date', example: '2022-06-18' }, if: can_update_limits
           expose :billable_members_count, documentation: { type: 'integer', example: 2 } do |namespace, options|
+            next unless can_read_billing?(namespace, options)
+
             namespace.billable_members_count(options[:requested_hosted_plan])
           end
-          expose :seats_in_use, documentation: { type: 'integer', example: 5 }, if: has_gitlab_subscription do |namespace, _|
+          expose :seats_in_use, documentation: { type: 'integer', example: 5 }, if: has_gitlab_subscription do |namespace, options|
+            next unless can_read_billing?(namespace, options)
+
             namespace.gitlab_subscription.seats_in_use
           end
-          expose :max_seats_used, documentation: { type: 'integer', example: 100 }, if: has_gitlab_subscription do |namespace, _|
+          expose :max_seats_used, documentation: { type: 'integer', example: 100 }, if: has_gitlab_subscription do |namespace, options|
+            next unless can_read_billing?(namespace, options)
+
             namespace.gitlab_subscription.max_seats_used
           end
-          expose :max_seats_used_changed_at, documentation: { type: 'date', example: '2022-06-18' }, if: has_gitlab_subscription do |namespace, _|
+          expose :max_seats_used_changed_at, documentation: { type: 'date', example: '2022-06-18' }, if: has_gitlab_subscription do |namespace, options|
+            next unless can_read_billing?(namespace, options)
+
             namespace.gitlab_subscription.max_seats_used_changed_at
           end
-          expose :end_date, documentation: { type: 'date', example: '2022-06-18' }, if: has_gitlab_subscription do |namespace, _|
+          expose :end_date, documentation: { type: 'date', example: '2022-06-18' }, if: has_gitlab_subscription do |namespace, options|
+            next unless can_read_billing?(namespace, options)
+
             namespace.gitlab_subscription.end_date
           end
           expose :plan, documentation: { type: 'string', example: 'default' }, if: can_admin_namespace do |namespace, _|
@@ -38,6 +48,14 @@ module EE
           end
           expose :trial, documentation: { type: 'boolean' }, if: can_admin_namespace do |namespace, _|
             namespace.trial?
+          end
+
+          private
+
+          def can_read_billing?(namespace, opts)
+            return true unless ::Feature.enabled?(:restrict_namespace_api_billing_fields) # rubocop:disable Gitlab/FeatureFlagWithoutActor -- No actor needed
+
+            ::Ability.allowed?(opts[:current_user], :read_billing, namespace)
           end
         end
       end
