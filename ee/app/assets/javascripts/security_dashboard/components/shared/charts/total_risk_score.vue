b583eaa1c9341b66f563689d1ed78adb3e8aa852
David Pisek <dpisek@gitlab.com>
2025-08-21 15:29:04 +0200

    Add total-risk-score chart
    This MR adds a new chart component which will
    render the "Total Risk Score" for the security
    dashboard.
    
    The chart is a gauge chart, with a colour-based
    meter to visualise the calculated risk of the
    current project or group.
    
    Besides adding a new Vue component,
    it also adds a storybook entry,
    which can be used to discover and test the
    component.

---
+++ ee/app/assets/javascripts/security_dashboard/components/shared/charts/total_risk_score.vue
@@ -0,0 +1,247 @@
+<script>
+import { GlResizeObserverDirective } from '@gitlab/ui';
+import { GlChart } from '@gitlab/ui/src/charts';
+import { s__ } from '~/locale';
+
+const CHART_BAR_COLORS = {
+  LOW: '#91D4A8',
+  MEDIUM: '#E9BE74',
+  HIGH: '#c55631',
+  CRITICAL: '#9d2915',
+};
+
+const LABEL_TEXT_COLORS = {
+  LOW: '#2DA160',
+  MEDIUM: '#C17D10',
+  HIGH: '#c55631',
+  CRITICAL: '#9d2915',
+};
+
+const RATING_LABELS = {
+  LOW: s__('SecurityReports|Low risk'),
+  MEDIUM: s__('SecurityReports|Medium risk'),
+  HIGH: s__('SecurityReports|High risk'),
+  CRITICAL: s__('SecurityReports|Critical risk'),
+};
+
+export default {
+  components: {
+    GlChart,
+  },
+  directives: {
+    GlResizeObserver: GlResizeObserverDirective,
+  },
+  props: {
+    score: {
+      type: Number,
+      required: true,
+      validator: (value) => value >= 0 && value <= 100,
+    },
+  },
+  data() {
+    return {
+      chartWidth: 0,
+      chartHeight: 0,
+    };
+  },
+  computed: {
+    label() {
+      return RATING_LABELS[this.rating];
+    },
+    chartBarColor() {
+      return CHART_BAR_COLORS[this.rating];
+    },
+    labelTextColor() {
+      return LABEL_TEXT_COLORS[this.rating];
+    },
+    rating() {
+      if (this.score <= 25) {
+        return 'LOW';
+      }
+
+      if (this.score <= 50) {
+        return 'MEDIUM';
+      }
+
+      if (this.score <= 75) {
+        return 'HIGH';
+      }
+
+      return 'CRITICAL';
+    },
+    chartOptions() {
+      return {
+        series: [this.outerMeterRing, this.progressMeterRing],
+      };
+    },
+    gaugeDimensions() {
+      // the center is slightly raised, because a gauge chart is usually a full circle, but we don't display the bottom part of the circle
+      const centerXPercent = 0.5;
+      const centerYPercent = 0.6;
+
+      const gapBetweenRingsInPx = 1;
+      const outerMeterMaxWidthInPx = 15;
+      const outerRingWidthRatio = 0.2;
+
+      const cx = this.chartWidth * centerXPercent;
+      const cy = this.chartHeight * centerYPercent;
+
+      // the radius of the outer meter ring is the smallest distance from the center to the edge of the chart
+      const outerMeterRadiusInPx = Math.min(cx, this.chartWidth - cx, cy, this.chartHeight - cy);
+
+      const outerMeterRingWidth = Math.min(
+        outerMeterMaxWidthInPx,
+        Math.round(outerMeterRadiusInPx * outerRingWidthRatio),
+      );
+
+      const progressMeterRadiusInPx =
+        outerMeterRadiusInPx - outerMeterRingWidth - gapBetweenRingsInPx;
+      const progressMeterRingWidth = outerMeterRingWidth * 2;
+
+      return {
+        centerXPercent,
+        centerYPercent,
+        outerMeter: {
+          radius: outerMeterRadiusInPx,
+          ringWidth: outerMeterRingWidth,
+        },
+        progressMeter: {
+          radius: progressMeterRadiusInPx,
+          ringWidth: progressMeterRingWidth,
+        },
+      };
+    },
+    outerMeterRing() {
+      return {
+        type: 'gauge',
+        startAngle: 220,
+        endAngle: -40,
+        min: 0,
+        max: 100,
+        splitNumber: 4,
+        center: [
+          `${this.gaugeDimensions.centerXPercent * 100}%`,
+          `${this.gaugeDimensions.centerYPercent * 100}%`,
+        ],
+        radius: this.gaugeDimensions.outerMeter.radius,
+        axisLine: {
+          lineStyle: {
+            width: this.gaugeDimensions.outerMeter.ringWidth,
+            color: [
+              [0.25, CHART_BAR_COLORS.LOW],
+              [0.5, CHART_BAR_COLORS.MEDIUM],
+              [0.75, CHART_BAR_COLORS.HIGH],
+              [1, CHART_BAR_COLORS.CRITICAL],
+            ],
+          },
+        },
+        pointer: {
+          show: false,
+        },
+        axisTick: {
+          show: true,
+          lineStyle: {
+            color: '#fff',
+            width: 1,
+          },
+          length: 6,
+          distance: -this.gaugeDimensions.outerMeter.ringWidth,
+        },
+        splitLine: {
+          show: false,
+        },
+        axisLabel: {
+          show: false,
+        },
+        // the risk rating label (e.g. "Low risk")
+        detail: {
+          show: true,
+          width: 100,
+          height: 40,
+          offsetCenter: [0, -20],
+          fontSize: 45,
+          color: this.labelTextColor,
+        },
+        title: {
+          show: true,
+          offsetCenter: [0, 15], // Position below the risk rating label
+          fontSize: 17,
+          color: this.labelTextColor,
+        },
+        // the score value displayed as a number
+        data: [
+          {
+            value: this.score,
+            name: this.label,
+          },
+        ],
+      };
+    },
+    progressMeterRing() {
+      return {
+        type: 'gauge',
+        startAngle: 220,
+        endAngle: -40,
+        min: 0,
+        max: 100,
+        splitNumber: 4,
+        center: [
+          `${this.gaugeDimensions.centerXPercent * 100}%`,
+          `${this.gaugeDimensions.centerYPercent * 100}%`,
+        ],
+        radius: this.gaugeDimensions.progressMeter.radius,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            width: this.gaugeDimensions.progressMeter.ringWidth,
+            color: [
+              // the actual data representation
+              [this.score / 100, this.chartBarColor],
+              // transparent to support dark and light mode
+              [1, 'transparent'],
+            ],
+          },
+        },
+        pointer: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+        splitLine: {
+          show: false,
+        },
+        axisLabel: {
+          show: false,
+        },
+        title: {
+          show: false,
+        },
+        detail: {
+          show: false,
+        },
+        data: [
+          {
+            value: this.score,
+          },
+        ],
+      };
+    },
+  },
+  methods: {
+    onResize({ contentRect: { width, height } }) {
+      this.chartWidth = width;
+      this.chartHeight = height;
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    v-gl-resize-observer="onResize"
+    class="gl-justify-content-center gl-align-items-center gl-flex gl-h-full gl-w-full"
+  >
+    <gl-chart :options="chartOptions" responsive height="auto" class="gl-h-full gl-w-full" />
+  </div>
+</template>
---
+++ ee/spec/frontend/security_dashboard/components/shared/charts/total_risk_score_spec.js
@@ -0,0 +1,171 @@
+import { nextTick } from 'vue';
+import { GlChart } from '@gitlab/ui/dist/charts';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import { createMockDirective, getBinding } from 'helpers/vue_mock_directive';
+import TotalRiskScore from 'ee/security_dashboard/components/shared/charts/total_risk_score.vue';
+
+describe('TotalRiskScore chart', () => {
+  let wrapper;
+
+  const DEFAULT_SCORE = 72;
+
+  const EXPECTED_CHART_BAR_COLORS = {
+    LOW: '#91D4A8',
+    MEDIUM: '#E9BE74',
+    HIGH: '#c55631',
+    CRITICAL: '#9d2915',
+  };
+
+  const EXPECTED_LABEL_TEXT_COLORS = {
+    LOW: '#2DA160',
+    MEDIUM: '#C17D10',
+    HIGH: '#c55631',
+    CRITICAL: '#9d2915',
+  };
+
+  const createComponent = (props = {}) => {
+    wrapper = shallowMountExtended(TotalRiskScore, {
+      propsData: {
+        score: DEFAULT_SCORE,
+        ...props,
+      },
+      directives: {
+        GlResizeObserver: createMockDirective('gl-resize-observer'),
+      },
+    });
+  };
+
+  const findGlChart = () => wrapper.findComponent(GlChart);
+  const getSeries = () => findGlChart().props('options').series;
+  const getOuterMeterSeries = () => getSeries()[0];
+  const getProgressMeterSeries = () => getSeries()[1];
+
+  it('renders GlChart responsively', () => {
+    createComponent();
+
+    expect(findGlChart().props()).toMatchObject({
+      responsive: true,
+      height: 'auto',
+    });
+  });
+
+  describe('outer meter ring', () => {
+    it('configures a gauge with the correct dimensions', () => {
+      createComponent();
+
+      expect(getOuterMeterSeries()).toMatchObject({
+        type: 'gauge',
+        startAngle: 220,
+        endAngle: -40,
+        min: 0,
+        max: 100,
+        splitNumber: 4,
+        center: ['50%', '60%'],
+      });
+    });
+
+    it('configures the outer meter ring with the correct colors', () => {
+      createComponent();
+
+      expect(getOuterMeterSeries().axisLine.lineStyle.color).toEqual([
+        [0.25, EXPECTED_CHART_BAR_COLORS.LOW],
+        [0.5, EXPECTED_CHART_BAR_COLORS.MEDIUM],
+        [0.75, EXPECTED_CHART_BAR_COLORS.HIGH],
+        [1, EXPECTED_CHART_BAR_COLORS.CRITICAL],
+      ]);
+    });
+
+    it.each`
+      givenScore | expectedLabel      | expectedColor
+      ${1}       | ${'Low risk'}      | ${EXPECTED_LABEL_TEXT_COLORS.LOW}
+      ${26}      | ${'Medium risk'}   | ${EXPECTED_LABEL_TEXT_COLORS.MEDIUM}
+      ${51}      | ${'High risk'}     | ${EXPECTED_LABEL_TEXT_COLORS.HIGH}
+      ${76}      | ${'Critical risk'} | ${EXPECTED_LABEL_TEXT_COLORS.CRITICAL}
+    `(
+      'when the score is "$givenScore", the outer meter ring has the correct title and detail colors',
+      ({ givenScore, expectedLabel, expectedColor }) => {
+        createComponent({ score: givenScore });
+
+        const outerSeries = getOuterMeterSeries();
+
+        expect(outerSeries.title.color).toBe(expectedColor);
+        expect(outerSeries.detail.color).toBe(expectedColor);
+        expect(outerSeries.data[0].name).toBe(expectedLabel);
+      },
+    );
+  });
+
+  describe('progress meter ring', () => {
+    it('passes the correct data to the progress meter ring', () => {
+      createComponent();
+
+      expect(getProgressMeterSeries().data).toEqual([{ value: DEFAULT_SCORE }]);
+    });
+
+    it('configures the progress meter ring with the correct dimensions', () => {
+      createComponent();
+
+      expect(getProgressMeterSeries()).toMatchObject({
+        type: 'gauge',
+        startAngle: 220,
+        endAngle: -40,
+        min: 0,
+        max: 100,
+      });
+    });
+
+    it.each`
+      givenScore | expectedColor
+      ${1}       | ${EXPECTED_CHART_BAR_COLORS.LOW}
+      ${26}      | ${EXPECTED_CHART_BAR_COLORS.MEDIUM}
+      ${51}      | ${EXPECTED_CHART_BAR_COLORS.HIGH}
+      ${76}      | ${EXPECTED_CHART_BAR_COLORS.CRITICAL}
+    `(
+      'when the score is "$givenScore", the progress meter ring has the correct color',
+      ({ givenScore, expectedColor }) => {
+        createComponent({ score: givenScore });
+
+        const progressSeries = getProgressMeterSeries();
+
+        expect(progressSeries.axisLine.lineStyle.color[0][0]).toEqual(givenScore / 100);
+        expect(progressSeries.axisLine.lineStyle.color[0][1]).toEqual(expectedColor);
+      },
+    );
+  });
+
+  describe('chart resizing', () => {
+    it.each`
+      givenChartWidth | givenChartHeight | expectedOuterRingWidth | expectedProgressRingWidth | expectedOuterRingRadius | expectedProgressRingRadius
+      ${400}          | ${200}           | ${15}                  | ${30}                     | ${80}                   | ${64}
+      ${200}          | ${100}           | ${8}                   | ${16}                     | ${40}                   | ${31}
+      ${2000}         | ${1000}          | ${15}                  | ${30}                     | ${400}                  | ${384}
+    `(
+      'given a chart width of "$givenChartWidth" and a chart height of "$givenChartHeight", the chart is showing the correct ring widths and radii',
+      async ({
+        givenChartWidth,
+        givenChartHeight,
+        expectedOuterRingWidth,
+        expectedProgressRingWidth,
+        expectedOuterRingRadius,
+        expectedProgressRingRadius,
+      }) => {
+        createComponent();
+
+        const resizeObserverEntry = {
+          contentRect: { width: givenChartWidth, height: givenChartHeight },
+        };
+        getBinding(wrapper.element, 'gl-resize-observer').value(resizeObserverEntry);
+        await nextTick();
+
+        const outerSeries = getOuterMeterSeries();
+        const progressSeries = getProgressMeterSeries();
+
+        expect(outerSeries.axisLine.lineStyle.width).toBe(expectedOuterRingWidth);
+        expect(progressSeries.axisLine.lineStyle.width).toBe(expectedProgressRingWidth);
+
+        expect(outerSeries.radius).toBe(expectedOuterRingRadius);
+        expect(progressSeries.radius).toBe(expectedProgressRingRadius);
+      },
+    );
+  });
+});
