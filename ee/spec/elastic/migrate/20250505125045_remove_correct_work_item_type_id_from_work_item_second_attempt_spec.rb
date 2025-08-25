504426ed701d3fb30356ea46cb938801b1fe47e9
Project token used in gitlab-org/quality/engineering-productivity/team/-/settings/ci_cd as `HOUSEKEEPER_GITLAB_API_TOKEN` <project_278964_bot_f4b2c8281a8bdeb1f3dbbba673e4fc6f@noreply.gitlab.com>
2025-08-25 17:05:25 +0000

    Mark 20250505125045 as obsolete
    This migration marks the 20250505125045 RemoveCorrectWorkItemTypeIdFromWorkItemSecondAttempt Advanced search migration as obsolete.
    
    [Search for references to `remove_correct_work_item_type_id_from_work_item_second_attempt` in code](https://gitlab.com/search?project_id=278964&scope=blobs&search=remove_correct_work_item_type_id_from_work_item_second_attempt&regex=false))
    
    At the moment, the `gitlab-housekeeper` is not always capable of removing all references so
    you must check the diff and pipeline failures to confirm if there are any issues.
    It is the responsibility of the assignee (picked from ~"group::global search") to push those changes to this branch.
    
    [Read more](https://docs.gitlab.com/ee/development/search/advanced_search_migration_styleguide.html#cleaning-up-advanced-search-migrations)
    about the process for marking Advanced search migrations as obsolete.
    
    All Advanced search migrations must have had at least one
    [required stop](https : // docs.gitlab.com / ee / development / database / required_stops.html)
    to process the migration. Therefore we mark any Advanced search migrations added before the
    last required stop as obsolete.
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    in [CI](https://gitlab.com/gitlab-org/quality/engineering-productivity/team/-/jobs/11042785619) using the `Keeps::MarkOldAdvancedSearchMigrationsAsObsolete` keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please create an issue with the
    label ~"GitLab Housekeeper" and consider pinging the author of this keep.
    
    Changelog: other
    EE: true

--- ee/spec/elastic/migrate/20250505125045_remove_correct_work_item_type_id_from_work_item_second_attempt_spec.rb
+++ ee/spec/elastic/migrate/20250505125045_remove_correct_work_item_type_id_from_work_item_second_attempt_spec.rb
@@ -5,14 +5,6 @@ require File.expand_path(
   'ee/elastic/migrate/20250505125045_remove_correct_work_item_type_id_from_work_item_second_attempt.rb'
 )
 
-RSpec.describe RemoveCorrectWorkItemTypeIdFromWorkItemSecondAttempt, :elastic_delete_by_query, :sidekiq_inline, feature_category: :global_search do
-  let(:version) { 20250505125045 }
-
-  include_examples 'migration removes field' do
-    let(:expected_throttle_delay) { 1.minute }
-    let(:objects) { create_list(:work_item, 6) }
-    let(:index_name) { ::Search::Elastic::Types::WorkItem.index_name }
-    let(:field) { :correct_work_item_type_id }
-    let(:type) { 'long' }
-  end
+RSpec.describe RemoveCorrectWorkItemTypeIdFromWorkItemSecondAttempt, feature_category: :global_search do
+  it_behaves_like 'a deprecated Advanced Search migration', 20250505125045
 end
