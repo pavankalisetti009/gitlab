d5aea2c30be3be521582e4e6d797ac5edae74082
Luke Duncalfe <lduncalfe@gitlab.com>
2025-08-25 14:11:18 +1200

    Support agent inputs
    Make all agents declare the same 3 inputs:
    
    - `context:{previous_agent_id}.final_answer`: The previous agent's final
      response
    - `conversation_history:{previous_node_id}`: The previous agent's
      message history
    - `context:goal`: Workflow goal
    
    With aliases, that can be used in their goal templates.
    
    It also refactors away the referring of the `agents` collection directly
    and instead all accessing of agents is through `nodes` which is a
    combination of agent + step.
    
    This is because the same agents may be used multiple times within the
    same flow, and we want to accurately refer to specific agent+step
    combinations when identifying them, determining the previous one, etc.
    
    https://gitlab.com/gitlab-org/gitlab/-/issues/560719

--- ee/spec/models/ai/catalog/flow_definition_spec.rb
+++ ee/spec/models/ai/catalog/flow_definition_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe Ai::Catalog::FlowDefinition, feature_category: :workflow_catalog 
       'steps' =>
       [
         { 'agent_id' => agent_1.id, 'current_version_id' => agent_1.latest_version.id, 'pinned_version_prefix' => nil },
-        { 'agent_id' => agent_2.id, 'current_version_id' => agent_2.latest_version.id, 'pinned_version_prefix' => nil }
+        { 'agent_id' => agent_2.id, 'current_version_id' => agent_2.latest_version.id,
+          'pinned_version_prefix' => '1.1' }
       ]
     }
   end
@@ -30,22 +31,24 @@ RSpec.describe Ai::Catalog::FlowDefinition, feature_category: :workflow_catalog 
     end
   end
 
-  describe '#steps_with_agents_preloaded' do
-    it 'returns agent version mappings for existing agents' do
-      mappings = flow_definition.steps_with_agents_preloaded
-
-      expect(mappings).to contain_exactly(
-        { agent: agent_1, current_version_id: agent_1.latest_version.id, pinned_version_prefix: nil },
-        { agent: agent_2, current_version_id: agent_2.latest_version.id, pinned_version_prefix: nil }
+  describe '#steps' do
+    it 'returns flow steps' do
+      expect(flow_definition.steps).to contain_exactly(
+        {
+          unique_id: "#{agent_1.id}/0",
+          agent: agent_1,
+          current_version_id: agent_1.latest_version.id,
+          pinned_version_prefix: nil,
+          idx: 0
+        },
+        {
+          unique_id: "#{agent_2.id}/1",
+          agent: agent_2,
+          current_version_id: agent_2.latest_version.id,
+          pinned_version_prefix: '1.1',
+          idx: 1
+        }
       )
-    end
-  end
-
-  describe '#agents' do
-    it 'returns unique agents from the workflow steps' do
-      agents = flow_definition.agents
-
-      expect(agents).to contain_exactly(agent_1, agent_2)
     end
   end
 end
