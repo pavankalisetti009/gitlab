8e8ee5c63b95fc870d50e970aff8fc079dc37c2f
Dmytro Biryukov <dbiryukov@gitlab.com>
2025-08-26 18:00:52 +0200

    Introduce a maintainer artifact_access_level
    Inject a maintainer level into accessiblity enum
    Address all the comments, refactor job_artifacts_policy spec
    Extend build policy specs
    Improve specs namings
    
    Changelog: changed

--- ee/spec/policies/ci/job_artifact_policy_spec.rb
+++ ee/spec/policies/ci/job_artifact_policy_spec.rb
@@ -3,171 +3,93 @@
 require 'spec_helper'
 
 RSpec.describe Ci::JobArtifactPolicy, :models, feature_category: :job_artifacts do
-  let_it_be(:current_user) { create(:user) }
-  let_it_be(:project) { create(:project, :repository) }
-  let_it_be(:pipeline) { create(:ci_pipeline, project: project) }
-  let_it_be(:job) { create(:ci_build, :success, pipeline: pipeline, project: project) }
+  using RSpec::Parameterized::TableSyntax
 
-  let(:policy) do
-    described_class.new(current_user, job_artifact)
+  subject(:policy) { described_class.new(current_user, job_artifact) }
+
+  describe 'job artifacts access for different roles' do
+    before do
+      case role
+      when :developer  then project.add_developer(current_user)
+      when :maintainer then project.add_maintainer(current_user)
+      when :owner      then project.add_owner(current_user)
+      when :reporter   then project.add_reporter(current_user)
+      when :guest      then project.add_guest(current_user)
+      when :auditor    then project.add_developer(current_user) # rubocop:disable Lint/DuplicateBranch -- we still need to mimic developer role for auditor
+      end
+    end
+
+    let_it_be(:project)  { create(:project, :repository, public_builds: true) }
+    let_it_be(:pipeline) { create(:ci_pipeline, project: project) }
+    let_it_be(:job)      { create(:ci_build, :success, pipeline: pipeline, project: project) }
+
+    let(:current_user) { role == :auditor ? create(:user, :auditor) : create(:user) }
+    let(:job_artifact) { create(:ci_job_artifact, artifact_trait, job: job, project: project) }
+
+    where(:role, :artifact_trait, :allowed) do
+      # No access
+      :no_access | :private    | false
+      :no_access | :public     | false
+      :no_access | :none       | false
+      :no_access | :maintainer_only_access | false
+
+      # Developer
+      :developer | :private    | true
+      :developer | :public     | true
+      :developer | :none       | false
+      :developer | :maintainer_only_access | false
+
+      # Maintainer
+      :maintainer | :private    | true
+      :maintainer | :public     | true
+      :maintainer | :none       | false
+      :maintainer | :maintainer_only_access | true
+
+      # Owner
+      :owner | :private    | true
+      :owner | :public     | true
+      :owner | :none       | false
+      :owner | :maintainer_only_access | true
+
+      # Auditor (member as developer)
+      :auditor | :private    | true
+      :auditor | :public     | true
+      :auditor | :none       | false
+      :auditor | :maintainer_only_access | false
+
+      # Reporter
+      :reporter | :private    | false
+      :reporter | :public     | true
+      :reporter | :none       | false
+      :reporter | :maintainer_only_access | false
+
+      # Guest
+      :guest | :private    | false
+      :guest | :public     | true
+      :guest | :none       | false
+      :guest | :maintainer_only_access | false
+    end
+
+    with_them do
+      it { is_expected.to(allowed ? be_allowed(:read_job_artifacts) : be_disallowed(:read_job_artifacts)) }
+    end
   end
 
-  describe 'rules' do
-    describe 'for user without access to the project' do
-      context 'when job artifact is private' do # access:developer
-        let(:job_artifact) { create(:ci_job_artifact, :private, job: job, project: project) }
+  context 'when guest user and project-based pipeline visibility is disabled' do
+    let_it_be(:project) { create(:project, :repository, public_builds: false) }
+    let_it_be(:pipeline) { create(:ci_pipeline, project: project) }
+    let_it_be(:job)      { create(:ci_build, :success, pipeline: pipeline, project: project) }
 
-        it 'disallows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
+    let_it_be(:guest) { create(:user) }
+    let(:current_user) { guest }
+    let(:job_artifact) { create(:ci_job_artifact, :public, job: job, project: project) }
 
-      context 'when job artifact is public' do # access:all
-        let(:job_artifact) { create(:ci_job_artifact, :public, job: job, project: project) }
-
-        it 'disallows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
-
-      context 'when job artifact access is set to none' do # public:false
-        let(:job_artifact) { create(:ci_job_artifact, :none, job: job, project: project) }
-
-        it 'disallows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
+    before do
+      project.add_guest(guest)
     end
 
-    describe 'for user with access to the project' do
-      before do
-        project.add_developer(current_user)
-      end
-
-      context 'when job artifact is private' do # access:developer
-        let(:job_artifact) { create(:ci_job_artifact, :private, job: job, project: project) }
-
-        it 'allows read_job_artifacts' do
-          expect(policy).to be_allowed :read_job_artifacts
-        end
-      end
-
-      context 'when job artifact is public' do # access:all
-        let(:job_artifact) { create(:ci_job_artifact, :public, job: job, project: project) }
-
-        it 'allows read_job_artifacts' do
-          expect(policy).to be_allowed :read_job_artifacts
-        end
-      end
-
-      context 'when job artifact access is set to none' do # public:false
-        let(:job_artifact) { create(:ci_job_artifact, :none, job: job, project: project) }
-
-        it 'disallows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
-    end
-
-    describe 'for auditor user' do
-      let_it_be(:current_user) { create(:user, :auditor) }
-
-      before do
-        project.add_developer(current_user)
-      end
-
-      context 'when job artifact is private' do # access:developer
-        let(:job_artifact) { create(:ci_job_artifact, :private, job: job, project: project) }
-
-        it 'allows read_job_artifacts' do
-          expect(policy).to be_allowed :read_job_artifacts
-        end
-      end
-
-      context 'when job artifact is public' do # access:all
-        let(:job_artifact) { create(:ci_job_artifact, :public, job: job, project: project) }
-
-        it 'allows read_job_artifacts' do
-          expect(policy).to be_allowed :read_job_artifacts
-        end
-      end
-
-      context 'when job artifact access is set to none' do # public:false
-        let(:job_artifact) { create(:ci_job_artifact, :none, job: job, project: project) }
-
-        it 'disallows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
-    end
-
-    describe 'for reporter user' do
-      before do
-        project.add_reporter(current_user)
-      end
-
-      context 'when job artifact is private' do # access:developer
-        let(:job_artifact) { create(:ci_job_artifact, :private, job: job, project: project) }
-
-        it 'disallows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
-
-      context 'when job artifact is public' do # access:all
-        let(:job_artifact) { create(:ci_job_artifact, :public, job: job, project: project) }
-
-        it 'allows read_job_artifacts' do
-          expect(policy).to be_allowed :read_job_artifacts
-        end
-      end
-
-      context 'when job artifact access is set to none' do # public:false
-        let(:job_artifact) { create(:ci_job_artifact, :none, job: job, project: project) }
-
-        it 'allows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
-    end
-
-    describe 'for guest user' do
-      let_it_be(:guest) { create(:user, guest_of: project) }
-      let(:current_user) { guest }
-
-      context 'when job artifact is private' do # access:developer
-        let(:job_artifact) { create(:ci_job_artifact, :private, job: job, project: project) }
-
-        it 'disallows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
-
-      context 'when job artifact is public' do # access:all
-        let(:job_artifact) { create(:ci_job_artifact, :public, job: job, project: project) }
-
-        it 'allows read_job_artifacts' do
-          expect(policy).to be_allowed :read_job_artifacts
-        end
-
-        context 'when Project-based pipeline visibility disabled' do
-          before do
-            project.update!(public_builds: false)
-          end
-
-          it 'disallows read_job_artifacts' do
-            expect(policy).to be_disallowed :read_job_artifacts
-          end
-        end
-      end
-
-      context 'when job artifact access is set to none' do # public:false
-        let(:job_artifact) { create(:ci_job_artifact, :none, job: job, project: project) }
-
-        it 'disallows read_job_artifacts' do
-          expect(policy).to be_disallowed :read_job_artifacts
-        end
-      end
+    it 'disallows read_job_artifacts' do
+      expect(policy).to be_disallowed(:read_job_artifacts)
     end
   end
 end
