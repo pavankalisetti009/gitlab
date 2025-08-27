5d0c54d812659aeecebd42d230736fe642dbb0c4
Mark Florian <mflorian@gitlab.com>
2025-08-27 17:19:58 +0100

    Ensure CQs and MQs are built when running the GDK
    This uses a hacky work around to some module-local state in Tailwind.

--- config/tailwind_cqs.config.js
+++ config/tailwind_cqs.config.js
@@ -5,13 +5,15 @@
  * we should remove this config and the code that uses it as building CQs will become the default.
  */
 
-const tailwindCQsMQsPlugin = require('@gitlab/ui/tailwind_cqs_mqs_plugin');
-const config = require('./tailwind.config');
+const { tailwindCQsMQsPlugin } = require('@gitlab/ui/tailwind_cqs_mqs_plugin');
+const { cloneDeep } = require('lodash');
+const config = cloneDeep(require('./tailwind.config'));
 
 /**
  * We have to assume that the tailwindCQsMQsPlugin is the last registered plugin in the default
  * config.
  */
-config.plugins.splice(-1, tailwindCQsMQsPlugin(true));
+const buildContainerQueries = true;
+config.presets[0].plugins.splice(-1, 1, tailwindCQsMQsPlugin(buildContainerQueries));
 
 module.exports = config;
