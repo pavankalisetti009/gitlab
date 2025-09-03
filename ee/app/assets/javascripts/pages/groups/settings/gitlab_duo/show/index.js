0a2ca72f629f0a3b76d0fe1dc607de744e6745d6
Buck O'Leary <bucoleary@gitlab.com>
2025-09-02 21:30:53 -0400

    Removing GitLab Duo admin banners
    Cleans up unused enable_duo_banner callout enums and their
    associated views and specs for self-managed and .com
    
    Changelog: removed

--- ee/app/assets/javascripts/pages/groups/settings/gitlab_duo/show/index.js
+++ ee/app/assets/javascripts/pages/groups/settings/gitlab_duo/show/index.js
@@ -1,5 +1,3 @@
 import { initGitLabDuoHome } from 'ee/ai/settings/gitlab_duo_home_bundle';
-import initEnableDuoBanner from 'ee/ai/init_enable_duo_banner';
 
 initGitLabDuoHome();
-initEnableDuoBanner();
--- ee/spec/views/admin/dashboard/index.html.haml_spec.rb
+++ ee/spec/views/admin/dashboard/index.html.haml_spec.rb
@@ -270,20 +270,6 @@ RSpec.describe 'admin/dashboard/index.html.haml', :enable_admin_mode, feature_ca
     end
   end
 
-  describe 'with enabled duo banner' do
-    it 'renders the partial' do
-      render
-
-      expect(rendered).to render_template(
-        partial: 'admin/enable_duo_banner_sm',
-        locals: {
-          title: s_('AiPowered|AI-native features now available in IDEs'),
-          callouts_feature_name: 'enable_duo_banner_admin_dashboard'
-        }
-      )
-    end
-  end
-
   context 'for duo_amazon_q_alert' do
     before do
       allow(view).to receive(:current_user).and_return(build_stubbed(:user))
