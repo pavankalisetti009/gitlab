933b7f3021c8fc27bf2f3ebd7408673404000d7f
Manoj M J <mmj@gitlab.com>
2025-09-09 17:59:29 +0200

    Fix extended logging for self-hosted DAP

--- ee/lib/gitlab/duo_workflow/client.rb
+++ ee/lib/gitlab/duo_workflow/client.rb
@@ -63,10 +63,18 @@ module Gitlab
 
       def self.metadata(user)
         {
-          extended_logging: Feature.enabled?(:duo_workflow_extended_logging, user),
+          extended_logging: enable_extended_logging?(user),
           is_team_member:
             ::Gitlab::Tracking::StandardContext.new.gitlab_team_member?(user&.id)
         }
+      end
+
+      private_class_method def self.enable_extended_logging?(user)
+        # For a self-hosted Duo instance, return the value of the
+        # instance setting.
+        return ::Ai::Setting.instance&.enabled_instance_verbose_ai_logs if self_hosted_url
+
+        Feature.enabled?(:duo_workflow_extended_logging, user)
       end
     end
   end
