f5b5fc565cbbf8ad651a977f83e254f7ac7fbde0
Rémy Coutable <remy@rymai.me>
2025-09-11 11:01:01 +0200

    Add support for allow_immediate_namespaces_deletion setting in admin
    Changelog: added
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/features/admin/admin_settings_spec.rb
+++ spec/features/admin/admin_settings_spec.rb
@@ -50,6 +50,21 @@ RSpec.describe 'Admin updates settings', feature_category: :shared do
         end
       end
 
+      it 'change deletion settings', :js do
+        within_testid('admin-visibility-access-settings') do
+          fill_in 'Deletion protection', with: 30
+          uncheck 'Allow immediate deletion'
+          click_button 'Save changes'
+        end
+
+        expect(page).to have_content 'Application settings saved successfully'
+
+        within_testid('admin-visibility-access-settings') do
+          expect(find_field('Deletion protection').value).to eq('30')
+          expect(find_field('Allow immediate deletion')).not_to be_checked
+        end
+      end
+
       it 'modify import sources' do
         expect(current_settings.import_sources).to be_empty
 
