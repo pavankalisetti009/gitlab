259721bc980471aa5d0baf1c2df2a4477eebff64
Alex Buijs <abuijs@gitlab.com>
2025-09-11 11:30:08 +0200

    Add boundary class and policy
    Add Authz::Boundary and Authz::BoundaryPolciy classes. Also implement
    PersonalAccessToken#permitted_for_boundary? method.

--- app/models/authz/granular_scope.rb
+++ app/models/authz/granular_scope.rb
@@ -7,8 +7,14 @@ module Authz
 
     validates :permissions, json_schema: { filename: 'granular_scope_permissions', size_limit: 64.kilobytes }
 
-    def boundary
-      namespace.present? ? namespace.class.sti_name : 'Instance'
+    def self.permitted_for_boundary?(boundary, permissions)
+      required_permissions = Array(permissions).map(&:to_sym)
+      token_permissions = token_permissions(boundary)
+      (required_permissions - token_permissions).empty?
+    end
+
+    def self.token_permissions(boundary)
+      find_by_namespace_id(boundary.namespace)&.permissions&.map(&:to_sym) || []
     end
   end
 end
