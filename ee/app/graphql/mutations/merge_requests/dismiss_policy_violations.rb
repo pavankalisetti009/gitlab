62d0a4fdfa38ac468aab326ac47c976b35a0f7b1
Imam Hossain <ihossain@gitlab.com>
2025-09-12 17:31:33 +0200

    Update DismissPolicyViolations mutation
    The GraphQL mutation DismissPolicyViolations now accepts dismissal_types (an enum) and a comment (string). A new GraphQL enum, DismissalType, is introduced to define the possible
    reasons for dismissal. The underlying service is updated to persist these details with the policy dismissal record.

--- ee/app/graphql/mutations/merge_requests/dismiss_policy_violations.rb
+++ ee/app/graphql/mutations/merge_requests/dismiss_policy_violations.rb
@@ -11,16 +11,27 @@ module Mutations
         required: true,
         description: 'IDs of warn mode policies with violations to dismiss.'
 
+      argument :dismissal_types,
+        [Types::SecurityOrchestration::DismissalTypeEnum],
+        required: true,
+        description: 'Type of dismissal for the policy violations.'
+
+      argument :comment,
+        GraphQL::Types::String,
+        required: true,
+        description: 'Comment explaining the dismissal of the policy violations.'
+
       def resolve(project_path:, iid:, **args)
         merge_request = authorized_find!(project_path: project_path, iid: iid)
 
         result = ::Security::ScanResultPolicies::DismissPolicyViolationsService.new(
           merge_request,
           current_user: current_user,
-          params: args.slice(:security_policy_ids)
+          params: args
         ).execute
 
         {
+          merge_request: merge_request,
           errors: result.success? ? [] : [result.message]
         }
       end
