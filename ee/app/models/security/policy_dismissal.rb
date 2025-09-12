de66f71262aa1628fe64c656cd0236c38531e15f
Imam Hossain <ihossain@gitlab.com>
2025-09-12 15:48:51 +0200

    Feat: Enhance policy dismissals with types and comments
    Adds dismissal_types (array of smallints) and a comment field to
    security_policy_dismissals.
    
    Changelog: changed

--- ee/app/models/security/policy_dismissal.rb
+++ ee/app/models/security/policy_dismissal.rb
@@ -3,6 +3,12 @@
 module Security
   class PolicyDismissal < ApplicationRecord
     self.table_name = 'security_policy_dismissals'
+    DISMISSAL_TYPES = {
+      policy_false_positive: 0,
+      scanner_false_positive: 1,
+      emergency_hot_fix: 2,
+      other: 3
+    }.freeze
 
     belongs_to :project, class_name: 'Project', optional: false
     belongs_to :merge_request, class_name: 'MergeRequest', optional: false
@@ -10,6 +16,16 @@ module Security
     belongs_to :user, class_name: 'User', optional: true
 
     validates :merge_request_id, uniqueness: { scope: :security_policy_id }
-    validates :security_findings_uuids, presence: true
+    validates :comment, length: { maximum: 255 }, allow_nil: true
+    validate  :dismissal_types_are_valid
+
+    private
+
+    def dismissal_types_are_valid
+      invalid_values = Array(dismissal_types) - DISMISSAL_TYPES.values
+      return if dismissal_types.present? && invalid_values.blank?
+
+      errors.add(:dismissal_types, "must be an array with allowed values: #{DISMISSAL_TYPES.values.join(', ')}")
+    end
   end
 end
--- ee/spec/models/security/policy_dismissal_spec.rb
+++ ee/spec/models/security/policy_dismissal_spec.rb
@@ -13,7 +13,28 @@ RSpec.describe Security::PolicyDismissal, feature_category: :security_policy_man
   describe 'validations' do
     subject(:policy_dismissal) { create(:policy_dismissal) }
 
-    it { is_expected.to validate_presence_of(:security_findings_uuids) }
+    it { is_expected.to allow_value(nil).for(:security_findings_uuids) }
+    it { is_expected.to validate_length_of(:comment).is_at_most(255).allow_nil }
+
     it { is_expected.to(validate_uniqueness_of(:merge_request_id).scoped_to(%i[security_policy_id])) }
+
+    context 'when validating dismissal_types' do
+      it 'is invalid if empty' do
+        policy_dismissal.dismissal_types = []
+        expect(policy_dismissal).not_to be_valid
+        expect(policy_dismissal.errors[:dismissal_types]).to include(/must be an array with allowed values/)
+      end
+
+      it 'is invalid if includes unknown value' do
+        policy_dismissal.dismissal_types = described_class::DISMISSAL_TYPES.values + [999]
+        expect(policy_dismissal).not_to be_valid
+        expect(policy_dismissal.errors[:dismissal_types]).to include(/must be an array with allowed values/)
+      end
+
+      it 'is valid if all values are allowed' do
+        policy_dismissal.dismissal_types = described_class::DISMISSAL_TYPES.values.sample(2)
+        expect(policy_dismissal).to be_valid
+      end
+    end
   end
 end
