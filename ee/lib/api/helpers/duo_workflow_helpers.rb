d3c19c7de1fad3ba028de2384c87a986377187b9
Eva Kadlecová <ekadlecova@gitlab.com>
2025-09-12 10:38:34 +0200

    Clean up duo_workflow_work_item_tools flag
    Changelog: added
    EE: true

--- ee/lib/api/helpers/duo_workflow_helpers.rb
+++ ee/lib/api/helpers/duo_workflow_helpers.rb
@@ -15,7 +15,6 @@ module API
       end
 
       def push_feature_flags
-        Gitlab::AiGateway.push_feature_flag(:duo_workflow_work_item_tools, current_user)
         Gitlab::AiGateway.push_feature_flag(:expanded_ai_logging, current_user)
         Gitlab::AiGateway.push_feature_flag(:use_duo_context_exclusion, current_user)
         Gitlab::AiGateway.push_feature_flag(:duo_agentic_chat_openai_gpt_5, current_user)
