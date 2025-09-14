d2df116ed38f45235051c9565de6afe417e045ec
Gal Katz <gkatz@gitlab.com>
2025-09-14 11:05:43 +0300

    Remove vulnerability_namespace_adjustment_worker feature flag
    Changelog: changed
    EE: true

--- ee/spec/workers/vulnerabilities/namespace_statistics/adjustment_worker_spec.rb
+++ ee/spec/workers/vulnerabilities/namespace_statistics/adjustment_worker_spec.rb
@@ -8,35 +8,21 @@ RSpec.describe Vulnerabilities::NamespaceStatistics::AdjustmentWorker, feature_c
   describe '#perform' do
     let(:namespace_ids) { [1, 2, 3] }
 
-    context 'when vulnerability_namespace_adjustment_worker feature is disabled' do
-      before do
-        stub_feature_flags(vulnerability_namespace_adjustment_worker: false)
-        allow(Vulnerabilities::NamespaceStatistics::AdjustmentService).to receive(:execute)
-      end
+    before do
+      allow(Vulnerabilities::NamespaceStatistics::AdjustmentService).to receive(:execute)
+    end
+
+    it 'calls Vulnerabilities::NamespaceStatistics::AdjustmentService with given namespace_ids' do
+      worker.perform(namespace_ids)
+      expect(Vulnerabilities::NamespaceStatistics::AdjustmentService).to have_received(:execute).with(namespace_ids)
+    end
+
+    context 'when namespace_ids is empty' do
+      let(:namespace_ids) { [] }
 
       it 'doesnt call Vulnerabilities::NamespaceStatistics::AdjustmentService' do
         worker.perform(namespace_ids)
         expect(Vulnerabilities::NamespaceStatistics::AdjustmentService).not_to have_received(:execute)
-      end
-    end
-
-    context 'when vulnerability_namespace_adjustment_worker is enabled' do
-      before do
-        allow(Vulnerabilities::NamespaceStatistics::AdjustmentService).to receive(:execute)
-      end
-
-      it 'calls Vulnerabilities::NamespaceStatistics::AdjustmentService with given namespace_ids' do
-        worker.perform(namespace_ids)
-        expect(Vulnerabilities::NamespaceStatistics::AdjustmentService).to have_received(:execute).with(namespace_ids)
-      end
-
-      context 'when namespace_ids is empty' do
-        let(:namespace_ids) { [] }
-
-        it 'doesnt call Vulnerabilities::NamespaceStatistics::AdjustmentService' do
-          worker.perform(namespace_ids)
-          expect(Vulnerabilities::NamespaceStatistics::AdjustmentService).not_to have_received(:execute)
-        end
       end
     end
   end
