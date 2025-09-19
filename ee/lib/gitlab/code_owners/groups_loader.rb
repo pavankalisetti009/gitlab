79bc46303e8d9017436cb4ce775287f743aecec0
Joe Woodward <jwoodward@gitlab.com>
2025-09-19 12:13:52 +0100

    Include groups invited to ancestral groups as Code Owners
    This change allows groups that have been shared into the project's group
    or any ancestor of that group to be specified as a Code Owner in the
    project's CODEOWNERS file.
    
    Only direct members of the group are eligible.
    
    Co-Authored-By: Emma Park <epark@gitlab.com>

--- ee/lib/gitlab/code_owners/groups_loader.rb
+++ ee/lib/gitlab/code_owners/groups_loader.rb
@@ -19,11 +19,21 @@ module Gitlab
       def groups
         return Group.none if names.blank?
 
-        relations = [
-          project.invited_groups.where_full_path_in(names, preload_routes: false)
-        ]
-        # Include the projects ancestor group(s) if they are listed as owners
-        relations << project.group.self_and_ancestors.where_full_path_in(names, preload_routes: false) if project.group
+        relations = [project.invited_groups.where_full_path_in(names, preload_routes: false)]
+
+        if project.group
+          # Include the project's group and ancestor(s) if they are listed as owners
+          relations << project.group
+            .self_and_ancestors
+            .where_full_path_in(names, preload_routes: false)
+
+          if Feature.enabled?(:check_inherited_groups_for_codeowners, project)
+            # Retrieve groups that have access shared with the project's group and ancestor(s)
+            relations << project.group
+              .shared_with_groups_of_ancestors_and_self.with_developer_maintainer_owner_access
+              .where_full_path_in(names, preload_routes: false)
+          end
+        end
 
         Group.from_union(relations).with_route.with_users
       end
--- ee/spec/lib/gitlab/code_owners/groups_loader_spec.rb
+++ ee/spec/lib/gitlab/code_owners/groups_loader_spec.rb
@@ -3,103 +3,194 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::CodeOwners::GroupsLoader, feature_category: :source_code_management do
-  let(:text) do
-    <<~TXT
-    This is a long text that mentions some groups.
-    @group-1 and @group-doesnt-exist take a walk in the park.
-    There they meet @group-2 and @group-1/with-nested/Group-3
-    TXT
-  end
-
-  let(:extractor) { Gitlab::CodeOwners::ReferenceExtractor.new(text) }
-  let(:project) { create(:project, :public) }
-  let(:entry) { double('Entries') }
-
   describe '#load_to' do
-    subject(:load_groups) do
-      described_class.new(project, names: extractor.names).load_to([entry])
-    end
+    let(:loader) { described_class.new(instance_double(Project)) }
+    let(:entry) { instance_double(Gitlab::CodeOwners::Entry) }
+    let(:groups) { 'groups collection' }
 
     before do
+      allow(loader).to receive(:groups).and_return(groups)
       allow(entry).to receive(:add_matching_groups_from)
     end
 
-    context 'input has no matching group paths' do
-      let(:text) { 'My test' }
+    it 'asks each entry to assign matching groups' do
+      loader.load_to([entry])
 
-      it 'returns an empty list of groups' do
-        load_groups
+      expect(loader).to have_received(:groups)
+      expect(entry).to have_received(:add_matching_groups_from).with(groups)
+    end
+  end
 
-        expect(entry).to have_received(:add_matching_groups_from).with([])
-      end
+  describe '#groups' do
+    let(:names_kwarg) { group_names }
+
+    # Group relationship diagram:
+    #
+    # # Parent group and ancestors
+    # parent_group
+    #   |- project_group
+    #     |- project
+    #
+    #
+    # # Direct project access:
+    #
+    # project --- guest_group (Guest)
+    #          |- planner_group (Planner)
+    #          |- reporter_group (Reporter)
+    #          |- developer_group (Developer)
+    #          |- maintainer_group (Maintainer)
+    #          |- owner_group (Owner)
+    #          |- nested_group (Developer)
+    #
+    # # Groups the project group and ancestors are shared with:
+    #
+    # project_group --- project_group_shared_with_group (Developer)
+    #                |- project_group_shared_with_reporter_group (Reporter)
+    # parent_group  --- parent_group_shared_with_group (Developer)
+    #                |- parent_group_shared_with_reporter_group (Reporter)
+    #
+    # # Groups shared with the project group and ancestors
+    #
+    # group_shared_with_project_group --- project_group (Developer)
+    # group_shared_with_parent_group  --- parent_group (Developer)
+    #
+    # # No relationship to project
+    #
+    # external_group
+
+    let_it_be(:project) { create(:project, :in_subgroup) }
+    let_it_be(:guest_group) { create(:project_group_link, :guest, project: project).group }
+    let_it_be(:planner_group) { create(:project_group_link, :planner, project: project).group }
+    let_it_be(:reporter_group) { create(:project_group_link, :reporter, project: project).group }
+    let_it_be(:developer_group) { create(:project_group_link, :developer, project: project).group }
+    let_it_be(:maintainer_group) { create(:project_group_link, :maintainer, project: project).group }
+    let_it_be(:owner_group) { create(:project_group_link, :owner, project: project).group }
+    let_it_be(:project_group) { project.group }
+    let_it_be(:parent_group) { project_group.parent }
+    let_it_be(:external_group) { create(:group, :nested) }
+
+    let_it_be(:nested_group) do
+      create(:project_group_link, :developer, project: project, group: create(:group, :nested)).group
     end
 
-    context 'nil input' do
-      let(:text) { nil }
-
-      it 'returns an empty relation when nil was passed' do
-        load_groups
-
-        expect(entry).to have_received(:add_matching_groups_from).with([])
-      end
+    let_it_be(:project_group_shared_with_group) do
+      create(:group_group_link, shared_group: project_group).shared_with_group
     end
 
-    context 'input matches group paths' do
-      let(:project) { create(:project, :private) }
-
-      it 'returns the groups case insensitive for names' do
-        group = create(:group, path: "GROUP-1")
-        create(:group, path: "GROUP-2")
-        project.invited_groups << group
-
-        load_groups
-
-        expect(entry).to have_received(:add_matching_groups_from).with([group])
-      end
+    let_it_be(:parent_group_shared_with_group) do
+      create(:group_group_link, shared_group: parent_group).shared_with_group
     end
 
-    context "input matches project.group" do
-      let(:group) { create(:group) }
-      let(:project) { create(:project, :repository, namespace: group) }
-      let(:text) { "@#{project.group.full_path}" }
-
-      it "returns the project's group" do
-        load_groups
-
-        expect(entry).to have_received(:add_matching_groups_from).with([group])
-      end
+    let_it_be(:parent_group_shared_with_reporter_group) do
+      create(:group_group_link, :reporter, shared_group: parent_group).shared_with_group
     end
 
-    context 'input as array of strings' do
-      let(:text) { super().lines }
-
-      it 'is treated as one string' do
-        group_1 = create(:group, path: 'GROup-1')
-        group_2 = create(:group, path: 'GROUP-2')
-        create(:group, path: 'group-3')
-        project.invited_groups << [group_1, group_2]
-
-        load_groups
-
-        expect(entry).to have_received(:add_matching_groups_from) do |args|
-          expect(args).to contain_exactly(group_2, group_1)
-        end
-      end
+    let_it_be(:project_group_shared_with_reporter_group) do
+      create(:group_group_link, :reporter, shared_group: project_group).shared_with_group
     end
 
-    context 'nested groups' do
-      it 'returns nested groups by mentioned full paths' do
-        group_1 = create(:group, path: 'GROup-1')
-        group_2 = create(:group, path: 'with-nested', parent: group_1)
-        create(:group, path: 'not-invited')
-        nested_group = create(:group, path: 'group-3', parent: group_2)
-        project.invited_groups << [group_1, group_2, nested_group]
+    let_it_be(:group_shared_with_project_group) do
+      create(:group_group_link, shared_with_group: project_group).shared_group
+    end
 
-        load_groups
+    let_it_be(:group_shared_with_parent_group) do
+      create(:group_group_link, shared_with_group: parent_group).shared_group
+    end
 
-        expect(entry).to have_received(:add_matching_groups_from) do |args|
-          expect(args).to contain_exactly(group_1, nested_group)
-        end
+    let_it_be(:invited_groups) do
+      [
+        guest_group,
+        planner_group,
+        reporter_group,
+        developer_group,
+        maintainer_group,
+        owner_group,
+        nested_group
+      ]
+    end
+
+    let_it_be(:project_group_and_ancestors) do
+      [
+        project_group,
+        parent_group
+      ]
+    end
+
+    let_it_be(:project_group_and_ancestors_shared_with_groups) do
+      [
+        project_group_shared_with_group,
+        parent_group_shared_with_group
+      ]
+    end
+
+    let_it_be(:project_group_and_ancestors_shared_with_groups_without_access) do
+      [
+        project_group_shared_with_reporter_group,
+        parent_group_shared_with_reporter_group
+      ]
+    end
+
+    let_it_be(:groups_shared_with_project_group_and_ancestors) do
+      [
+        group_shared_with_project_group,
+        group_shared_with_parent_group
+      ]
+    end
+
+    let_it_be(:groups) do
+      [
+        *invited_groups,
+        external_group,
+        *project_group_and_ancestors,
+        *project_group_and_ancestors_shared_with_groups,
+        *project_group_and_ancestors_shared_with_groups_without_access,
+        *groups_shared_with_project_group_and_ancestors
+      ]
+    end
+
+    let_it_be(:group_names) { groups.map(&:full_path) }
+
+    subject { described_class.new(project, names: names_kwarg).groups }
+
+    it 'returns all valid groups', :aggregate_failures do
+      is_expected.to match_array(
+        invited_groups +
+        project_group_and_ancestors +
+        project_group_and_ancestors_shared_with_groups
+      )
+
+      is_expected.not_to include(
+        external_group,
+        *project_group_and_ancestors_shared_with_groups_without_access,
+        *groups_shared_with_project_group_and_ancestors
+      )
+    end
+
+    context 'when names attr is nil' do
+      let(:names_kwarg) { nil }
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'when names attr is empty' do
+      let(:names_kwarg) { [] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'there are no matches' do
+      let(:names_kwarg) { ['foo/bar'] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'when check_inherited_groups_for_codeowners is disabled' do
+      before do
+        stub_feature_flags(check_inherited_groups_for_codeowners: false)
+      end
+
+      it "excludes groups invited to project's group and ancestors" do
+        is_expected.not_to include(*project_group_and_ancestors_shared_with_groups)
       end
     end
   end
