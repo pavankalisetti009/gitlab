aeb8a389805c3b88064c5f053cd8470bd34aa317
fdegier <fdegier@gitlab.com>
2025-09-23 14:51:04 +0200

    Remove unused Duo invoke path
    EE: true

--- ee/app/graphql/ee/types/repository/blob_type.rb
+++ ee/app/graphql/ee/types/repository/blob_type.rb
@@ -15,17 +15,10 @@ module EE
 
           field :show_duo_workflow_action, GraphQL::Types::Boolean, null: true,
             description: 'Indicator to show Duo Agent Platform action.'
-
-          field :duo_workflow_invoke_path, GraphQL::Types::String, null: true,
-            description: 'Path to invoke Duo Agent Platform.'
         end
 
         def show_duo_workflow_action
           show_duo_workflow_action?(object)
-        end
-
-        def duo_workflow_invoke_path
-          api_v4_ai_duo_workflows_workflows_path
         end
       end
     end
