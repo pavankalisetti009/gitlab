20e1598bcee96541825f9dfc4cd5911e703fc2e8
GitLab Duo <duo.workflow.agent@gitlab.com>
2025-09-23 16:23:19 +0000

    Remove unused system_access_instance_microsoft_applications tables
    The system_access_instance_microsoft_applications and
    system_access_instance_microsoft_graph_access_tokens tables
    were created in milestone 17.5 but were never actually used.
    A different approach was taken to solve the Microsoft Azure
    integration for self-managed instances.
    
    This commit removes:
    - system_access_instance_microsoft_applications table
    - system_access_instance_microsoft_graph_access_tokens table
    - SystemAccess::InstanceMicrosoftApplication model
    - SystemAccess::InstanceMicrosoftGraphAccessToken model
    - Associated factory definitions
    - RuboCop exceptions for table creation migrations
    
    The table documentation has been moved to db/docs/deleted_tables/
    with removal metadata.
    
    Changelog: removed

--- ee/app/models/system_access/instance_microsoft_graph_access_token.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# rubocop:disable Gitlab/BoundedContexts -- Spliting existing table
-module SystemAccess
-  class InstanceMicrosoftGraphAccessToken < ApplicationRecord
-  end
-end
-# rubocop:enable Gitlab/BoundedContexts
