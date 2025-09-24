330e9b385b48b776dea3a622c3016d2c2a3df778
Ammar Alakkad <aalakkad@gitlab.com>
2025-09-24 17:07:42 +0300

    Add unified usage billing dashboard initialization

--- ee/app/assets/javascripts/pages/admin/gitlab_duo/usage/index.js
+++ ee/app/assets/javascripts/pages/admin/gitlab_duo/usage/index.js
@@ -1,27 +1,4 @@
-import Vue from 'vue';
-import AdminUsageDashboard from 'ee/usage_quotas/usage_billing/components/app.vue';
+import { initUsageBillingDashboard } from 'ee/usage_quotas/usage_billing/index';
 
-function initAdminUsageDashboard() {
-  const el = document.getElementById('js-instance-usage-billing-dashboard');
-
-  if (!el) {
-    return null;
-  }
-
-  const { purchaseCommitmentUrl, userUsagePath, fetchUsageDataApiUrl } = el.dataset;
-
-  return new Vue({
-    el,
-    name: 'AdminUsageBillingDashboardApp',
-    provide: {
-      userUsagePath,
-      purchaseCommitmentUrl,
-      fetchUsageDataApiUrl,
-    },
-    render(createElement) {
-      return createElement(AdminUsageDashboard);
-    },
-  });
-}
-
-initAdminUsageDashboard();
+const el = document.getElementById('js-instance-usage-billing-dashboard');
+initUsageBillingDashboard(el);
