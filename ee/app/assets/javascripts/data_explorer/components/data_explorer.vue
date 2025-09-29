d91e5789277f04f071f17cfe6e4e3e7f392cd1b1
Alex Pennells <apennells@gitlab.com>
2025-09-29 06:40:56 -0400

    Add DataExplorer.vue skeleton component
    Adds a barebones version of the Data Explorer, which will
    be expanded on in future iterations.

---
+++ ee/app/assets/javascripts/data_explorer/components/data_explorer.vue
@@ -0,0 +1,69 @@
+<script>
+// This component is still under active development. Updates on the current progress
+// can be found in https://gitlab.com/groups/gitlab-org/-/epics/19359
+import EMPTY_CHART_SVG from '@gitlab/svgs/dist/illustrations/empty-state/empty-chart-md.svg?url';
+import { GlFormTextarea, GlButton, GlCard, GlEmptyState, GlLink } from '@gitlab/ui';
+import ModalCopyButton from '~/vue_shared/components/modal_copy_button.vue';
+import { helpPagePath } from '~/helpers/help_page_helper';
+
+export default {
+  name: 'DataExplorer',
+  components: {
+    GlFormTextarea,
+    GlButton,
+    GlCard,
+    GlEmptyState,
+    GlLink,
+    ModalCopyButton,
+  },
+  glqlDocsLink: helpPagePath('user/glql/_index'),
+  EMPTY_CHART_SVG,
+  data() {
+    return { query: '' };
+  },
+  computed: {
+    hasQueryText() {
+      return this.query.trim() !== '';
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="gl-flex gl-flex-col gl-gap-3">
+    <div class="gl-relative">
+      <gl-form-textarea
+        v-model="query"
+        :aria-label="s__('DataExplorer|Data query')"
+        :placeholder="s__('DataExplorer|// TYPE YOUR QUERY HERE')"
+        :no-resize="false"
+        rows="16"
+        textarea-classes="!gl-p-6 !gl-font-monospace"
+      />
+      <modal-copy-button
+        v-if="hasQueryText"
+        :title="s__('DataExplorer|Copy query')"
+        :text="query"
+        class="gl-absolute gl-right-6 gl-top-6 gl-z-1"
+      />
+
+      <gl-link :href="$options.glqlDocsLink" class="gl-absolute gl-bottom-6 gl-right-6 gl-z-1">{{
+        s__('DataExplorer|What is GLQL?')
+      }}</gl-link>
+    </div>
+
+    <div>
+      <gl-button variant="confirm" icon="play" :disabled="!hasQueryText">{{
+        s__('DataExplorer|Run query')
+      }}</gl-button>
+    </div>
+
+    <gl-card>
+      <gl-empty-state
+        :title="s__('DataExplorer|Preview not available')"
+        :description="s__('DataExplorer|Start by typing a GLQL query.')"
+        :svg-path="$options.EMPTY_CHART_SVG"
+      />
+    </gl-card>
+  </div>
+</template>
---
+++ ee/spec/frontend/data_explorer/components/data_explorer_spec.js
@@ -0,0 +1,70 @@
+import { shallowMount } from '@vue/test-utils';
+import { GlFormTextarea, GlLink, GlButton, GlEmptyState } from '@gitlab/ui';
+import DataExplorer from 'ee/data_explorer/components/data_explorer.vue';
+import ModalCopyButton from '~/vue_shared/components/modal_copy_button.vue';
+
+describe('Data Explorer', () => {
+  let wrapper;
+
+  const createWrapper = () => {
+    wrapper = shallowMount(DataExplorer);
+  };
+
+  const getTextArea = () => wrapper.findComponent(GlFormTextarea);
+
+  describe('when rendered', () => {
+    beforeEach(() => {
+      createWrapper();
+    });
+
+    it('renders the text area', () => {
+      expect(getTextArea().exists()).toBe(true);
+    });
+
+    it('does not render the copy query button', () => {
+      expect(wrapper.findComponent(ModalCopyButton).exists()).toBe(false);
+    });
+
+    it('renders the GLQL docs link', () => {
+      const link = wrapper.findComponent(GlLink);
+      expect(link.text()).toBe('What is GLQL?');
+      expect(link.attributes('href')).toBe('/help/user/glql/_index');
+    });
+
+    it('renders a disabled `Run query` button', () => {
+      const button = wrapper.findComponent(GlButton);
+      expect(button.text()).toBe('Run query');
+      expect(button.attributes('disabled')).toBeDefined();
+    });
+
+    it('renders an empty state for the query preview', () => {
+      const emptyState = wrapper.findComponent(GlEmptyState);
+      expect(emptyState.props()).toMatchObject({
+        title: 'Preview not available',
+        description: 'Start by typing a GLQL query.',
+        svgPath: expect.any(String),
+      });
+    });
+
+    describe('with query input', () => {
+      const query = 'zzzzz';
+
+      beforeEach(() => {
+        return getTextArea().vm.$emit('input', query);
+      });
+
+      it('renders the copy query button', () => {
+        expect(wrapper.findComponent(ModalCopyButton).props()).toMatchObject({
+          title: 'Copy query',
+          text: query,
+        });
+      });
+
+      it('renders a clickable `Run query` button', () => {
+        const button = wrapper.findComponent(GlButton);
+        expect(button.text()).toBe('Run query');
+        expect(button.attributes('disabled')).toBeUndefined();
+      });
+    });
+  });
+});
