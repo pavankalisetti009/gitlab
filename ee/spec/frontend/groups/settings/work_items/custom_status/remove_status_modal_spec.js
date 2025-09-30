1048b31081ac1e0872d7f944da83bfd9b12d5f53
Coung Ngo <cngo@gitlab.com>
2025-09-30 16:15:34 +0100

    Complete "Remove status" modal flow
    Behind work_item_status_mvc2 feature flag

--- ee/spec/frontend/groups/settings/work_items/custom_status/remove_status_modal_spec.js
+++ ee/spec/frontend/groups/settings/work_items/custom_status/remove_status_modal_spec.js
@@ -1,5 +1,4 @@
 import { GlAlert, GlModal, GlIcon } from '@gitlab/ui';
-import { cloneDeep } from 'lodash';
 import Vue, { nextTick } from 'vue';
 import VueApollo from 'vue-apollo';
 import lifecycleUpdateMutation from 'ee/groups/settings/work_items/custom_status/graphql/lifecycle_update.mutation.graphql';
@@ -15,7 +14,53 @@ Vue.use(VueApollo);
 describe('RemoveStatusModal', () => {
   let wrapper;
 
-  const mockUpdateResponse = {
+  const triageStatus = {
+    id: 'gid://gitlab/WorkItems::Statuses::Custom::Status/10',
+    name: 'Triage!',
+    iconName: 'status-neutral',
+    color: '#995715',
+    description: null,
+    category: 'triage',
+    __typename: 'WorkItemStatus',
+  };
+  const toDoStatus = {
+    id: 'gid://gitlab/WorkItems::Statuses::Custom::Status/20',
+    name: 'To do!',
+    iconName: 'status-waiting',
+    color: '#737278',
+    description: null,
+    category: 'to_do',
+    __typename: 'WorkItemStatus',
+  };
+  const inProgressStatus = {
+    id: 'gid://gitlab/WorkItems::Statuses::Custom::Status/30',
+    name: 'In progress!',
+    iconName: 'status-running',
+    color: '#1f75cb',
+    description: null,
+    category: 'in_progress',
+    __typename: 'WorkItemStatus',
+  };
+  const doneStatus = {
+    id: 'gid://gitlab/WorkItems::Statuses::Custom::Status/40',
+    name: 'Mignon Kub',
+    iconName: 'status-success',
+    color: '#108548',
+    description: null,
+    category: 'done',
+    __typename: 'WorkItemStatus',
+  };
+  const canceledStatus = {
+    id: 'gid://gitlab/WorkItems::Statuses::Custom::Status/50',
+    name: `Won't do!`,
+    iconName: 'status-cancelled',
+    color: '#DD2B0E',
+    description: null,
+    category: 'canceled',
+    __typename: 'WorkItemStatus',
+  };
+
+  const lifecycleUpdateMutationHandler = jest.fn().mockResolvedValue({
     data: {
       lifecycleUpdate: {
         lifecycle: mockLifecycles[0],
@@ -23,15 +68,12 @@ describe('RemoveStatusModal', () => {
         __typename: 'LifecycleUpdatePayload',
       },
     },
-  };
-
-  const lifecycleUpdateMutationHandler = jest.fn().mockResolvedValue(mockUpdateResponse);
+  });
 
   const findModal = () => wrapper.findComponent(GlModal);
   const findCurrentStatus = () => wrapper.findByTestId('current-status-value');
   const findNewStatusLabel = () => wrapper.find('label');
   const findNewStatusListbox = () => wrapper.findComponent(RemoveStatusModalListbox);
-  const findNewDefaultLabel = () => wrapper.findByTestId('new-default-label');
   const findNewDefaultListbox = () => wrapper.findByTestId('new-default-listbox');
 
   const createComponent = ({ props = {}, updateHandler = lifecycleUpdateMutationHandler } = {}) => {
@@ -46,24 +88,26 @@ describe('RemoveStatusModal', () => {
     });
   };
 
-  describe('default', () => {
+  describe('when status has items and other statuses exist in same state', () => {
     beforeEach(() => {
-      createComponent();
+      const lifecycle = {
+        id: 'gid://gitlab/WorkItems::Statuses::Custom::Lifecycle/9',
+        name: 'Lifecycle!',
+        workItemTypes: [],
+        statuses: [triageStatus, toDoStatus, inProgressStatus, doneStatus, canceledStatus],
+        statusCounts: [{ status: toDoStatus, count: '10' }],
+        __typename: 'WorkItemLifecycle',
+      };
+      createComponent({ props: { lifecycle, statusToRemove: toDoStatus } });
     });
 
-    it('renders the modal with correct title', () => {
+    it('renders the modal with title', () => {
       expect(findModal().props('title')).toBe('Remove status');
-    });
-
-    it('emits hidden event when modal is hidden', () => {
-      findModal().vm.$emit('hidden');
-
-      expect(wrapper.emitted('hidden')).toEqual([[]]);
     });
 
     it('renders "new status" body text', () => {
       expect(wrapper.text()).toContain(
-        `0 items currently use the status 'Karon Homenick'. Select a new status for these items.`,
+        `10 items currently use the status 'To do!'. Select a new status for these items.`,
       );
     });
 
@@ -72,162 +116,179 @@ describe('RemoveStatusModal', () => {
       expect(findCurrentStatus().findComponent(GlIcon).attributes('style')).toBe(
         'color: rgb(115, 114, 120);',
       );
-      expect(findCurrentStatus().text()).toBe('Karon Homenick');
-    });
-  });
-
-  it('renders the new status selection label', () => {
-    createComponent();
-
-    expect(findNewStatusLabel().text()).toBe('New status');
-  });
-
-  it('filters listbox items to remove statusToRemove and keep statuses from same open/closed state', () => {
-    const expectedStatus = {
-      id: 'gid://gitlab/WorkItems::Statuses::Custom::Status/199',
-      name: 'Another open state',
-      category: 'to_do',
-    };
-    const lifecycle = cloneDeep(mockLifecycles[0]);
-    lifecycle.statuses.push(expectedStatus);
-    createComponent({ props: { lifecycle } });
-
-    expect(findNewStatusListbox().props('items')).toEqual([
-      {
-        ...expectedStatus,
-        text: expectedStatus.name,
-        value: expectedStatus.id,
-      },
-    ]);
-  });
-
-  it('pre-selects the first available status in the listbox', () => {
-    createComponent({ props: { statusToRemove: mockLifecycles[0].statuses[1] } }); // first closed status
-
-    expect(findNewStatusListbox().props('selected')).toBe(mockLifecycles[0].statuses[2]); // second closed status
-  });
-
-  it('updates selectedNewStatusId when a new status is selected', async () => {
-    createComponent();
-    const newSelection = mockLifecycles[0].statuses[2];
-
-    findNewStatusListbox().vm.$emit('input', newSelection.id);
-    await nextTick();
-
-    expect(findNewStatusListbox().props('value')).toBe(newSelection.id);
-  });
-
-  describe('when default status is selected', () => {
-    beforeEach(() => {
-      createComponent({ props: { statusToRemove: mockLifecycles[0].statuses[0] } });
+      expect(findCurrentStatus().text()).toBe('To do!');
     });
 
-    it('renders "new default" body text', () => {
-      expect(wrapper.text()).toContain(
-        `This status is set as the Open default. Select a new Open default to remove this status.`,
-      );
+    it('renders the new status selection label', () => {
+      expect(findNewStatusLabel().text()).toBe('New status');
     });
 
-    it('renders the new default selection label', () => {
-      expect(findNewDefaultLabel().text()).toBe('Open default');
+    it('filters listbox items to remove statusToRemove and keep statuses from same open/closed state', () => {
+      // toDoStatus is not included since we're deleting it
+      // doneStatus and canceledStatus are not included since they are closed states, and toDoStatus is an open state
+      expect(findNewStatusListbox().props('items')).toMatchObject([triageStatus, inProgressStatus]);
     });
 
-    it('updates selectedNewDefaultId when a new default is selected', async () => {
-      const newSelection = mockLifecycles[0].statuses[2];
+    it('pre-selects the first available status in the listbox', () => {
+      expect(findNewStatusListbox().props('selected')).toEqual(triageStatus);
+    });
 
-      findNewDefaultListbox().vm.$emit('input', newSelection.id);
+    it('updates selectedNewStatusId when a new status is selected', async () => {
+      findNewStatusListbox().vm.$emit('input', inProgressStatus.id);
       await nextTick();
 
-      expect(findNewDefaultListbox().props('value')).toBe(newSelection.id);
-    });
-  });
-
-  describe('when default status is not selected', () => {
-    const statusToRemove = {
-      id: 'gid://gitlab/WorkItems::Statuses::Custom::Status/199',
-      name: 'Another open state',
-      category: 'to_do',
-    };
-
-    beforeEach(() => {
-      createComponent({ props: { statusToRemove } });
+      expect(findNewStatusListbox().props('value')).toBe(inProgressStatus.id);
     });
 
-    it('does not render "new default" body text', () => {
-      expect(wrapper.text()).not.toContain(
-        `This status is set as the Open default. Select a new Open default to remove this status.`,
-      );
+    it('emits hidden event when modal is hidden', () => {
+      findModal().vm.$emit('hidden');
+
+      expect(wrapper.emitted('hidden')).toEqual([[]]);
     });
 
-    it('does not render the new default selection label', () => {
-      expect(findNewDefaultLabel().exists()).toBe(false);
-    });
+    describe('when deleting status', () => {
+      it('sends mutation to map old status to new status', async () => {
+        findNewStatusListbox().vm.$emit('input', inProgressStatus.id);
+        findModal().vm.$emit('primary', { preventDefault: jest.fn() });
+        await waitForPromises();
 
-    it('does not render the new default listbox', () => {
-      expect(findNewDefaultListbox().exists()).toBe(false);
-    });
-  });
-
-  describe('when removing status', () => {
-    it('sends mutation to map old status to new status', async () => {
-      createComponent();
-
-      findNewStatusListbox().vm.$emit(
-        'input',
-        'gid://gitlab/WorkItems::Statuses::Custom::Status/167',
-      );
-      findModal().vm.$emit('primary', { preventDefault: jest.fn() });
-      await nextTick();
-
-      expect(lifecycleUpdateMutationHandler).toHaveBeenCalledWith({
-        input: {
-          id: 'gid://gitlab/WorkItems::Statuses::Custom::Lifecycle/37',
-          namespacePath: 'full/path',
-          statusMappings: [
-            {
-              newStatusId: 'gid://gitlab/WorkItems::Statuses::Custom::Status/167',
-              oldStatusId: 'gid://gitlab/WorkItems::Statuses::Custom::Status/165',
-            },
-          ],
-        },
-      });
-    });
-
-    it('sends mutation to update to a new default status', async () => {
-      createComponent();
-
-      findNewDefaultListbox().vm.$emit(
-        'input',
-        'gid://gitlab/WorkItems::Statuses::Custom::Status/167',
-      );
-      findModal().vm.$emit('primary', { preventDefault: jest.fn() });
-      await nextTick();
-
-      expect(lifecycleUpdateMutationHandler).toHaveBeenCalledWith({
-        input: {
-          id: 'gid://gitlab/WorkItems::Statuses::Custom::Lifecycle/37',
-          namespacePath: 'full/path',
-          defaultOpenStatusIndex: 2,
-        },
-      });
-    });
-
-    it('displays an error message if the update fails', async () => {
-      const mockErrorUpdateResponse = {
-        data: {
-          lifecycleUpdate: {
-            lifecycle: null,
-            errors: ['I am error'],
-            __typename: 'LifecycleUpdatePayload',
+        // Remove toDoStatus and map it to inProgressStatus
+        expect(lifecycleUpdateMutationHandler).toHaveBeenCalledWith({
+          input: {
+            id: 'gid://gitlab/WorkItems::Statuses::Custom::Lifecycle/9',
+            namespacePath: 'full/path',
+            statuses: [triageStatus, inProgressStatus, doneStatus, canceledStatus].map(
+              ({ id }) => ({ id }),
+            ),
+            statusMappings: [
+              {
+                newStatusId: inProgressStatus.id,
+                oldStatusId: toDoStatus.id,
+              },
+            ],
           },
-        },
-      };
-      createComponent({ updateHandler: jest.fn().mockResolvedValue(mockErrorUpdateResponse) });
-
-      findModal().vm.$emit('primary', { preventDefault: jest.fn() });
-      await waitForPromises();
-
-      expect(wrapper.findComponent(GlAlert).text()).toBe('I am error');
+        });
+        expect(wrapper.emitted('lifecycle-updated')).toEqual([[]]);
+      });
     });
+  });
+
+  describe('when status is default status and other statuses exist in same state', () => {
+    beforeEach(() => {
+      const lifecycle = {
+        id: 'gid://gitlab/WorkItems::Statuses::Custom::Lifecycle/9',
+        name: 'Lifecycle!',
+        defaultClosedStatus: doneStatus,
+        workItemTypes: [],
+        statuses: [triageStatus, toDoStatus, inProgressStatus, doneStatus, canceledStatus],
+        statusCounts: [{ status: toDoStatus, count: '10' }],
+        __typename: 'WorkItemLifecycle',
+      };
+      createComponent({ props: { lifecycle, statusToRemove: doneStatus } });
+    });
+
+    it('renders the modal with title', () => {
+      expect(findModal().props('title')).toBe('Remove status');
+    });
+
+    it('renders "new status" body text', () => {
+      expect(wrapper.text()).toContain(
+        'This status is set as the Closed default. Select a new Closed default to remove this status.',
+      );
+    });
+
+    it('renders the new status selection label', () => {
+      expect(findNewStatusLabel().text()).toBe('Closed default');
+    });
+
+    it('filters listbox items to remove statusToRemove and keep statuses from same open/closed state', () => {
+      // toDoStatus is not included since we're deleting it
+      // doneStatus and canceledStatus are not included since they are closed states, and toDoStatus is an open state
+      expect(findNewStatusListbox().props('items')).toMatchObject([canceledStatus]);
+    });
+
+    it('pre-selects the first available status in the listbox', () => {
+      expect(findNewStatusListbox().props('selected')).toEqual(canceledStatus);
+    });
+
+    it('updates selectedNewStatusId when a new status is selected', async () => {
+      findNewStatusListbox().vm.$emit('input', canceledStatus.id);
+      await nextTick();
+
+      expect(findNewStatusListbox().props('value')).toBe(canceledStatus.id);
+    });
+
+    it('emits hidden event when modal is hidden', () => {
+      findModal().vm.$emit('hidden');
+
+      expect(wrapper.emitted('hidden')).toEqual([[]]);
+    });
+
+    describe('when deleting status', () => {
+      it('sends mutation to update to a new default status', async () => {
+        findNewDefaultListbox().vm.$emit('input', canceledStatus.id);
+        findModal().vm.$emit('primary', { preventDefault: jest.fn() });
+        await waitForPromises();
+
+        // Remove doneStatus, map it to canceledStatus, set default closed status to canceledStatus
+        expect(lifecycleUpdateMutationHandler).toHaveBeenCalledWith({
+          input: {
+            id: 'gid://gitlab/WorkItems::Statuses::Custom::Lifecycle/9',
+            namespacePath: 'full/path',
+            defaultClosedStatusIndex: 4,
+            statuses: [triageStatus, toDoStatus, inProgressStatus, canceledStatus].map(
+              ({ id }) => ({ id }),
+            ),
+            statusMappings: [
+              {
+                newStatusId: canceledStatus.id,
+                oldStatusId: doneStatus.id,
+              },
+            ],
+          },
+        });
+        expect(wrapper.emitted('lifecycle-updated')).toEqual([[]]);
+      });
+    });
+  });
+
+  describe('when status is default status and no other statuses exist in same state', () => {
+    beforeEach(() => {
+      const lifecycle = {
+        id: 'gid://gitlab/WorkItems::Statuses::Custom::Lifecycle/9',
+        name: 'Lifecycle!',
+        defaultClosedStatus: doneStatus,
+        workItemTypes: [],
+        statuses: [toDoStatus, doneStatus],
+        statusCounts: [{ status: toDoStatus, count: '10' }],
+        __typename: 'WorkItemLifecycle',
+      };
+      createComponent({ props: { lifecycle, statusToRemove: doneStatus } });
+    });
+
+    it('renders text', () => {
+      expect(wrapper.text()).toBe(
+        'This status is set as the Closed default, and no other Closed statuses exist. Create a "Done" or "Canceled" status to remove this status.',
+      );
+    });
+  });
+
+  it('displays an error message if the update fails', async () => {
+    const mockErrorUpdateResponse = {
+      data: {
+        lifecycleUpdate: {
+          lifecycle: null,
+          errors: ['I am error'],
+          __typename: 'LifecycleUpdatePayload',
+        },
+      },
+    };
+    createComponent({ updateHandler: jest.fn().mockResolvedValue(mockErrorUpdateResponse) });
+
+    findModal().vm.$emit('primary', { preventDefault: jest.fn() });
+    await waitForPromises();
+
+    expect(wrapper.findComponent(GlAlert).text()).toBe('I am error');
+    expect(wrapper.emitted('lifecycle-updated')).toBeUndefined();
   });
 });
