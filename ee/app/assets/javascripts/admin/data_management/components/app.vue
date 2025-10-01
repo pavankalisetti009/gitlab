827f735c1c686b61e7d4d19701ab9d059422ced3
Shane Maglangit <smaglangit@gitlab.com>
2025-10-01 18:30:21 +0800

    Add initial data management page

---
+++ ee/app/assets/javascripts/admin/data_management/components/app.vue
@@ -0,0 +1,18 @@
+<script>
+import PageHeading from '~/vue_shared/components/page_heading.vue';
+
+export default {
+  name: 'AdminDataManagementApp',
+  components: {
+    PageHeading,
+  },
+};
+</script>
+
+<template>
+  <page-heading>
+    <template #heading>
+      {{ __('Data management') }}
+    </template>
+  </page-heading>
+</template>
