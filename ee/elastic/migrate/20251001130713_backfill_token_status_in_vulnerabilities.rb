f15707dce45acf4b57282e942b11c26edf27d6e1
Ugo Nnanna Okeadu <uokeadu@gitlab.com>
2025-10-03 17:41:10 +0200

    Add migration to backfill token_status in vulnerabilities
    - Reindex Vulnerabilities::Read documents to populate the
    new token_status field in ES
    
    Changelog: added
    EE: true

---
+++ ee/elastic/migrate/20251001130713_backfill_token_status_in_vulnerabilities.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class BackfillTokenStatusInVulnerabilities < Elastic::Migration
+  include ::Search::Elastic::MigrationReindexBasedOnSchemaVersion
+
+  skip_if -> { !(saas_with_es? || dedicated_with_es?) }
+
+  batch_size 10_000
+  batched!
+  throttle_delay 15.seconds
+  retry_on_failure
+
+  DOCUMENT_TYPE = Vulnerability
+  NEW_SCHEMA_VERSION = 25_37
+
+  class << self
+    def saas_with_es?
+      Gitlab::Saas.feature_available?(:advanced_search)
+    end
+
+    def dedicated_with_es?
+      Gitlab::CurrentSettings.gitlab_dedicated_instance?
+    end
+  end
+
+  delegate :saas_with_es?, :dedicated_with_es?, to: :class
+end
