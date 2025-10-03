add6821ecc5f59c64e02a89431a29bf58b8620d6
fdegier <fdegier@gitlab.com>
2025-10-03 12:50:22 +0200

    Add Fix pipeline button to pipeline details page
    Changelog: added
    EE: true

--- ee/spec/frontend/ci/pipeline_details/mock_data.js
+++ ee/spec/frontend/ci/pipeline_details/mock_data.js
@@ -2,6 +2,7 @@
 import pipelineHeaderSuccess from 'test_fixtures/graphql/pipelines/pipeline_header_success.json';
 import pipelineHeaderRunning from 'test_fixtures/graphql/pipelines/pipeline_header_running.json';
 import pipelineHeaderFinishedComputeMinutes from 'test_fixtures/graphql/pipelines/pipeline_header_compute_minutes.json';
+import pipelineHeaderFailed from 'test_fixtures/graphql/pipelines/pipeline_header_failed.json';
 
 export const pipelineHeaderMergeTrain = {
   data: {
@@ -21,4 +22,25 @@ export const mockPipelineStatusResponse = {
   },
 };
 
-export { pipelineHeaderFinishedComputeMinutes, pipelineHeaderRunning, pipelineHeaderSuccess };
+export const pipelineHeaderFailedNoPermissions = {
+  data: {
+    project: {
+      id: '1',
+      pipeline: {
+        ...pipelineHeaderFailed.data.project.pipeline,
+        userPermissions: {
+          destroyPipeline: false,
+          cancelPipeline: false,
+          updatePipeline: false,
+        },
+      },
+    },
+  },
+};
+
+export {
+  pipelineHeaderFinishedComputeMinutes,
+  pipelineHeaderRunning,
+  pipelineHeaderSuccess,
+  pipelineHeaderFailed,
+};
