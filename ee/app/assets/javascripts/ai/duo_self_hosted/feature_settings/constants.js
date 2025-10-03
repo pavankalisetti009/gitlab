088fbc9b2f05976153e19b7aca4da81aedf87ac9
Cindy Halim <chalim@gitlab.com>
2025-10-03 12:56:04 -0400

    Add disclaimer modal for GitLab managed models

--- ee/app/assets/javascripts/ai/duo_self_hosted/feature_settings/constants.js
+++ ee/app/assets/javascripts/ai/duo_self_hosted/feature_settings/constants.js
@@ -6,3 +6,6 @@ export const PROVIDERS = {
 };
 export const GITLAB_DEFAULT_MODEL = '';
 export const DUO_AGENT_PLATFORM_FEATURE = 'duo_agent_platform';
+
+export const SUPPRESS_GITLAB_MANAGED_MODELS_DISCLAIMER_MODAL_KEY =
+  'suppress_gitlab_managed_models_disclaimer_modal';
