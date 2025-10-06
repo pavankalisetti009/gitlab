839adc2c0b27a0af17d6092ade3abc5c7f3519cc
Sheldon Led <sheldonled@gitlab.com>
2025-10-06 11:33:46 +0100

    Add poolUsage type under subscriptionUsage

--- ee/spec/policies/base_policy_spec.rb
+++ ee/spec/policies/base_policy_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 
 RSpec.describe BasePolicy do
   include ExternalAuthorizationServiceHelpers
+  include AdminModeHelper
 
   let(:auditor) { build(:auditor) }
 
@@ -30,6 +31,24 @@ RSpec.describe BasePolicy do
   describe 'admin all resources' do
     it 'forbids auditors' do
       is_expected.to be_disallowed(:admin_all_resources)
+    end
+  end
+
+  describe ':read_subscription_usage', feature_category: :consumables_cost_management do
+    let(:user) { build_stubbed(:user) }
+    let(:admin) { build_stubbed(:admin) }
+
+    it { expect(described_class.new(user, nil)).to be_disallowed(:read_subscription_usage) }
+    it { expect(described_class.new(admin, nil)).to be_disallowed(:read_subscription_usage) }
+
+    context 'when in admin mode' do
+      before do
+        enable_admin_mode!(user)
+        enable_admin_mode!(admin)
+      end
+
+      it { expect(described_class.new(user, nil)).to be_disallowed(:read_subscription_usage) }
+      it { expect(described_class.new(admin, nil)).to be_allowed(:read_subscription_usage) }
     end
   end
 end
