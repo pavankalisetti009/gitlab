026a79f70c7bef35ebefa772df9d42a8b4f45126
Kiesha Herman <kherman@gitlab.com>
2025-10-05 20:03:11 -0700

    Update sign up / sign in page headers
    Changelog: changed
    EE: true

--- ee/app/helpers/ee/appearances_helper.rb
+++ ee/app/helpers/ee/appearances_helper.rb
@@ -8,5 +8,27 @@ module EE
     def default_brand_title
       _('GitLab Enterprise Edition')
     end
+
+    override :brand_title
+    def brand_title
+      return super unless ::Onboarding.enabled?
+
+      case request.path
+      when new_user_session_path
+        sign_in_page_title
+      when new_user_registration_path
+        sign_up_page_title
+      else
+        super
+      end
+    end
+
+    def sign_in_page_title
+      _("Sign in to GitLab")
+    end
+
+    def sign_up_page_title
+      _("Get started with GitLab")
+    end
   end
 end
