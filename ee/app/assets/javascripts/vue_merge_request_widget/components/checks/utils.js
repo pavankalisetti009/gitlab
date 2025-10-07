e0843d7659c7209dd268f24f9472eca0b03ff553
Artur Fedorov <afedorov@gitlab.com>
2025-10-07 13:35:15 +0000

    Add reading of bypass statuses to MR widget
    MR widget can read bypass statuses and
    render bypass button
    
    Changelog: added
    EE: true

--- ee/app/assets/javascripts/vue_merge_request_widget/components/checks/utils.js
+++ ee/app/assets/javascripts/vue_merge_request_widget/components/checks/utils.js
@@ -1,8 +1,8 @@
 import { EXCEPTION_MODE, WARN_MODE } from './constants';
 
-export const getSelectedModeOption = ({ securityPoliciesPath = '', allowBypass = false }) => {
-  if (securityPoliciesPath && allowBypass) return '';
-  if (securityPoliciesPath) return WARN_MODE;
+export const getSelectedModeOption = ({ hasBypassPolicies = false, allowBypass = false }) => {
+  if (hasBypassPolicies && allowBypass) return '';
+  if (hasBypassPolicies) return WARN_MODE;
   if (allowBypass) return EXCEPTION_MODE;
   return '';
 };
