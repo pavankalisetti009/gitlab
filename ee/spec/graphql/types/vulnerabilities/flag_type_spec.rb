fa7cde50bd39a9f8e2e2b3c73c6f7a1945446575
SAM FIGUEROA <sfigueroa@gitlab.com>
2025-10-07 07:19:02 +0200

    Add graphql for vulnerability flags
    - Adding to be able to show FP detections on vulnerabilities
    
    - Refs:
      https://gitlab.com/gitlab-org/gitlab/-/work_items/567392
      https://gitlab.com/gitlab-org/gitlab/-/merge_requests/205994\#note_2776742874
    
    Changelog: added
    EE: true
    Experimental: true

---
+++ ee/spec/graphql/types/vulnerabilities/flag_type_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ::Types::Vulnerabilities::FlagType, feature_category: :vulnerability_management do
+  include GraphqlHelpers
+
+  let_it_be(:fields) { %i[confidence_score created_at description id origin status updated_at] }
+
+  it { expect(described_class).to have_graphql_fields(*fields) }
+
+  it { expect(described_class).to require_graphql_authorizations(:read_vulnerability) }
+
+  describe 'field types' do
+    specify { expect(described_class.fields['confidenceScore']).to have_graphql_type(GraphQL::Types::Float) }
+    specify { expect(described_class.fields['createdAt']).to have_graphql_type(Types::TimeType, null: false) }
+    specify { expect(described_class.fields['description']).to have_graphql_type(GraphQL::Types::String) }
+    specify { expect(described_class.fields['id']).to have_graphql_type(GraphQL::Types::ID, null: false) }
+    specify { expect(described_class.fields['origin']).to have_graphql_type(GraphQL::Types::String) }
+
+    specify do
+      expect(described_class.fields['status'])
+        .to have_graphql_type(Types::Vulnerabilities::Flags::FalsePositiveDetectionStatusEnum)
+    end
+
+    specify { expect(described_class.fields['updatedAt']).to have_graphql_type(Types::TimeType, null: false) }
+  end
+end
