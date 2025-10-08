0d505f66dbbb6e1dbab3d59ad9e78bee12813006
Alexander Turinske <aturinske@gitlab.com>
2025-10-07 18:32:22 -0600

    Update vuln detail page with security policy info
    - create new component for security policy bypass information
    - conditionally show it using a feature flag
    - update tests
    
    Changelog: added
    
    EE: true

---
+++ ee/app/assets/javascripts/vulnerabilities/components/security_policy_bypass_description.vue
@@ -0,0 +1,91 @@
+<script>
+import { GlIcon, GlLink, GlSprintf } from '@gitlab/ui';
+import { sprintf, s__ } from '~/locale';
+import TimeAgoTooltip from '~/vue_shared/components/time_ago_tooltip.vue';
+
+export default {
+  components: {
+    GlIcon,
+    GlLink,
+    GlSprintf,
+    TimeAgoTooltip,
+  },
+  props: {
+    bypass: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    bypassReasons() {
+      return Array.isArray(this.bypass.dismissalTypes) ? this.bypass.dismissalTypes.join(', ') : '';
+    },
+    bypassReasonsText() {
+      return sprintf(s__('VulnerabilityManagement|Reason category: %{reasons}'), {
+        reasons: this.bypassReasons,
+      });
+    },
+    bypassMRText() {
+      return s__('VulnerabilityManagement|Bypassed by %{user} in merge request %{mr}');
+    },
+    comment() {
+      return this.bypass.comment;
+    },
+    commentText() {
+      return sprintf(s__('VulnerabilityManagement|Reason detail: %{comment}'), {
+        comment: this.comment,
+      });
+    },
+    showUserAndMR() {
+      const { mergeRequestPath, mergeRequestReference, userName, userPath } = this.bypass;
+      return mergeRequestPath && mergeRequestReference && userName && userPath;
+    },
+    statusText() {
+      return s__('VulnerabilityManagement|%{statusStart}Bypassed%{statusEnd} · %{timeago}');
+    },
+    time() {
+      return this.bypass.createdAt;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div class="gl-mt-6">
+      <div
+        class="gl-float-left !gl-m-0 -gl-mt-1 gl-ml-2 gl-flex gl-h-6 gl-w-6 gl-items-center gl-justify-center gl-rounded-full gl-bg-strong gl-text-subtle"
+      >
+        <gl-icon name="warning" class="circle-icon-container" variant="subtle" />
+      </div>
+      <div class="gl-ml-5">
+        <gl-sprintf v-if="time" :message="statusText">
+          <template #status="{ content }">
+            <span class="gl-ml-5 gl-font-bold" data-testid="status">{{ content }}</span>
+          </template>
+          <template #timeago>
+            <time-ago-tooltip ref="timeAgo" :time="time" />
+          </template>
+        </gl-sprintf>
+        <ul class="gl-ml-5">
+          <li>{{ s__('SecurityOrchestration|Security policy violated') }}</li>
+          <li v-if="bypassReasons">{{ bypassReasonsText }}</li>
+          <li v-if="comment">{{ commentText }}</li>
+          <li v-if="showUserAndMR">
+            <gl-sprintf :message="bypassMRText">
+              <template #user>
+                <gl-link target="_blank" :href="bypass.userPath">{{ bypass.userName }}</gl-link>
+              </template>
+              <template #mr>
+                <gl-link target="_blank" :href="bypass.mergeRequestPath">{{
+                  bypass.mergeRequestReference
+                }}</gl-link>
+              </template>
+            </gl-sprintf>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
