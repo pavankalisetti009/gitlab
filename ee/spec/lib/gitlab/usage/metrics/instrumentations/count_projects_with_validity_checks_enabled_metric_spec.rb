51c995226c637aa02ed8e7a59d169760d616fe5e
Radu Birsan <rbirsan@gitlab.com>
2025-10-09 06:59:45 +0000

    Tracks the number of tokens sent to UpdateTokenStatusService
    Tracks the number of tokens being parsed by UpdateTokenStatusService
    and updates test files.
    
    Changelog: added
    EE: true

--- ee/spec/lib/gitlab/usage/metrics/instrumentations/count_projects_with_validity_checks_enabled_metric_spec.rb
+++ ee/spec/lib/gitlab/usage/metrics/instrumentations/count_projects_with_validity_checks_enabled_metric_spec.rb
@@ -3,13 +3,17 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Usage::Metrics::Instrumentations::CountProjectsWithValidityChecksEnabledMetric, feature_category: :service_ping do
-  let(:expected_value) { 3 }
+  it_behaves_like 'a correct instrumented metric value and query', { time_frame: 'all', data_source: 'database' } do
+    let(:expected_value) { 3 }
+    let(:expected_query) do
+      "SELECT COUNT(\"project_security_settings\".\"project_id\") FROM \"project_security_settings\" " \
+        "WHERE \"project_security_settings\".\"validity_checks_enabled\" = TRUE"
+    end
 
-  before do
-    projects = create_list(:project, 3)
-    projects.each { |project| project.security_setting.update!(validity_checks_enabled: true) }
-    create(:project)
+    before do
+      projects = create_list(:project, 3)
+      projects.each { |project| project.security_setting.update!(validity_checks_enabled: true) }
+      create(:project)
+    end
   end
-
-  it_behaves_like 'a correct instrumented metric value', { time_frame: 'all', data_source: 'database' }
 end
