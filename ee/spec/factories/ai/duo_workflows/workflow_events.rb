12c60603de4413ddc6b9c70ee624bbcea34fecd6
Alper Akgun <aakgun@gitlab.com>
2025-10-13 20:42:13 +0300

    Update unique index i_duo_workflows_events_on_correlation_id for cells
    Changelog: changed

--- ee/spec/factories/ai/duo_workflows/workflow_events.rb
+++ ee/spec/factories/ai/duo_workflows/workflow_events.rb
@@ -3,7 +3,6 @@
 FactoryBot.define do
   factory :duo_workflows_event, class: 'Ai::DuoWorkflows::Event' do
     workflow { association(:duo_workflows_workflow) }
-    project { association(:project) }
     event_type { 'pause' }
     event_status { 'queued' }
     correlation_id_value { nil }
