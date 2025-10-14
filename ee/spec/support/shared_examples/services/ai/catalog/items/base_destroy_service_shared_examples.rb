b075b159799610b0afa8a3ca3778ac99458dffd9
Luke Duncalfe <lduncalfe@eml.cc>
2025-10-14 17:26:48 +1300

    Delete project's `ItemConsumer` with `Item`
    Always delete the `ItemConsumer` of the project that owns the `Item`.
    We do this before the `Item` deletion happens, so our soft-deletion
    logic will hard delete if only that project had added the item.
    
    Part of:
    https://gitlab.com/gitlab-org/gitlab/-/issues/575127

--- ee/spec/support/shared_examples/services/ai/catalog/items/base_destroy_service_shared_examples.rb
+++ ee/spec/support/shared_examples/services/ai/catalog/items/base_destroy_service_shared_examples.rb
@@ -41,25 +41,27 @@ RSpec.shared_examples Ai::Catalog::Items::BaseDestroyService do
       end
 
       context 'when item exists' do
-        it 'destroys the item successfully' do
-          expect { execute_service }.to change { Ai::Catalog::Item.count }.by(-1)
-        end
+        shared_examples 'hard deletes the item' do
+          it 'destroys the item successfully' do
+            expect { execute_service }.to change { Ai::Catalog::Item.count }.by(-1)
+          end
 
-        it 'destroys item versions' do
-          expect { execute_service }.to change { Ai::Catalog::ItemVersion.count }.by(-1)
-        end
+          it 'destroys item versions' do
+            expect { execute_service }.to change { Ai::Catalog::ItemVersion.count }.by(-1)
+          end
 
-        it 'triggers track_ai_item_events', :clean_gitlab_redis_shared_state do
-          expect { execute_service }
-            .to trigger_internal_events('delete_ai_catalog_item')
-            .with(user: user, project: project, additional_properties: { label: item.item_type })
-            .and increment_usage_metrics('counts.count_total_delete_ai_catalog_item')
-        end
+          it 'triggers delete_ai_catalog_item', :clean_gitlab_redis_shared_state do
+            expect { execute_service }
+              .to trigger_internal_events('delete_ai_catalog_item')
+              .with(user: user, project: project, additional_properties: { label: item.item_type })
+              .and increment_usage_metrics('counts.count_total_delete_ai_catalog_item')
+          end
 
-        it 'returns success response' do
-          result = execute_service
+          it 'returns success response' do
+            result = execute_service
 
-          expect(result.success?).to be(true)
+            expect(result.success?).to be(true)
+          end
         end
 
         shared_examples 'soft deletes the item' do
@@ -75,7 +77,7 @@ RSpec.shared_examples Ai::Catalog::Items::BaseDestroyService do
             expect { execute_service }.not_to change { Ai::Catalog::ItemVersion.count }
           end
 
-          it 'does not trigger track_ai_item_events', :clean_gitlab_redis_shared_state do
+          it 'triggers delete_ai_catalog_item', :clean_gitlab_redis_shared_state do
             expect { execute_service }
               .to trigger_internal_events('delete_ai_catalog_item')
               .with(user: user, project: project, additional_properties: { label: item.item_type })
@@ -88,12 +90,59 @@ RSpec.shared_examples Ai::Catalog::Items::BaseDestroyService do
           end
         end
 
-        context 'when item is already being used (has consumers)' do
+        it_behaves_like 'hard deletes the item'
+
+        context 'when item is only being used by the project' do
           before do
             create(:ai_catalog_item_consumer, item: item, project: project)
           end
 
+          it_behaves_like 'hard deletes the item'
+        end
+
+        context 'when item is being used by other projects' do
+          before do
+            create(:ai_catalog_item_consumer, item: item, project: create(:project))
+          end
+
           it_behaves_like 'soft deletes the item'
+        end
+
+        context 'when item is being used by both the project and another project' do
+          let_it_be(:other_project) { create(:project) }
+
+          before do
+            create(:ai_catalog_item_consumer, item: item, project: other_project)
+            create(:ai_catalog_item_consumer, item: item, project: project)
+          end
+
+          it "deletes just the project's consumer record, and soft deletes the item" do
+            expect { execute_service }.to change { item.deleted_at }.from(nil)
+
+            expect(other_project.configured_ai_catalog_items.size).to eq(1)
+            expect(project.configured_ai_catalog_items).to be_empty
+          end
+        end
+
+        context 'when an error happens when deleting the consumer' do
+          before do
+            create(:ai_catalog_item_consumer, item: item, project: project)
+
+            allow_next_instance_of(Ai::Catalog::ItemConsumers::DestroyService) do |service|
+              allow(service).to receive(:execute).and_return(ServiceResponse.error(message: 'Failure'))
+            end
+          end
+
+          it 'does not destroy the item' do
+            expect { execute_service }.not_to change { Ai::Catalog::Item.count }
+          end
+
+          it 'returns error response' do
+            result = execute_service
+
+            expect(result).to be_error
+            expect(result.errors).to contain_exactly('Failure')
+          end
         end
 
         context 'when item is a dependency of other items (has item_version_dependency)' do
