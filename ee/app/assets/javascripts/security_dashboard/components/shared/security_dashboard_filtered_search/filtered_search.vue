39cdddfe9f3383751c3be3bda3dde4eba7f03432
Lorenz van Herwaarden <lvanherwaarden@gitlab.com>
2025-10-15 22:21:15 +0200

    Sync url parameters with global filters on new security dashboard
    This MR implements URL parameter synchronization for the global filters
    on the new security dashboard:
    
    1. Set up initial filter values on first load
    2. Push new url state based on token changes

--- ee/app/assets/javascripts/security_dashboard/components/shared/security_dashboard_filtered_search/filtered_search.vue
+++ ee/app/assets/javascripts/security_dashboard/components/shared/security_dashboard_filtered_search/filtered_search.vue
@@ -1,4 +1,5 @@
 <script>
+import { nextTick } from 'vue';
 import { GlFilteredSearch } from '@gitlab/ui';
 import { isEqual } from 'lodash';
 import { ALL_ID } from 'ee/security_dashboard/components/shared/filters/constants';
@@ -19,48 +20,91 @@ export default {
       value: [],
     };
   },
+  created() {
+    const params = new URLSearchParams(window.location.search);
+
+    const { initialValue, newFilters } = this.tokens.reduce(
+      (acc, token) => {
+        const paramValue = params.get(token.type);
+        const data = paramValue?.split(',').filter(Boolean);
+
+        if (data?.length > 0) {
+          acc.newFilters[token.type] = data;
+          acc.initialValue.push({
+            type: token.type,
+            value: {
+              data,
+              operator: token.operators[0]?.value,
+            },
+          });
+        }
+
+        return acc;
+      },
+      { initialValue: [], newFilters: {} },
+    );
+
+    this.value = initialValue;
+    this.updateFilters(newFilters);
+  },
   methods: {
-    handleInput(inputValues) {
-      const isFilterToken = ({ type }) => this.tokens.some((token) => token.type === type);
-      const filterTokens = inputValues.filter(isFilterToken);
-
-      if (filterTokens.length === 0) {
-        this.clear();
-        return;
-      }
-
-      // When the user clicks on the selected value (placeholder), the filtered search
-      // empties the selection handler and sends a null value
-      // We don't want to cause a new API call when this happens. Instead
-      // we want to wait until user either destroys the token or selects a new token.
-      if (filterTokens.some(({ value }) => !value.data)) {
-        return;
-      }
-
-      const newFilters = {};
-      filterTokens.forEach(({ type, value }) => {
-        newFilters[type] = value.data.filter((v) => v !== ALL_ID);
-      });
-
-      if (isEqual(this.filters, newFilters)) {
-        return;
-      }
+    getTokenValue(type) {
+      const token = this.value.find((t) => t.type === type);
+      return token?.value?.data?.filter((v) => v !== ALL_ID);
+    },
+    updateFilters(newFilters) {
+      if (isEqual(newFilters, this.filters)) return;
 
       this.filters = newFilters;
-      this.$emit('filters-changed', newFilters);
+      this.$emit('filters-changed', this.filters);
+    },
+    updateUrlParams() {
+      const params = this.tokens.reduce((acc, { type }) => {
+        const filterValue = this.filters[type];
+        if (filterValue?.length > 0) {
+          acc.set(type, filterValue.join(','));
+        } else {
+          acc.delete(type);
+        }
+        return acc;
+      }, new URLSearchParams(window.location.search));
+
+      const newUrl = params.toString()
+        ? `${window.location.pathname}?${params.toString()}`
+        : window.location.pathname;
+      window.history.pushState({}, '', newUrl);
+    },
+    async handleTokenComplete({ type }) {
+      // Need to wait for `this.value` to have been updated
+      await nextTick();
+
+      const value = this.getTokenValue(type);
+      if (!value) return;
+
+      const newFilters = { ...this.filters, [type]: value };
+      this.updateFilters(newFilters);
+      this.updateUrlParams();
+    },
+    handleTokenDestroy({ type }) {
+      const { [type]: removed, ...rest } = this.filters;
+      const newFilters = rest;
+      this.updateFilters(newFilters);
+      this.updateUrlParams();
     },
     clear() {
-      this.$emit('filters-changed', {});
+      this.updateFilters({});
+      this.updateUrlParams();
     },
   },
 };
 </script>
 <template>
   <gl-filtered-search
+    v-model="value"
     :placeholder="s__('SecurityReports|Filter results...')"
     :available-tokens="tokens"
-    :value="value"
-    @input="handleInput"
+    @token-complete="handleTokenComplete"
+    @token-destroy="handleTokenDestroy"
     @clear="clear"
   />
 </template>
