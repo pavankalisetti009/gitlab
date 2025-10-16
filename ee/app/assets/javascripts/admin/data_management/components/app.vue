96b66b706e933695ca0a7a79db094f4cc5d237ee
Shane Maglangit <smaglangit@gitlab.com>
2025-10-16 21:57:47 +0800

    Support switching models on data management page

--- ee/app/assets/javascripts/admin/data_management/components/app.vue
+++ ee/app/assets/javascripts/admin/data_management/components/app.vue
@@ -2,7 +2,13 @@
 import GeoListTopBar from 'ee/geo_shared/list/components/geo_list_top_bar.vue';
 import GeoList from 'ee/geo_shared/list/components/geo_list.vue';
 import { sprintf, s__ } from '~/locale';
-import { DEFAULT_SORT, GEO_TROUBLESHOOTING_LINK } from 'ee/admin/data_management/constants';
+import {
+  DEFAULT_SORT,
+  GEO_TROUBLESHOOTING_LINK,
+  TOKEN_TYPES,
+} from 'ee/admin/data_management/constants';
+import { processFilters } from 'ee/admin/data_management/filters';
+import { setUrlParams, visitUrl } from '~/lib/utils/url_utility';
 
 export default {
   name: 'AdminDataManagementApp',
@@ -29,6 +35,15 @@ export default {
       };
     },
   },
+  methods: {
+    handleListboxChange(value) {
+      const filters = [{ type: TOKEN_TYPES.MODEL, value }];
+
+      const { query, url } = processFilters(filters);
+
+      visitUrl(setUrlParams(query, url.href, true));
+    },
+  },
   defaultSort: DEFAULT_SORT,
 };
 </script>
@@ -43,6 +58,7 @@ export default {
       :filtered-search-option-label="__('Search by ID')"
       :active-listbox-item="modelClass.name"
       :active-sort="$options.defaultSort"
+      @listboxChange="handleListboxChange"
     />
     <geo-list :is-loading="false" :has-items="false" :empty-state="emptyState" />
   </div>
