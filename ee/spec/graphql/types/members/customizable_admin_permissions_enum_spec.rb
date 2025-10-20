c34872c7838ca0927555079a42956e967f4683c1
treagitlab <trea@gitlab.com>
2025-10-20 10:04:50 -0700

    Quarantine flaky group::authorization specs

--- ee/spec/graphql/types/members/customizable_admin_permissions_enum_spec.rb
+++ ee/spec/graphql/types/members/customizable_admin_permissions_enum_spec.rb
@@ -2,6 +2,6 @@
 
 require 'spec_helper'
 
-RSpec.describe Types::Members::CustomizableAdminPermissionsEnum, feature_category: :permissions do
+RSpec.describe Types::Members::CustomizableAdminPermissionsEnum, feature_category: :permissions, quarantine: 'https://gitlab.com/gitlab-org/gitlab/-/issues/552228' do
   it_behaves_like 'graphql customizable permission'
 end
