cd2a2a7d46980d37928a0843528d08ac3c64b45d
Marc Saleiko <msaleiko@gitlab.com>
2025-10-31 10:21:07 +0100

    Raise errors in fixed items model methods
    Raise errors if the passed attribute doesn't
    exist for the model class like
    Active Records handles it.
    
    This affects the methods .where and .find_by
    
    Also raise an error when using find and no item
    was found with the given id.
    
    This is done to reach parity with how Active Records
    handles these situations.

--- ee/app/graphql/types/work_items/widgets/status_filter_input_type.rb
+++ ee/app/graphql/types/work_items/widgets/status_filter_input_type.rb
@@ -12,11 +12,9 @@ module Types
           prepare: ->(global_id, _) {
             return if global_id.nil?
 
-            # ::WorkItems::Statuses::SystemDefined::Status is a FixedItemsModel and not AR
-            # so we need to manually resolve the global ID to the model instance.
-            # In the future we'll also support custom statuses through this interface
-            status = global_id.model_class.find(global_id.model_id.to_i)
-            raise GraphQL::ExecutionError, "System-defined status doesn't exist." if status.nil?
+            status = GitlabSchema.find_by_gid(global_id)
+
+            raise GraphQL::ExecutionError, "Status doesn't exist." if status.nil?
 
             status
           }
