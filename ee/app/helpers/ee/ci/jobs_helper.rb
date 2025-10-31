48b4af9913c52c14f3c277da1c0e304030c9b8d4
Minahil Nichols <minahilnichols@gitlab.com>
2025-10-30 20:33:43 -0400

    Remove invalid link when purchasing minutes from Job page

--- ee/app/helpers/ee/ci/jobs_helper.rb
+++ ee/app/helpers/ee/ci/jobs_helper.rb
@@ -7,10 +7,7 @@ module EE
 
       override :jobs_data
       def jobs_data(project, build)
-        super.merge({
-          "subscriptions_more_minutes_url" => ::Gitlab::Routing.url_helpers.subscription_portal_more_minutes_url,
-          "duo_features_enabled" => project.duo_features_enabled.to_s
-        })
+        super.merge({ "duo_features_enabled" => project.duo_features_enabled.to_s })
       end
     end
   end
