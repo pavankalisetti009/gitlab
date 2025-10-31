18af7f2d92ab8c62f091fb687724d81846538da6
Duo Developer <service_account_74bf35d22fa54b6a31fff9335be17264@noreply.gitlab.com>
2025-10-31 21:36:28 +0000

    Duo Workflow: Add spec coverage for scopes
    This adds tests for the scopes added in Geo::VerificationStateDefinition
    
    EE: true

--- ee/spec/models/concerns/geo/verification_state_definition_spec.rb
+++ ee/spec/models/concerns/geo/verification_state_definition_spec.rb
@@ -22,6 +22,10 @@ RSpec.describe Geo::VerificationStateDefinition, feature_category: :geo_replicat
     end
   end
 
+  let_it_be(:record1) { dummy_model_class.create! }
+  let_it_be(:record2) { dummy_model_class.create! }
+  let_it_be(:record3) { dummy_model_class.create! }
+
   let(:dummy_model) { dummy_model_class.new }
 
   describe '#verification_state_name_no_prefix' do
@@ -39,6 +43,143 @@ RSpec.describe Geo::VerificationStateDefinition, feature_category: :geo_replicat
 
         expect(dummy_model.verification_state_name_no_prefix).to eq(expected_result)
       end
+    end
+  end
+
+  describe '.after_cursor' do
+    context 'when cursor is nil' do
+      it 'returns all records' do
+        result = dummy_model_class.after_cursor(nil)
+
+        expect(result).to match_array([record1, record2, record3])
+      end
+    end
+
+    context 'when cursor is provided' do
+      it 'returns records with primary key greater than cursor' do
+        result = dummy_model_class.after_cursor(record1.id)
+
+        expect(result).to match_array([record2, record3])
+      end
+
+      it 'returns empty relation when cursor is greater than all records' do
+        max_id = dummy_model_class.maximum(:id)
+        result = dummy_model_class.after_cursor(max_id + 1)
+
+        expect(result).to be_empty
+      end
+
+      it 'returns all records when cursor is less than minimum id' do
+        min_id = dummy_model_class.minimum(:id)
+        result = dummy_model_class.after_cursor(min_id - 1)
+
+        expect(result).to match_array([record1, record2, record3])
+      end
+
+      it 'handles various cursor types' do
+        # String that looks like a number
+        expect { dummy_model_class.after_cursor('123') }.not_to raise_error
+
+        # Boolean values
+        expect { dummy_model_class.after_cursor(true) }.not_to raise_error
+        expect { dummy_model_class.after_cursor(false) }.not_to raise_error
+
+        # Float values
+        expect { dummy_model_class.after_cursor(123.45) }.not_to raise_error
+      end
+    end
+
+    context 'when cursor is zero' do
+      it 'returns all records' do
+        result = dummy_model_class.after_cursor(0)
+
+        expect(result).to match_array([record1, record2, record3])
+      end
+    end
+
+    it 'is chainable with other scopes' do
+      result = dummy_model_class.after_cursor(record1.id).where(id: record2.id)
+
+      expect(result).to contain_exactly(record2)
+    end
+
+    it 'is chainable with verification_state_not_pending scope' do
+      record2.update!(verification_state: ::Geo::VerificationState::VERIFICATION_STATE_VALUES[:verification_started])
+
+      result = dummy_model_class.after_cursor(record1.id).verification_state_not_pending
+
+      expect(result).to contain_exactly(record2)
+    end
+  end
+
+  describe '.keyset_order' do
+    it 'applies keyset ordering based on provided attributes' do
+      order_attributes = [
+        Gitlab::Pagination::Keyset::ColumnOrderDefinition.new(
+          attribute_name: 'id',
+          order_expression: dummy_model_class.arel_table[:id].asc
+        )
+      ]
+
+      result = dummy_model_class.keyset_order(order_attributes)
+
+      expect(result.to_a).to eq([record1, record2, record3])
+    end
+
+    it 'applies descending keyset ordering' do
+      order_attributes = [
+        Gitlab::Pagination::Keyset::ColumnOrderDefinition.new(
+          attribute_name: 'id',
+          order_expression: dummy_model_class.arel_table[:id].desc
+        )
+      ]
+
+      result = dummy_model_class.keyset_order(order_attributes)
+
+      expect(result.to_a).to eq([record3, record2, record1])
+    end
+
+    it 'is chainable with other scopes' do
+      order_attributes = [
+        Gitlab::Pagination::Keyset::ColumnOrderDefinition.new(
+          attribute_name: 'id',
+          order_expression: dummy_model_class.arel_table[:id].asc
+        )
+      ]
+
+      result = dummy_model_class.where(id: [record1.id, record3.id]).keyset_order(order_attributes)
+
+      expect(result.to_a).to eq([record1, record3])
+    end
+
+    it 'handles empty order attributes gracefully' do
+      result = dummy_model_class.keyset_order([])
+
+      expect(result.to_sql).not_to include('ORDER BY')
+      expect(result.to_a.size).to eq(3)
+    end
+  end
+
+  describe '.verification_state_not_pending' do
+    let_it_be(:success_record) { dummy_model_class.create!(verification_state: 2, verification_checksum: 'abc') }
+    let_it_be(:failed_record) { dummy_model_class.create!(verification_state: 3, verification_failure: 'error') }
+
+    it 'excludes records with pending verification state' do
+      result = dummy_model_class.verification_state_not_pending
+
+      expect(result).not_to include(record1, record2, record3)
+    end
+
+    it 'returns records with non pending state' do
+      result = dummy_model_class.verification_state_not_pending
+
+      expect(result).to match_array([success_record, failed_record])
+    end
+
+    it 'is chainable with other scopes' do
+      result = dummy_model_class.verification_state_not_pending.where(id: failed_record.id)
+
+      expect(result).to contain_exactly(failed_record)
     end
   end
 
