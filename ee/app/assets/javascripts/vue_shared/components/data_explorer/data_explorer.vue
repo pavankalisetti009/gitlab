dda1f20b9b1a42f9ef7e2610368e8f954c8d6cc9
Alex Pennells <apennells@gitlab.com>
2025-11-03 11:51:30 -0500

    Implement data explorer preview panel
    Adds the GLQL resolver to the Data explorer page so that
    you can now run GLQL queries on the page and see the rendered output.

--- ee/app/assets/javascripts/vue_shared/components/data_explorer/data_explorer.vue
+++ ee/app/assets/javascripts/vue_shared/components/data_explorer/data_explorer.vue
@@ -2,28 +2,60 @@
 // This component is still under active development. Updates on the current progress
 // can be found in https://gitlab.com/groups/gitlab-org/-/epics/19359
 import EMPTY_CHART_SVG from '@gitlab/svgs/dist/illustrations/empty-state/empty-chart-md.svg?url';
-import { GlFormTextarea, GlButton, GlCard, GlEmptyState, GlLink } from '@gitlab/ui';
+import {
+  GlFormTextarea,
+  GlButton,
+  GlDashboardPanel,
+  GlEmptyState,
+  GlLink,
+  GlAlert,
+} from '@gitlab/ui';
 import ModalCopyButton from '~/vue_shared/components/modal_copy_button.vue';
 import { helpPagePath } from '~/helpers/help_page_helper';
+import GlqlResolver from '~/glql/components/common/resolver.vue';
 
 export default {
   name: 'DataExplorer',
   components: {
     GlFormTextarea,
+    GlAlert,
     GlButton,
-    GlCard,
+    GlDashboardPanel,
     GlEmptyState,
     GlLink,
     ModalCopyButton,
+    GlqlResolver,
   },
   glqlDocsLink: helpPagePath('user/glql/_index'),
   EMPTY_CHART_SVG,
   data() {
-    return { query: '' };
+    return {
+      query: '',
+      submittedQuery: '',
+      retryCount: 0,
+      previewTitle: '',
+      previewError: undefined,
+    };
   },
   computed: {
     hasQueryText() {
       return this.query.trim() !== '';
+    },
+  },
+  methods: {
+    handleRunQuery() {
+      this.previewError = undefined;
+
+      if (this.submittedQuery === this.query) {
+        this.retryCount += 1;
+      } else {
+        this.retryCount = 0;
+        this.submittedQuery = this.query;
+      }
+    },
+    handleResolverChange({ error, config: { title = '' } = {} }) {
+      this.previewTitle = title;
+      this.previewError = error;
     },
   },
 };
@@ -39,6 +71,7 @@ export default {
         :no-resize="false"
         rows="16"
         textarea-classes="!gl-p-6 !gl-font-monospace"
+        autofocus
       />
       <modal-copy-button
         v-if="hasQueryText"
@@ -53,18 +86,29 @@ export default {
     </div>
 
     <div>
-      <gl-button variant="confirm" icon="play" :disabled="!hasQueryText">{{
+      <gl-button variant="confirm" icon="play" :disabled="!hasQueryText" @click="handleRunQuery">{{
         s__('DataExplorer|Run query')
       }}</gl-button>
     </div>
 
-    <gl-card>
-      <!-- TODO: Add resolver.vue (https://gitlab.com/gitlab-org/gitlab/-/issues/570014) -->
-      <gl-empty-state
-        :title="s__('DataExplorer|Preview not available')"
-        :description="s__('DataExplorer|Start by typing a GLQL query.')"
-        :svg-path="$options.EMPTY_CHART_SVG"
-      />
-    </gl-card>
+    <gl-dashboard-panel :title="previewTitle">
+      <template #body>
+        <gl-alert v-if="previewError" variant="danger" :dismissible="false">{{
+          previewError.message
+        }}</gl-alert>
+        <glql-resolver
+          v-else-if="submittedQuery"
+          :key="retryCount"
+          :glql-query="submittedQuery"
+          @change="handleResolverChange"
+        />
+        <gl-empty-state
+          v-else
+          :title="s__('DataExplorer|Preview not available')"
+          :description="s__('DataExplorer|Start by typing a GLQL query.')"
+          :svg-path="$options.EMPTY_CHART_SVG"
+        />
+      </template>
+    </gl-dashboard-panel>
   </div>
 </template>
--- ee/spec/frontend/vue_shared/components/data_explorer/data_explorer_spec.js
+++ ee/spec/frontend/vue_shared/components/data_explorer/data_explorer_spec.js
@@ -1,7 +1,15 @@
 import { shallowMount } from '@vue/test-utils';
-import { GlFormTextarea, GlLink, GlButton, GlEmptyState } from '@gitlab/ui';
+import {
+  GlFormTextarea,
+  GlLink,
+  GlButton,
+  GlEmptyState,
+  GlDashboardPanel,
+  GlAlert,
+} from '@gitlab/ui';
 import DataExplorer from 'ee/vue_shared/components/data_explorer/data_explorer.vue';
 import ModalCopyButton from '~/vue_shared/components/modal_copy_button.vue';
+import GlqlResolver from '~/glql/components/common/resolver.vue';
 
 describe('Data Explorer', () => {
   let wrapper;
@@ -11,6 +19,10 @@ describe('Data Explorer', () => {
   };
 
   const getTextArea = () => wrapper.findComponent(GlFormTextarea);
+  const getSubmitBtn = () => wrapper.findComponent(GlButton);
+  const getResolver = () => wrapper.findComponent(GlqlResolver);
+  const getErrorAlert = () => wrapper.findComponent(GlAlert);
+  const getPanel = () => wrapper.findComponent(GlDashboardPanel);
 
   describe('when rendered', () => {
     beforeEach(() => {
@@ -32,9 +44,12 @@ describe('Data Explorer', () => {
     });
 
     it('renders a disabled `Run query` button', () => {
-      const button = wrapper.findComponent(GlButton);
-      expect(button.text()).toBe('Run query');
-      expect(button.attributes('disabled')).toBeDefined();
+      expect(getSubmitBtn().text()).toBe('Run query');
+      expect(getSubmitBtn().attributes('disabled')).toBeDefined();
+    });
+
+    it('renders a dashboard panel with no title', () => {
+      expect(getPanel().props('title')).toBe('');
     });
 
     it('renders an empty state for the query preview', () => {
@@ -64,6 +79,57 @@ describe('Data Explorer', () => {
         const button = wrapper.findComponent(GlButton);
         expect(button.text()).toBe('Run query');
         expect(button.attributes('disabled')).toBeUndefined();
+      });
+
+      it('does not render the GLQL resolver', () => {
+        expect(getResolver().exists()).toBe(false);
+      });
+
+      describe('when query is submitted', () => {
+        beforeEach(() => {
+          return getSubmitBtn().vm.$emit('click');
+        });
+
+        it('submits the inputted query to the resolver', () => {
+          expect(getResolver().props('glqlQuery')).toBe(query);
+        });
+
+        describe('when resolver returns a title', () => {
+          const title = 'MATHEMATICAL!!!';
+          beforeEach(() => {
+            getResolver().vm.$emit('change', { config: { title } });
+          });
+
+          it('uses the title for the dashboard panel', () => {
+            expect(getPanel().props('title')).toBe(title);
+          });
+        });
+
+        describe('when resolver throws an error', () => {
+          const message = 'oh glob';
+
+          beforeEach(() => {
+            getResolver().vm.$emit('change', { error: new Error(message) });
+          });
+
+          it('does not render the GLQL resolver', () => {
+            expect(getResolver().exists()).toBe(false);
+          });
+
+          it('shows an alert with the error message', () => {
+            expect(getErrorAlert().text()).toEqual(message);
+          });
+
+          describe('when running the failed query again', () => {
+            beforeEach(() => {
+              return getSubmitBtn().vm.$emit('click');
+            });
+
+            it('hides the error message', () => {
+              expect(getErrorAlert().exists()).toBe(false);
+            });
+          });
+        });
       });
     });
   });
