343527086e31aa7ab8ad268d217ed5b99d3d09e3
Deepika Guliani <dguliani@gitlab.com>
2025-11-08 00:39:51 +1100

    fix: persist issue modal state when switching to full page

--- app/assets/javascripts/work_items/components/create_work_item_modal.vue
+++ app/assets/javascripts/work_items/components/create_work_item_modal.vue
@@ -147,7 +147,10 @@ export default {
       if (this.relatedItem) {
         query += previousQueryParam ? '&' : '?';
         query += `${RELATED_ITEM_ID_URL_QUERY_PARAM}=${this.relatedItem.id}`;
+        previousQueryParam = true;
       }
+      query += previousQueryParam ? '&' : '?';
+      query += `initialCreationContext=${this.creationContext}`;
       return query;
     },
     newWorkItemPath() {
@@ -278,6 +281,7 @@ export default {
           query: {
             [RELATED_ITEM_ID_URL_QUERY_PARAM]: this.relatedItem?.id,
             type: NAME_TO_ENUM_MAP[this.selectedWorkItemTypeName],
+            initialCreationContext: this.creationContext,
           },
         });
       } else {
