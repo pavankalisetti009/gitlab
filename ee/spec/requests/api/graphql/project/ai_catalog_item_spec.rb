e133fb0668d499d066ea1b5fd2a65124460acb3e
Luke Duncalfe <lduncalfe@gitlab.com>
2025-11-11 10:54:49 +1300

    Adds showSoftDeleted arg to Project.aiCatalogItem
    This adds an optional `showSoftDeleted` argument to return a
    soft-deleted item through the `Project.aiCatalogItem` field.
    
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/208880
    https://gitlab.com/gitlab-org/gitlab/-/issues/575127

--- ee/spec/requests/api/graphql/project/ai_catalog_item_spec.rb
+++ ee/spec/requests/api/graphql/project/ai_catalog_item_spec.rb
@@ -10,22 +10,43 @@ RSpec.describe 'Query.project.aiCatalogItem', feature_category: :workflow_catalo
   let_it_be(:user) { create(:user) }
   let_it_be(:item) { create(:ai_catalog_item, project: project) }
 
-  let(:query) do
-    <<~QUERY
-    {
-      project(fullPath: "#{project.full_path}") {
-        aiCatalogItem(id: "#{global_id_of(item)}") {
-          id
-        }
-      }
-    }
-    QUERY
-  end
+  let(:args) { { id: global_id_of(item) } }
 
-  let(:item_data) { graphql_data.dig('project', 'aiCatalogItem', 'id') }
+  let(:query) do
+    graphql_query_for(
+      :project,
+      { full_path: project.full_path },
+      query_graphql_field(:ai_catalog_item,
+        attributes_to_graphql(args).to_s,
+        all_graphql_fields_for('AiCatalogItem', max_depth: 1)
+      )
+    )
+  end
 
   before do
     enable_ai_catalog
+  end
+
+  shared_examples 'returns null' do
+    let(:item_data) { graphql_data.dig('project', 'aiCatalogItem') }
+
+    specify do
+      post_graphql(query, current_user: user)
+
+      expect(response).to have_gitlab_http_status(:success)
+      expect(item_data).to be_nil
+    end
+  end
+
+  shared_examples 'returns the item that matches the given ID' do
+    let(:item_data) { graphql_data.dig('project', 'aiCatalogItem', 'id') }
+
+    specify do
+      post_graphql(query, current_user: user)
+
+      expect(response).to have_gitlab_http_status(:success)
+      expect(item_data).to eq(global_id_of(item).to_s)
+    end
   end
 
   context 'when the user can read items on the project' do
@@ -33,10 +54,20 @@ RSpec.describe 'Query.project.aiCatalogItem', feature_category: :workflow_catalo
       project.add_developer(user)
     end
 
-    it 'returns the item that matches the given ID' do
-      post_graphql(query, current_user: user)
+    it_behaves_like 'returns the item that matches the given ID'
 
-      expect(item_data).to eq(global_id_of(item).to_s)
+    context 'when item is soft-deleted' do
+      before do
+        item.update!(deleted_at: Time.current)
+      end
+
+      it_behaves_like 'returns null'
+
+      context 'when showSoftDeleted is true' do
+        let(:args) { super().merge(show_soft_deleted: true) }
+
+        it_behaves_like 'returns the item that matches the given ID'
+      end
     end
 
     context 'when the global_ai_catalog flag is disabled' do
@@ -44,29 +75,17 @@ RSpec.describe 'Query.project.aiCatalogItem', feature_category: :workflow_catalo
         stub_feature_flags(global_ai_catalog: false)
       end
 
-      it 'returns null' do
-        post_graphql(query, current_user: user)
-
-        expect(item_data).to be_nil
-      end
+      it_behaves_like 'returns null'
     end
 
     context 'when item ID belongs to another project' do
       let_it_be(:item) { create(:ai_catalog_item, project: create(:project)) }
 
-      it 'returns null' do
-        post_graphql(query, current_user: user)
-
-        expect(item_data).to be_nil
-      end
+      it_behaves_like 'returns null'
     end
   end
 
   context 'when the user cannot read items on the project' do
-    it 'returns null' do
-      post_graphql(query, current_user: user)
-
-      expect(item_data).to be_nil
-    end
+    it_behaves_like 'returns null'
   end
 end
