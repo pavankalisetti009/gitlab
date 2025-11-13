1c51e7a80f429bcc22e49cf54d3df65c4c0fbe25
Rushik Subba <srushik@gitlab.com>
2025-11-14 02:37:17 +0530

    Add static diversity factor multiplier
    Add static diversity factor multiplier of 0.4
    to project and group risk score calculations
    
    EE: true

--- ee/lib/vulnerabilities/aggregate_risk_score.rb
+++ ee/lib/vulnerabilities/aggregate_risk_score.rb
@@ -2,6 +2,12 @@
 
 module Vulnerabilities
   class AggregateRiskScore
+    # Todo - The actual diversity factor calculation requires
+    # the unique cwe count among all vulnerabilities which,
+    # we don't have a way to find out right now. So keeping this a constant.
+    # More info - https://gitlab.com/groups/gitlab-org/-/epics/17002#note_2442241535
+    DIVERSITY_FACTOR = 0.4
+
     # Calculate aggregate risk score for scoped vulnerabilities
     # This can be for a group or a project or any set of vulnerabilities.
     # Risk Score = (Sum(Vulnerability Scores) + Sum(Vulnerability_age_in_month) x 0.005) / sqrt(num_vulnerabilities)
@@ -17,11 +23,17 @@ module Vulnerabilities
       # division by 1000 because created_at_sum is in milli seconds where as Time.zone.now.to_f gives seconds
       date_diff = (active_vulnerabilities_count * Time.zone.now.to_f) - (created_at_sum / 1000)
       date_diff_in_months = date_diff / 86400 / 30
+      age_modifier = 0.005 * date_diff_in_months
 
-      age_factor = 0.005 * date_diff_in_months
-      total_risk_score = ((risk_scores_sum + age_factor) / Math.sqrt(active_vulnerabilities_count)).round(4)
-
-      [1.0, total_risk_score.to_f].min
+      [
+        1.0,
+        total_risk_score(risk_scores_sum, age_modifier, active_vulnerabilities_count)
+      ].min
     end
+
+    def self.total_risk_score(risk_scores_sum, age_modifier, active_vulnerabilities_count)
+      (DIVERSITY_FACTOR * ((risk_scores_sum + age_modifier) / Math.sqrt(active_vulnerabilities_count))).round(4).to_f
+    end
+    private_class_method :total_risk_score
   end
 end
