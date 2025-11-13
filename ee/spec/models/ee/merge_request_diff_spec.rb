46daeb484579fda39c6cfa7e848dc7527b56116c
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2025-11-13 12:17:07 -0300

    Allow selective sync by orgs for Merge Request Diff

--- ee/spec/models/ee/merge_request_diff_spec.rb
+++ ee/spec/models/ee/merge_request_diff_spec.rb
@@ -2,71 +2,9 @@
 
 require 'spec_helper'
 
-RSpec.describe MergeRequestDiff, feature_category: :geo_replication do
-  using RSpec::Parameterized::TableSyntax
-  include EE::GeoHelpers
-
-  let_it_be(:group) { create(:group) }
-  let_it_be(:project) { create(:project, :repository, group: group) }
-  let_it_be(:other_project) { create(:project, :repository) }
-
+RSpec.describe MergeRequestDiff, feature_category: :code_review_workflow do
   before do
     stub_external_diffs_setting(enabled: true)
-  end
-
-  include_examples 'a verifiable model for verification state' do
-    let(:verifiable_model_record) { build(:merge_request_diff, :external, external_diff_store: ::ObjectStorage::Store::LOCAL) }
-    let(:unverifiable_model_record) { build(:merge_request_diff) }
-  end
-
-  describe '#after_save' do
-    let(:mr_diff) { build(:merge_request_diff, :external, external_diff_store: ::ObjectStorage::Store::LOCAL) }
-    let(:primary) { create(:geo_node, :primary) }
-
-    before do
-      stub_current_geo_node(primary)
-    end
-
-    context 'when diff is stored externally and locally' do
-      it 'does not create verification details when diff is without files' do
-        mr_diff[:state] = :without_files
-
-        expect { mr_diff.save! }.not_to change { MergeRequestDiffDetail.count }
-      end
-
-      it 'does not create verification details when diff is empty' do
-        mr_diff[:state] = :empty
-
-        expect { mr_diff.save! }.not_to change { MergeRequestDiffDetail.count }
-      end
-
-      it 'creates verification details' do
-        mr_diff[:state] = :collected
-
-        expect { mr_diff.save! }.to change { MergeRequestDiffDetail.count }.by(1)
-      end
-
-      context 'for a remote stored diff' do
-        before do
-          allow_next_instance_of(MergeRequestDiff) do |mr_diff|
-            allow(mr_diff).to receive(:update_external_diff_store).and_return(true)
-          end
-        end
-
-        it 'does not create verification details' do
-          mr_diff[:state] = :collected
-          mr_diff[:external_diff_store] = ::ObjectStorage::Store::REMOTE
-
-          expect { mr_diff.save! }.not_to change { MergeRequestDiffDetail.count }
-        end
-      end
-    end
-
-    context 'when diff is not stored externally' do
-      it 'does not create verification details' do
-        expect { create(:merge_request_diff, stored_externally: false) }.not_to change { MergeRequestDiffDetail.count }
-      end
-    end
   end
 
   describe '.search' do
@@ -133,6 +71,10 @@ RSpec.describe MergeRequestDiff, feature_category: :geo_replication do
   end
 
   describe '.project_id_in' do
+    let_it_be(:group) { create(:group) }
+    let_it_be(:project) { create(:project, :repository, group: group) }
+    let_it_be(:other_project) { create(:project, :repository) }
+
     it 'only includes diffs for the provided projects' do
       diff = create(:merge_request, source_project: project).merge_request_diff
       other_diff = create(:merge_request, source_project: other_project).merge_request_diff
@@ -142,66 +84,107 @@ RSpec.describe MergeRequestDiff, feature_category: :geo_replication do
     end
   end
 
-  describe '.replicables_for_current_secondary' do
-    context 'without selective sync or object storage' do
-      let(:secondary) { create(:geo_node) }
+  describe 'Geo replication', feature_category: :geo_replication do
+    describe 'associations' do
+      it 'has one verification state table class' do
+        is_expected
+          .to have_one(:merge_request_diff_detail)
+          .inverse_of(:merge_request_diff)
+          .autosave(false)
+      end
+    end
+
+    include_examples 'a verifiable model for verification state' do
+      let(:verifiable_model_record) { build(:merge_request_diff, :external, external_diff_store: ::ObjectStorage::Store::LOCAL) }
+      let(:unverifiable_model_record) { build(:merge_request_diff) }
+    end
+
+    describe '#after_save' do
+      let(:mr_diff) { build(:merge_request_diff, :external, external_diff_store: ::ObjectStorage::Store::LOCAL) }
+
+      let_it_be(:primary) { create(:geo_node, :primary) }
 
       before do
-        stub_current_geo_node(secondary)
+        stub_current_geo_node(primary)
       end
 
-      it 'excludes diffs stored in the database' do
-        stub_external_diffs_setting(enabled: false)
+      context 'when diff is stored externally and locally' do
+        it 'does not create verification details when diff is without files' do
+          mr_diff[:state] = :without_files
 
-        create(:merge_request, source_project: project)
-
-        expect(described_class.replicables_for_current_secondary(1..described_class.last.id)).to be_empty
-      end
-
-      it 'excludes empty diffs' do
-        create(:merge_request, source_project: create(:project))
-
-        expect(described_class.replicables_for_current_secondary(1..described_class.last.id)).to be_empty
-      end
-    end
-
-    where(:selective_sync_enabled, :object_storage_sync_enabled, :diff_in_object_storage, :synced_states) do
-      true  | true  | true  | 1
-      true  | true  | false | 1
-      true  | false | true  | 0
-      true  | false | false | 1
-      false | false | false | 2
-      false | false | true  | 0
-      false | true  | true  | 2
-      false | true  | false | 2
-      true  | true  | false | 1
-    end
-
-    with_them do
-      let(:secondary) do
-        node = build(:geo_node, sync_object_storage: object_storage_sync_enabled)
-
-        if selective_sync_enabled
-          node.selective_sync_type = 'namespaces'
-          node.namespaces = [group]
+          expect { mr_diff.save! }.not_to change { MergeRequestDiffDetail.count }
         end
 
-        node.save!
-        node
+        it 'does not create verification details when diff is empty' do
+          mr_diff[:state] = :empty
+
+          expect { mr_diff.save! }.not_to change { MergeRequestDiffDetail.count }
+        end
+
+        it 'creates verification details' do
+          mr_diff[:state] = :collected
+
+          expect { mr_diff.save! }.to change { MergeRequestDiffDetail.count }.by(1)
+        end
+
+        context 'for a remote stored diff' do
+          before do
+            allow_next_instance_of(MergeRequestDiff) do |mr_diff|
+              allow(mr_diff).to receive(:update_external_diff_store).and_return(true)
+            end
+          end
+
+          it 'does not create verification details' do
+            mr_diff[:state] = :collected
+            mr_diff[:external_diff_store] = ::ObjectStorage::Store::REMOTE
+
+            expect { mr_diff.save! }.not_to change { MergeRequestDiffDetail.count }
+          end
+        end
       end
 
-      before do
-        stub_current_geo_node(secondary)
+      context 'when diff is not stored externally' do
+        it 'does not create verification details' do
+          expect { create(:merge_request_diff, stored_externally: false) }.not_to change { MergeRequestDiffDetail.count }
+        end
+      end
+    end
 
-        stub_external_diffs_object_storage(ExternalDiffUploader, direct_upload: true) if diff_in_object_storage
+    describe 'replication/verification' do
+      let_it_be(:group_1) { create(:group, organization: create(:organization)) }
+      let_it_be(:group_2) { create(:group, organization: create(:organization)) }
+      let_it_be(:nested_group_1) { create(:group, parent: group_1) }
+      let_it_be(:project_1) { create(:project, group: group_1) }
+      let_it_be(:project_2) { create(:project, group: nested_group_1) }
+      let_it_be(:project_3) { create(:project, group: group_2) }
 
-        create(:merge_request, source_project: project)
-        create(:merge_request, source_project: other_project)
+      let_it_be(:merge_request_1) { create(:merge_request, :skip_diff_creation, source_project: project_1) }
+      let_it_be(:merge_request_2) { create(:merge_request, :skip_diff_creation, source_project: project_2) }
+      let_it_be(:merge_request_3) { create(:merge_request, :skip_diff_creation, source_project: project_1, source_branch: 'improve/awesome') }
+      let_it_be(:merge_request_4) { create(:merge_request, :skip_diff_creation, source_project: project_3) }
+
+      # Merge request diff for the root group
+      let_it_be(:first_replicable_and_in_selective_sync) do
+        create(:external_merge_request_diff, merge_request: merge_request_1)
       end
 
-      it 'returns the proper number of merge request diff states' do
-        expect(described_class.replicables_for_current_secondary(1..described_class.last.id)).to have_attributes(count: synced_states)
+      # Merge request diff for a subgroup
+      let_it_be(:second_replicable_and_in_selective_sync) do
+        create(:external_merge_request_diff, merge_request: merge_request_2)
       end
+
+      # Merge request diff for a subgroup and on object storage
+      let!(:third_replicable_on_object_storage_and_in_selective_sync) do
+        stub_external_diffs_object_storage(ExternalDiffUploader, direct_upload: true)
+        create(:external_merge_request_diff, merge_request: merge_request_3, external_diff_store: ::ObjectStorage::Store::REMOTE)
+      end
+
+      # Merge request diff for a group not in selective sync
+      let_it_be(:last_replicable_and_not_in_selective_sync) do
+        create(:external_merge_request_diff, merge_request: merge_request_4)
+      end
+
+      include_examples 'Geo Framework selective sync behavior'
     end
   end
 end
