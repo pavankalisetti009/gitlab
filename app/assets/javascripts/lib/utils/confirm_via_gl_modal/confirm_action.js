05d92ed7256308cf24118a3add7691e347056f23
Mark Florian <mflorian@gitlab.com>
2025-11-19 12:02:44 +0000

    Fix the confirm action spec under Vue 3

--- app/assets/javascripts/lib/utils/confirm_via_gl_modal/confirm_action.js
+++ app/assets/javascripts/lib/utils/confirm_via_gl_modal/confirm_action.js
@@ -1,68 +1,80 @@
 import Vue from 'vue';
 import { InternalEvents } from '~/tracking';
 
-export function confirmAction(
-  message,
-  {
-    primaryBtnVariant,
-    primaryBtnText,
-    secondaryBtnVariant,
-    secondaryBtnText,
-    cancelBtnVariant,
-    cancelBtnText,
-    modalHtmlMessage,
-    title,
-    hideCancel,
-    size,
-    trackingEvent,
-  } = {},
-) {
-  return new Promise((resolve) => {
-    let confirmed = false;
-    let component;
+export function createConfirmAction({ mountFn, destroyFn }) {
+  return function confirmAction(
+    message,
+    {
+      primaryBtnVariant,
+      primaryBtnText,
+      secondaryBtnVariant,
+      secondaryBtnText,
+      cancelBtnVariant,
+      cancelBtnText,
+      modalHtmlMessage,
+      title,
+      hideCancel,
+      size,
+      trackingEvent,
+    } = {},
+  ) {
+    return new Promise((resolve) => {
+      let confirmed = false;
+      let component;
 
-    const ConfirmAction = Vue.extend({
-      components: {
-        ConfirmModal: () => import('./confirm_modal.vue'),
-      },
-      render(h) {
-        return h(
-          'confirm-modal',
-          {
-            props: {
-              secondaryText: secondaryBtnText,
-              secondaryVariant: secondaryBtnVariant,
-              primaryVariant: primaryBtnVariant,
-              primaryText: primaryBtnText,
-              cancelVariant: cancelBtnVariant,
-              cancelText: cancelBtnText,
-              title,
-              modalHtmlMessage,
-              hideCancel,
-              size,
-            },
-            on: {
-              confirmed() {
-                confirmed = true;
-                if (trackingEvent) {
-                  InternalEvents.trackEvent(trackingEvent.name, {
-                    label: trackingEvent.label,
-                    property: trackingEvent.property,
-                    value: trackingEvent.value,
-                  });
-                }
+      const ConfirmAction = {
+        name: 'ConfirmActionRoot',
+        components: {
+          ConfirmModal: () => import('./confirm_modal.vue'),
+        },
+        render(h) {
+          return h(
+            'confirm-modal',
+            {
+              props: {
+                secondaryText: secondaryBtnText,
+                secondaryVariant: secondaryBtnVariant,
+                primaryVariant: primaryBtnVariant,
+                primaryText: primaryBtnText,
+                cancelVariant: cancelBtnVariant,
+                cancelText: cancelBtnText,
+                title,
+                modalHtmlMessage,
+                hideCancel,
+                size,
               },
-              closed() {
-                component.$destroy();
-                resolve(confirmed);
+              on: {
+                confirmed() {
+                  confirmed = true;
+                  if (trackingEvent) {
+                    InternalEvents.trackEvent(trackingEvent.name, {
+                      label: trackingEvent.label,
+                      property: trackingEvent.property,
+                      value: trackingEvent.value,
+                    });
+                  }
+                },
+                closed() {
+                  destroyFn(component);
+                  resolve(confirmed);
+                },
               },
             },
-          },
-          [message],
-        );
-      },
+            [message],
+          );
+        },
+      };
+
+      component = mountFn(ConfirmAction);
     });
-
-    component = new ConfirmAction().$mount();
-  });
+  };
 }
+
+export const confirmAction = createConfirmAction({
+  mountFn(Component) {
+    return new Vue(Component).$mount();
+  },
+  destroyFn(instance) {
+    instance.$destroy();
+  },
+});
