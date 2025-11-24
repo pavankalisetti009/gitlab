83a49d6cafcc0fdb9f07dce94b1d90629f722892
ŁUKASZ KORBASIEWICZ <lkorbasiewicz@gitlab.com>
2025-11-24 13:09:41 +0100

    Add authorization check for model management

--- ee/app/controllers/admin/gitlab_duo/self_hosted_controller.rb
+++ ee/app/controllers/admin/gitlab_duo/self_hosted_controller.rb
@@ -8,6 +8,7 @@ module Admin
       urgency :low
 
       before_action :authorize_feature!
+      before_action :authorize_model_management!
 
       def index; end
 
@@ -15,6 +16,14 @@ module Admin
 
       def authorize_feature!
         return if can_any?(current_user, %i[manage_self_hosted_models_settings manage_instance_model_selection])
+
+        render_404
+      end
+
+      def authorize_model_management!
+        vueroute = params.permit(:vueroute)[:vueroute]
+        return unless vueroute&.start_with?('models/new') || vueroute&.match?(%r{\Amodels/\d+/edit\z})
+        return if can?(current_user, :manage_self_hosted_models_settings)
 
         render_404
       end
