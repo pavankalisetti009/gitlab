0acbbb867802ac982e006470a636b7820c82fbff
Dominic Bauer <dbauer@gitlab.com>
2025-11-24 16:48:45 +0100

    Unsubscribe and no-op `CreateWarnModeAuditEventsWorker`

--- ee/app/workers/security/scan_result_policies/recreate_project_warn_mode_audit_events_worker.rb
+++ ee/app/workers/security/scan_result_policies/recreate_project_warn_mode_audit_events_worker.rb
@@ -3,27 +3,14 @@
 module Security
   module ScanResultPolicies
     class RecreateProjectWarnModeAuditEventsWorker
-      include Gitlab::EventStore::Subscriber
+      include ApplicationWorker
 
       feature_category :security_policy_management
       data_consistency :sticky
       idempotent!
 
-      def handle_event(event)
-        project_id = event.data[:project_id]
-
-        project = Project.find_by_id(project_id) || return
-
-        return unless project.licensed_feature_available?(:security_orchestration_policies)
-        return unless Feature.enabled?(:security_policy_approval_warn_mode, project)
-
-        AuditEvent.transaction do
-          project.approval_policies.select(&:warn_mode?).each do |policy|
-            Security::ScanResultPolicies::CreateProjectWarnModeAuditEventService
-              .new(project, policy)
-              .execute
-          end
-        end
+      def perform(*)
+        # no-op
       end
     end
   end
