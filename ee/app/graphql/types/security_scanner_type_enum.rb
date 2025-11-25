b26451b753ab49677f7c8cff23d80eb87efabd94
Russell Dickenson <rdickenson@gitlab.com>
2025-11-25 18:27:48 +1000

    Sentence case container scanning

--- ee/app/graphql/types/security_scanner_type_enum.rb
+++ ee/app/graphql/types/security_scanner_type_enum.rb
@@ -9,8 +9,10 @@ module Types
       upcase_type = scanner.upcase.to_s
       human_type = case scanner
                    when :dast, :sast then upcase_type
-                   when :api_fuzzing then 'API Fuzzing'
-                   else scanner.to_s.titleize
+                   when :api_fuzzing then 'API fuzzing'
+                   when :sast_advanced then 'SAST advanced'
+                   when :sast_iac then 'SAST IaC'
+                   else scanner.to_s.humanize
                    end
 
       value upcase_type, description: "#{human_type} scanner"
