a25abc148c3b9aea387ad0beb5f6dd964b6e8071
Vanessa Otto <votto@gitlab.com>
2025-11-25 11:53:20 +0100

    Remove dead code to add item consumer of agents and flows

--- ee/app/assets/javascripts/ai/catalog/pages/ai_catalog_agents.vue
+++ ee/app/assets/javascripts/ai/catalog/pages/ai_catalog_agents.vue
@@ -1,20 +1,15 @@
 <script>
 import { GlFilteredSearch } from '@gitlab/ui';
-import * as Sentry from '~/sentry/sentry_browser_wrapper';
-import { s__, sprintf } from '~/locale';
 import { fetchPolicies } from '~/lib/graphql';
 import { InternalEvents } from '~/tracking';
 import {
   VISIBILITY_LEVEL_PUBLIC_STRING,
   VISIBILITY_LEVEL_PRIVATE_STRING,
 } from '~/visibility_level/constants';
-import ErrorsAlert from '~/vue_shared/components/errors_alert.vue';
 import { FILTERED_SEARCH_TERM } from '~/vue_shared/components/filtered_search_bar/constants';
 import aiCatalogAgentsQuery from '../graphql/queries/ai_catalog_agents.query.graphql';
-import createAiCatalogItemConsumer from '../graphql/mutations/create_ai_catalog_item_consumer.mutation.graphql';
 import AiCatalogListHeader from '../components/ai_catalog_list_header.vue';
 import AiCatalogList from '../components/ai_catalog_list.vue';
-import AiCatalogItemConsumerModal from '../components/ai_catalog_item_consumer_modal.vue';
 import { AI_CATALOG_AGENTS_SHOW_ROUTE } from '../router/constants';
 import {
   AGENT_VISIBILITY_LEVEL_DESCRIPTIONS,
@@ -22,23 +17,18 @@ import {
   TRACK_EVENT_VIEW_AI_CATALOG_ITEM_INDEX,
   TRACK_EVENT_TYPE_AGENT,
 } from '../constants';
-import { prerequisitesError } from '../utils';
 
 export default {
   name: 'AiCatalogAgents',
   components: {
     AiCatalogListHeader,
     AiCatalogList,
-    AiCatalogItemConsumerModal,
-    ErrorsAlert,
     GlFilteredSearch,
   },
   mixins: [InternalEvents.mixin()],
   data() {
     return {
       aiCatalogAgents: [],
-      aiCatalogAgentToBeAdded: null,
-      errors: [],
       pageInfo: {},
       searchTerm: '',
     };
@@ -92,55 +82,6 @@ export default {
     });
   },
   methods: {
-    setAiCatalogAgentToBeAdded(agent) {
-      this.aiCatalogAgentToBeAdded = agent;
-    },
-    async addAgentToTarget(target) {
-      const agent = this.aiCatalogAgentToBeAdded;
-
-      const input = {
-        itemId: agent.id,
-        target,
-      };
-
-      this.setAiCatalogAgentToBeAdded(null);
-
-      try {
-        const { data } = await this.$apollo.mutate({
-          mutation: createAiCatalogItemConsumer,
-          variables: {
-            input,
-          },
-        });
-
-        if (data) {
-          const { errors } = data.aiCatalogItemConsumerCreate;
-          if (errors.length > 0) {
-            // TODO: Once we have a project selector, we could add the project name in this message.
-            this.errors = [
-              sprintf(s__('AICatalog|Could not enable agent: %{agentName}'), {
-                agentName: agent.name,
-              }),
-              ...errors,
-            ];
-            return;
-          }
-
-          const name = data.aiCatalogItemConsumerCreate.itemConsumer.project?.name || '';
-
-          this.$toast.show(sprintf(s__('AICatalog|Agent enabled in %{name}.'), { name }));
-        }
-      } catch (error) {
-        this.errors = [
-          prerequisitesError(
-            s__(
-              'AICatalog|Could not enable agent in the project. Check that the project meets the %{linkStart}prerequisites%{linkEnd} and try again.',
-            ),
-          ),
-        ];
-        Sentry.captureException(error);
-      }
-    },
     handleNextPage() {
       this.$apollo.queries.aiCatalogAgents.refetch({
         ...this.$apollo.queries.aiCatalogAgents.variables,
@@ -172,7 +113,6 @@ export default {
 <template>
   <div>
     <ai-catalog-list-header />
-    <errors-alert class="gl-mt-5" :errors="errors" @dismiss="errors = []" />
 
     <div class="gl-border-b gl-bg-subtle gl-p-5">
       <gl-filtered-search
@@ -190,13 +130,6 @@ export default {
       :search="searchTerm"
       @next-page="handleNextPage"
       @prev-page="handlePrevPage"
-    />
-    <ai-catalog-item-consumer-modal
-      v-if="aiCatalogAgentToBeAdded"
-      :item="aiCatalogAgentToBeAdded"
-      open
-      @submit="addAgentToTarget"
-      @hide="setAiCatalogAgentToBeAdded(null)"
     />
   </div>
 </template>
