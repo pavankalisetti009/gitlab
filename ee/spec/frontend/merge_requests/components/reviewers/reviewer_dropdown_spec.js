276a5c6b0d987fcaf06cf427f5712ee4aefc34d0
Phil Hughes <me@iamphill.com>
2025-11-28 11:31:03 +0000

    Added approval rule text to reviewer dropdown user items
    Adds the approval rule text to each user in the reviewer dropdown if
    they are apart of a approval rule. This existed in the old non-Vue based
    dropdown but was missed when we moved over to the Vue version.

---
+++ ee/spec/frontend/merge_requests/components/reviewers/reviewer_dropdown_spec.js
@@ -0,0 +1,103 @@
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import { mountExtended } from 'helpers/vue_test_utils_helper';
+import createMockApollo from 'helpers/mock_apollo_helper';
+import waitForPromises from 'helpers/wait_for_promises';
+import ReviewerDropdown from '~/merge_requests/components/reviewers/reviewer_dropdown.vue';
+import userPermissionsQuery from '~/merge_requests/components/reviewers/queries/user_permissions.query.graphql';
+import userAutocompleteWithMRPermissionsQuery from 'ee_else_ce/graphql_shared/queries/project_autocomplete_users_with_mr_permissions.query.graphql';
+
+let wrapper;
+let autocompleteUsersMock;
+
+Vue.use(VueApollo);
+
+Vue.config.ignoredElements = ['gl-emoji'];
+
+const createMockUser = ({
+  id = 1,
+  name = 'Administrator',
+  username = 'root',
+  mergeRequestInteraction = {},
+} = {}) => ({
+  __typename: 'UserCore',
+  id: `gid://gitlab/User/${id}`,
+  avatarUrl:
+    'https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80\u0026d=identicon',
+  webUrl: `/${username}`,
+  webPath: `/${username}`,
+  status: null,
+  mergeRequestInteraction: {
+    canMerge: true,
+    ...mergeRequestInteraction,
+  },
+  username,
+  name,
+});
+
+function createComponent(adminMergeRequest = true, customUsers = []) {
+  autocompleteUsersMock = jest.fn().mockResolvedValue({
+    data: {
+      workspace: {
+        id: 1,
+        users: customUsers,
+      },
+    },
+  });
+
+  const apolloProvider = createMockApollo(
+    [
+      [userAutocompleteWithMRPermissionsQuery, autocompleteUsersMock],
+      [
+        userPermissionsQuery,
+        jest.fn().mockResolvedValue({
+          data: {
+            project: {
+              id: 1,
+              mergeRequest: { id: 1, userPermissions: { adminMergeRequest, canMerge: true } },
+            },
+          },
+        }),
+      ],
+    ],
+    {},
+    {
+      typePolicies: { Query: { fields: { project: { merge: true } } } },
+    },
+  );
+
+  wrapper = mountExtended(ReviewerDropdown, {
+    apolloProvider,
+    provide: {
+      projectPath: 'gitlab-org/gitlab',
+      issuableId: '1',
+      issuableIid: '1',
+      directlyInviteMembers: true,
+    },
+    stubs: {
+      'gl-emoji': true,
+    },
+  });
+}
+
+const findApprovalRule = () => wrapper.findByTestId('approval-rule');
+
+describe('Reviewer dropdown component', () => {
+  it('renders dropdown approval rule', async () => {
+    createComponent(true, [
+      createMockUser({
+        mergeRequestInteraction: {
+          applicableApprovalRules: [{ id: 1, name: 'Frontend', type: 'CODE_OWNER' }],
+        },
+      }),
+    ]);
+
+    await waitForPromises();
+
+    wrapper.findByTestId('base-dropdown-toggle').trigger('click');
+
+    await waitForPromises();
+
+    expect(findApprovalRule().text()).toContain('Code Owner');
+  });
+});
