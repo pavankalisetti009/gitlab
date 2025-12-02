7204050d2bba406c92870fbef2ff67f00374e8a8
Nick Duff <nduff@gitlab.com>
2025-12-02 14:38:55 +1300

    Revert "Merge branch 'sf/feature/audit-stream-ip-restrictions' into 'master'"
    This reverts merge request !211595

--- ee/lib/gitlab/ip_restriction/enforcer.rb
+++ ee/lib/gitlab/ip_restriction/enforcer.rb
@@ -14,6 +14,8 @@ module Gitlab
       def allows_current_ip?
         return true unless group&.licensed_feature_available?(:group_ip_restriction)
 
+        current_ip_address = Gitlab::IpAddressState.current
+
         return true unless current_ip_address
 
         allows_address?(current_ip_address)
@@ -22,10 +24,6 @@ module Gitlab
       private
 
       attr_reader :group
-
-      def current_ip_address
-        @current_ip_address ||= Gitlab::IpAddressState.current
-      end
 
       def allows_address?(address)
         root_ancestor_ip_restrictions = group.root_ancestor_ip_restrictions
@@ -37,45 +35,15 @@ module Gitlab
         # Check against configured internal ranges
         allowed ||= globally_configured_ip_ranges.match?(address)
 
-        details = access_details(allowed)
-        log(details)
-        audit(details)
-
-        allowed
-      end
-
-      def log(details)
         logger.info(
           message: 'Attempting to access IP restricted group',
-          **details
-        )
-      end
-
-      def current_user
-        username = ::Gitlab::ApplicationContext.current_context_attribute(:user)
-        return unless username
-
-        User.find_by_username(username)
-      end
-
-      def audit(details)
-        ::Gitlab::Audit::Auditor.audit({
-          name: 'ip_restricted_group_accessed',
-          author: current_user || ::Gitlab::Audit::UnauthenticatedAuthor.new,
-          scope: group,
-          target: group,
-          message: 'Attempting to access IP restricted group',
-          additional_details: details
-        })
-      end
-
-      def access_details(allowed)
-        {
-          allowed: allowed,
-          ip_address: current_ip_address,
           group_full_path: group.full_path,
+          ip_address: address,
+          allowed: allowed,
           global_allowlist_in_use: globally_configured_ip_ranges.present?
-        }
+        )
+
+        allowed
       end
 
       def globally_configured_ip_ranges
