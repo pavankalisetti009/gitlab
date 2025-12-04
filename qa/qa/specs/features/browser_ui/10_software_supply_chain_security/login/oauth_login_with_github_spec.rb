333a4201b9c22e948708148903d01c1a1389e956
Adil Farrukh <afarrukh@gitlab.com>
2025-12-04 11:16:53 -0500

    Quarnatine flaky github oauth spec

--- qa/qa/specs/features/browser_ui/10_software_supply_chain_security/login/oauth_login_with_github_spec.rb
+++ qa/qa/specs/features/browser_ui/10_software_supply_chain_security/login/oauth_login_with_github_spec.rb
@@ -3,7 +3,7 @@
 module QA
   RSpec.describe 'Software Supply Chain Security', :orchestrated, :oauth, feature_category: :system_access do
     describe 'OAuth' do
-      it 'connects and logs in with GitHub OAuth',
+      it 'connects and logs in with GitHub OAuth', :quarantine,
         testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/402405' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
 
