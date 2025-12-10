8e0b17021254ed7a64777973ffada29b39c4c92d
Sylvia Shen <sshen@gitlab.com>
2025-12-10 16:37:04 +0000

    Add feature_enabled? and user_has_access? in VirtualRegistries::Packages::Maven

--- ee/app/graphql/resolvers/virtual_registries/packages/maven/upstreams_resolver.rb
+++ ee/app/graphql/resolvers/virtual_registries/packages/maven/upstreams_resolver.rb
@@ -7,19 +7,27 @@ module Resolvers
         class UpstreamsResolver < BaseResolver
           type ::Types::VirtualRegistries::Packages::Maven::UpstreamType.connection_type, null: true
 
+          alias_method :group, :object
+
           argument :upstream_name, GraphQL::Types::String,
             required: false,
             default_value: nil,
             description: 'Search upstreams by name.'
 
           def resolve(**args)
-            return unless ::VirtualRegistries::Packages::Maven.virtual_registry_available?(object, current_user)
+            return unless ::VirtualRegistries::Packages::Maven.feature_enabled?(group)
 
             ::VirtualRegistries::UpstreamsFinder.new(
               upstream_class: ::VirtualRegistries::Packages::Maven::Upstream,
-              group: object,
+              group: group,
               params: args.slice(:upstream_name)
             ).execute
+          end
+
+          private
+
+          def authorized?(**_args)
+            ::VirtualRegistries::Packages::Maven.user_has_access?(group, current_user)
           end
         end
       end
