6e197738781be8cd0c90a1aca934af2ec6b87a28
Russell Dickenson <rdickenson@gitlab.com>
2026-01-05 18:12:27 +1000

    Change feature name case in GraphQL API docs

--- ee/app/graphql/mutations/security/ci_configuration/configure_dependency_scanning.rb
+++ ee/app/graphql/mutations/security/ci_configuration/configure_dependency_scanning.rb
@@ -6,9 +6,9 @@ module Mutations
       class ConfigureDependencyScanning < BaseSecurityAnalyzer
         graphql_name 'ConfigureDependencyScanning'
         description <<~DESC
-          Configure Dependency Scanning for a project by enabling Dependency Scanning in a new or modified
-          `.gitlab-ci.yml` file in a new branch. The new branch and a URL to
-          create a Merge Request are a part of the response.
+          Enable dependency scanning `.gitlab-ci.yml` file in a new branch.
+          The new branch and a URL to create a merge request are a part of
+          the response.
         DESC
 
         def configure_analyzer(project, **_args)
