e1acd86bb53340ce2836c7bb23f076de165226b5
Asherah Connor <aconnor@gitlab.com>
2026-01-06 00:35:10 +1100

    Don't do arbitrary placeholder replacement on HTML
    Merge branch 'security-562561' into 'master'
    
    See merge request gitlab-org/security/gitlab!5485
    
    Changelog: security

--- ee/spec/lib/banzai/filter/placeholders_post_filter_spec.rb
+++ ee/spec/lib/banzai/filter/placeholders_post_filter_spec.rb
@@ -9,19 +9,34 @@ RSpec.describe Banzai::Filter::PlaceholdersPostFilter, feature_category: :markdo
     Banzai.render_and_post_process(text, context)
   end
 
-  let!(:latest_tag) { "<span data-placeholder=\"%{latest_tag}\">#{project_tag}</span>" }
+  let!(:latest_tag) do
+    frag = Nokogiri::HTML.fragment("<span>")
+    span = frag.children.first
+    span['data-placeholder'] = '%{latest_tag}'
+    span.content = project_tag
+    frag.to_html
+  end
+
   let!(:project_tag) do
     if project.repository_exists?
       TagsFinder.new(project.repository, per_page: 1, sort: 'updated_desc')&.execute&.first&.name
     end
   end
 
-  context 'when tag has js- triggers' do
+  context 'when tag has HTML' do
     let_it_be(:xss) do
+      # We were once vulnerable to HTML entered directly, so we started sanitising it.
+      # Unfortunately, in an HTML attribute (like 'title') there's no difference between
+      # "<i>" and "&lt;i&gt;", but the latter doesn't get sanitised out when parsed as-is,
+      # so the new attack just escapes the original! Test both.
       '<i/class=js-toggle-container><i/class=js-toggle-lazy-diff>' \
         '<i/class="file-holder"data-lines-path="/flightjs/xss/-/raw/main/a.json">' \
         '<i/class=gl-opacity-0><i/class="modal-backdrop"style="top&colon;-99px">' \
-        '<i/class=diff-content><table><tbody/>'
+        '<i/class=diff-content><table><tbody/>' \
+        '&lt;i/class=js-toggle-container&gt;&lt;i/class=js-toggle-lazy-diff&gt;' \
+        '&lt;i/class="file-holder"data-lines-path="/group-x/b/-/raw/main/a.json"&gt;' \
+        '&lt;i/class=gl-opacity-0&gt;&lt;i/class="modal-backdrop"style="top&colon;-99px"&gt;' \
+        '&lt;i/class=diff-content&gt;&lt;table&gt;&lt;tbody/&gt;'
     end
 
     let_it_be(:user) { create(:user) }
@@ -35,15 +50,15 @@ RSpec.describe Banzai::Filter::PlaceholdersPostFilter, feature_category: :markdo
       project.add_member(user, Gitlab::Access::OWNER)
     end
 
-    it 'sanitizes and removes any js- triggers and tags' do
-      expect(Banzai::Filter::SanitizationFilter).to receive(:new).twice.and_call_original
+    it "doesn't permit that HTML to enter the label title" do
+      expect(Banzai::Filter::SanitizationFilter).to receive(:new).once.and_call_original
 
       markdown = '<span data-placeholder>foo ~"key::value"</span>'
       html = run_pipeline(markdown, project: project, current_user: user)
 
-      expect(html).not_to include 'js-'
-      expect(html)
-        .to include 'title="&lt;span class=\'gl-font-bold\'&gt;Scoped label&lt;/span&gt;&lt;br&gt;xss "'
+      frag = Nokogiri::HTML.fragment(html)
+      title = frag.css('a[title][data-html="true"]').first['title']
+      expect(title).not_to include("<i")
     end
   end
 end
