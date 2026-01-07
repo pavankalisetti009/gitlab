3f2cbf1e2faed4ec9f71e17f30f8379639bff69b
Schmil Monderer <smonderer@gitlab.com>
2026-01-07 12:25:15 +0000

    Add support for updating default tracked context when default branch changes
    This commit implements the functionality to keep the default tracked
    context name synchronized with the project's default branch name.
    
    Changes include:
    1. New event: Repositories::RepositoryCreatedEvent
    2. New service: Security::ProjectTrackedContexts::UpdateDefaultService
    3. New worker: Security::ProjectTrackedContexts::UpdateDefaultWorker
    4. Event publishing in HasRepository concern
    5. Event subscriptions in EventStore
    
    Changelog: added
    EE: true

---
+++ ee/spec/workers/security/project_tracked_contexts/update_default_context_worker_spec.rb
@@ -0,0 +1,161 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Security::ProjectTrackedContexts::UpdateDefaultContextWorker, feature_category: :vulnerability_management do
+  let_it_be(:project) { create(:project, :repository) }
+
+  describe '#handle_event' do
+    shared_examples 'updates default tracked context' do
+      let(:event) do
+        event_class.new(data: {
+          container_id: project.id,
+          container_type: 'Project'
+        })
+      end
+
+      subject(:handle_event) { described_class.new.handle_event(event) }
+
+      context 'when vulnerabilities_across_contexts FF is enabled' do
+        before do
+          stub_feature_flags(vulnerabilities_across_contexts: true)
+        end
+
+        context 'when project exists' do
+          before do
+            allow(project).to receive(:default_branch).and_return('main')
+          end
+
+          it 'calls UpdateDefaultService' do
+            expect(Security::ProjectTrackedContexts::UpdateDefaultContextService)
+              .to receive(:new).with(project).and_call_original
+
+            handle_event
+          end
+
+          context 'when UpdateDefaultService succeeds' do
+            it 'does not log any warnings' do
+              expect(Gitlab::AppLogger).not_to receive(:warn)
+
+              handle_event
+            end
+          end
+
+          context 'when UpdateDefaultService fails' do
+            let(:service_result) do
+              ServiceResponse.error(
+                message: 'Some error',
+                payload: {
+                  tracked_context: build(:security_project_tracked_context)
+                }
+              )
+            end
+
+            before do
+              allow_next_instance_of(Security::ProjectTrackedContexts::UpdateDefaultContextService) do |service|
+                allow(service).to receive(:execute).and_return(service_result)
+              end
+            end
+
+            it 'logs a warning' do
+              expect(Gitlab::AppLogger).to receive(:warn).with(
+                message: "Failed to update default tracked context for project: Some error",
+                project_id: project.id,
+                project_path: project.full_path
+              )
+
+              handle_event
+            end
+          end
+        end
+
+        context 'when project does not exist' do
+          let(:event) do
+            event_class.new(data: {
+              container_id: non_existing_record_id,
+              container_type: 'Project'
+            })
+          end
+
+          it 'does not raise an error' do
+            expect { handle_event }.not_to raise_error
+          end
+
+          it 'does not call UpdateDefaultService' do
+            expect(Security::ProjectTrackedContexts::UpdateDefaultContextService).not_to receive(:new)
+
+            handle_event
+          end
+        end
+      end
+
+      context 'when vulnerabilities_across_contexts FF is disabled' do
+        before do
+          stub_feature_flags(vulnerabilities_across_contexts: false)
+        end
+
+        context 'when project exists' do
+          before do
+            allow(project).to receive(:default_branch).and_return('main')
+          end
+
+          it 'does not call UpdateDefaultService' do
+            expect(Security::ProjectTrackedContexts::UpdateDefaultContextService).not_to receive(:new)
+
+            handle_event
+          end
+        end
+
+        context 'when project does not exist' do
+          let(:event) do
+            event_class.new(data: {
+              container_id: non_existing_record_id,
+              container_type: 'Project'
+            })
+          end
+
+          it 'does not raise an error' do
+            expect { handle_event }.not_to raise_error
+          end
+
+          it 'does not call UpdateDefaultService' do
+            expect(Security::ProjectTrackedContexts::UpdateDefaultContextService).not_to receive(:new)
+
+            handle_event
+          end
+        end
+      end
+
+      context 'when container_type is not Project' do
+        let(:event) do
+          event_class.new(data: {
+            container_id: project.id,
+            container_type: 'Snippet'
+          })
+        end
+
+        it 'does not call UpdateDefaultService' do
+          expect(Security::ProjectTrackedContexts::UpdateDefaultContextService).not_to receive(:new)
+
+          handle_event
+        end
+      end
+
+      include_examples 'an idempotent worker' do
+        let(:job_args) { [event] }
+      end
+    end
+
+    context 'when handling DefaultBranchChangedEvent' do
+      let(:event_class) { Repositories::DefaultBranchChangedEvent }
+
+      it_behaves_like 'updates default tracked context'
+    end
+
+    context 'when handling RepositoryCreatedEvent' do
+      let(:event_class) { Repositories::RepositoryCreatedEvent }
+
+      it_behaves_like 'updates default tracked context'
+    end
+  end
+end
