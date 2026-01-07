06ec7fce1d7cf76e70faac0c7e065edef9affa93
Eva Kadlecová <ekadlecova@gitlab.com>
2026-01-07 01:53:32 +0100

    Authorize Note and Discussion for ai_workflows scope
    Changelog: fixed

--- ee/spec/requests/api/discussions_spec.rb
+++ ee/spec/requests/api/discussions_spec.rb
@@ -27,4 +27,16 @@ RSpec.describe API::Discussions, feature_category: :team_planning do
       let(:note)     { epic_note }
     end
   end
+
+  context 'when authenticated with a token that has the ai_workflows scope' do
+    let!(:work_item) { create(:work_item, project: project, author: user) }
+    let!(:work_item_note) { create(:discussion_note_on_issue, noteable: work_item, project: project, author: user) }
+
+    it_behaves_like 'forbids quick actions for ai_workflows scope' do
+      let(:method) { :post }
+      let(:url) { "/projects/#{project.id}/issues/#{work_item.iid}/discussions/#{work_item_note.discussion_id}/notes" }
+      let(:field) { :body }
+      let(:success_status) { :created }
+    end
+  end
 end
