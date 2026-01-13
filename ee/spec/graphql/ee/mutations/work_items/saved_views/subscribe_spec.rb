0470f4c551046536561755a6bcf2313e3b3290f7
Matt D'Angelo <mdangelo@gitlab.com>
2026-01-13 18:55:57 +1030

    Add subscribe / unsubscribe logic

---
+++ ee/spec/graphql/ee/mutations/work_items/saved_views/subscribe_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Mutations::WorkItems::SavedViews::Subscribe, feature_category: :portfolio_management do
+  include GraphqlHelpers
+
+  let_it_be(:project) { create(:project) }
+  let_it_be(:current_user) { create(:user, planner_of: [project]) }
+  let_it_be(:saved_view) { create(:saved_view, namespace: project.project_namespace) }
+
+  let(:current_ctx) { { current_user: current_user } }
+
+  def resolve_mutation(**args)
+    resolve(described_class, args: args, ctx: current_ctx)
+  end
+
+  describe '#resolve' do
+    context 'when the subscription limit is reached' do
+      before do
+        stub_licensed_features(increased_saved_views_limit: true)
+
+        namespace = project.project_namespace
+        limit = WorkItems::SavedViews::UserSavedView.user_saved_view_limit(namespace)
+        create_list(:saved_view, limit, namespace: namespace).each do |sv|
+          create(:user_saved_view, user: current_user, saved_view: sv, namespace: namespace)
+        end
+      end
+
+      it 'returns an error' do
+        result = resolve_mutation(id: saved_view.to_global_id)
+
+        expect(result[:saved_view]).to be_nil
+        expect(result[:errors]).to eq(['Saved view limit exceeded.'])
+      end
+
+      it 'does not create a new subscription' do
+        expect { resolve_mutation(id: saved_view.to_global_id) }
+          .not_to change { WorkItems::SavedViews::UserSavedView.count }
+      end
+    end
+  end
+end
