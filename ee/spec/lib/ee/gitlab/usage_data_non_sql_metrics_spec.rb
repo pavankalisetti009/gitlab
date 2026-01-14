d01d40a03097db27a1c21ae913a26a9a7dbb4da6
Tim Beauchamp <tbeauchamp@gitlab.com>
2026-01-14 13:30:00 -0400

    Add product_analytics feature category to RSpec test

--- ee/spec/lib/ee/gitlab/usage_data_non_sql_metrics_spec.rb
+++ ee/spec/lib/ee/gitlab/usage_data_non_sql_metrics_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Gitlab::UsageDataNonSqlMetrics do
+RSpec.describe Gitlab::UsageDataNonSqlMetrics, feature_category: :service_ping do
   include UsageDataHelpers
 
   before do
