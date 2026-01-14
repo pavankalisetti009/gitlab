31834fe4c43695b1073be912d6a8d92ff24f1319
Natanael Silva <nsilva@gitlab.com>
2026-01-13 16:42:21 -0800

    Swap wiki_repository_states FKs to loose FKs

--- ee/spec/models/geo/wiki_repository_state_spec.rb
+++ ee/spec/models/geo/wiki_repository_state_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Geo::WikiRepositoryState, type: :model, feature_category: :geo_re
     it {
       is_expected
         .to belong_to(:project_wiki_repository)
-        .class_name('Projects::WikiRepository')
-        .inverse_of(:wiki_repository_state)
+              .class_name('Projects::WikiRepository')
+              .inverse_of(:wiki_repository_state)
     }
   end
 
@@ -19,5 +19,19 @@ RSpec.describe Geo::WikiRepositoryState, type: :model, feature_category: :geo_re
     it { is_expected.to validate_presence_of(:verification_state) }
     it { is_expected.to validate_uniqueness_of(:project_wiki_repository) }
     it { is_expected.to validate_length_of(:verification_failure).is_at_most(255) }
+  end
+
+  context 'with loose foreign key on wiki_repository_states.project_id' do
+    it_behaves_like 'cleanup by a loose foreign key' do
+      let_it_be(:parent) { create(:project) }
+      let_it_be(:model) { create(:geo_wiki_repository_state, project_id: parent.id) }
+    end
+  end
+
+  context 'with loose foreign key on wiki_repository_states.project_wiki_repository_id' do
+    it_behaves_like 'cleanup by a loose foreign key' do
+      let_it_be(:parent) { create(:project_wiki_repository) }
+      let_it_be(:model) { create(:geo_wiki_repository_state, project_wiki_repository: parent) }
+    end
   end
 end
