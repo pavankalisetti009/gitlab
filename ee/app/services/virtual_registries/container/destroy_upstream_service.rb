50facec0bf25dd710ae8d82b858c97706365ec2a
Phil Hughes <me@iamphill.com>
2026-01-15 11:32:57 +0000

    Add mutation to delete container virtual registry upstreams
    Adds a mutation to the GraphQL API that will allow container virtual
    registry upstreams to be deleted.
    
    This can be used like:
    
    ```graphql
    mutation {
      containerUpstreamDelete(input: {
    id:"gid://gitlab/VirtualRegistries::Container::Upstream/6"}) {
        errors
        upstream {
          id
        }
      }
    }
    ```
    
    https://gitlab.com/gitlab-org/gitlab/-/work_items/579703

---
+++ ee/app/services/virtual_registries/container/destroy_upstream_service.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module VirtualRegistries
+  module Container
+    class DestroyUpstreamService < BaseContainerService
+      alias_method :upstream, :container
+
+      def initialize(upstream:, current_user:)
+        super(container: upstream, current_user: current_user)
+      end
+
+      def execute
+        unless ::VirtualRegistries::Container.virtual_registry_available?(upstream.group, current_user)
+          return ServiceResponse.error(message: _('Container virtual registry not available'), reason: :unavailable)
+        end
+
+        upstream.transaction do
+          ::VirtualRegistries::Container::RegistryUpstream
+            .sync_higher_positions(upstream.registry_upstreams)
+          upstream.destroy
+        end
+
+        if upstream.destroyed?
+          ServiceResponse.success(payload: upstream)
+        else
+          ServiceResponse.error(message: upstream.errors.full_messages)
+        end
+      end
+    end
+  end
+end
