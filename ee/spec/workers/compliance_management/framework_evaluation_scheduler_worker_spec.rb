c8f8ae7588c2ae46a97e143724778f981463570c
Hitesh Raghuvanshi <hraghuvanshi@gitlab.com>
2026-01-21 16:49:13 +0530

    Cleaning ff optimize_framework_worker_query
    Improved performance of compliance framework evaluations
    
    Changelog: performance
    EE: true

--- ee/spec/workers/compliance_management/framework_evaluation_scheduler_worker_spec.rb
+++ ee/spec/workers/compliance_management/framework_evaluation_scheduler_worker_spec.rb
@@ -6,102 +6,84 @@ RSpec.describe ComplianceManagement::FrameworkEvaluationSchedulerWorker, feature
   describe '#perform' do
     let(:worker) { described_class.new }
 
-    shared_examples 'performs framework evaluations' do
-      it_behaves_like 'an idempotent worker'
+    it_behaves_like 'an idempotent worker'
 
-      context 'when frameworks exist' do
-        let_it_be(:framework_1) { create(:compliance_framework) }
-        let_it_be(:framework_2) { create(:compliance_framework) }
-        let_it_be(:framework_without_projects) { create(:compliance_framework) }
+    context 'when frameworks exist' do
+      let_it_be(:framework_1) { create(:compliance_framework) }
+      let_it_be(:framework_2) { create(:compliance_framework) }
+      let_it_be(:framework_without_projects) { create(:compliance_framework) }
 
-        let_it_be(:projects) { create_list(:project, 7) }
+      let_it_be(:projects) { create_list(:project, 7) }
 
-        before do
-          [framework_1, framework_2, framework_without_projects].each do |framework|
-            create(:compliance_requirement, framework: framework).tap do |req|
-              create(:compliance_requirements_control, compliance_requirement: req)
-            end
+      before do
+        [framework_1, framework_2, framework_without_projects].each do |framework|
+          create(:compliance_requirement, framework: framework).tap do |req|
+            create(:compliance_requirements_control, compliance_requirement: req)
           end
-
-          projects[0..3].each do |project|
-            create(:compliance_framework_project_setting,
-              project: project,
-              compliance_management_framework: framework_1)
-          end
-
-          projects[4..6].each do |project|
-            create(:compliance_framework_project_setting,
-              project: project,
-              compliance_management_framework: framework_2)
-          end
-
-          stub_const("#{described_class}::PROJECT_BATCH_SIZE", 2)
-          stub_const("#{described_class}::FRAMEWORK_BATCH_SIZE", 1)
         end
 
-        it 'processes frameworks and projects in batches' do
-          expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
-            .to receive(:perform_async) do |framework_id, project_ids|
-            expect(framework_id).to eq(framework_1.id)
-            expect(project_ids.size).to eq(2)
-          end.ordered.once
-
-          expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
-            .to receive(:perform_async) do |framework_id, project_ids|
-            expect(framework_id).to eq(framework_1.id)
-            expect(project_ids.size).to eq(2)
-          end.ordered.once
-
-          expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
-            .to receive(:perform_async) do |framework_id, project_ids|
-            expect(framework_id).to eq(framework_2.id)
-            expect(project_ids.size).to eq(2)
-          end.ordered.once
-
-          expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
-            .to receive(:perform_async) do |framework_id, project_ids|
-            expect(framework_id).to eq(framework_2.id)
-            expect(project_ids.size).to eq(1)
-          end.ordered.once
-
-          expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
-            .not_to receive(:perform_async)
-                      .with(framework_without_projects.id, anything)
-
-          worker.perform
+        projects[0..3].each do |project|
+          create(:compliance_framework_project_setting,
+            project: project,
+            compliance_management_framework: framework_1)
         end
+
+        projects[4..6].each do |project|
+          create(:compliance_framework_project_setting,
+            project: project,
+            compliance_management_framework: framework_2)
+        end
+
+        stub_const("#{described_class}::PROJECT_BATCH_SIZE", 2)
+        stub_const("#{described_class}::FRAMEWORK_BATCH_SIZE", 1)
       end
 
-      context 'when no frameworks exist' do
-        before do
-          allow(ComplianceManagement::Framework)
-            .to receive(:with_active_controls)
-                  .and_return(ComplianceManagement::Framework.none)
-        end
+      it 'processes frameworks and projects in batches' do
+        expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
+          .to receive(:perform_async) do |framework_id, project_ids|
+          expect(framework_id).to eq(framework_1.id)
+          expect(project_ids.size).to eq(2)
+        end.ordered.once
 
-        it 'does not schedule any jobs' do
-          expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
-            .not_to receive(:perform_async)
+        expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
+          .to receive(:perform_async) do |framework_id, project_ids|
+          expect(framework_id).to eq(framework_1.id)
+          expect(project_ids.size).to eq(2)
+        end.ordered.once
 
-          worker.perform
-        end
+        expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
+          .to receive(:perform_async) do |framework_id, project_ids|
+          expect(framework_id).to eq(framework_2.id)
+          expect(project_ids.size).to eq(2)
+        end.ordered.once
+
+        expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
+          .to receive(:perform_async) do |framework_id, project_ids|
+          expect(framework_id).to eq(framework_2.id)
+          expect(project_ids.size).to eq(1)
+        end.ordered.once
+
+        expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
+          .not_to receive(:perform_async)
+                    .with(framework_without_projects.id, anything)
+
+        worker.perform
       end
     end
 
-    context 'when optimize_framework_worker_query feature flag is disabled' do
+    context 'when no frameworks exist' do
       before do
-        stub_feature_flags(optimize_framework_worker_query: false)
+        allow(ComplianceManagement::Framework)
+          .to receive(:with_active_controls)
+                .and_return(ComplianceManagement::Framework.none)
       end
 
-      it_behaves_like 'performs framework evaluations'
-    end
+      it 'does not schedule any jobs' do
+        expect(ComplianceManagement::ProjectComplianceEvaluatorWorker)
+          .not_to receive(:perform_async)
 
-    context 'when optimize_framework_worker_query feature flag is enabled' do
-      before do
-        stub_feature_flags(optimize_framework_worker_query: true)
+        worker.perform
       end
-
-      it_behaves_like 'performs framework evaluations'
     end
   end
 end
