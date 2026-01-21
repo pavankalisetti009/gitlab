5728541213f808401c7892cdb23663ba34f5fd04
Adrien Narinesingh <adinzey@gitlab.com>
2026-01-21 02:27:10 -0800

    Implement resolver functions for fields
    Fields vulnerability_count,
    is_protected and commit
    require functions to define.
    Updated spec coverage to reflect this.

--- ee/app/graphql/types/security/tracked_ref_type.rb
+++ ee/app/graphql/types/security/tracked_ref_type.rb
@@ -23,10 +23,13 @@ module Types
         description: 'Whether the ref is the default branch.'
 
       field :is_protected, GraphQL::Types::Boolean, null: false,
-        description: 'Whether the ref is protected.'
+        description: 'Whether the ref is protected.',
+        calls_gitaly: true,
+        resolver_method: :protected?
 
       field :commit, Types::Repositories::CommitType, null: true,
-        description: 'Latest commit on the ref.'
+        description: 'Latest commit on the ref.',
+        calls_gitaly: true
 
       field :vulnerabilities_count, GraphQL::Types::Int, null: false,
         description: 'Count of open vulnerabilities on the ref.'
@@ -40,6 +43,67 @@ module Types
 
       def state
         object.tracked? ? 'TRACKED' : 'UNTRACKED'
+      end
+
+      def protected?
+        return false unless ref_exists_in_repository?
+
+        case object.context_type
+        when 'branch'
+          project.protected_branches.matching(object.context_name).any?
+        when 'tag'
+          project.protected_tags.matching(object.context_name).any?
+        else
+          false
+        end
+      end
+
+      def commit
+        return unless ref_exists_in_repository?
+
+        raw_commit = fetch_raw_commit
+        return unless raw_commit
+
+        Commit.new(raw_commit, project)
+      rescue Gitlab::Git::Repository::NoRepository, Rugged::ReferenceError => e
+        Gitlab::ErrorTracking.track_exception(e, project_id: project.id, ref_name: object.context_name)
+        nil
+      end
+
+      def vulnerabilities_count
+        object.vulnerability_reads.count
+      rescue StandardError => e
+        Gitlab::ErrorTracking.track_exception(e, tracked_context_id: object.id)
+        0
+      end
+
+      private
+
+      def fetch_raw_commit
+        case object.context_type
+        when 'branch'
+          project.repository.commit(object.context_name)
+        when 'tag'
+          tag = project.repository.find_tag(object.context_name)
+          tag&.dereferenced_target
+        end
+      end
+
+      def project
+        @project ||= object.project
+      end
+
+      def ref_exists_in_repository?
+        return false unless project.repository_exists?
+
+        case object.context_type
+        when 'branch'
+          project.repository.branch_exists?(object.context_name)
+        when 'tag'
+          project.repository.tag_exists?(object.context_name)
+        else
+          false
+        end
       end
     end
   end
--- ee/spec/graphql/types/security/tracked_ref_type_spec.rb
+++ ee/spec/graphql/types/security/tracked_ref_type_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Types::Security::TrackedRefType, feature_category: :vulnerability
   include GraphqlHelpers
   using RSpec::Parameterized::TableSyntax
 
-  let_it_be(:project) { create(:project) }
+  let_it_be(:project) { create(:project, :repository) }
   let_it_be(:user) { create(:user) }
 
   before_all { project.add_developer(user) }
@@ -14,7 +14,7 @@ RSpec.describe Types::Security::TrackedRefType, feature_category: :vulnerability
   specify { expect(described_class).to require_graphql_authorizations(:read_security_project_tracked_refs) }
 
   describe 'custom field resolvers' do
-    let(:tracked_ref) { create(:security_project_tracked_context, :tracked, project: project) }
+    let_it_be(:tracked_ref) { create(:security_project_tracked_context, :tracked, project: project) }
     let(:type_instance) { described_class.send(:new, tracked_ref, {}) }
 
     describe '#state' do
@@ -24,9 +24,172 @@ RSpec.describe Types::Security::TrackedRefType, feature_category: :vulnerability
       end
 
       with_them do
-        it "returns #{params[:expected_state]} when tracked? is #{params[:tracked_status]}" do
+        it "returns correct state" do
           allow(tracked_ref).to receive(:tracked?).and_return(tracked_status)
           expect(type_instance.state).to eq(expected_state)
+        end
+      end
+    end
+
+    describe '#vulnerabilities_count' do
+      it 'returns count of vulnerability reads' do
+        allow(tracked_ref).to receive_message_chain(:vulnerability_reads, :count).and_return(5)
+        expect(type_instance.vulnerabilities_count).to eq(5)
+      end
+
+      it 'returns 0 when error occurs' do
+        allow(tracked_ref).to receive(:vulnerability_reads).and_raise(StandardError, 'DB error')
+        expect(type_instance.vulnerabilities_count).to eq(0)
+      end
+    end
+
+    describe '#protected?' do
+      before do
+        allow(type_instance).to receive_messages(project: project)
+      end
+
+      where(:context_type, :ref_exists, :has_protected_match, :expected_result) do
+        'branch'   | true  | true  | true
+        'branch'   | true  | false | false
+        'branch'   | false | false | false
+        'tag'      | true  | true  | true
+        'tag'      | true  | false | false
+        'tag'      | false | false | false
+        'unknown'  | true  | false | false
+      end
+
+      with_them do
+        it "returns correct protection status" do
+          allow(type_instance).to receive(:ref_exists_in_repository?).and_return(ref_exists)
+          allow(tracked_ref).to receive(:context_type).and_return(context_type)
+
+          if context_type == 'branch'
+            matching_result = has_protected_match ? [instance_double(ProtectedBranch)] : []
+            allow(project).to receive_message_chain(:protected_branches, :matching).and_return(matching_result)
+          elsif context_type == 'tag'
+            matching_result = has_protected_match ? [instance_double(ProtectedTag)] : []
+            allow(project).to receive_message_chain(:protected_tags, :matching).and_return(matching_result)
+          end
+
+          expect(type_instance.protected?).to eq(expected_result)
+        end
+      end
+    end
+
+    describe '#commit' do
+      before do
+        allow(type_instance).to receive(:project).and_return(project)
+      end
+
+      where(:context_type, :ref_exists, :raw_commit_result, :expected_result) do
+        'branch' | false | nil           | nil
+        'tag'    | false | nil           | nil
+        'branch' | true  | 'raw_commit'  | 'commit_object'
+        'tag'    | true  | 'raw_commit'  | 'commit_object'
+        'branch' | true  | nil           | nil
+        'tag'    | true  | nil           | nil
+      end
+
+      with_them do
+        it "handles different commit scenarios" do
+          allow(type_instance).to receive(:ref_exists_in_repository?).and_return(ref_exists)
+          allow(tracked_ref).to receive(:context_type).and_return(context_type)
+
+          if ref_exists
+            allow(type_instance).to receive(:fetch_raw_commit).and_return(raw_commit_result)
+
+            if raw_commit_result
+              expect(Commit).to receive(:new).with(raw_commit_result, project).and_return('commit_object')
+            end
+          end
+
+          expect(type_instance.commit).to eq(expected_result)
+        end
+      end
+
+      where(:error_type) do
+        [Gitlab::Git::Repository::NoRepository]
+        [Rugged::ReferenceError]
+      end
+
+      with_them do
+        it "handles repository errors gracefully" do
+          allow(type_instance).to receive(:ref_exists_in_repository?).and_return(true)
+          allow(type_instance).to receive(:fetch_raw_commit).and_raise(error_type)
+
+          expect(Gitlab::ErrorTracking).to receive(:track_exception).with(
+            an_instance_of(error_type),
+            hash_including(project_id: project.id, ref_name: tracked_ref.context_name)
+          )
+
+          expect(type_instance.commit).to be_nil
+        end
+      end
+    end
+
+    describe '#fetch_raw_commit' do
+      before do
+        allow(type_instance).to receive(:project).and_return(project)
+      end
+
+      where(:context_type, :setup_method, :expected_result) do
+        'branch'       | :setup_branch  | 'branch_commit'
+        'tag'          | :setup_tag     | 'tag_commit'
+        'tag'          | :setup_nil_tag | nil
+        'unknown_type' | nil            | nil
+        'invalid'      | nil            | nil
+        ''             | nil            | nil
+      end
+
+      with_them do
+        it "handles different context types correctly" do
+          allow(tracked_ref).to receive(:context_type).and_return(context_type)
+
+          case setup_method
+          when :setup_branch
+            allow(project).to receive_message_chain(:repository, :commit).and_return('branch_commit')
+          when :setup_tag
+            tag = instance_double(Gitlab::Git::Tag, dereferenced_target: 'tag_commit')
+            allow(project).to receive_message_chain(:repository, :find_tag).and_return(tag)
+          when :setup_nil_tag
+            allow(project).to receive_message_chain(:repository, :find_tag).and_return(nil)
+          end
+
+          expect(type_instance.send(:fetch_raw_commit)).to eq(expected_result)
+        end
+      end
+    end
+
+    describe '#ref_exists_in_repository?' do
+      before do
+        allow(type_instance).to receive(:project).and_return(project)
+      end
+
+      where(:repository_exists, :context_type, :ref_exists_result, :expected_result) do
+        false   | 'branch'   | true  | false
+        false   | 'tag'      | true  | false
+        false   | 'unknown'  | true  | false
+        true    | 'branch'   | true  | true
+        true    | 'branch'   | false | false
+        true    | 'tag'      | true  | true
+        true    | 'tag'      | false | false
+        true    | 'unknown'  | true  | false
+        true    | 'invalid'  | true  | false
+        true    | ''         | true  | false
+      end
+
+      with_them do
+        it "handles different repository and context scenarios" do
+          allow(project).to receive(:repository_exists?).and_return(repository_exists)
+          allow(tracked_ref).to receive(:context_type).and_return(context_type)
+
+          if repository_exists && context_type == 'branch'
+            allow(project).to receive_message_chain(:repository, :branch_exists?).and_return(ref_exists_result)
+          elsif repository_exists && context_type == 'tag'
+            allow(project).to receive_message_chain(:repository, :tag_exists?).and_return(ref_exists_result)
+          end
+
+          expect(type_instance.send(:ref_exists_in_repository?)).to eq(expected_result)
         end
       end
     end
