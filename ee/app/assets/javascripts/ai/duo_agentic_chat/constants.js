517b61cd3e1adcf068567ff47304f19be30c7f83
Tomas Bulva <tbulva@gitlab.com>
2026-01-23 15:28:11 +0100

    Integration of binary feedback in agentic chat
    - Added feature flag duo_chat_binary_feedback
    - Added added internal event to track the feedback
    - Added integration with the duo-ui

--- ee/app/assets/javascripts/ai/duo_agentic_chat/constants.js
+++ ee/app/assets/javascripts/ai/duo_agentic_chat/constants.js
@@ -5,3 +5,5 @@ export const DEFAULT_MIN_HEIGHT = 400;
 // error codes
 export const WORKFLOW_NOT_FOUND_CODE = 'WORKFLOW_NOT_FOUND';
 export const NO_RESOURCE_PERMISSIONS = 'NO_RESOURCE_PERMISSIONS';
+
+export const FEEDBACK_TRACKING_EVENT = 'ai_duo_agentic_chat_feedback_submitted';
--- ee/lib/ee/gitlab/gon_helper.rb
+++ ee/lib/ee/gitlab/gon_helper.rb
@@ -40,6 +40,7 @@ module EE
         push_frontend_feature_flag(:duo_ui_next, current_user)
         push_frontend_feature_flag(:agentic_chat_ga, current_user)
         push_frontend_feature_flag(:dap_use_foundational_flows_setting, current_user)
+        push_frontend_feature_flag(:duo_chat_binary_feedback, current_user)
       end
 
       # Exposes if a licensed feature is available.
