fb0e8ef3e3b24fbf3cbf77ab6856f308d72d00ce
Tim Beauchamp <tbeauchamp@gitlab.com>
2026-01-23 16:48:08 -0400

    Add source_code_management feature_category to spec files

--- ee/spec/workers/post_receive_spec.rb
+++ ee/spec/workers/post_receive_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe PostReceive, feature_category: :shared do
+RSpec.describe PostReceive, feature_category: :source_code_management do
   include AfterNextHelpers
   include ::EE::GeoHelpers
 
