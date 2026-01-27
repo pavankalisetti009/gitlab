d3b3bff290af69c86a1bc8944c0fa78c77708bfa
Manoj M J <mmj@gitlab.com>
2026-01-27 19:38:04 +0100

    Introduce feature flag `self_hosted_dap_per_request_billing`

---
+++ ee/lib/ai/self_hosted_dap_billing.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Ai
+  module SelfHostedDapBilling
+    def self.should_bill?(feature_setting)
+      feature_setting&.self_hosted? &&
+        self_hosted_dap_billing_enabled?
+    end
+
+    def self.self_hosted_dap_billing_enabled?
+      return false if Feature.disabled?(:self_hosted_dap_per_request_billing, :instance)
+      # There is no self-hosted billing on gitlab.com instance
+      return false if ::Gitlab::Saas.feature_available?(:gitlab_com_subscriptions)
+      # There is no self-hosted billing on offline cloud license. Offline
+      # cloud licenses use a different SKU for self-hosted DAP billing.
+      return false if ::License.current&.offline_cloud_license?
+
+      true
+    end
+  end
+end
