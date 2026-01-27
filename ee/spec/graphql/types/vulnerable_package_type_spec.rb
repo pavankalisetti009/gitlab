b24871713b457a18a43fc2a2254828c3f1b25e43
Tim Beauchamp <tbeauchamp@gitlab.com>
2026-01-27 07:39:49 -0400

    Add feature_category: :vulnerability_management to spec files (part 2/2)

--- ee/spec/graphql/types/vulnerable_package_type_spec.rb
+++ ee/spec/graphql/types/vulnerable_package_type_spec.rb
@@ -2,6 +2,6 @@
 
 require 'spec_helper'
 
-RSpec.describe GitlabSchema.types['VulnerablePackage'] do
+RSpec.describe GitlabSchema.types['VulnerablePackage'], feature_category: :vulnerability_management do
   it { expect(described_class).to have_graphql_fields(:name, :path) }
 end
