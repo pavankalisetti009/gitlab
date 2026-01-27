d168779baa104fc8592f18f7919e55e6622cfb07
Nicolas Dular <ndular@gitlab.com>
2026-01-27 12:53:59 +0100

    Read epic presenter data from work_item
    Update EpicPresenter to read attributes from the correlating
    work_item instead of the epic model, following the pattern
    established in EpicEntity.

--- ee/spec/presenters/epic_presenter_spec.rb
+++ ee/spec/presenters/epic_presenter_spec.rb
@@ -6,16 +6,82 @@ RSpec.describe EpicPresenter, feature_category: :portfolio_management do
   include ::UsersHelper
   include Gitlab::Routing.url_helpers
 
-  let(:user) { create(:user) }
-  let(:group) { create(:group, path: "pukeko_parent_group") }
-  let(:parent_epic) { create(:epic, group: group, start_date: Date.new(2000, 1, 10), due_date: Date.new(2000, 1, 20), iid: 10) }
-  let(:epic) { create(:epic, group: group, author: user, parent: parent_epic) }
+  let_it_be(:user) { create(:user) }
+  let_it_be(:group) { create(:group, path: "pukeko_parent_group") }
+  let_it_be(:label1) { create(:group_label, group: group, title: 'Bug') }
+  let_it_be(:label2) { create(:group_label, group: group, title: 'Feature') }
+  let_it_be(:parent_epic) { create(:epic, group: group, start_date: Date.new(2000, 1, 10), due_date: Date.new(2000, 1, 20), iid: 10) }
+  let_it_be_with_reload(:epic) { create(:epic, group: group, author: user, parent: parent_epic) }
+  let_it_be_with_reload(:work_item) { epic.work_item }
 
   subject(:presenter) { described_class.new(epic, current_user: user) }
 
+  describe 'reading data from work item' do
+    let_it_be(:color) { create(:color, work_item: work_item, color: '#0052cc') }
+
+    before_all do
+      epic.update!(labels: [label1, label2])
+
+      work_item.update!(
+        title: 'Work Item Title',
+        description: 'Work Item Description',
+        confidential: true,
+        state: :opened,
+        created_at: Time.zone.parse('2022-03-01 10:00:00'),
+        updated_at: Time.zone.parse('2022-03-15 14:30:00')
+      )
+    end
+
+    it 'reads created_at from work item' do
+      expect(presenter.created_at).to eq(work_item.created_at)
+    end
+
+    it 'reads updated_at from work item' do
+      expect(presenter.updated_at).to eq(work_item.updated_at)
+    end
+
+    it 'reads state from work item' do
+      expect(presenter.state).to eq('opened')
+    end
+
+    it 'reads lock_version from work item' do
+      expect(presenter.lock_version).to eq(work_item.lock_version)
+    end
+
+    it 'reads labels from work item' do
+      label_titles = presenter.labels.map(&:title)
+      expect(label_titles).to contain_exactly('Bug', 'Feature')
+    end
+
+    it 'reads author from work item' do
+      expect(presenter.author).to eq(work_item.author)
+    end
+
+    it 'reads confidential from work item' do
+      expect(presenter.confidential).to be(true)
+      expect(presenter.confidential?).to be(true)
+    end
+
+    it 'reads title from work item' do
+      expect(presenter.title).to eq('Work Item Title')
+    end
+
+    it 'reads description from work item' do
+      expect(presenter.description).to eq('Work Item Description')
+    end
+
+    it 'reads color from work item' do
+      expect(presenter.color).to eq('#0052cc')
+    end
+
+    it 'reads text_color from work item' do
+      expect(presenter.text_color).to eq('#FFFFFF')
+    end
+  end
+
   describe '#show_data' do
-    let(:milestone1) { create(:milestone, title: 'make me a sandwich', start_date: '2010-01-01', due_date: '2019-12-31') }
-    let(:milestone2) { create(:milestone, title: 'make me a pizza', start_date: '2020-01-01', due_date: '2029-12-31') }
+    let_it_be(:milestone1) { create(:milestone, title: 'make me a sandwich', start_date: '2010-01-01', due_date: '2019-12-31') }
+    let_it_be(:milestone2) { create(:milestone, title: 'make me a pizza', start_date: '2020-01-01', due_date: '2029-12-31') }
 
     before do
       epic.update!(
@@ -95,36 +161,47 @@ RSpec.describe EpicPresenter, feature_category: :portfolio_management do
     end
   end
 
-  describe 'use work item logic to present dates' do
-    using RSpec::Parameterized::TableSyntax
+  describe 'date attributes read from work item' do
+    context 'with fixed dates' do
+      let_it_be(:dates_source) do
+        create(:work_items_dates_source,
+          :fixed,
+          work_item: work_item,
+          start_date: Date.new(2010, 1, 1),
+          due_date: Date.new(2010, 1, 3)
+        )
+      end
 
-    let_it_be(:epic) do
-      build_stubbed(
-        :epic,
-        :with_synced_work_item,
-        start_date: Date.new(2025, 10, 14),
-        start_date_fixed: Date.new(2025, 10, 13),
-        start_date_is_fixed: true,
-        due_date: Date.new(2025, 10, 18),
-        due_date_fixed: Date.new(2025, 10, 19),
-        due_date_is_fixed: false
-      )
+      it 'reads dates from work item', :aggregate_failures do
+        expect(presenter.start_date).to eq(work_item.start_date)
+        expect(presenter.start_date).to eq(Date.new(2010, 1, 1))
+
+        expect(presenter.start_date_is_fixed?).to be(true)
+        expect(presenter.start_date_fixed).to eq(Date.new(2010, 1, 1))
+
+        expect(presenter.due_date).to eq(work_item.due_date)
+        expect(presenter.due_date).to eq(Date.new(2010, 1, 3))
+
+        expect(presenter.due_date_is_fixed?).to be(true)
+        expect(presenter.due_date_fixed).to eq(Date.new(2010, 1, 3))
+      end
     end
 
-    where(:field, :result) do
-      :start_date | Date.new(2025, 10, 13)
-      :start_date_fixed | Date.new(2025, 10, 13)
-      :start_date_is_fixed? | true
-      :due_date | Date.new(2025, 10, 19)
-      :due_date_fixed | Date.new(2025, 10, 19)
-      :due_date_is_fixed? | true
-    end
+    context 'with inherited dates' do
+      let_it_be(:dates_source) do
+        create(:work_items_dates_source,
+          work_item: work_item,
+          start_date: Date.new(2010, 1, 2),
+          due_date: Date.new(2010, 1, 4)
+        )
+      end
 
-    with_them do
-      it "presents epic date field using the work item WorkItems::Widgets::StartAndDueDate logic" do
-        value = presenter.public_send(field)
+      it 'reads the dates from the work item', :aggregate_failures do
+        expect(presenter.start_date_is_fixed?).to be(false)
+        expect(presenter.start_date).to eq(Date.new(2010, 1, 2))
 
-        expect(value).to eq(result)
+        expect(presenter.due_date_is_fixed?).to be(false)
+        expect(presenter.due_date).to eq(Date.new(2010, 1, 4))
       end
     end
   end
