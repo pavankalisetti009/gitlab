3564bfd506c901c50c2414cf2c535160baf0b7c3
Marc Saleiko <marc.saleiko@komadesign.de>
2026-01-28 16:55:46 +0100

    Use correct return value and allow calling methods with unstubbed params

--- spec/support/helpers/work_items/widget_helpers.rb
+++ spec/support/helpers/work_items/widget_helpers.rb
@@ -3,8 +3,9 @@
 module WorkItems
   module WidgetHelpers
     # Stubs one or more widgets for a single work item instance.
-    # This mocks both get_widget and has_widget? methods to return false for disabled widgets,
-    # simulating that the work item doesn't have those widgets available.
+    # This mocks both get_widget and has_widget? methods to simulate disabled widgets.
+    # When a widget is disabled, get_widget returns nil (not the widget object) and
+    # has_widget? returns false. Unstubbed widgets will use the original behavior.
     #
     # Examples:
     #   stub_work_item_widget(work_item, notes: false)
@@ -15,36 +16,51 @@ module WorkItems
     # the original widget but may not be useful if the type doesn't have it.
     # This can be improved in future iterations.
     def stub_work_item_widget(work_item, **widgets)
+      # Set up default behavior to call original for any widget not explicitly stubbed
+      allow(work_item).to receive(:get_widget).and_call_original
+      allow(work_item).to receive(:has_widget?).and_call_original
+
       widgets.each do |widget, enabled|
-        allow(work_item).to receive(:get_widget).and_call_original
-        allow(work_item).to receive(:get_widget).with(widget).and_return(enabled ? work_item.get_widget(widget) : false)
-        allow(work_item).to receive(:has_widget?).and_call_original
+        if enabled
+          allow(work_item).to receive(:get_widget).with(widget).and_call_original
+        else
+          allow(work_item).to receive(:get_widget).with(widget).and_return(nil)
+        end
+
         allow(work_item).to receive(:has_widget?).with(widget).and_return(enabled)
       end
     end
 
     # Stubs one or more widgets for all work item instances via allow_any_instance_of.
     # This is useful for testing behavior when widgets are not available across all work items.
+    # When a widget is disabled, get_widget returns nil (not the widget object) and
+    # has_widget? returns false. Unstubbed widgets will use the original behavior.
     #
     # Note: We stub Issue instead of WorkItem because WorkItem is a child class of Issue.
     # When using allow_any_instance_of with a parent class, it automatically applies to all
-    # instances of child classes as well.
+    # instances of child classes as well. This is why stubbing Issue covers both Issue and
+    # WorkItem instances.
     #
     # Examples:
     #   stub_all_work_item_widgets(notes: false)
     #   stub_all_work_item_widgets(labels: false, assignees: false, milestone: false)
     #   stub_all_work_item_widgets(notes: true)
     def stub_all_work_item_widgets(**widgets)
-      widgets.each do |widget, enabled|
-        next if enabled
+      # rubocop:disable RSpec/AnyInstanceOf -- To simulate work item without certain widgets
+      # Set up default behavior to call original for any widget not explicitly stubbed
+      allow_any_instance_of(Issue).to receive(:get_widget).and_call_original
+      allow_any_instance_of(Issue).to receive(:has_widget?).and_call_original
 
-        # rubocop:disable RSpec/AnyInstanceOf -- To simulate work item without weight widget
-        allow_any_instance_of(Issue).to receive(:get_widget).and_call_original
-        allow_any_instance_of(Issue).to receive(:get_widget).with(widget).and_return(false)
-        allow_any_instance_of(Issue).to receive(:has_widget?).and_call_original
-        allow_any_instance_of(Issue).to receive(:has_widget?).with(widget).and_return(false)
-        # rubocop:enable RSpec/AnyInstanceOf
+      widgets.each do |widget, enabled|
+        if enabled
+          allow_any_instance_of(Issue).to receive(:get_widget).with(widget).and_call_original
+        else
+          allow_any_instance_of(Issue).to receive(:get_widget).with(widget).and_return(nil)
+        end
+
+        allow_any_instance_of(Issue).to receive(:has_widget?).with(widget).and_return(enabled)
       end
+      # rubocop:enable RSpec/AnyInstanceOf
     end
   end
 end
