4133c2c3cc920ce21879a1f347600c41502df79e
🤖 GitLab Bot 🤖 <gitlab-bot@gitlab.com>
2026-01-28 00:53:29 +0000

    Mark 20251001130713 as obsolete
    This migration marks the 20251001130713 BackfillTokenStatusInVulnerabilities Advanced search migration as obsolete.
    
    [Search for references to `backfill_token_status_in_vulnerabilities` in code](https://gitlab.com/search?project_id=278964&scope=blobs&search=backfill_token_status_in_vulnerabilities&regex=false))
    
    At the moment, the `gitlab-housekeeper` is not always capable of removing all references so
    you must check the diff and pipeline failures to confirm if there are any issues.
    It is the responsibility of the assignee (picked from ~"group::global search") to push those changes to this branch.
    
    [Read more](https://docs.gitlab.com/ee/development/search/advanced_search_migration_styleguide.html#cleaning-up-advanced-search-migrations)
    about the process for marking Advanced search migrations as obsolete.
    
    All Advanced search migrations must have had at least one
    [required stop](https : // docs.gitlab.com / ee / development / database / required_stops.html)
    to process the migration. Therefore we mark any Advanced search migrations added before the
    last required stop as obsolete.
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    in [CI](https://gitlab.com/gitlab-org/quality/engineering-productivity/team/-/jobs/12755598793) using the `Keeps::MarkOldAdvancedSearchMigrationsAsObsolete` keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please create an issue with the
    label ~"GitLab Housekeeper" and consider pinging the author of this keep.
    
    Changelog: other
    EE: true

--- ee/spec/elastic/migrate/20251001130713_backfill_token_status_in_vulnerabilities_spec.rb
+++ ee/spec/elastic/migrate/20251001130713_backfill_token_status_in_vulnerabilities_spec.rb
@@ -3,21 +3,6 @@
 require 'spec_helper'
 require File.expand_path('ee/elastic/migrate/20251001130713_backfill_token_status_in_vulnerabilities.rb')
 
-RSpec.describe BackfillTokenStatusInVulnerabilities, feature_category: :vulnerability_management do
-  let(:version) { 20251001130713 }
-
-  describe 'migration', :elastic_delete_by_query, :sidekiq_inline do
-    before do
-      allow(::Vulnerabilities::TriggerFalsePositiveDetectionWorkflowWorker).to receive(:perform_async)
-    end
-
-    include_examples 'migration reindex based on schema_version' do
-      let(:expected_throttle_delay) { 15.seconds }
-      let(:expected_batch_size) { 10_000 }
-
-      let(:objects) do
-        create_list(:vulnerability_read, 3)
-      end
-    end
-  end
+RSpec.describe BackfillTokenStatusInVulnerabilities, feature_category: :global_search do
+  it_behaves_like 'a deprecated Advanced Search migration', 20251001130713
 end
