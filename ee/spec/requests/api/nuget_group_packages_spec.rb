5f5382b80b3bd3f08d755a5a2c3288232c047fe9
Alex Buijs <abuijs@gitlab.com>
2026-02-03 09:24:01 +0100

    Enable granular PAT for NuGet Packages endpoints
    Add granular PAT support for the NuGet Packages API endpoints.
    
    Changelog: changed

--- ee/spec/requests/api/nuget_group_packages_spec.rb
+++ ee/spec/requests/api/nuget_group_packages_spec.rb
@@ -21,5 +21,30 @@ RSpec.describe API::NugetGroupPackages, feature_category: :package_registry do
     subject { get api(url), headers: headers }
 
     it_behaves_like 'applying ip restriction for group'
+
+    it_behaves_like 'authorizing granular token permissions', :read_nuget_package do
+      let(:boundary_object) { group }
+      let(:request) { get api(url), headers: basic_auth_header(user.username, pat.token) }
+    end
+  end
+
+  describe 'GET /api/v4/groups/:id/-/packages/nuget/metadata/*package_name/index' do
+    let_it_be(:packages) { create_list(:nuget_package, 5, :with_metadatum, name: package_name, project: project) }
+    let(:url) { "/groups/#{group.id}/-/packages/nuget/metadata/#{package_name}/index.json" }
+
+    it_behaves_like 'authorizing granular token permissions', :read_nuget_package do
+      let(:boundary_object) { group }
+      let(:request) { get api(url), headers: basic_auth_header(user.username, pat.token) }
+    end
+  end
+
+  describe 'GET /api/v4/groups/:id/-/packages/nuget/query' do
+    let_it_be(:query_parameters) { { q: 'query', take: 5, skip: 0, prerelease: true } }
+    let(:url) { "/groups/#{group.id}/-/packages/nuget/query?#{query_parameters.to_query}" }
+
+    it_behaves_like 'authorizing granular token permissions', :search_nuget_package do
+      let(:boundary_object) { group }
+      let(:request) { get api(url), headers: basic_auth_header(user.username, pat.token) }
+    end
   end
 end
