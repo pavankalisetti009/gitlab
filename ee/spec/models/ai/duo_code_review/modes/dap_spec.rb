418d0056d6c4b3148fea7d58adc308083ef04648
Kinshuk Singh <kinsingh@gitlab.com>
2026-02-04 10:57:25 -0500

    Fix Duo Enterprise add-on check to use seat assignment instead of namespace membership

--- ee/spec/models/ai/duo_code_review/modes/dap_spec.rb
+++ ee/spec/models/ai/duo_code_review/modes/dap_spec.rb
@@ -5,8 +5,8 @@ require 'spec_helper'
 RSpec.describe Ai::DuoCodeReview::Modes::Dap, feature_category: :code_suggestions do
   subject(:mode) { described_class.new(user: user, container: container) }
 
-  let_it_be(:project) { create(:project) }
   let_it_be(:group) { create(:group) }
+  let_it_be(:project) { create(:project, group: group) }
   let_it_be(:merge_request) { create(:merge_request, source_project: project) }
   let_it_be(:user) { create(:user, developer_of: project) }
   let_it_be(:container) { project }
@@ -38,9 +38,6 @@ RSpec.describe Ai::DuoCodeReview::Modes::Dap, feature_category: :code_suggestion
     # User permissions
     let(:user_allowed_to_use_duo_agent_platform) { true }
 
-    # Add-ons
-    let(:user_has_duo_enterprise_add_on) { false }
-
     before do
       allow(::Gitlab::Llm::StageCheck).to receive(:available?)
         .with(container, :duo_workflow)
@@ -65,11 +62,6 @@ RSpec.describe Ai::DuoCodeReview::Modes::Dap, feature_category: :code_suggestion
           ServiceResponse.success(payload: nil)
         )
       end
-
-      allow(::GitlabSubscriptions::AddOnPurchase)
-        .to receive(:for_active_add_ons)
-        .with([:duo_enterprise], user)
-        .and_return(instance_double(ActiveRecord::Relation, exists?: user_has_duo_enterprise_add_on))
     end
 
     shared_examples 'not active' do
@@ -97,16 +89,74 @@ RSpec.describe Ai::DuoCodeReview::Modes::Dap, feature_category: :code_suggestion
     end
 
     context 'when user has duo_enterprise add-on' do
-      let(:user_has_duo_enterprise_add_on) { true }
+      context 'on SaaS' do
+        let_it_be(:add_on) { create(:gitlab_subscription_add_on, :duo_enterprise) }
+        let_it_be(:add_on_purchase) do
+          create(:gitlab_subscription_add_on_purchase, :active, add_on: add_on, namespace: group)
+        end
 
-      context 'and duo_code_review_dap_internal_users feature flag is enabled' do
-        include_examples 'active'
+        let_it_be(:user_add_on_assignment) do
+          create(:gitlab_subscription_user_add_on_assignment, add_on_purchase: add_on_purchase, user: user)
+        end
+
+        before do
+          stub_saas_features(gitlab_duo_saas_only: true)
+        end
+
+        context 'and duo_code_review_dap_internal_users feature flag is enabled' do
+          include_examples 'active'
+        end
+
+        context 'and duo_code_review_dap_internal_users feature flag is disabled' do
+          let(:duo_code_review_dap_internal_users) { false }
+
+          include_examples 'not active'
+        end
+
+        context 'when user does not have a seat assignment' do
+          let(:user_allowed_to_use_duo_agent_platform) { false }
+
+          before do
+            user_add_on_assignment.destroy!
+          end
+
+          include_examples 'not active'
+        end
       end
 
-      context 'and duo_code_review_dap_internal_users feature flag is disabled' do
-        let(:duo_code_review_dap_internal_users) { false }
+      context 'on self-managed' do
+        let_it_be(:add_on) { create(:gitlab_subscription_add_on, :duo_enterprise) }
+        let_it_be(:add_on_purchase) do
+          create(:gitlab_subscription_add_on_purchase, :active, :self_managed, add_on: add_on)
+        end
 
-        include_examples 'not active'
+        let_it_be(:user_add_on_assignment) do
+          create(:gitlab_subscription_user_add_on_assignment, add_on_purchase: add_on_purchase, user: user)
+        end
+
+        before do
+          stub_saas_features(gitlab_duo_saas_only: false)
+        end
+
+        context 'and duo_code_review_dap_internal_users feature flag is enabled' do
+          include_examples 'active'
+        end
+
+        context 'and duo_code_review_dap_internal_users feature flag is disabled' do
+          let(:duo_code_review_dap_internal_users) { false }
+
+          include_examples 'not active'
+        end
+
+        context 'when user does not have a seat assignment' do
+          let(:user_allowed_to_use_duo_agent_platform) { false }
+
+          before do
+            user_add_on_assignment.destroy!
+          end
+
+          include_examples 'not active'
+        end
       end
     end
 
