20c613db021f85f28c171d48c8d624bb97ee38c2
Erick Bajao <fbajao@gitlab.com>
2026-02-06 02:20:37 +0800

    Implement update group secret mutation

---
+++ ee/app/graphql/mutations/secrets_management/group_secrets/update.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+module Mutations
+  module SecretsManagement
+    module GroupSecrets
+      class Update < BaseMutation
+        graphql_name 'GroupSecretUpdate'
+
+        include ResolvesGroup
+        include Gitlab::InternalEventsTracking
+        include Helpers::ErrorMessagesHelpers
+        include ::SecretsManagement::MutationErrorHandling
+
+        authorize :write_secret
+
+        argument :group_path, GraphQL::Types::ID,
+          required: true,
+          description: 'Group of the secret.'
+
+        argument :name, GraphQL::Types::String,
+          required: true,
+          description: 'Name of the group secret to update.'
+
+        argument :description, GraphQL::Types::String,
+          required: false,
+          description: 'New description of the group secret.'
+
+        argument :secret, GraphQL::Types::String,
+          required: false,
+          description: 'New value of the group secret.'
+
+        argument :environment, GraphQL::Types::String,
+          required: false,
+          description: 'New environment that can access the secret.'
+
+        argument :protected, GraphQL::Types::Boolean,
+          required: false,
+          description: 'Whether the secret is only accessible from protected branches.'
+
+        argument :metadata_cas, GraphQL::Types::Int,
+          required: true,
+          description: 'This should match the current metadata version of the group secret being updated.'
+
+        field :group_secret,
+          Types::SecretsManagement::GroupSecretType,
+          null: true,
+          description: "Updated group secret."
+
+        def resolve(group_path:, name:, metadata_cas:, **args)
+          group = authorized_find!(group_path: group_path)
+
+          result = ::SecretsManagement::GroupSecrets::UpdateService
+            .new(group, current_user)
+            .execute(
+              name: name,
+              description: args[:description],
+              value: args[:secret],
+              environment: args[:environment],
+              protected: args[:protected],
+              metadata_cas: metadata_cas
+            )
+
+          if result.success?
+            track_secret_update_event(group)
+            {
+              group_secret: result.payload[:secret],
+              errors: []
+            }
+          elsif result.reason == :not_found
+            raise_resource_not_available_error!("Group secret does not exist.")
+          else
+            {
+              group_secret: nil,
+              errors: error_messages(result, [:group_secret])
+            }
+          end
+        end
+
+        private
+
+        def find_object(group_path:)
+          resolve_group(full_path: group_path)
+        end
+
+        def track_secret_update_event(group)
+          track_internal_event(
+            'update_group_ci_secret',
+            user: current_user,
+            namespace: group
+          )
+        end
+      end
+    end
+  end
+end
---
+++ ee/spec/requests/api/graphql/secrets_management/group_secrets/update_spec.rb
@@ -0,0 +1,254 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Update group secret', :gitlab_secrets_manager, :freeze_time, feature_category: :secrets_management do
+  include GraphqlHelpers
+
+  let_it_be_with_reload(:group) { create(:group) }
+  let_it_be(:owner_user) { create(:user) }
+  let_it_be(:current_user) { create(:user) }
+  let_it_be(:mutation_name) { :group_secret_update }
+
+  let(:secrets_manager) { create(:group_secrets_manager, group: group) }
+
+  let(:group_secret_attributes) do
+    {
+      name: 'TEST_SECRET',
+      description: 'test description',
+      environment: 'production',
+      protected: true,
+      value: 'test value'
+    }
+  end
+
+  let(:params) do
+    {
+      group_path: group.full_path,
+      name: group_secret_attributes[:name],
+      description: 'updated description',
+      environment: 'staging',
+      protected: false,
+      metadata_cas: 2
+    }
+  end
+
+  let(:mutation) { graphql_mutation(mutation_name, params) }
+  let(:mutation_response) { graphql_mutation_response(mutation_name) }
+
+  subject(:post_mutation) { post_graphql_mutation(mutation, current_user: current_user) }
+
+  before_all do
+    group.add_owner(owner_user)
+  end
+
+  before do
+    stub_last_activity_update
+    provision_group_secrets_manager(secrets_manager, owner_user)
+
+    # Create initial secret
+    SecretsManagement::GroupSecrets::CreateService.new(group, owner_user).execute(
+      **group_secret_attributes
+    )
+  end
+
+  after do
+    cancel_exclusive_group_secret_operation_lease(group)
+  end
+
+  context 'when current user is not part of the group' do
+    it_behaves_like 'a mutation on an unauthorized resource'
+  end
+
+  context 'when current user does not have write permissions in openbao' do
+    before_all do
+      group.add_maintainer(current_user)
+    end
+
+    it_behaves_like 'a mutation on an unauthorized resource'
+  end
+
+  shared_examples_for 'a successful update request' do
+    it 'updates the group secret', :aggregate_failures do
+      post_mutation
+
+      expect(response).to have_gitlab_http_status(:success)
+      expect(mutation_response['errors']).to be_empty
+
+      expect(graphql_data_at(mutation_name, :group_secret))
+        .to match(a_graphql_entity_for(
+          group: a_graphql_entity_for(group),
+          name: group_secret_attributes[:name],
+          description: 'updated description',
+          environment: 'staging',
+          protected: false,
+          metadata_version: 3,
+          status: 'COMPLETED'
+        ))
+    end
+
+    it_behaves_like 'an API request requiring an exclusive group secret operation lease'
+
+    it_behaves_like 'internal event tracking' do
+      let(:event) { 'update_group_ci_secret' }
+      let(:user) { current_user }
+      let(:namespace) { group }
+      let(:category) { 'Mutations::SecretsManagement::GroupSecrets::Update' }
+    end
+  end
+
+  context 'when current user was granted write permissions in openbao' do
+    before_all do
+      group.add_maintainer(current_user)
+    end
+
+    before do
+      update_group_secrets_permission(
+        user: current_user, group: group, actions: %w[write read], principal: {
+          id: Gitlab::Access.sym_options[:maintainer], type: 'Role'
+        }
+      )
+    end
+
+    it_behaves_like 'a successful update request'
+
+    context 'with partial updates' do
+      let(:params) do
+        {
+          group_path: group.full_path,
+          name: group_secret_attributes[:name],
+          description: 'updated description only',
+          metadata_cas: 2
+        }
+      end
+
+      it 'updates only the specified fields', :aggregate_failures do
+        post_mutation
+
+        expect(response).to have_gitlab_http_status(:success)
+        expect(mutation_response['errors']).to be_empty
+
+        expect(graphql_data_at(mutation_name, :group_secret))
+          .to match(a_graphql_entity_for(
+            name: group_secret_attributes[:name],
+            description: 'updated description only',
+            environment: group_secret_attributes[:environment],
+            protected: group_secret_attributes[:protected],
+            metadata_version: 3
+          ))
+
+        # Verify value wasn't changed
+        secret_path = secrets_manager.ci_data_path(group_secret_attributes[:name])
+        expect_kv_secret_to_have_value(
+          group.secrets_manager.full_group_namespace_path,
+          group.secrets_manager.ci_secrets_mount_path,
+          secret_path,
+          'test value'
+        )
+      end
+    end
+
+    context 'with value update' do
+      let(:params) do
+        {
+          group_path: group.full_path,
+          name: group_secret_attributes[:name],
+          secret: 'new-secret-value',
+          metadata_cas: 2
+        }
+      end
+
+      it 'updates the value', :aggregate_failures do
+        post_mutation
+
+        expect(response).to have_gitlab_http_status(:success)
+        expect(mutation_response['errors']).to be_empty
+
+        expect(graphql_data_at(mutation_name, :group_secret))
+          .to match(a_graphql_entity_for(
+            metadata_version: 3
+          ))
+
+        # Verify value was updated
+        secret_path = secrets_manager.ci_data_path(group_secret_attributes[:name])
+        expect_kv_secret_to_have_value(
+          group.secrets_manager.full_group_namespace_path,
+          group.secrets_manager.ci_secrets_mount_path,
+          secret_path,
+          'new-secret-value'
+        )
+      end
+    end
+
+    context 'when metadata_cas is not provided' do
+      let(:params) do
+        {
+          group_path: group.full_path,
+          name: group_secret_attributes[:name],
+          description: 'updated description'
+        }
+      end
+
+      it 'returns an error', :aggregate_failures do
+        post_mutation
+
+        expect_graphql_errors_to_include(
+          "GroupSecretUpdateInput! was provided invalid value for metadataCas"
+        )
+      end
+    end
+
+    context 'when secret does not exist' do
+      let(:params) do
+        {
+          group_path: group.full_path,
+          name: 'NON_EXISTENT_SECRET',
+          description: 'updated description',
+          metadata_cas: 2
+        }
+      end
+
+      it 'returns a top-level error with message' do
+        post_mutation
+
+        expect(mutation_response).to be_nil
+        expect(graphql_errors.count).to eq(1)
+        expect(graphql_errors.first['message']).to eq('Group secret does not exist.')
+      end
+
+      it_behaves_like 'internal event not tracked'
+    end
+
+    context 'and service results to a failure' do
+      before do
+        allow_next_instance_of(SecretsManagement::GroupSecrets::UpdateService) do |service|
+          allow(service).to receive(:execute).and_return(ServiceResponse.error(message: 'some error'))
+        end
+      end
+
+      it 'returns the service error' do
+        post_mutation
+
+        expect(mutation_response['errors']).to include('some error')
+      end
+
+      it_behaves_like 'internal event not tracked'
+    end
+
+    context 'and group_secrets_manager feature flag is disabled' do
+      before do
+        stub_feature_flags(group_secrets_manager: false)
+      end
+
+      it_behaves_like 'a mutation on an unauthorized resource'
+    end
+  end
+
+  context 'when current user is group owner' do
+    before_all do
+      group.add_owner(current_user)
+    end
+
+    it_behaves_like 'a successful update request'
+  end
+end
