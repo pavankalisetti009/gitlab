86932de9801db79ce273a82774903af4384c9a31
Vanessa Otto <votto@gitlab.com>
2026-02-09 02:41:37 +0100

    Show membership table of service account users
    For external agents and custom flows this table gives an overview in
    which projects the service account user is member of and in which role.
    
    EE: true
    Changelog: added
    
    Remove access table and tabs from agent and flow show pages
    
    Show project memberships in drawer
    
    Add activity column
    
    Clean up HTML, imports, revert movement of alert

---
+++ ee/app/assets/javascripts/ai/catalog/components/service_account_avatar.vue
@@ -0,0 +1,28 @@
+<script>
+import { GlAvatarLabeled, GlAvatarLink } from '@gitlab/ui';
+
+export default {
+  name: 'ServiceAccountAvatar',
+  components: {
+    GlAvatarLabeled,
+    GlAvatarLink,
+  },
+  props: {
+    serviceAccount: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-avatar-link :href="serviceAccount.webPath" :title="serviceAccount.name">
+    <gl-avatar-labeled
+      :size="32"
+      :src="serviceAccount.avatarUrl"
+      :label="serviceAccount.name"
+      :sub-label="`@${serviceAccount.username}`"
+    />
+  </gl-avatar-link>
+</template>
