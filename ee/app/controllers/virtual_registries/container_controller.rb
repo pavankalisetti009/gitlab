323e3095a26d54dc718348908f4514456f52d4e0
Radamanthus Batnag <rbatnag@gitlab.com>
2026-02-12 04:12:42 +0800

    DRY up JWT authentication for Dependency Proxy and Virtual Registry
    Both VirtualRegistries::ContainersController and
    Groups::DependencyProxy::ApplicationController duplicate identical
    JWT authentication logic.
    
    This commit extracts the shared behavior between the two controllers:
    - JWT token extraction and validation via
      DependencyProxy::AuthTokenService
    - Session skipping for API-style requests
    - Bearer token challenge responses
    - Support for User, PersonalAccessToken, and DeployToken auth
    - Sign-in logic and special handling for project bots and service
      accounts

--- ee/app/controllers/virtual_registries/container_controller.rb
+++ ee/app/controllers/virtual_registries/container_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 module VirtualRegistries
-  # TODO: Extract shared JWT authentication logic
-  # Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/578299
   class ContainerController < ::ApplicationController
+    include ::JwtAuthenticatable
     include WorkhorseRequest
     include SendFileUpload
     include ::PackagesHelper
     include Gitlab::Utils::StrongMemoize
 
-    EMPTY_AUTH_RESULT = Gitlab::Auth::Result.new(nil, nil, nil, nil).freeze
     PERMITTED_PARAMS = %i[id file path].freeze
 
     EXTRA_RESPONSE_HEADERS = {
@@ -79,10 +77,6 @@ module VirtualRegistries
 
     private
 
-    def skip_session
-      request.session_options[:skip] = true
-    end
-
     def ensure_feature_available!
       render_404 unless registry && ::VirtualRegistries::Container.feature_enabled?(registry.group)
     end
@@ -94,42 +88,6 @@ module VirtualRegistries
 
     def permitted_params
       params.permit(PERMITTED_PARAMS)
-    end
-
-    # JWT Authentication (duplicated from Groups::DependencyProxy::ApplicationController)
-    def authenticate_user_from_jwt_token!
-      authenticate_with_http_token do |token, _|
-        @authentication_result = EMPTY_AUTH_RESULT
-
-        user_or_token = ::DependencyProxy::AuthTokenService.user_or_token_from_jwt(token)
-
-        case user_or_token
-        when User
-          set_auth_result(user_or_token, :user)
-          sign_in(user_or_token) if can_sign_in?(user_or_token)
-        when PersonalAccessToken
-          set_auth_result(user_or_token.user, :personal_access_token)
-        when DeployToken
-          set_auth_result(user_or_token, :deploy_token)
-        end
-      end
-
-      request_bearer_token! unless authenticated_user
-    end
-
-    def set_auth_result(actor, type)
-      @authentication_result = Gitlab::Auth::Result.new(actor, nil, type, [])
-    end
-
-    def can_sign_in?(user_or_token)
-      return false if user_or_token.project_bot? || user_or_token.service_account?
-
-      true
-    end
-
-    def request_bearer_token!
-      response.headers['WWW-Authenticate'] = ::DependencyProxy::Registry.authenticate_header
-      render plain: '', status: :unauthorized
     end
 
     def ensure_user_has_access!
