b4565312032e42b89ec12cfa7dd392e4c304d096
Sascha Eggenberger <seggenberger@gitlab.com>
2026-02-11 07:33:20 +0100

    Remove ompliance moved banner

--- ee/app/helpers/ee/users/group_callouts_helper.rb
+++ ee/app/helpers/ee/users/group_callouts_helper.rb
@@ -4,7 +4,6 @@ module EE
   module Users
     module GroupCalloutsHelper
       ALL_SEATS_USED_ALERT = 'all_seats_used_alert'
-      COMPLIANCE_FRAMEWORK_SETTINGS_MOVED_CALLOUT = 'compliance_framework_settings_moved_callout'
       PROJECT_PREMIUM_MESSAGE_CALLOUT = 'project_premium_message_callout'
       REPOSITORY_PREMIUM_MESSAGE_CALLOUT = 'repository_premium_message_callout'
       MRS_PREMIUM_MESSAGE_CALLOUT = 'mrs_premium_message_callout'
