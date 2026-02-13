732184f47432bc202b1c276809c53b4601a9efdb
Aaron Masluk <aaronmasluk@outlook.com>
2026-02-14 02:03:15 +1100

    Add NOT NULL constraint to dependency_proxy_manifest_states.group_id

--- db/structure.sql
+++ db/structure.sql
@@ -18841,6 +18841,7 @@ CREATE TABLE dependency_proxy_manifest_states (
     verification_checksum bytea,
     verification_failure text,
     group_id bigint,
+    CONSTRAINT check_48a15459cd CHECK ((group_id IS NOT NULL)),
     CONSTRAINT check_fdd5d9791b CHECK ((char_length(verification_failure) <= 255))
 );
 
